1a5208491fdf3f0872b45823f7c4006561e8dfb5
Pippin Williamson <pippin@pippinsplugins.com>
2012-05-07 13:44:34 -0500

    Added  to the edd_cart_item filter

--- includes/cart-template.php
+++ includes/cart-template.php
@@ -42,11 +42,11 @@ function edd_get_cart_item_template($cart_key, $item, $ajax = false) {
 	$remove_url = edd_remove_item_url($cart_key, $post, $ajax);
 	$title = get_the_title($id); 
 	if(!empty($item['options'])) {
-		$title .= ' - ' . edd_get_price_name($item['id'], $item['options']);							
+		$title .= ' - ' . edd_get_price_name($id, $item['options']);							
 	}
 	$remove = '<a href="' . $remove_url . '" data-cart-item="' . $cart_key . '" data-action="edd_remove_from_cart" class="edd-remove-from-cart">' . __('remove', 'edd') . '</a>';	
 	$item = '<li class="edd-cart-item"><span class="edd-cart-item-title">' . $title . '</span> <span class="edd-cart-item-separator">-</span> ' . $remove . '</li>';
-	return apply_filters('edd_cart_item', $item);
+	return apply_filters('edd_cart_item', $item, $id);
 }
 
 // gets the message for an empty cart
