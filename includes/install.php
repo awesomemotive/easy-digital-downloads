55de9762013d4079f0dd5fd6c6e2c0bd178997eb
Pippin Williamson <pippin@pippinsplugins.com>
2012-05-16 16:42:39 -0500

    Syntax Error
    Missing text domain and closing )

--- includes/install.php
+++ includes/install.php
@@ -16,7 +16,7 @@ function edd_install() {
 		$success = wp_insert_post(
 			array(
 				'post_title' => __('Purchase Confirmation', 'edd'),
-				'post_content' => __('Thank you for your purchase!',
+				'post_content' => __('Thank you for your purchase!', 'edd'),
 				'post_status' => 'publish',
 				'post_author' => 1,
 				'post_type' => 'page'
