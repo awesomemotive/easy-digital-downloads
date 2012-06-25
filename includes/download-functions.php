4750449aa825b319d6ca9225cdb04d1c4293decd
Pippin Williamson <pippin@pippinsplugins.com>
2012-06-25 15:34:44 -0500

    Updated edd_has_user_purchased() to support variable price IDs as well. We can now check whether a user has purchased a particular price option, instead of just the product in general.

--- includes/download-functions.php
+++ includes/download-functions.php
@@ -489,25 +489,51 @@ function edd_verify_download_link($download_id, $key, $email, $expire) {
  * @since       1.0 
  * @param       int $user_id - the ID of the user to check
  * @param       int $download_Id - the ID of the download to check for
+ * @param       int $variable_price_id - the variable price ID to check for
  * @return      boolean - true if has purchased, false otherwise
 */
 
-function edd_has_user_purchased($user_id, $download_id) {
+function edd_has_user_purchased($user_id, $download_id, $variable_price_id = null) {
 	$users_purchases = edd_get_users_purchases($user_id);
+
+	$return = false;
 
 	if($users_purchases) {
 		foreach($users_purchases as $purchase) {
+
 			$purchase_meta = get_post_meta($purchase->ID, '_edd_payment_meta', true);
 			$purchased_files = maybe_unserialize($purchase_meta['downloads']);
+
 			if(is_array($purchased_files)) {
+
 				foreach($purchased_files as $download) {
+
 					if($download['id'] == $download_id) {
-						return true;
+
+						if( !is_null( $variable_price_id ) && $variable_price_id !== false ) {
+
+							if( $variable_price_id == $download['options']['price_id'] ) {
+								
+								return true;
+							
+							} else {
+							
+								$return = false;
+							
+							}
+
+						} else {
+							
+							$return = true;
+						
+						}
+
 					}
+
 				}
+
 			}
 		}
 	}
-	// user has not purchased the download
-	return false;
+	return $return;
 }
