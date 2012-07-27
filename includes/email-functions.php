b7ceccf36e399e51e2e6132d827108f1537790b1
Elliott Stocks <elliottstocks@hotmail.co.uk>
2012-07-27 20:25:09 +0200

    Update includes/email-functions.php
    Decode all HTML characters using UTF-8. Issue with £ not being displayed correctly. UTF-8 is default on PHP 5.4.0 onwards.

--- includes/email-functions.php
+++ includes/email-functions.php
@@ -56,7 +56,7 @@ function edd_email_purchase_receipt($payment_id, $admin_notice = true) {
 		$gateway = edd_get_gateway_admin_label( get_post_meta($payment_id, '_edd_payment_gateway', true) );
 		
 		$admin_message .= $download_list . "\n";
-		$admin_message .= __('Amount: ', 'edd') . " " . html_entity_decode(edd_currency_filter($payment_data['amount'])) . "\n\n";
+		$admin_message .= __('Amount: ', 'edd') . " " . html_entity_decode(edd_currency_filter($payment_data['amount']), ENT_COMPAT, 'UTF-8') . "\n\n";
 		$admin_message .= __('Payment Method: ', 'edd') . " " . $gateway . "\n\n";
 		$admin_message .= __('Thank you', 'edd');
 		$admin_message = apply_filters('edd_admin_purchase_notification', $admin_message, $payment_id, $payment_data);
