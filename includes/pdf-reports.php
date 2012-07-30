d1f6ba0b280ffed5dc7e293f6a2979e70167e1b3
Pippin Williamson <pippin@pippinsplugins.com>
2012-07-30 17:46:36 -0500

    Fixed problem with missing prices and earnings

--- includes/pdf-reports.php
+++ includes/pdf-reports.php
@@ -60,62 +60,64 @@ function edd_generate_pdf( $data ) {
 		
 		$pdf->SetWidths( array( 75, 30, 50, 50, 45, 30 ) );
 		
-		$query = new WP_Query( $query_string . 'post_type=download&year=' . $year );
+		$downloads = get_posts( array( 'post_type' => 'download', 'year' => $year ) );
+		if( $downloads ) :
+			foreach( $downloads as $download ) :
 
-		while ( $query->have_posts() ) : $query->the_post();
-			$pdf->SetFillColor( 255, 255, 255 );
-			
-			$title = get_the_title();
-			
-			if ( edd_has_variable_prices( $post->ID ) ) {
-				$prices = get_post_meta( $post->ID, 'edd_variable_prices', true );
-				$total = count( $prices ) - 1;
-				if ( $prices[0]['amount'] < $prices[$total]['amount'] ) {
-					$min = $prices[0]['amount'];
-					$max = $prices[$total]['amount'];
-				} else {
-					$min = $prices[$total]['amount'];
-					$max = $prices[0]['amount'];
+				$pdf->SetFillColor( 255, 255, 255 );
+				
+				$title = get_the_title( $download->ID );
+				
+				if ( edd_has_variable_prices( $download->ID ) ) {
+					$prices = get_post_meta( $download->ID, 'edd_variable_prices', true );
+					$total = count( $prices ) - 1;
+					if ( $prices[0]['amount'] < $prices[$total]['amount'] ) {
+						$min = $prices[0]['amount'];
+						$max = $prices[$total]['amount'];
+					} else {
+						$min = $prices[$total]['amount'];
+						$max = $prices[0]['amount'];
+					}
+					$price = html_entity_decode( sprintf( '%s - %s', edd_currency_filter( $min ), edd_currency_filter( $max ) ) );
+					} else {
+						$price = html_entity_decode( edd_currency_filter( edd_get_download_price( $download->ID ) ) );
 				}
-				$price = html_entity_decode( sprintf( '%s - %s', edd_currency_filter( $min ), edd_currency_filter( $max ) ) );
-				} else {
-					$price = html_entity_decode( edd_currency_filter( edd_get_download_price( $post->ID ) ) );
-			}
-			
-			$categories = strip_tags( get_the_term_list( $post->ID, 'download_category', '', ', ', '' ) );
-			$tags = strip_tags( get_the_term_list( $post->ID, 'download_tag', '', ', ', '' ) );
-			$sales = edd_get_download_sales_stats( $post->ID );
-			$link = get_permalink( $post->ID );
-			$earnings = html_entity_decode ( edd_currency_filter( edd_get_download_earnings_stats( $post->ID ) ) );
-			
-			$pdf->Row( array( $title, $price, $categories, $tags, $sales, $earnings ) );
-		endwhile;
-
+				
+				$categories = strip_tags( get_the_term_list( $download->ID, 'download_category', '', ', ', '' ) );
+				$tags = strip_tags( get_the_term_list( $download->ID, 'download_tag', '', ', ', '' ) );
+				$sales = edd_get_download_sales_stats( $download->ID );
+				$link = get_permalink( $download->ID );
+				$earnings = html_entity_decode ( edd_currency_filter( edd_get_download_earnings_stats( $download->ID ) ) );
+				
+				$pdf->Row( array( $title, $price, $categories, $tags, $sales, $earnings ) );
+			endforeach;
+		endif;
 		$pdf->Ln();
 		$pdf->SetTextColor( 50, 50, 50 );
 		$pdf->SetFont( 'Helvetica', '', 14 );
 		$pdf->Cell( 0, 10, 'Graph View', 0, 2, 'L', false );
 		$pdf->SetFont( 'Helvetica', '', 12 );
 
-		$image = chart_image();
+		//$image = chart_image();
 
-		$img = html_entity_decode('http://chart.apis.google.com/chart?cht=lc&amp;chs=900x330&amp;chtt=Sales+and+Earnings+by+Month+for+all+Products&amp;chd=t%3A0.00%2C0.00%2C0.00%2C0.00%2C0.00%2C235.00%2C205.00%2C0.00%2C0.00%2C0.00%2C0.00%2C0.00%7C0.00%2C0.00%2C0.00%2C0.00%2C0.00%2C62.00%2C28.00%2C0.00%2C0.00%2C0.00%2C0.00%2C0.00&amp;chco=1b58a3%2Cff6c1c&amp;chds=0%2C235&amp;chdl=Earnings%7CSales&amp;chdlp=s&amp;chm=o%2Cffffff%2C0%2C-1%2C9%7Co%2C000000%2C0%2C-1%2C7%7CN%2C000000%2C0%2C-1%2C10%7Co%2Cffffff%2C1%2C-1%2C8%7Co%2C4D89F9%2C1%2C-1%2C6%7CN%2C4D89F9%2C1%2C-1%2C10&amp;chxt=y%2Cx&amp;chxl=0%3A%7C0%7C235%7C1%3A%7CJan%7CFeb%7CMar%7CApr%7CMay%7CJune%7CJuly%7CAug%7CSept%7COct%7CNov%7CDec&amp;chxtc=1%2C5&amp;chxs=0%2C666666%2C11%2C1%2Clt');
+		//$img = html_entity_decode('http://chart.apis.google.com/chart?cht=lc&amp;chs=900x330&amp;chtt=Sales+and+Earnings+by+Month+for+all+Products&amp;chd=t%3A0.00%2C0.00%2C0.00%2C0.00%2C0.00%2C235.00%2C205.00%2C0.00%2C0.00%2C0.00%2C0.00%2C0.00%7C0.00%2C0.00%2C0.00%2C0.00%2C0.00%2C62.00%2C28.00%2C0.00%2C0.00%2C0.00%2C0.00%2C0.00&amp;chco=1b58a3%2Cff6c1c&amp;chds=0%2C235&amp;chdl=Earnings%7CSales&amp;chdlp=s&amp;chm=o%2Cffffff%2C0%2C-1%2C9%7Co%2C000000%2C0%2C-1%2C7%7CN%2C000000%2C0%2C-1%2C10%7Co%2Cffffff%2C1%2C-1%2C8%7Co%2C4D89F9%2C1%2C-1%2C6%7CN%2C4D89F9%2C1%2C-1%2C10&amp;chxt=y%2Cx&amp;chxl=0%3A%7C0%7C235%7C1%3A%7CJan%7CFeb%7CMar%7CApr%7CMay%7CJune%7CJuly%7CAug%7CSept%7COct%7CNov%7CDec&amp;chxtc=1%2C5&amp;chxs=0%2C666666%2C11%2C1%2Clt');
 
 		$pdf->SetX( 25 );
-		$pdf->WriteHTML( '<img src="'.$img.'" width="900">' );
+		//$pdf->WriteHTML( '<img src="'.$img.'" width="900">' );
 		$pdf->Ln( 7 );
 
 		$pdf->Output();
 
 
 	}
-
 }
+add_action('edd_generate_pdf', 'edd_generate_pdf');
+
 
 function chart_image() {
 	include_once('libraries/googlechartlib/GoogleChart.php');
-include_once('libraries/googlechartlib/markers/GoogleChartShapeMarker.php');
-include_once('libraries/googlechartlib/markers/GoogleChartTextMarker.php');
+	include_once('libraries/googlechartlib/markers/GoogleChartShapeMarker.php');
+	include_once('libraries/googlechartlib/markers/GoogleChartTextMarker.php');
 
 $chart = new GoogleChart( 'lc', 900, 330 );
 
@@ -220,5 +222,3 @@ $chart->addMarker( $value_marker );
 return $chart->toHTML();
 
 }
-
-add_action('edd_generate_pdf', 'edd_generate_pdf');
