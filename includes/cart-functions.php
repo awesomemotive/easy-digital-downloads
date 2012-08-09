0f573df32b99603f5baf5ce815f2661438cf01e5
Pippin Williamson <pippin@pippinsplugins.com>
2012-08-09 18:23:11 -0500

    Updated edd_remove_item_url() to use the current page's URL instead of the home page URL

--- includes/cart-functions.php
+++ includes/cart-functions.php
@@ -387,8 +387,8 @@ function edd_add_collection_to_cart($taxonomy, $terms) {
 
 function edd_remove_item_url($cart_key, $post, $ajax = false) {
 	global $post;
-	$current_page = ($ajax || !isset($post->ID)) ? home_url() : get_permalink($post->ID);
-	$remove_url = add_query_arg('cart_item', $cart_key, add_query_arg('edd_action', 'remove', $current_page));
+	$current_page = edd_get_current_page_url();
+	$remove_url = add_query_arg( array('cart_item' => $cart_key, 'edd_action' => 'remove' ), $current_page);
 	return apply_filters('edd_remove_item_url', $remove_url);
 }
 
