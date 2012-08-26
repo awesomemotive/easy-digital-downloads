142eaf61bc7f3157b0d71166544ec5617f96cfd0
Pippin Williamson <pippin@pippinsplugins.com>
2012-08-26 18:49:45 -0500

    Fixed problem with large hour expirations in 209

--- includes/download-functions.php
+++ includes/download-functions.php
@@ -479,14 +479,19 @@ function edd_get_download_file_url($key, $email, $filekey, $download_id) {
 	
 	global $edd_options;
 
-	$hours = isset($edd_options['download_link_expiration']) && is_numeric($edd_options['download_link_expiration']) ? absint($edd_options['download_link_expiration']) : 24;
+	$hours = isset( $edd_options['download_link_expiration'] ) 
+			&& is_numeric( $edd_options['download_link_expiration'] ) 
+			? absint($edd_options['download_link_expiration']) : 24;
 
+	if( ! ( $date = strtotime( '+' . $hours . 'hours' ) ) )
+		$date = 2147472000; // highest possible date, January 19, 2038
+		
 	$params = array(
 		'download_key' => $key,
-		'email' => rawurlencode($email),
+		'email' => rawurlencode( $email ),
 		'file' => $filekey,
 		'download' => $download_id, 
-		'expire' => urlencode(base64_encode(strtotime('+' . $hours . 'hours', time())))
+		'expire' => rawurlencode( base64_encode( $date ) )
 	);
 
 	$params = apply_filters('edd_download_file_url_args', $params);
