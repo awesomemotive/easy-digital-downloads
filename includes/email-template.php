5057a6b66b40683d063f6ce609fdedbf4cc5a89d
Pippin Williamson <pippin@pippinsplugins.com>
2012-08-28 11:07:51 -0500

    Moved the function to register templates to the top

--- includes/email-template.php
+++ includes/email-template.php
@@ -11,6 +11,23 @@
 
 
 /**
+ * Get Email Templates
+ *
+ * @access     private
+ * @since      1.0.8.2
+ * @return     array
+*/
+
+function edd_get_email_templates() {
+	$templates = array(
+		'default' => __('Default Template', 'edd'),
+		'none' => __('No template, plain text only', 'edd')
+	);
+	return apply_filters( 'edd_email_templates', $templates );
+}	
+
+
+/**
  * Email Template Tags
  *
  * @access      private
@@ -288,19 +305,3 @@ function edd_default_email_styling( $email_body ) {
 }
 add_filter('edd_purchase_receipt_default', 'edd_default_email_styling');
 
-
-/**
- * Get Email Templates
- *
- * @access     private
- * @since      1.0.8.2
- * @return     array
-*/
-
-function edd_get_email_templates() {
-	$templates = array(
-		'default' => __('Default Template', 'edd'),
-		'none' => __('No template, plain text only', 'edd')
-	);
-	return apply_filters( 'edd_email_templates', $templates );
-}	
