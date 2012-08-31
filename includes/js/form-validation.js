850c4da5cccc78dbada481c348d6208c1d0fd7ab
Ghost1227 <dgriffiths@ghost1227.com>
2012-08-31 09:43:47 -0500

    Moved js to form-validation

--- includes/js/form-validation.js
+++ includes/js/form-validation.js
@@ -20,4 +20,21 @@ jQuery(document).ready(function($) {
 			}
 		});
 	});
+
+    // update state/province field on checkout page
+    $('select[name=billing_country]').change(function() {
+        if( $('select[name=billing_country]').val() == 'US') {
+            $('input.card-state-other').css('display', 'none');
+            $('input.card-state-us').css('display', '');
+            $('input.card-state-ca').css('display', 'none');
+        } else if( $('select[name=billing_country]').val() == 'CA') {
+            $('input.card-state-other').css('display', 'none');
+            $('input.card-state-us').css('display', 'none');
+            $('input.card-state-ca').css('display', '');
+        } else {
+            $('input.card-state-other').css('display', '');
+            $('input.card-state-us').css('display', 'none');
+            $('input.card-state-ca').css('display', 'none');
+        }
+    });
 });
