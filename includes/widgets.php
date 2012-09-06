a549b9a6fd19399ad9fce0c18493a87752b30e16
Pippin Williamson <pippin@pippinsplugins.com>
2012-09-06 16:16:16 -0500

    Improved formatting on new Download History widgets

--- includes/widgets.php
+++ includes/widgets.php
@@ -220,19 +220,20 @@ class edd_purchase_history_widget extends WP_Widget {
                             $price_id = isset($download['options']['price_id']) ? $download['options']['price_id'] : null;
 							$download_files = edd_get_download_files( $id, $price_id );
 							echo '<div class="edd-purchased-widget-purchase edd-purchased-widget-purchase-' . $purchase->ID . '" id="edd-purchased-widget-purchase-' . $id . '">';
-                            echo '<div class="edd-purchased-widget-purchase-name">' . get_the_title($id) . '</div>';
-                            echo '<ul class="edd-purchased-widget-file-list">';
-                            if( ! edd_no_redownload() ) {
-                                if($download_files) {
-                                    foreach($download_files as $filekey => $file) {
-										$download_url = edd_get_download_file_url($purchase_data['key'], $purchase_data['email'], $filekey, $id);
-										echo '<li class="edd-purchased-widget-file"><a href="' . $download_url . '" class="edd-purchased-widget-file-link">' .  $file['name'] . '</a></li>';
+                                echo '<div class="edd-purchased-widget-purchase-name">' . get_the_title($id) . '</div>';
+                                echo '<ul class="edd-purchased-widget-file-list">';
+                                if( ! edd_no_redownload() ) {
+                                    if($download_files) {
+                                        foreach($download_files as $filekey => $file) {
+    										$download_url = edd_get_download_file_url($purchase_data['key'], $purchase_data['email'], $filekey, $id);
+    										echo '<li class="edd-purchased-widget-file"><a href="' . $download_url . '" class="edd-purchased-widget-file-link">' .  $file['name'] . '</a></li>';
+                                        }
+                                    } else {
+                                        echo '<li class="edd-purchased-widget-no-file">' . __('No downloadable files found.', 'edd');
                                     }
-                                } else {
-                                    echo '<li class="edd-purchased-widget-no-file">' . __('No downloadable files found.', 'edd');
                                 }
-                            }
-                            echo '</ul></div>';
+                                echo '</ul>';
+                            echo '</div>';
                         }
                     }
                 }
