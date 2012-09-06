5ec42d4148fff60446258b9e1e5a91f4b1e86543
Ghost1227 <dgriffiths@ghost1227.com>
2012-09-06 14:12:12 -0500

    Upstream issue #26

--- includes/js/form-validation.js
+++ includes/js/form-validation.js
@@ -20,4 +20,30 @@ jQuery(document).ready(function($) {
 			}
 		});
 	});
+
+    // update state/province field on checkout page
+    $('select[name=billing_country]').change(function() {
+        if( $('select[name=billing_country]').val() == 'US') {
+            $('#card_state_other').css('display', 'none');
+            $('#card_state_us').css('display', '');
+            $('#card_state_ca').css('display', 'none');
+        } else if( $('select[name=billing_country]').val() == 'CA') {
+            $('#card_state_other').css('display', 'none');
+            $('#card_state_us').css('display', 'none');
+            $('#card_state_ca').css('display', '');
+        } else {
+            $('#card_state_other').css('display', '');
+            $('#card_state_us').css('display', 'none');
+            $('#card_state_ca').css('display', 'none');
+        }
+    });
+    $('#card_state_other').change(function() {
+        $('#card_state').val($('#card_state_other').val());
+    }
+    $('#card_state_us').change(function() {
+		$('#card_state').val($('#card_state_us').val());
+    }
+    $('#card_state_ca').change(function() {
+        $('#card_state').val($('#card_state_ca').val());
+    }
 });
