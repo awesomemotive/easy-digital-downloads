572c7219754f7e8712a7de312b394f31069ed9a0
Pippin Williamson <pippin@pippinsplugins.com>
2012-09-11 01:06:49 -0500

    Added new templating functions

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -368,3 +368,106 @@ function edd_get_purchase_download_links( $purchase_data ) {
 
 	return $links;
 }
+
+
+/**
+ * Returns the path to the EDD templates directory
+ *
+ * @access      private
+ * @since       1.1.9
+ * @return      string
+*/
+
+function edd_get_templates_dir() {
+	return EDD_PLUGIN_DIR . '/includes/templates/';
+}
+
+
+/**
+ * Retrieves a template part
+ *
+ * @since v1.1.9
+ *
+ * Taken from bbPress
+ *
+ * @param string $slug
+ * @param string $name Optional. Default null
+ * @uses edd_locate_template()
+ * @uses load_template()
+ * @uses get_template_part()
+ */
+
+function edd_get_template_part( $slug, $name = null ) {
+
+	// Execute code for this part
+	do_action( 'get_template_part_' . $slug, $slug, $name );
+
+	// Setup possible parts
+	$templates = array();
+	if ( isset( $name ) )
+		$templates[] = $slug . '-' . $name . '.php';
+	$templates[] = $slug . '.php';
+
+	// Allow template parst to be filtered
+	$templates = apply_filters( 'edd_get_template_part', $templates, $slug, $name );
+
+	// Return the part that is found
+	return edd_locate_template( $templates, true, false );
+
+}
+
+
+/**
+ * Retrieve the name of the highest priority template file that exists.
+ *
+ * Searches in the STYLESHEETPATH before TEMPLATEPATH so that themes which
+ * inherit from a parent theme can just overload one file. If the template is
+ * not found in either of those, it looks in the theme-compat folder last.
+ *
+ * Taken from bbPress
+ *
+ * @since v1.1.9
+ *
+ * @param string|array $template_names Template file(s) to search for, in order.
+ * @param bool $load If true the template file will be loaded if it is found.
+ * @param bool $require_once Whether to require_once or require. Default true.
+ *                            Has no effect if $load is false.
+ * @return string The template filename if one is located.
+ */
+function edd_locate_template( $template_names, $load = false, $require_once = true ) {
+
+	// No file found yet
+	$located = false;
+
+	// Try to find a template file
+	foreach ( (array) $template_names as $template_name ) {
+
+		// Continue if template is empty
+		if ( empty( $template_name ) )
+			continue;
+
+		// Trim off any slashes from the template name
+		$template_name = ltrim( $template_name, '/' );
+
+		// Check child theme first
+		if ( file_exists( trailingslashit( STYLESHEETPATH ) . 'edd_templates/' . $template_name ) ) {
+			$located = trailingslashit( STYLESHEETPATH ) . 'edd_templates/' . $template_name;
+			break;
+
+		// Check parent theme next
+		} elseif ( file_exists( trailingslashit( TEMPLATEPATH ) . 'edd_templates/' . $template_name ) ) {
+			$located = trailingslashit( TEMPLATEPATH ) . 'edd_templates/' . $template_name;
+			break;
+
+		// Check theme compatibility last
+		} elseif ( file_exists( trailingslashit( edd_get_templates_dir() ) . $template_name ) ) {
+			$located = trailingslashit( edd_get_templates_dir() ) . $template_name;
+			break;
+		}
+	}
+
+	if ( ( true == $load ) && !empty( $located ) )
+		load_template( $located, $require_once );
+
+	return $located;
+}
