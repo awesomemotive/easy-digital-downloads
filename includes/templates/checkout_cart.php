d86481b3cecc714c5b690f9723f734b3e36c6694
Pippin Williamson <pippin@pippinsplugins.com>
2012-09-26 18:12:55 -0500

    Added check for whether taxes are enabled to only show the tax amount if taxes are enabled

--- includes/templates/checkout_cart.php
+++ includes/templates/checkout_cart.php
@@ -47,9 +47,11 @@
 		<tr class="edd_cart_footer_row">
 			<?php do_action( 'edd_checkout_table_footer_first' ); ?>
 			<th colspan="3" class="edd_cart_total">
+				<?php if( edd_use_taxes() ) : ?>
 				<div class="edd_cart_tax">
 					<?php _e( 'Tax', 'edd' ); ?>:&nbsp;<span class="edd_cart_tax_amount"><?php echo esc_html( edd_cart_tax() ); ?></span>
 				</div>
+				<?php endif; ?>
 				<div class="edd_final_total">
 					<?php _e( 'Total', 'edd' ); ?>:&nbsp;<span class="edd_cart_amount"><?php echo esc_html( edd_currency_filter( edd_format_amount( edd_get_cart_amount() ) ) ); ?></span>
 				</div>
