90ecf14761c13d5ad57e5c4e1dbf384c8a8ae8fb
Pippin Williamson <pippin@pippinsplugins.com>
2012-09-26 18:12:31 -0500

    Added initial tax calculation functions

--- includes/tax-functions.php
+++ includes/tax-functions.php
@@ -1,2 +1,54 @@
 <?php
 
+/**
+ * Checks if taxes are enabled
+ *
+ * @access      public
+ * @since       1.2.3
+ * @return      bool
+*/
+
+function edd_use_taxes() {
+	global $edd_options;
+
+	return isset( $edd_options['enable_taxes'] );
+}
+
+
+/**
+ * Get taxation rate
+ *
+ * @access      public
+ * @since       1.2.3
+ * @return      float
+*/
+
+function edd_get_tax_rate() {
+	global $edd_options;
+
+	$rate = isset( $edd_options['tax_rate'] ) ? (float) $edd_options['tax_rate'] : 0;
+
+	if( $rate > 1 ) {
+		// convert to a number we can use
+		$rate = $rate / 100;
+	}
+	return apply_filters( 'edd_tax_rate', $rate );
+}
+
+
+/**
+ * Calculate taxed amount
+ *
+ * @access      public
+ * @since       1.2.3
+ * @param 		$amount float The original amount to calculate a tax cost
+ * @return      float
+*/
+
+function edd_calculate_tax( $amount ) {
+
+	$rate 	= edd_get_tax_rate();
+	$tax 	= $amount * $rate; // the tax amount
+
+	return apply_filters( 'edd_taxed_amount', $tax, $rate );
+}
