0ca5c489961ccf6e08809d9a56e4207e98087b9c
Pippin Williamson <pippin@pippinsplugins.com>
2012-09-26 18:53:00 -0500

    Updated admin-scripts.js to master version

--- includes/js/admin-scripts.js
+++ includes/js/admin-scripts.js
@@ -1,4 +1,120 @@
 jQuery(document).ready(function ($) {
+
+    /**
+     * Download Configuration Metabox
+     */
+    var EDD_Download_Configuration = {
+        init : function() {
+            this.add();
+            this.remove();
+            this.prices();
+            this.files();
+        },
+
+        add : function() {
+            $( 'body' ).on( 'click', '.submit .edd_add_repeatable', function(e) {
+                e.preventDefault();
+
+                var button = $( this ),
+                    row    = button.parent().parent().prev( 'tr' ),
+                    clone  = row.clone();
+
+                /** manually update any select box values */
+                clone.find( 'select' ).each(function() {
+                    $( this ).val( row.find( 'select[name="' + $( this ).attr( 'name' ) + '"]' ).val() );
+                });
+
+                var count  = row.parent().find( 'tr' ).length - 1;
+
+                clone.removeClass( 'edd_add_blank' );
+                
+
+                clone.find( 'td input, td select' ).val( '' );
+                clone.find( 'input, select' ).each(function() {
+                        var name    = $( this ).attr( 'name' );
+
+                        name = name.replace( /\[(\d+)\]/, '[' + parseInt( count ) + ']');
+                        
+                        $( this )
+                            .attr( 'name', name )
+                            .attr( 'id', name );
+                    });
+
+                clone.insertAfter( row );
+                
+            });
+        },
+
+        remove : function() {
+            $( 'body' ).on( 'click', '.edd_remove_repeatable', function(e) {
+                e.preventDefault();
+                
+                var row   = $(this).parent().parent( 'tr' ),
+                    count = row.parent().find( 'tr' ).length - 1,
+                    type  = $(this).data('type');
+
+                if( count > 1 ) {
+                    $( 'input, select', row ).val( '' );
+                    row.fadeOut( 'fast' ).remove();
+                } else {
+                    switch( type ) {
+                        case 'price' :
+                            alert( edd_vars.one_price_min );
+                            break;
+                        case 'file' :
+                            alert( edd_vars.one_file_min );
+                            break;
+                        default:
+                            alert( edd_vars.one_field_min );
+                            break;
+                    }
+                }
+            });
+        },
+
+        prices : function() {
+            $( 'body' ).on( 'change', '#edd_variable_pricing', function(e) {
+                $( '.edd_pricing_fields' ).toggle();
+                $( '.edd_repeatable_condition_field' ).toggle();
+                $( '#edd_download_files table .pricing' ).toggle();
+            });
+        },
+
+        files : function() {
+            if ( $( '.edd_upload_image_button' ).length > 0 ) {
+                window.formfield = '';
+                
+                $('.edd_upload_image_button').on('click', function(e) {
+                    e.preventDefault();
+                    window.formfield = $(this).parent().prev();
+                    window.tbframe_interval = setInterval(function() {
+                        jQuery('#TB_iframeContent').contents().find('.savesend .button').val(edd_vars.use_this_file).end().find('#insert-gallery, .wp-post-thumbnail').hide();
+                    }, 2000);
+                    if (edd_vars.post_id != null ) {
+                        var post_id = 'post_id=' + edd_vars.post_id + '&';
+                    }
+                    tb_show(edd_vars.add_new_download, 'media-upload.php?' + post_id +'TB_iframe=true');
+                });
+                
+                window.original_send_to_editor = window.send_to_editor;
+                window.send_to_editor = function (html) {            
+                    if (window.formfield) {
+                        imgurl = $('a', '<div>' + html + '</div>').attr('href');
+                        window.formfield.val(imgurl);
+                        window.clearInterval(window.tbframe_interval);
+                        tb_remove();
+                    } else {
+                        window.original_send_to_editor(html);
+                    }
+                    window.formfield = '';
+                    window.imagefield = false;
+                }               
+            }
+        }
+    }
+
+    EDD_Download_Configuration.init();  
+
     //$('#edit-slug-box').remove();
     
     // date picker
@@ -8,176 +124,57 @@ jQuery(document).ready(function ($) {
             dateFormat: dateFormat
         });
     }
-
-	// toggle the pricing options
-	$('#edd_variable_pricing').on('click', function() {
-		$('.edd_pricing_fields').toggle();
-		$('.edd_repeatable_condition_field').toggle();
-	});
-	
-	// toggle the color options
-	$('#edd_purchase_style input').change(function() {
-		$('#edd_purchase_color').toggle();
-	});
-	
-	// add new price option 
-    $(".edd_add_new_price").on('click', function (e) {
-        var $this = $(this),
-            container = $this.closest('tr'),
-            field = $this.closest('td').find("div.edd_variable_prices_wrapper:last").clone(true),
-            fieldLocation = $this.closest('td').find('div.edd_variable_prices_wrapper:last');
-
-        // get the hidden field that has the name value
-        var name_field = $("input.edd_variable_prices_name_field", container);
-
-        // set the base of the new field name
-        var id = $(name_field).attr("id");
-
-        // set the new field val to blank
-        $('input[type="text"]', field).val("");
-
-        // set up a count var
-        var count = $('.edd_variable_prices_wrapper', container).size();
-        
-		field.find( 'input, select' ).each(function() {
-			var name = $( this ).attr( 'name' );
-			
-			name = name.replace( /\[(\d+)\]/, function() {
-				return '[' + count + ']'
-			});
-			
-			$( this )
-				.attr( 'name', name )
-				.attr( 'id', name );
-		});
-
-        field.insertAfter(fieldLocation, $this.closest('td'));
-
-        return false;
-    });
-	
-    // add new repeatable upload field
-    $(".edd_add_new_upload_field").on('click', function () {
-        var $this = $(this),
-            container = $this.closest('tr'),
-            field = $this.closest('td').find("div.edd_repeatable_upload_wrapper:last").clone(true),
-            fieldLocation = $this.closest('td').find('div.edd_repeatable_upload_wrapper:last');
-
-        // get the hidden field that has the name value
-        var name_field = $("input.edd_repeatable_upload_name_field", container);
-
-        // set the base of the new field name
-        var name = $(name_field).attr("id");
-
-        // set the new field val to blank
-        $('input[type="text"]', field).val("");
-
-        // set up a count var
-        var count = $('.edd_repeatable_upload_wrapper', container).size();
-
-        file_name = name + '[' + count + '][name]';
-        file_url = name + '[' + count + '][file]';
-		condition = name + '[' + count + '][condition]';
-
-        $('input.edd_repeatable_name_field', field).attr("name", file_name).attr("id", file_name);
-        $('input.edd_repeatable_upload_field', field).attr("name", file_url).attr("id", file_url);
-        $('select.edd_repeatable_condition_field', field).attr("name", condition).attr("id", condition);
-
-        field.insertAfter(fieldLocation, $this.closest('td'));
-
-        return false;
-    });
-
-    // remove repeatable field
-    $('.edd_remove_repeatable').on('click', function (e) {
-        e.preventDefault();
-        var field = $(this).parent();
-        $('input', field).val("");
-        field.remove();
-        return false;
-    });
-
-    if ($('.edd_upload_image_button').length > 0) {
-        // Media Uploader
-        window.formfield = '';
-        
-        $('.edd_upload_image_button').on('click', function (e) {
-            e.preventDefault();
-            window.formfield = $('.edd_upload_field', $(this).parent());
-    		window.tbframe_interval = setInterval(function() {
-    		    jQuery('#TB_iframeContent').contents().find('.savesend .button').val(edd_vars.use_this_file).end().find('#insert-gallery, .wp-post-thumbnail').hide();
-    		}, 2000);
-            if (edd_vars.post_id != null ) {
-                var post_id = 'post_id=' + edd_vars.post_id + '&';
+    
+    $('#edd-add-download').on('click', function() {
+        var downloads = [];
+        $('.edd-download-to-add').each(function() {
+            if($(this).is(':checked')) {
+                var id = $(this).val();
+                data = {
+                    action: 'edd_get_download_title',
+                    download_id: id
+                };
+                $.post(ajaxurl, data, function (response) {
+                    if (response != 'fail') {
+                        var html = '<div class="purchased_download_' + id + '"><input type="hidden" name="edd-purchased-downloads[]" value="' + id + '"/><strong>' + response + '</strong> - <a href="#" class="edd-remove-purchased-download" data-action="remove_purchased_download" data-id="' + id + '">Remove</a></div>';
+                        $(html).insertBefore('#edit-downloads');
+                    }
+                });
             }
-            tb_show(edd_vars.add_new_download, 'media-upload.php?' + post_id +'TB_iframe=true');
         });
-        
-        window.original_send_to_editor = window.send_to_editor;
-        window.send_to_editor = function (html) {            
-            if (window.formfield) {
-                imgurl = $('a', '<div>' + html + '</div>').attr('href');
-                window.formfield.val(imgurl);
-                window.clearInterval(window.tbframe_interval);
-                tb_remove();
-            } else {
-                window.original_send_to_editor(html);
+        tb_remove();
+        return false;
+    });
+    
+    $('#purchased-downloads').on('click', '.edd-remove-purchased-download', function() {
+        var $this = $(this);
+        data = {
+            action: $this.data('action'),
+            download_id: $this.data('id')
+        };
+        $.post(ajaxurl, data, function (response) {
+            if (response != 'fail') {
+                $('.purchased_download_' + $this.data('id')).remove();
             }
-            window.formfield = '';
-            window.imagefield = false;
+        });
+        return false;
+    });
+    
+    // show / hide the send purchase receipt check box on the Edit payment screen
+    $('#edd_payment_status').change(function() {
+        if( $('#edd_payment_status option:selected').val() == 'publish' ) {
+            $('#edd_payment_notification').slideDown();
+        } else {
+            $('#edd_payment_notification').slideUp();
         }
-    }
-	
-	$('#edd-add-download').on('click', function() {
-		var downloads = [];
-		$('.edd-download-to-add').each(function() {
-			if($(this).is(':checked')) {
-				var id = $(this).val();
-				data = {
-					action: 'edd_get_download_title',
-					download_id: id
-				};
-				$.post(ajaxurl, data, function (response) {
-					if (response != 'fail') {
-						var html = '<div class="purchased_download_' + id + '"><input type="hidden" name="edd-purchased-downloads[]" value="' + id + '"/><strong>' + response + '</strong> - <a href="#" class="edd-remove-purchased-download" data-action="remove_purchased_download" data-id="' + id + '">Remove</a></div>';
-						$(html).insertBefore('#edit-downloads');
-					}
-				});
-			}
-		});
-		tb_remove();
-		return false;
-	});
-	
-	$('#purchased-downloads').on('click', '.edd-remove-purchased-download', function() {
-		var $this = $(this);
-		data = {
-			action: $this.data('action'),
-			download_id: $this.data('id')
-		};
-		$.post(ajaxurl, data, function (response) {
-			if (response != 'fail') {
-				$('.purchased_download_' + $this.data('id')).remove();
-			}
-		});
-		return false;
-	});
-	
-	// show / hide the send purchase receipt check box on the Edit payment screen
-	$('#edd_payment_status').change(function() {
-		if( $('#edd_payment_status option:selected').val() == 'publish' ) {
-			$('#edd_payment_notification').slideDown();
-		} else {
-			$('#edd_payment_notification').slideUp();
-		}
-	});	
-	
-	$('.edd_payment .row-actions .delete a').on('click', function() {
-		if( confirm( edd_vars.delete_payment ) ) {
-			return true;	
-		}		
-		return false;
-	});	
+    }); 
+    
+    $('.edd_payment .row-actions .delete a').on('click', function() {
+        if( confirm( edd_vars.delete_payment ) ) {
+            return true;    
+        }       
+        return false;
+    }); 
 
     $('#the-list').on('click', '.editinline', function() {
         inlineEditPost.revert();
@@ -207,5 +204,5 @@ jQuery(document).ready(function ($) {
             height: 'auto'
         });
     }
-	
+    
 });
