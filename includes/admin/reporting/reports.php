47b3cdec1cec2ba365518d37b14f666776eb3e0a
Pippin Williamson <pippin@pippinsplugins.com>
2012-09-29 11:42:12 -0500

    Coding standards improvements for #384

--- includes/admin/reporting/reports.php
+++ includes/admin/reporting/reports.php
@@ -22,18 +22,18 @@
 
 function edd_reports_page() {
 	global $edd_options;	
-	$current_page = admin_url('edit.php?post_type=download&page=edd-reports');
+	$current_page = admin_url( 'edit.php?post_type=download&page=edd-reports' );
 	?>
 	<div class="wrap">
-		<h2><?php _e('Reports', 'edd'); ?></h2>
-		<?php do_action('edd_reports_page_top'); ?>
-		<?php do_action('edd_reports_page'); ?>
-		<?php do_action('edd_reports_page_bottom'); ?>
+		<h2><?php _e( 'Reports', 'edd' ); ?></h2>
+		<?php do_action( 'edd_reports_page_top' ); ?>
+		<?php do_action( 'edd_reports_page' ); ?>
+		<?php do_action( 'edd_reports_page_bottom' ); ?>
 		<p>
-			<a class="button" href="<?php echo wp_nonce_url( add_query_arg( array( 'edd-action' => 'generate_pdf' ) ), 'edd_generate_pdf' ); ?>"><?php _e('Download Sales and Earnings PDF Report for all Products', 'edd'); ?></a>
-			<a class="button" href="<?php echo wp_nonce_url( add_query_arg( array( 'edd-action' => 'email_export' ) ), 'edd_email_export' ); ?>"><?php _e('Download a CSV Customers List', 'edd'); ?></a>
+			<a class="button" href="<?php echo wp_nonce_url( add_query_arg( array( 'edd-action' => 'generate_pdf' ) ), 'edd_generate_pdf' ); ?>"><?php _e( 'Download Sales and Earnings PDF Report for all Products', 'edd' ); ?></a>
+			<a class="button" href="<?php echo wp_nonce_url( add_query_arg( array( 'edd-action' => 'email_export' ) ), 'edd_email_export' ); ?>"><?php _e( 'Download a CSV Customers List', 'edd') ; ?></a>
 		</p>
-		<p><?php _e('Please Note: Transactions created while in test mode are not included on this page or in the PDF reports.', 'edd'); ?></p>
+		<p><?php _e( 'Please Note: Transactions created while in test mode are not included on this page or in the PDF reports.', 'edd' ); ?></p>
 	</div><!--end wrap-->
 	<?php
 }
