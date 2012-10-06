eb9ef3346cb05bf61c16913c41712614b9a87e77
Sunny Ratilal <ratilal.sunny@gmail.com>
2012-10-06 14:00:50 +0100

    Code formatting install.php

--- includes/install.php
+++ includes/install.php
@@ -22,51 +22,51 @@
 
 function edd_install() {
 	global $wpdb, $edd_options;
-	
-	// check if the purchase page option exists
-	if(!isset($edd_options['purchase_page'])) {
-	    // checkout
+
+	// Checks if the purchase page option exists
+	if( !isset( $edd_options['purchase_page'] ) ) {
+	    // Checkout Page
 		$checkout = wp_insert_post(
 			array(
-				'post_title' => __('Checkout', 'edd'),
-				'post_content' => '[download_checkout]',
-				'post_status' => 'publish',
-				'post_author' => 1,
-				'post_type' => 'page',
+				'post_title'     => __('Checkout', 'edd'),
+				'post_content'   => '[download_checkout]',
+				'post_status'    => 'publish',
+				'post_author'    => 1,
+				'post_type'      => 'page',
 				'comment_status' => 'closed'
 			)
 		);
-		// success
+		// Purchase Confirmation (Success) Page
 		$success = wp_insert_post(
 			array(
-				'post_title' => __('Purchase Confirmation', 'edd'),
-				'post_content' => __('Thank you for your purchase!', 'edd'),
-				'post_status' => 'publish',
-				'post_author' => 1,
-				'post_type' => 'page',
+				'post_title'     => __('Purchase Confirmation', 'edd'),
+				'post_content'   => __('Thank you for your purchase!', 'edd'),
+				'post_status'    => 'publish',
+				'post_author'    => 1,
+				'post_type'      => 'page',
 				'comment_status' => 'closed'
 			)
 		);
-		// history
+		// Purchase History (History) Page
 		$history = wp_insert_post(
 			array(
-				'post_title' => __('Purchase History', 'edd'),
-				'post_content' => '[download_history]',
-				'post_status' => 'publish',
-				'post_author' => 1,
-				'post_type' => 'page',
+				'post_title'     => __('Purchase History', 'edd'),
+				'post_content'   => '[download_history]',
+				'post_status'    => 'publish',
+				'post_author'    => 1,
+				'post_type'      => 'page',
 				'comment_status' => 'closed'
 			)
 		);
 	}
 	
-	// setup the download custom post type
+	// Setup the Downloads Custom Post Type
 	edd_setup_edd_post_types();
 	
-	// setup the download custom taxonomies
+	// Setup the Download Taxonomies
 	edd_setup_download_taxonomies();
 	
-	// clear permalinks
+	// Clear the permalinks
 	flush_rewrite_rules();
 }
 register_activation_hook(EDD_PLUGIN_FILE, 'edd_install');
