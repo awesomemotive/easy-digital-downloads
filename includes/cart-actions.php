5e90a27ab8194ca5f924e5d33dafe932afb8a1b6
Sunny Ratilal <ratilal.sunny@gmail.com>
2012-10-06 12:53:16 +0100

    Code formatting cart-actions.php

--- includes/cart-actions.php
+++ includes/cart-actions.php
@@ -20,12 +20,12 @@
  * @return      void
 */
 
-function edd_process_add_to_cart($data) {
+function edd_process_add_to_cart( $data ) {
 	$download_id = $data['download_id'];
-	$options = isset($data['edd_options']) ? $data['edd_options'] : array(); 
-	$cart = edd_add_to_cart($download_id, $options);
+	$options = isset( $data['edd_options'] ) ? $data['edd_options'] : array(); 
+	$cart = edd_add_to_cart( $download_id, $options );
 }
-add_action('edd_add_to_cart', 'edd_process_add_to_cart');
+add_action( 'edd_add_to_cart', 'edd_process_add_to_cart' );
 
 
 /**
@@ -38,11 +38,11 @@ add_action('edd_add_to_cart', 'edd_process_add_to_cart');
  * @return      void
 */
 
-function edd_process_remove_fromt_cart($data) {
+function edd_process_remove_fromt_cart( $data ) {
 	$cart_key = $_GET['cart_item'];
-	$cart = edd_remove_from_cart($cart_key);
+	$cart = edd_remove_from_cart( $cart_key );
 }
-add_action('edd_remove', 'edd_process_remove_fromt_cart');
+add_action( 'edd_remove', 'edd_process_remove_fromt_cart' );
 
 
 /**
@@ -55,10 +55,11 @@ add_action('edd_remove', 'edd_process_remove_fromt_cart');
  * @return      void
 */
 
-function edd_process_collection_purchase($data) {
-	$taxonomy = urldecode($data['taxonomy']);
-	$terms = urldecode($data['terms']);
-	$cart_items = edd_add_collection_to_cart($taxonomy, $terms);
-	wp_redirect(add_query_arg('added', '1', remove_query_arg(array('edd_action', 'taxonomy', 'terms')))); exit;
+function edd_process_collection_purchase( $data ) {
+	$taxonomy = urldecode( $data['taxonomy'] );
+	$terms = urldecode( $data['terms'] );
+	$cart_items = edd_add_collection_to_cart( $taxonomy, $terms );
+	wp_redirect( add_query_arg( 'added', '1', remove_query_arg( array( 'edd_action', 'taxonomy', 'terms' ) ) ) );
+	exit;
 }
-add_action('edd_purchase_collection', 'edd_process_collection_purchase');
+add_action( 'edd_purchase_collection', 'edd_process_collection_purchase' );
