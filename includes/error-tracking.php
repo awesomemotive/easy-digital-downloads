a56af6d1074c478f656d50a47d0b701f862bf69a
Sunny Ratilal <ratilal.sunny@gmail.com>
2012-10-06 13:42:37 +0100

    Code formatting error-tracking.php

--- includes/error-tracking.php
+++ includes/error-tracking.php
@@ -23,19 +23,19 @@
 
 function edd_print_errors() {
 	$errors = edd_get_errors();
-	if($errors) {
+	if( $errors ) {
 		echo '<div class="edd_errors">';
 		    // Loop error codes and display errors
-		   foreach($errors as $error_id => $error){
+		   foreach( $errors as $error_id => $error ){
 		        echo '<p class="edd_error" id="edd_error_' . $error_id . '"><strong>' . __('Error', 'edd') . '</strong>: ' . $error . '</p>';
 		    }
 		echo '</div>';
 		edd_clear_errors();
 	}
 }
-add_action('edd_payment_mode_bottom', 'edd_print_errors');
-add_action('edd_before_purchase_form', 'edd_print_errors');
-add_action('edd_before_checkout_register_form', 'edd_print_errors');
+add_action( 'edd_payment_mode_bottom', 'edd_print_errors' );
+add_action( 'edd_before_purchase_form', 'edd_print_errors' );
+add_action( 'edd_before_checkout_register_form', 'edd_print_errors' );
 
 
 /**
@@ -50,7 +50,7 @@ add_action('edd_before_checkout_register_form', 'edd_print_errors');
 */
 
 function edd_get_errors() {
-	if(isset($_SESSION['edd-errors'])) {
+	if( isset( $_SESSION['edd-errors'] ) ) {
 		$errors = $_SESSION['edd-errors'];
 		return $errors;
 	}
@@ -70,12 +70,12 @@ function edd_get_errors() {
  * @return      void
 */
 
-function edd_set_error($error_id, $error_message) {
+function edd_set_error( $error_id, $error_message ) {
 	$errors = edd_get_errors();
-	if(!$errors) {
+	if( !$errors ) {
 		$errors = array();
 	}
-	$errors[$error_id] = $error_message;
+	$errors[ $error_id ] = $error_message;
 	$_SESSION['edd-errors'] = $errors;
 }
 
@@ -91,5 +91,5 @@ function edd_set_error($error_id, $error_message) {
 */
 
 function edd_clear_errors() {
-	if(isset($_SESSION['edd-errors'])) $_SESSION['edd-errors'] = null;
+	if( isset( $_SESSION['edd-errors'] ) ) $_SESSION['edd-errors'] = null;
 }
