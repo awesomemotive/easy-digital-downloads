bbebf8e7ef7856bc645ed725d6b967bc41041832
Pippin Williamson <pippin@pippinsplugins.com>
2012-10-09 18:10:12 -0500

    Added new edd_download_history_files and updated edd_download_history_row_end with download ID

--- includes/templates/history-downloads.php
+++ includes/templates/history-downloads.php
@@ -1,6 +1,6 @@
 <?php
 $purchases = edd_get_users_purchases();	
-if($purchases) :
+if( $purchases ) :
 	do_action( 'edd_before_download_history' );
 	?>
 	<table id="edd_user_history">
@@ -41,18 +41,21 @@ if($purchases) :
 								foreach($download_files as $filekey => $file) {
 
 									$download_url = edd_get_download_file_url( $purchase_data['key'], $purchase_data['email'], $filekey, $id );
+									
 									echo'<div class="edd_download_file"><a href="' . esc_url( $download_url ) . '" class="edd_download_file_link">' . esc_html( $file['name'] ) . '</a></div>';
 								
+									do_action( 'edd_download_history_files', $filekey, $file, $id, $post->ID, $purchase_data );
+
 								} 
 							} else {
-								_e('No downloadable files found.', 'edd');
+								_e( 'No downloadable files found.', 'edd' );
 							}
 
 							echo '</td>';
 
 						} // end if ! edd_no_redownload()
 
-						do_action('edd_download_history_row_end', $post->ID);
+						do_action( 'edd_download_history_row_end', $post->ID, $id );
 
 					echo '</tr>';
 
