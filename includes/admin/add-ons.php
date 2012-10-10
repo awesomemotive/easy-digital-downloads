c9e7eb1baefcb875fa5bf6ca5757b10e9b86ab77
Pippin Williamson <pippin@pippinsplugins.com>
2012-10-10 10:52:42 -0500

    Fixed problem with add-ons page. #409

--- includes/admin/add-ons.php
+++ includes/admin/add-ons.php
@@ -28,30 +28,6 @@ add_action( 'admin_menu', 'edd_add_ons_init');
 
 
 /**
- * Add-ons Check Feed
- *
- * Gets the feed or sets an error.
- *
- * @access      private
- * @since       1.0 
- * @return      void
-*/
-
-function edd_add_ons_check_feed() {
-	global $edd_addons_feed_content;
-	$screen = get_current_screen();
-	if( isset( $screen->base ) && $screen->base === 'download_page_edd-addons' ) {
-		$cache = edd_add_ons_get_feed();
-		if ( false === $cache ) {
-			add_action( 'admin_notices', 'edd_admin_addons_notices' );
-		} else {
-			$edd_addons_feed_content = $cache;
-		}
-	}	 
-}
-
-
-/**
  * Add-ons Page
  *
  * Renders the add-ons page content.
@@ -62,13 +38,11 @@ function edd_add_ons_check_feed() {
 */
 
 function edd_add_ons_page() {
-	global $edd_addons_feed_content;
-
 	ob_start(); ?>
 	<div class="wrap" id="edd-add-ons">
 		<h2><?php _e( 'Add Ons for Easy Digital Downloads', 'edd' ); ?></h2>
 		<p><?php _e( 'These add-ons extend the functionality of Easy Digital Downloads.', 'edd' ); ?></p>
-		<?php echo $edd_addons_feed_content; ?>
+		<?php echo edd_add_ons_get_feed(); ?>
 		<p class="edd-browse-all">
 			<a href="http://easydigitaldownloads.com/extensions/?ref=1" class="button-primary" title="<?php _e( 'Browse All Extensions', 'edd' ); ?>" target="_blank"><?php _e( 'Browse All Extensions', 'edd' ); ?></a>
 		</p>
@@ -96,7 +70,10 @@ function edd_add_ons_get_feed() {
 				$cache = wp_remote_retrieve_body( $feed );
 				set_transient( 'easydigitaldownloads_add_ons_feed', $cache, 3600 );
 			}
+		} else {
+			$cache = '<div class="error"><p>' . __( 'There was an error retrieving the extensions list from the server. Please try again later.', 'edd' ) . '</div>';
 		}
+
 	}
 	return $cache;
 }
