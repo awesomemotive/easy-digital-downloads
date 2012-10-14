af6ce5cd5123539afd05fa18b9716fa22cccc5a1
Pippin Williamson <pippin@pippinsplugins.com>
2012-10-14 19:06:22 -0400

    Improved formatting of edd_get_ip and added new edd_get_ip filter

--- includes/misc-functions.php
+++ includes/misc-functions.php
@@ -156,18 +156,17 @@ function edd_string_is_image_url( $str ) {
  * @return      string
 */
 
-function edd_get_ip()
-{
-	if( !empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+function edd_get_ip() {
+	if( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
 		//check ip from share internet
 	  $ip = $_SERVER['HTTP_CLIENT_IP'];
-	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	} elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
 		//to check ip is pass from proxy
 	  $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 	} else {
 	  $ip = $_SERVER['REMOTE_ADDR'];
 	}
-	return $ip;
+	return apply_filters( 'edd_get_ip', $ip );
 }
 
 
