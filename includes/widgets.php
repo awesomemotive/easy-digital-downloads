ea0cef49ceebc765fca9d95219cec02c56e7a43e
Sunny Ratilal <ratilal.sunny@gmail.com>
2012-10-24 06:57:10 +0100

    Code formatting widgets.php

--- includes/widgets.php
+++ includes/widgets.php
@@ -40,43 +40,43 @@ class edd_cart_widget extends WP_Widget {
     }
 
     /** @see WP_Widget::widget */
-    function widget($args, $instance)
+    function widget( $args, $instance )
     {
-        extract($args);
-        $title = apply_filters('widget_title', $instance['title']);
-        $quantity = isset($instance['quantity']) ? $instance['quantity'] : false;
+        extract( $args );
+        $title = apply_filters( 'widget_title', $instance['title'] );
+        $quantity = isset( $instance['quantity'] ) ? $instance['quantity'] : false;
 
         global $post, $edd_options;
 
         echo $before_widget;
-        if ($title) {
-            if ($quantity == 1) {
+        if ( $title ) {
+            if ( $quantity == 1 ) {
                 $quantity = ' - <span class="edd-cart-quantity">' . edd_get_cart_quantity() . '</span>';
             } else {
                 $quantity = '';
             }
             echo $before_title . $title . $quantity . $after_title;
         }
-        do_action('edd_before_cart_widget');
-        edd_shopping_cart(true);
-        do_action('edd_after_cart_widget');
+        do_action( 'edd_before_cart_widget' );
+        edd_shopping_cart( true );
+        do_action( 'edd_after_cart_widget' );
         echo $after_widget;
     }
 
     /** @see WP_Widget::update */
-    function update($new_instance, $old_instance)
+    function update( $new_instance, $old_instance )
     {
         $instance = $old_instance;
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['quantity'] = strip_tags($new_instance['quantity']);
+        $instance['title'] = strip_tags( $new_instance['title'] );
+        $instance['quantity'] = strip_tags( $new_instance['quantity'] );
         return $instance;
     }
 
     /** @see WP_Widget::form */
-    function form($instance)
+    function form( $instance )
     {
-        $title = isset($instance['title']) ? esc_attr($instance['title']) : '';
-        $quantity = isset($instance['quantity']) ? esc_attr($instance['quantity']) : '';
+        $title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+        $quantity = isset( $instance['quantity'] ) ? esc_attr( $instance['quantity'] ) : '';
         ?>
     <p>
         <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'edd'); ?></label>
@@ -84,10 +84,10 @@ class edd_cart_widget extends WP_Widget {
                name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>"/>
     </p>
     <p>
-        <label for="<?php echo $this->get_field_id('quantity'); ?>"><?php _e('Show Quantity:', 'edd'); ?></label>
-        <input id="<?php echo $this->get_field_id('quantity'); ?>"
-               name="<?php echo $this->get_field_name('quantity'); ?>" type="checkbox"
-               value="1" <?php checked('1', $quantity); ?>/>
+        <label for="<?php echo $this->get_field_id( 'quantity' ); ?>"><?php _e('Show Quantity:', 'edd'); ?></label>
+        <input id="<?php echo $this->get_field_id( 'quantity' ); ?>"
+               name="<?php echo $this->get_field_name( 'quantity' ); ?>" type="checkbox"
+               value="1" <?php checked( '1', $quantity ); ?>/>
     </p>
     <?php
     }
@@ -109,13 +109,13 @@ class edd_categories_tags_widget extends WP_Widget {
     /** constructor */
     function edd_categories_tags_widget()
     {
-        parent::WP_Widget(false, __('Downloads Categories / Tags', 'edd'), array('description' => __('Display the downloads categories or tags', 'edd')));
+        parent::WP_Widget( false, __('Downloads Categories / Tags', 'edd'), array( 'description' => __('Display the downloads categories or tags', 'edd') ) );
     }
 
     /** @see WP_Widget::widget */
-    function widget($args, $instance)
+    function widget( $args, $instance )
     {
-        extract($args);
+        extract( $args );
         $title = apply_filters('widget_title', $instance['title']);
         $tax = $instance['taxonomy'];
         
@@ -126,48 +126,48 @@ class edd_categories_tags_widget extends WP_Widget {
             echo $before_title . $title . $after_title;
         }
         
-        do_action('edd_before_taxonomy_widget');
-        $terms = get_terms($tax);
+        do_action( 'edd_before_taxonomy_widget' );
+        $terms = get_terms( $tax );
 
-            if (is_wp_error($terms)) {
+            if ( is_wp_error( $terms ) ) {
                 return;
             } else {
                 echo "<ul class=\"edd-taxonomy-widget\">\n";
-                foreach ($terms as $term) {
+                foreach ( $terms as $term ) {
                     echo '<li><a href="' . get_term_link( $term ) . '" title="' . esc_attr( $term->name ) . '" rel="bookmark">' . $term->name . '</a></li>'."\n";
                 }
                 echo "</ul>\n";
             }
         
-        do_action('edd_after_taxonomy_widget');
+        do_action( 'edd_after_taxonomy_widget' );
         echo $after_widget;
     }
 
     /** @see WP_Widget::update */
-    function update($new_instance, $old_instance)
+    function update( $new_instance, $old_instance )
     {
         $instance = $old_instance;
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['taxonomy'] = strip_tags($new_instance['taxonomy']);
+        $instance['title'] = strip_tags( $new_instance['title'] );
+        $instance['taxonomy'] = strip_tags( $new_instance['taxonomy'] );
         return $instance;
     }
 
     /** @see WP_Widget::form */
-    function form($instance)
+    function form( $instance )
     {
-        $title = isset($instance['title']) ? esc_attr($instance['title']) : '';
-        $taxonomy = isset($instance['taxonomy']) ? esc_attr($instance['taxonomy']) : 'download_category';
+        $title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+        $taxonomy = isset( $instance['taxonomy'] ) ? esc_attr( $instance['taxonomy'] ) : 'download_category';
         ?>
         <p>
-            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'edd'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
-                   name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>"/>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'edd'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
+                   name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>"/>
         </p>
         <p>
-            <label for="<?php echo $this->get_field_id('taxonomy'); ?>"><?php _e('Taxonomy:', 'edd'); ?></label>
-            <select name="<?php echo $this->get_field_name('taxonomy'); ?>" id="<?php echo $this->get_field_id('taxonomy'); ?>">
-                <option value="download_category" <?php selected('download_category',$taxonomy); ?>><?php _e('Categories', 'edd'); ?></option>
-                <option value="download_tag" <?php selected('download_tag',$taxonomy); ?>><?php _e('Tags', 'edd'); ?></option>
+            <label for="<?php echo $this->get_field_id( 'taxonomy' ); ?>"><?php _e('Taxonomy:', 'edd'); ?></label>
+            <select name="<?php echo $this->get_field_name( 'taxonomy' ); ?>" id="<?php echo $this->get_field_id( 'taxonomy' ); ?>">
+                <option value="download_category" <?php selected( 'download_category', $taxonomy ); ?>><?php _e('Categories', 'edd'); ?></option>
+                <option value="download_tag" <?php selected( 'download_tag', $taxonomy ); ?>><?php _e('Tags', 'edd'); ?></option>
             </select>
         </p>
     <?php
@@ -190,42 +190,42 @@ class edd_purchase_history_widget extends WP_Widget {
     /** constructor */
     function edd_purchase_history_widget()
     {
-        parent::WP_Widget(false, __('Purchase History', 'edd'), array('description' => __('Display a user\'s purchase history', 'edd')));
+        parent::WP_Widget( false, __('Purchase History', 'edd'), array( 'description' => __('Display a user\'s purchase history', 'edd') ) );
     }
 
     /** @see WP_Widget::widget */
-    function widget($args, $instance)
+    function widget( $args, $instance )
     {
-        extract($args);
-        $title = apply_filters('widget_title', $instance['title']);
+        extract( $args );
+        $title = apply_filters( 'widget_title', $instance['title'] );
 
         global $user_ID, $edd_options;
 
-        if(is_user_logged_in()) {
+        if( is_user_logged_in() ) {
 
-            $purchases = edd_get_users_purchases($user_ID);
+            $purchases = edd_get_users_purchases( $user_ID );
 
-            if($purchases) {
+            if( $purchases ) {
                 echo $before_widget;
-                if ($title) {
+                if ( $title ) {
                     echo $before_title . $title . $after_title;
                 }
 
-                foreach($purchases as $purchase) {
-                    $purchase_data = get_post_meta($purchase->ID, '_edd_payment_meta', true);
-                    $downloads = edd_get_downloads_of_purchase($purchase->ID);
-                    if($downloads) {
-                        foreach($downloads as $download) {
-                            $id = isset($purchase_data['cart_details']) ? $download['id'] : $download;
-                            $price_id = isset($download['options']['price_id']) ? $download['options']['price_id'] : null;
+                foreach ( $purchases as $purchase ) {
+                    $purchase_data = get_post_meta( $purchase->ID, '_edd_payment_meta', true );
+                    $downloads = edd_get_downloads_of_purchase( $purchase->ID );
+                    if ( $downloads ) {
+                        foreach ( $downloads as $download ) {
+                            $id = isset( $purchase_data['cart_details'] ) ? $download['id'] : $download;
+                            $price_id = isset( $download['options']['price_id'] ) ? $download['options']['price_id'] : null;
 							$download_files = edd_get_download_files( $id, $price_id );
 							echo '<div class="edd-purchased-widget-purchase edd-purchased-widget-purchase-' . $purchase->ID . '" id="edd-purchased-widget-purchase-' . $id . '">';
-                                echo '<div class="edd-purchased-widget-purchase-name">' . get_the_title($id) . '</div>';
+                                echo '<div class="edd-purchased-widget-purchase-name">' . get_the_title( $id ) . '</div>';
                                 echo '<ul class="edd-purchased-widget-file-list">';
-                                if( ! edd_no_redownload() ) {
-                                    if($download_files) {
-                                        foreach($download_files as $filekey => $file) {
-    										$download_url = edd_get_download_file_url($purchase_data['key'], $purchase_data['email'], $filekey, $id);
+                                if( !edd_no_redownload() ) {
+                                    if( $download_files ) {
+                                        foreach( $download_files as $filekey => $file ) {
+    										$download_url = edd_get_download_file_url( $purchase_data['key'], $purchase_data['email'], $filekey, $id );
     										echo '<li class="edd-purchased-widget-file"><a href="' . $download_url . '" class="edd-purchased-widget-file-link">' .  $file['name'] . '</a></li>';
                                         }
                                     } else {
@@ -243,22 +243,22 @@ class edd_purchase_history_widget extends WP_Widget {
     }
 
     /** @see WP_Widget::update */
-    function update($new_instance, $old_instance)
+    function update( $new_instance, $old_instance )
     {
         $instance = $old_instance;
-        $instance['title'] = strip_tags($new_instance['title']);
+        $instance['title'] = strip_tags( $new_instance['title'] );
         return $instance;
     }
 
     /** @see WP_Widget::form */
-    function form($instance)
+    function form( $instance )
     {
-        $title = isset($instance['title']) ? esc_attr($instance['title']) : '';
+        $title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
         ?>
         <p>
-            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'edd'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
-                   name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>"/>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title', 'edd'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
+                   name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>"/>
         </p>
     <?php
     }
@@ -276,9 +276,9 @@ class edd_purchase_history_widget extends WP_Widget {
 */
 
 function edd_register_widgets() {
-    register_widget('edd_cart_widget');
-    register_widget('edd_categories_tags_widget');
-    register_widget('edd_purchase_history_widget');
+    register_widget( 'edd_cart_widget' );
+    register_widget( 'edd_categories_tags_widget' );
+    register_widget( 'edd_purchase_history_widget' );
 }
 add_action('widgets_init', 'edd_register_widgets');
 
