b636e4bc2bf8179153c60e4a69270c12bbc00d27
Pippin Williamson <pippin@pippinsplugins.com>
2012-11-05 17:59:40 -0600

    Fixed bug with PressTrends integration

--- includes/misc-functions.php
+++ includes/misc-functions.php
@@ -731,7 +731,7 @@ function edd_presstrends() {
 			$plugin_name .= $plugin_info['Name'] . '&';
 		}
 		// CHANGE __FILE__ PATH IF LOCATED OUTSIDE MAIN PLUGIN FILE
-		$plugin_data         = get_plugin_data( __FILE__ );
+		$plugin_data         = get_plugin_data( EDD_PLUGIN_FILE );
 		$posts_with_comments = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->posts WHERE post_type='post' AND comment_count > 0" );
 		$data                = array(
 			'url'             => stripslashes( str_replace( array( 'http://', '/', ':' ), '', site_url() ) ),
