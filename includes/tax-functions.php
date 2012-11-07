437b18ead36d919e7cf77ed08a3e2a4369860898
Pippin Williamson <pippin@pippinsplugins.com>
2012-11-07 15:46:22 -0600

    Added new edd_record_taxed_amount() function to automatically record tax info with purchases.

--- includes/tax-functions.php
+++ includes/tax-functions.php
@@ -3,7 +3,8 @@
  * Tax Functions
  *
  * These are functions used for checking if taxes are enabled, calculating taxes, etc.
- * Functions for retrieving tax amounts and such for individual payments are in includes/payment-functions.php
+ * Functions for retrieving tax amounts and such for individual payments are in 
+ * includes/payment-functions.php and includes/cart-functions.php
  *
  * @package     Easy Digital Downloads
  * @subpackage  Payment Functions
@@ -72,3 +73,38 @@ function edd_calculate_tax( $amount ) {
 
 	return apply_filters( 'edd_taxed_amount', $tax, $rate );
 }
+
+
+/**
+ * Stores the tax info in the payment meta
+ *
+ * @access      public
+ * @since       1.3.3
+ * @param 		$payment_meta array The meta data to store with the payment
+ * @param 		$payment_data array The info sent from process-purchase.php
+ * @return      array
+*/
+
+function edd_record_taxed_amount( $payment_meta, $payment_data ) {
+
+	if( ! edd_use_taxes() )
+		return $payment_meta;
+
+	if( edd_local_taxes_only() && isset( $_POST['edd_tax_opt_in'] ) ) {
+		
+		// calculate local taxes
+		$payment_meta['subtotal'] 	= edd_get_cart_amount( false );
+		$payment_meta['tax'] 		= edd_get_cart_tax();
+
+	} elseif( ! edd_local_taxes_only() ) {
+		
+		// calculate global taxes
+		$payment_meta['subtotal'] 	= edd_get_cart_amount( false );
+		$payment_meta['tax'] 		= edd_get_cart_tax();
+	
+	}
+
+	return $payment_meta;
+
+}
+add_filter( 'edd_payment_meta', 'edd_record_taxed_amount', 10, 2 );
