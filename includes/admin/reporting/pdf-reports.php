57fb88faa4ae7fc0d515eff9a422bd507eea7f2c
Sunny Ratilal <ratilal.sunny@gmail.com>
2012-11-19 05:56:15 +0000

    Revert "Removed Git merge notices"
    This reverts commit 61dcb455aec68079a0e4ce6afc8f153866c0ece1.

--- includes/admin/reporting/pdf-reports.php
+++ includes/admin/reporting/pdf-reports.php
@@ -28,6 +28,7 @@ function edd_generate_pdf( $data ) {
 
 	if ( wp_verify_nonce( $edd_pdf_reports_nonce, 'edd_generate_pdf' ) ) {
 
+<<<<<<< .merge_file_amivGa
 		include_once( EDD_PLUGIN_DIR . '/includes/libraries/html2pdf/html2pdf.class.php' );
 
 		$daterange = date_i18n( get_option( 'date_format' ), mktime( 0, 0, 0, 1, 1, date( 'Y' ) ) ) . ' ' . utf8_decode( __( 'to', 'edd' ) ) . ' ' . date_i18n( get_option( 'date_format' ) );
@@ -63,9 +64,53 @@ function edd_generate_pdf( $data ) {
 					</tr>
 				</thead>
 		<?php
+=======
+		include_once( EDD_PLUGIN_DIR . '/includes/libraries/fpdf/fpdf.php' );
+		include_once( EDD_PLUGIN_DIR . '/includes/libraries/fpdf/edd_pdf.php' );
+
+		$daterange = date_i18n( get_option( 'date_format' ), mktime( 0, 0, 0, 1, 1, date( 'Y' ) ) ) . ' ' . utf8_decode( __( 'to', 'edd' ) ) . ' ' . date_i18n( get_option( 'date_format' ) );
+
+		$pdf = new edd_pdf();
+		$pdf->AddPage( 'L', 'A4' );
+
+		$pdf->SetTitle( utf8_decode( __( 'Sales and earnings reports for the current year for all products', 'edd') ) );
+		$pdf->SetAuthor( utf8_decode( __( 'Easy Digital Downloads', 'edd' ) ) );
+		$pdf->SetCreator( utf8_decode( __( 'Easy Digital Downloads', 'edd' ) ) );
+
+		$pdf->Image( EDD_PLUGIN_URL . 'includes/images/edd-logo.png', 205, 10 );
+
+		$pdf->SetMargins( 8, 8, 8 );
+		$pdf->SetX( 8 );
+
+		$pdf->SetFont( 'Helvetica', '', 16 );
+		$pdf->SetTextColor( 50, 50, 50 );
+		$pdf->Cell( 0, 3, utf8_decode( __( 'Sales and earnings reports for the current year for all products', 'edd' ) ), 0, 2, 'L', false );
+
+		$pdf->SetFont( 'Helvetica', '', 13 );
+		$pdf->Ln();
+		$pdf->SetTextColor( 150, 150, 150 );
+		$pdf->Cell( 0, 6, utf8_decode( __( 'Date Range: ', 'edd' ) ) . $daterange, 0, 2, 'L', false );
+		$pdf->Ln();
+		$pdf->SetTextColor( 50, 50, 50 );
+		$pdf->SetFont( 'Helvetica', '', 14 ); 
+		$pdf->Cell( 0, 10, utf8_decode( __( 'Table View', 'edd' ) ), 0, 2, 'L', false );
+		$pdf->SetFont( 'Helvetica', '', 12 );
+
+		$pdf->SetFillColor( 238, 238, 238 );
+		$pdf->Cell( 70, 6, utf8_decode( __( 'Product Name', 'edd' ) ), 1, 0, 'L', true );
+		$pdf->Cell( 30, 6, utf8_decode( __( 'Price', 'edd' ) ), 1, 0, 'L', true );
+		$pdf->Cell( 50, 6, utf8_decode( __( 'Categories', 'edd' ) ), 1, 0, 'L', true );
+		$pdf->Cell( 50, 6, utf8_decode( __( 'Tags', 'edd' ) ), 1, 0, 'L', true );
+		$pdf->Cell( 45, 6, utf8_decode( __( 'Number of Sales', 'edd' ) ), 1, 0, 'L', true );
+		$pdf->Cell( 35, 6, utf8_decode( __( 'Earnings to Date', 'edd' ) ), 1, 1, 'L', true );
+>>>>>>> .merge_file_mMEjEf
 
 		$year = date('Y');
+		$downloads = get_posts( array( 'post_type' => 'download', 'year' => $year, 'posts_per_page' => -1 ) );
+		if( $downloads ):
+			$pdf->SetWidths( array( 70, 30, 50, 50, 45, 35 ) );
 
+<<<<<<< .merge_file_amivGa
 		$downloads = get_posts(
 			array(
 				'post_type' => 'download',
@@ -79,6 +124,12 @@ function edd_generate_pdf( $data ) {
 
 		if ( $downloads ):
 			foreach ( $downloads as $download ):
+=======
+			foreach( $downloads as $download ):
+
+				$pdf->SetFillColor( 255, 255, 255 );
+				
+>>>>>>> .merge_file_mMEjEf
 				$title = utf8_decode( get_the_title( $download->ID ) );
 				
 				if ( edd_has_variable_prices( $download->ID ) ) {
@@ -111,6 +162,7 @@ function edd_generate_pdf( $data ) {
 				$sales = edd_get_download_sales_stats( $download->ID );
 				$link = get_permalink( $download->ID );
 				$earnings = html_entity_decode ( edd_currency_filter( edd_get_download_earnings_stats( $download->ID ) ) );
+<<<<<<< .merge_file_amivGa
 
 				?>
 				<tr>
@@ -126,11 +178,27 @@ function edd_generate_pdf( $data ) {
 		else: ?>
 			<tr><td><?php echo utf8_decode( sprintf( __( 'No %s found.', 'edd' ), edd_get_label_plural() ) ); ?></td></tr>
 			<?php
+=======
+				
+				$pdf->Row( array( $title, $price, $categories, $tags, $sales, $earnings ) );
+			endforeach;
+		else:
+			$pdf->SetWidths( array( 280 ) );
+			$title = utf8_decode( sprintf( __( 'No %s found.', 'edd' ), edd_get_label_plural() ) );
+			$pdf->Row( array( $title ) );
+>>>>>>> .merge_file_mMEjEf
 		endif;
+		
+		$pdf->Ln();
+		$pdf->SetTextColor( 50, 50, 50 );
+		$pdf->SetFont( 'Helvetica', '', 14 );
+		$pdf->Cell( 0, 10, utf8_decode( __('Graph View', 'edd') ), 0, 2, 'L', false );
+		$pdf->SetFont( 'Helvetica', '', 12 );
 
 		$image = html_entity_decode( urldecode( edd_draw_chart_image() ) );
 		$image = str_replace( ' ', '%20', $image );
 
+<<<<<<< .merge_file_amivGa
 		?>
 			</table>
 			<h2 style="font-size: 14pt; color: #323232"><?php echo utf8_decode( __( 'Graph View', 'edd' ) ); ?></h2>
@@ -144,6 +212,13 @@ function edd_generate_pdf( $data ) {
 		$content .= ob_get_clean();
 		$pdf->WriteHTML( $content );
 		$pdf->Output( 'edd-report-' . date_i18n('Y-m-d') . '.pdf', apply_filters( 'edd_pdf_report_download_type', 'I' ) );
+=======
+		$pdf->SetX( 25 );
+		$pdf->Image( $image .'&file=.png' );
+		$pdf->Ln( 7 );
+		$pdf->Output( 'edd-report-' . date_i18n('Y-m-d') . '.pdf', 'D' );
+
+>>>>>>> .merge_file_mMEjEf
 	}
 }
 add_action( 'edd_generate_pdf', 'edd_generate_pdf' );
