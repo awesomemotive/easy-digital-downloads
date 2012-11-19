5579784ec370e1a68a3687598ca51af7849a30a2
Sunny Ratilal <ratilal.sunny@gmail.com>
2012-11-19 05:49:55 +0000

    PDF Reports

--- includes/admin/reporting/pdf-reports.php
+++ includes/admin/reporting/pdf-reports.php
@@ -28,54 +28,57 @@ function edd_generate_pdf( $data ) {
 
 	if ( wp_verify_nonce( $edd_pdf_reports_nonce, 'edd_generate_pdf' ) ) {
 
-		include_once( EDD_PLUGIN_DIR . '/includes/libraries/fpdf/fpdf.php' );
-		include_once( EDD_PLUGIN_DIR . '/includes/libraries/fpdf/edd_pdf.php' );
+		include_once( EDD_PLUGIN_DIR . '/includes/libraries/html2pdf/html2pdf.class.php' );
 
 		$daterange = date_i18n( get_option( 'date_format' ), mktime( 0, 0, 0, 1, 1, date( 'Y' ) ) ) . ' ' . utf8_decode( __( 'to', 'edd' ) ) . ' ' . date_i18n( get_option( 'date_format' ) );
 
-		$pdf = new edd_pdf();
-		$pdf->AddPage( 'L', 'A4' );
+		$pdf = new HTML2PDF( 'L', 'A4' );
+		$pdf->pdf->SetDisplayMode( 'real' );
+		$pdf->pdf->SetTitle( utf8_decode( __( 'Sales and earnings reports for the current year for all products', 'edd') ) );
+		$pdf->pdf->SetAuthor( utf8_decode( __( 'Easy Digital Downloads', 'edd' ) ) );
 
-		$pdf->SetTitle( utf8_decode( __( 'Sales and earnings reports for the current year for all products', 'edd') ) );
-		$pdf->SetAuthor( utf8_decode( __( 'Easy Digital Downloads', 'edd' ) ) );
-		$pdf->SetCreator( utf8_decode( __( 'Easy Digital Downloads', 'edd' ) ) );
+		$content = '<page style="font-family: freesans" backtop="10px" backbottom="10px" backleft="4mm" backright="4mm">';
 
-		$pdf->Image( EDD_PLUGIN_URL . 'includes/images/edd-logo.png', 205, 10 );
-
-		$pdf->SetMargins( 8, 8, 8 );
-		$pdf->SetX( 8 );
-
-		$pdf->SetFont( 'Helvetica', '', 16 );
-		$pdf->SetTextColor( 50, 50, 50 );
-		$pdf->Cell( 0, 3, utf8_decode( __( 'Sales and earnings reports for the current year for all products', 'edd' ) ), 0, 2, 'L', false );
-
-		$pdf->SetFont( 'Helvetica', '', 13 );
-		$pdf->Ln();
-		$pdf->SetTextColor( 150, 150, 150 );
-		$pdf->Cell( 0, 6, utf8_decode( __( 'Date Range: ', 'edd' ) ) . $daterange, 0, 2, 'L', false );
-		$pdf->Ln();
-		$pdf->SetTextColor( 50, 50, 50 );
-		$pdf->SetFont( 'Helvetica', '', 14 ); 
-		$pdf->Cell( 0, 10, utf8_decode( __( 'Table View', 'edd' ) ), 0, 2, 'L', false );
-		$pdf->SetFont( 'Helvetica', '', 12 );
-
-		$pdf->SetFillColor( 238, 238, 238 );
-		$pdf->Cell( 70, 6, utf8_decode( __( 'Product Name', 'edd' ) ), 1, 0, 'L', true );
-		$pdf->Cell( 30, 6, utf8_decode( __( 'Price', 'edd' ) ), 1, 0, 'L', true );
-		$pdf->Cell( 50, 6, utf8_decode( __( 'Categories', 'edd' ) ), 1, 0, 'L', true );
-		$pdf->Cell( 50, 6, utf8_decode( __( 'Tags', 'edd' ) ), 1, 0, 'L', true );
-		$pdf->Cell( 45, 6, utf8_decode( __( 'Number of Sales', 'edd' ) ), 1, 0, 'L', true );
-		$pdf->Cell( 35, 6, utf8_decode( __( 'Earnings to Date', 'edd' ) ), 1, 1, 'L', true );
+		ob_start(); ?>
+			<style type="text/css">
+			<!--
+			body { font-family: freesans; }
+			th { background-color: #eeeeee; padding: 8px 0; border-bottom: 1px solid #666; }
+			td { padding: 5px 0; }
+			-->
+			</style>
+			<img src="<?php echo EDD_PLUGIN_URL ?>/includes/images/edd-logo.png" />
+			<h1 style="color: #323232f; margin-top: 8px; margin-bottom: 0;"><?php echo utf8_decode( __( 'Sales and earnings reports for the current year for all products', 'edd' ) ); ?></h1>
+			<p style="color: #969696; font-size: 12pt; margin-bottom: 0;"><?php echo utf8_decode( __( 'Date Range: ', 'edd' ) ) . $daterange; ?></p>
+			<h2 style="font-size: 14pt; color: #323232"><?php echo utf8_decode( __( 'Table View', 'edd' ) ); ?></h2>
+			<table cellpadding="2" cellspacing="2;" style="border: 1px solid #666;">
+				<thead>
+					<tr>
+						<th style="width: 70mm; padding-left: 2px"><?php echo utf8_decode( __( 'Product Name', 'edd' ) ); ?></th>
+						<th style="width: 30mm"><?php echo utf8_decode( __( 'Price', 'edd' ) ) ?></th>
+						<th style="width: 50mm;"><?php echo utf8_decode( __( 'Categories', 'edd' ) ) ?></th>
+						<th style="width: 50mm;"><?php echo utf8_decode( __( 'Tags', 'edd' ) ) ?></th>
+						<th style="width: 45mm;"><?php echo utf8_decode( __( 'Number of Sales', 'edd' ) ) ?></th>
+						<th style="width: 35mm;"><?php echo utf8_decode( __( 'Earnings to Date', 'edd' ) ) ?></th>
+					</tr>
+				</thead>
+		<?php
 
 		$year = date('Y');
-		$downloads = get_posts( array( 'post_type' => 'download', 'year' => $year, 'posts_per_page' => -1 ) );
-		if( $downloads ):
-			$pdf->SetWidths( array( 70, 30, 50, 50, 45, 35 ) );
 
-			foreach( $downloads as $download ):
+		$downloads = get_posts(
+			array(
+				'post_type' => 'download',
+				'year' => $year,
+				'posts_per_page' => -1,
+				'cache_results' => false,
+				'update_post_term_cache' => false,
+				'no_found_rows' => true
+			)
+		);
 
-				$pdf->SetFillColor( 255, 255, 255 );
-				
+		if ( $downloads ):
+			foreach ( $downloads as $download ):
 				$title = utf8_decode( get_the_title( $download->ID ) );
 				
 				if ( edd_has_variable_prices( $download->ID ) ) {
@@ -108,29 +111,39 @@ function edd_generate_pdf( $data ) {
 				$sales = edd_get_download_sales_stats( $download->ID );
 				$link = get_permalink( $download->ID );
 				$earnings = html_entity_decode ( edd_currency_filter( edd_get_download_earnings_stats( $download->ID ) ) );
-				
-				$pdf->Row( array( $title, $price, $categories, $tags, $sales, $earnings ) );
+
+				?>
+				<tr>
+					<td style="padding-left: 2px;"><?php echo $title; ?></td>
+					<td><?php echo $price; ?></td>
+					<td><?php echo $categories; ?></td>
+					<td><?php echo $tags; ?></td>
+					<td><?php echo $sales; ?></td>
+					<td><?php echo $earnings; ?></td>
+				</tr>
+				<?php
 			endforeach;
-		else:
-			$pdf->SetWidths( array( 280 ) );
-			$title = utf8_decode( sprintf( __( 'No %s found.', 'edd' ), edd_get_label_plural() ) );
-			$pdf->Row( array( $title ) );
+		else: ?>
+			<tr><td><?php echo utf8_decode( sprintf( __( 'No %s found.', 'edd' ), edd_get_label_plural() ) ); ?></td></tr>
+			<?php
 		endif;
-		
-		$pdf->Ln();
-		$pdf->SetTextColor( 50, 50, 50 );
-		$pdf->SetFont( 'Helvetica', '', 14 );
-		$pdf->Cell( 0, 10, utf8_decode( __('Graph View', 'edd') ), 0, 2, 'L', false );
-		$pdf->SetFont( 'Helvetica', '', 12 );
 
 		$image = html_entity_decode( urldecode( edd_draw_chart_image() ) );
 		$image = str_replace( ' ', '%20', $image );
 
-		$pdf->SetX( 25 );
-		$pdf->Image( $image .'&file=.png' );
-		$pdf->Ln( 7 );
-		$pdf->Output( 'edd-report-' . date_i18n('Y-m-d') . '.pdf', 'D' );
+		?>
+			</table>
+			<h2 style="font-size: 14pt; color: #323232"><?php echo utf8_decode( __( 'Graph View', 'edd' ) ); ?></h2>
+			<p align="center"><img src="<?php echo $image ?>&amp;file=.png" /></p>
+			<page_footer>
+				<p align="center; font-size: 9pt"><i><?php echo utf8_decode( __( 'Page', 'edd' ) ); ?> [[page_cu]]/[[page_nb]]</i></p>
+			</page_footer>
+		</page>
+		<?php
 
+		$content .= ob_get_clean();
+		$pdf->WriteHTML( $content );
+		$pdf->Output( 'edd-report-' . date_i18n('Y-m-d') . '.pdf', apply_filters( 'edd_pdf_report_download_type', 'I' ) );
 	}
 }
 add_action( 'edd_generate_pdf', 'edd_generate_pdf' );
