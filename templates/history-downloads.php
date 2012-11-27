24cc8c77be7e2f2bbbb759934798ff350005307c
Pippin Williamson <pippin@pippinsplugins.com>
2012-11-27 10:07:43 -0600

    Improved formatting for download history

--- templates/history-downloads.php
+++ templates/history-downloads.php
@@ -1,32 +1,31 @@
 <?php
 $purchases = edd_get_users_purchases();	
 if( $purchases ) :
-	do_action( 'edd_before_download_history' );
-	?>
+	do_action( 'edd_before_download_history' ); ?>
 	<table id="edd_user_history">
 		<thead>
 			<tr class="edd_download_history_row">
-				<?php do_action('edd_download_history_header_start'); ?>
-				<th class="edd_download_download_name"><?php _e('Download Name', 'edd'); ?></th>
+				<?php do_action( 'edd_download_history_header_start' ); ?>
+				<th class="edd_download_download_name"><?php _e( 'Download Name', 'edd' ); ?></th>
 				<?php if( ! edd_no_redownload() ) { ?>
-					<th class="edd_download_download_files"><?php _e('Files', 'edd'); ?></th>
+					<th class="edd_download_download_files"><?php _e( 'Files', 'edd' ); ?></th>
 				<?php } ?>							
-				<?php do_action('edd_download_history_header_end'); ?>
+				<?php do_action( 'edd_download_history_header_end' ); ?>
 			</tr>
 		</thead>
 		<?php 
-		foreach($purchases as $post) : setup_postdata( $post );
+		foreach( $purchases as $post ) : setup_postdata( $post );
 
 			$downloads 		= edd_get_downloads_of_purchase( $post->ID );
 			$purchase_data 	= edd_get_payment_meta( $post->ID );
 
-			if($downloads) {
+			if( $downloads ) {
 				foreach( $downloads as $download ) {
 
 					echo '<tr class="edd_download_history_row">';
 
-						$id 			= isset( $purchase_data['cart_details'] ) ? $download['id'] : $download;
-						$price_id 		= isset($download['options']['price_id']) ? $download['options']['price_id'] : null;
+						$id 			= isset( $purchase_data['cart_details'] )   ? $download['id']                  : $download;
+						$price_id 		= isset( $download['options']['price_id'] ) ? $download['options']['price_id'] : null;
 						$download_files = edd_get_download_files( $id, $price_id );
 
 						do_action( 'edd_download_history_row_start', $post->ID, $id );
@@ -37,12 +36,12 @@ if( $purchases ) :
 
 							echo '<td class="edd_download_download_files">';
 
-							if($download_files) {
+							if( $download_files ) {
 								foreach($download_files as $filekey => $file) {
 
 									$download_url = edd_get_download_file_url( $purchase_data['key'], $purchase_data['email'], $filekey, $id );
 									
-									echo'<div class="edd_download_file"><a href="' . esc_url( $download_url ) . '" class="edd_download_file_link">' . esc_html( $file['name'] ) . '</a></div>';
+									echo '<div class="edd_download_file"><a href="' . esc_url( $download_url ) . '" class="edd_download_file_link">' . esc_html( $file['name'] ) . '</a></div>';
 								
 									do_action( 'edd_download_history_files', $filekey, $file, $id, $post->ID, $purchase_data );
 
