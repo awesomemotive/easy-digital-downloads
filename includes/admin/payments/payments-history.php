4be57c2b692330240f2c6633427cbac2cccc2394
Pippin Williamson <pippin@pippinsplugins.com>
2012-11-26 20:26:17 -0600

    Moved include_once() for payments table to payment-history.php

--- includes/admin/payments/payments-history.php
+++ includes/admin/payments/payments-history.php
@@ -26,6 +26,7 @@ function edd_payment_history_page() {
 	if( isset( $_GET['edd-action'] ) && $_GET['edd-action'] == 'edit-payment' ) {
 		include_once( EDD_PLUGIN_DIR . '/includes/admin/payments/edit-payment.php' );
 	} else {
+		include_once( EDD_PLUGIN_DIR . 'includes/admin/payments/class-payments-table.php' );
 		$payments_table = new EDD_Payment_History_Table();
 		$payments_table->prepare_items();
 
