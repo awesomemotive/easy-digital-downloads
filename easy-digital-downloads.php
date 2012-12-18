09e6a0a383668bff7b6a42cb4a860540370eb72c
Pippin Williamson <pippin@pippinsplugins.com>
2012-12-17 23:36:30 -0600

    Restructured gateway functions for #569

--- easy-digital-downloads.php
+++ easy-digital-downloads.php
@@ -147,7 +147,7 @@ final class Easy_Digital_Downloads {
 		include_once( EDD_PLUGIN_DIR . 'includes/formatting.php' );
 		include_once( EDD_PLUGIN_DIR . 'includes/widgets.php' );
 		include_once( EDD_PLUGIN_DIR . 'includes/mime-types.php' );
-		include_once( EDD_PLUGIN_DIR . 'includes/gateway-functions.php' );
+		include_once( EDD_PLUGIN_DIR . 'includes/gateways/functions.php' );
 		include_once( EDD_PLUGIN_DIR . 'includes/gateways/paypal-standard.php' );
 		include_once( EDD_PLUGIN_DIR . 'includes/gateways/manual.php' );
 		include_once( EDD_PLUGIN_DIR . 'includes/discount-functions.php' );
