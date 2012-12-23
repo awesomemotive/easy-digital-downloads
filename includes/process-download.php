edaf65379af776d6fb18160c0d0b87292810b32b
Pippin Williamson <pippin@pippinsplugins.com>
2012-12-23 14:20:22 -0600

    Refactoring process download for #576

--- includes/process-download.php
+++ includes/process-download.php
@@ -24,24 +24,18 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 function edd_process_download() {
 
-	// Allow args to be provided via a filter.
-	$args = (array) apply_filters( 'edd_process_download_args', array() );
-
-	if( empty( $args ) && ( ! isset( $_GET['download'] ) || ! isset( $_GET['email'] ) || ! isset( $_GET['file'] ) ) )
-		return;
-
-	$defaults = array(
+	$args = apply_filters( 'edd_process_download_args', array(
 		'download' => ( isset( $_GET['download'] ) )     ? (int) $_GET['download']                          : '',
 		'email'    => ( isset( $_GET['email'] ) )        ? rawurldecode( $_GET['email'] )                   : '',
 		'expire'   => ( isset( $_GET['expire'] ) )       ? base64_decode( rawurldecode( $_GET['expire'] ) ) : '',
 		'file_key' => ( isset( $_GET['file'] ) )         ? (int) $_GET['file']                              : '',
 		'key'      => ( isset( $_GET['download_key'] ) ) ? $_GET['download_key']                            : ''
-	);
+	) );
 
-	// Throw away invalid args and fill any missing defaults.
-	$args = array_intersect_key( $args, $defaults );
-	$args = wp_parse_args( $args, $defaults );
-	extract( $args, EXTR_SKIP );
+	if( $args['download'] === '' || $args['email'] === '' || $args['file_key'] === '' )
+		return false;
+
+    extract( $args );
 
 	$payment = edd_verify_download_link( $download, $key, $email, $expire, $file_key );
 
