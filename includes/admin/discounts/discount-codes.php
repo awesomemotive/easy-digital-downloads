1ba6bca099b624817c592153891e42328e43ad58
Sunny Ratilal <ratilal.sunny@gmail.com>
2012-12-25 10:40:40 +0000

    Changed Discount Codes page to new code that utilises WP_List_Table

--- includes/admin/discounts/discount-codes.php
+++ includes/admin/discounts/discount-codes.php
@@ -24,6 +24,41 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 function edd_discounts_page() {
 	global $edd_options;
+
+	if( isset( $_GET['edd-action'] ) && $_GET['edd-action'] == 'edit_discount' ) {
+		include_once( EDD_PLUGIN_DIR . 'includes/admin/discounts/edit-discount.php' );
+	} else {
+		include_once( EDD_PLUGIN_DIR . 'includes/admin/discounts/class-discount-codes-table.php' );
+		$discount_codes_table = new EDD_Discount_Codes_Table();
+		$discount_codes_table->prepare_items();
+
+?>
+	<div class="wrap">
+		<h2><?php _e( 'Discount Codes', 'edd' ); ?></h2>
+		<?php do_action( 'edd_discounts_page_top' ); ?>
+		<form id="edd-discounts-filter" method="get" action="<?php echo admin_url( 'edit.php?post_type=download&page=edd-discounts' ); ?>">
+
+	        <?php $discount_codes_table->search_box( __( 'Search', 'edd' ), 'edd-discounts' ); ?>
+
+			<input type="hidden" name="post_type" value="download" />
+	        <input type="hidden" name="page" value="edd-discounts" />
+
+	        <?php $discount_codes_table->views() ?>
+
+	        <?php $discount_codes_table->display() ?>
+	    </form>
+	    <?php include_once( EDD_PLUGIN_DIR . 'includes/admin/discounts/add-discount.php' ); ?>
+		<?php do_action( 'edd_discounts_page_bottom' ); ?>
+	</div>
+<?php
+
+	}
+}
+
+
+
+function edd_old_discounts_page() {
+	global $edd_options;
 	$current_page = get_bloginfo( 'wpurl' ) . '/wp-admin/admin.php?edit.php?post_type=download&page=edd-discounts';
 	?>
 	<div class="wrap">
