5614dc80a75dfedee5986829606b1e455727b572
Pippin Williamson <pippin@pippinsplugins.com>
2012-12-26 09:52:45 -0600

    Added payment_id and purchase data params to new admin notification filters.

--- includes/emails/functions.php
+++ includes/emails/functions.php
@@ -62,7 +62,7 @@ function edd_email_purchase_receipt( $payment_id, $admin_notice = true ) {
 		/* send an email notification to the admin */
 		$admin_email   = edd_get_admin_notice_emails();
 
-		$admin_subject = apply_filters( 'edd_admin_purchase_notification_subject', __('New download purchase', 'edd') );
+		$admin_subject = apply_filters( 'edd_admin_purchase_notification_subject', __('New download purchase', 'edd'), $payment_id, $payment_data );
 
 		$admin_message = __('Hello', 'edd') . "\n\n" . sprintf( __('A %s purchase has been made', 'edd'), edd_get_label_plural() ) . ".\n\n";
 		$admin_message .= sprintf( __('%s sold:', 'edd'), edd_get_label_plural() ) .  "\n\n";
@@ -84,9 +84,9 @@ function edd_email_purchase_receipt( $payment_id, $admin_notice = true ) {
 		$admin_message .= __('Thank you', 'edd');
 		$admin_message = apply_filters( 'edd_admin_purchase_notification', $admin_message, $payment_id, $payment_data );
 
-		$admin_headers = apply_filters( 'edd_admin_purchase_notification_headers', array() );
+		$admin_headers = apply_filters( 'edd_admin_purchase_notification_headers', array(), $payment_id, $payment_data );
 		
-		$admin_attachments = apply_filters( 'edd_admin_purchase_notification_attachments', array() );
+		$admin_attachments = apply_filters( 'edd_admin_purchase_notification_attachments', array(), $payment_id, $payment_data );
 
 		wp_mail( $admin_email, $admin_subject, $admin_message, $admin_headers, $admin_attachments );
 	}
