60af2e68534e97346b4e600b5abd93b58a2cd22e
Chris Christoff <chriscct7@gmail.com>
2012-12-26 08:25:43 -0500

    application/java-archive

--- includes/process-download.php
+++ includes/process-download.php
@@ -215,6 +215,7 @@ function edd_get_file_ctype( $extension ) {
 		case 'ink'		: $ctype	= "application/inkml+xml"; break;
 		case 'inkml'	: $ctype	= "application/inkml+xml"; break;
 		case 'ipfix'	: $ctype	= "application/ipfix"; break;
+		case 'jar'		: $ctype	= "application/java-archive"; break;
 		case 'jnlp'		: $ctype	= "application/x-java-jnlp-file"; break;
 		case 'jp2'		: $ctype	= "image/jp2"; break;
 		case 'jpe'		: $ctype	= "image/jpeg"; break;
