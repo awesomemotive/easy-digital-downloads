a5f99f3100821e9eac25a94932f52a2913c92b21
Chris Christoff <chriscct7@gmail.com>
2012-12-26 09:31:53 -0500

    correction

--- includes/process-download.php
+++ includes/process-download.php
@@ -144,7 +144,7 @@ function edd_get_file_ctype( $extension ) {
 		case 'aif'		: $ctype	= "audio/x-aiff"; break;
 		case 'aifc'		: $ctype	= "audio/x-aiff"; break;
 		case 'aiff'		: $ctype	= "audio/x-aiff"; break;
-		case 'asc'		: $ctype	= "text/plain"; break;
+		case 'asc'		: $ctype	= "application/pgp-signature"; break;
 		case 'atom'		: $ctype	= "application/atom+xml"; break;
 		case 'atomcat'	: $ctype	= "application/atomcat+xml"; break		
 		case 'atomsvc'	: $ctype	= "application/atomsvc+xml"; break		
