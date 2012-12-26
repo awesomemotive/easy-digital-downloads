e72408fb0fc5616490f18e0ef3ab52095c317be6
Chris Christoff <chriscct7@gmail.com>
2012-12-26 09:48:17 -0500

    excel

--- includes/process-download.php
+++ includes/process-download.php
@@ -394,7 +394,8 @@ function edd_get_file_ctype( $extension ) {
 		case 'xlsx'		: $ctype	= "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"; break;
 		case 'xlsb'		: $ctype	= "application/vnd.ms-excel.sheet.binary.macroEnabled.12"; break;
 		case 'xlt'		: $ctype	= "application/vnd.ms-excel"; break;
-		case 'xltx'		: $ctype	= "application/vnd.openxmlformats-officedocument.spreadsheetml.template"; break;		
+		case 'xltx'		: $ctype	= "application/vnd.openxmlformats-officedocument.spreadsheetml.template"; break;
+		case 'xlw'		: $ctype	= "application/vnd.ms-excel"; break;
 		case 'xml'		: $ctype	= "application/xml"; break;
 		case 'xpm'		: $ctype	= "image/x-xpixmap"; break;
 		case 'xsl'		: $ctype	= "application/xml"; break;
