ce1dc664d37d872b48663067d3d3da20822f9d7c
Chris Christoff <chriscct7@gmail.com>
2012-12-26 08:08:17 -0500

    application/cdmi-queue

--- includes/process-download.php
+++ includes/process-download.php
@@ -159,7 +159,7 @@ function edd_get_file_ctype( $extension ) {
 		case 'cdmic'	: $ctype	= "application/cdmi-container"; break;
 		case 'cdmid'	: $ctype	= "application/cdmi-domain"; break;
 		case 'cdmio'	: $ctype	= "application/cdmi-object"; break;
-		case 'cdmiq'	: $ctype	= "application/x-netcdf"; break;
+		case 'cdmiq'	: $ctype	= "application/cdmi-queue"; break;
 		case 'cdf'		: $ctype	= "application/x-netcdf"; break;
 		case 'cgm'		: $ctype	= "image/cgm"; break;
 		case 'class'	: $ctype	= "application/octet-stream"; break;
