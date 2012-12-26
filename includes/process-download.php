d25591a0e6032f8d931f22d1492b514574a66a0b
Chris Christoff <chriscct7@gmail.com>
2012-12-26 08:36:48 -0500

    application/marcxml+xml

--- includes/process-download.php
+++ includes/process-download.php
@@ -253,6 +253,7 @@ function edd_get_file_ctype( $extension ) {
 		case 'mpg'		: $ctype	= "video/mpeg"; break;
 		case 'mpga'		: $ctype	= "audio/mpeg"; break;
 		case 'mrc'		: $ctype	= "application/marc"; break;
+		case 'mrcx'		: $ctype	= "application/marcxml+xml"; break;
 		case 'ms'		: $ctype	= "application/x-troff-ms"; break;
 		case 'msh'		: $ctype	= "model/mesh"; break;
 		case 'mxu'		: $ctype	= "video/vnd.mpegurl"; break;
