20a8909fa6dae7e49f359ede2e42b2fc14b471d5
Sunny Ratilal <ratilal.sunny@gmail.com>
2012-12-26 10:22:38 +0000

    Code formatting admin/reporting/logs.php

--- includes/admin/reporting/logs.php
+++ includes/admin/reporting/logs.php
@@ -11,7 +11,7 @@
 
 
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 
 /**
@@ -23,13 +23,11 @@ if ( !defined( 'ABSPATH' ) ) exit;
  */
 
 function edd_logs_view_sales() {
-
 	include( dirname( __FILE__ ) . '/class-sales-logs-list-table.php' );
 
 	$logs_table = new EDD_Sales_Log_Table();
 	$logs_table->prepare_items();
 	$logs_table->display();
-
 }
 add_action( 'edd_logs_view_sales', 'edd_logs_view_sales' );
 
@@ -43,12 +41,11 @@ add_action( 'edd_logs_view_sales', 'edd_logs_view_sales' );
  */
 
 function edd_logs_view_file_downloads() {
-
 	include( dirname( __FILE__ ) . '/class-file-downloads-logs-list-table.php' );
 
 	$logs_table = new EDD_File_Downloads_Log_Table();
 	$logs_table->prepare_items();
-?>
+	?>
 	<div class="wrap">
 		<?php do_action( 'edd_logs_file_downloads_top' ); ?>
 		<form id="edd-logs-filter" method="get" action="<?php echo admin_url( 'edit.php?post_type=download&page=edd-reports&tab=logs' ); ?>">
@@ -57,8 +54,8 @@ function edd_logs_view_file_downloads() {
 			$logs_table->display();
 			?>
 			<input type="hidden" name="post_type" value="download" />
-	        <input type="hidden" name="page" value="edd-reports" />
-	        <input type="hidden" name="tab" value="logs" />
+			<input type="hidden" name="page" value="edd-reports" />
+			<input type="hidden" name="tab" value="logs" />
 		</form>
 		<?php do_action( 'edd_logs_file_downloads_bottom' ); ?>
 	</div>
@@ -76,13 +73,11 @@ add_action( 'edd_logs_view_file_downloads', 'edd_logs_view_file_downloads' );
  */
 
 function edd_logs_view_gateway_errors() {
-
 	include( dirname( __FILE__ ) . '/class-gateway-error-logs-list-table.php' );
 
 	$logs_table = new EDD_Gateway_Error_Log_Table();
 	$logs_table->prepare_items();
 	$logs_table->display();
-
 }
 add_action( 'edd_logs_view_gateway_errors', 'edd_logs_view_gateway_errors' );
 
@@ -97,15 +92,16 @@ add_action( 'edd_logs_view_gateway_errors', 'edd_logs_view_gateway_errors' );
 
 function edd_log_default_views() {
 	$views = array(
-		'file_downloads'	=> __( 'File Downloads', 'edd' ),
-		'sales' 			=> __( 'Sales', 'edd' ),
-		'gateway_errors'	=> __( 'Payment Errors', 'edd' )
+		'file_downloads'  => __( 'File Downloads', 'edd' ),
+		'sales' 		  => __( 'Sales', 'edd' ),
+		'gateway_errors'  => __( 'Payment Errors', 'edd' )
 	);
 
 	$views = apply_filters( 'edd_log_views', $views );
 
 	return $views;
 }
+
 
 /**
  * Renders the Reports page views drop down
@@ -118,12 +114,11 @@ function edd_log_default_views() {
 function edd_log_views() {
 	$views        = edd_log_default_views();
 	$current_view = isset( $_GET['view'] ) ? $_GET['view'] : 'file_downloads';
-
 	?>
 	<form id="edd-logs-filter" method="get" action="edit.php">
 		<select id="edd-logs-view" name="view">
 			<option value="-1"><?php _e( 'Log Type', 'edd' ); ?></option>
-			<?php foreach( $views as $view_id => $label ): ?>
+			<?php foreach ( $views as $view_id => $label ): ?>
 				<option value="<?php echo esc_attr( $view_id ); ?>" <?php selected( $view_id, $current_view ); ?>><?php echo $label; ?></option>
 			<?php endforeach; ?>
 		</select>
@@ -134,7 +129,7 @@ function edd_log_views() {
 		<input type="hidden" name="page" value="edd-reports"/>
 		<input type="hidden" name="tab" value="logs"/>
 
-		<input type="submit" class="button-secondary" value="<?php _e( 'Apply', 'edd' ); ?>"/>
+		<?php submit_button( __( 'Apply', 'edd' ), 'secondary', 'submit', false ); ?>
 	</form>
 	<?php
 }
