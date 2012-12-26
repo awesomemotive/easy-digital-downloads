c5efdc1bb2053653db43ed5ea6e985e5796c8e23
Sunny Ratilal <ratilal.sunny@gmail.com>
2012-12-26 10:40:54 +0000

    Code formatting admin/admin-notices.php

--- includes/admin/admin-notices.php
+++ includes/admin/admin-notices.php
@@ -10,7 +10,8 @@
 */
 
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) exit;
+
 
 /**
  * Admin Messages
@@ -23,22 +24,27 @@ if ( !defined( 'ABSPATH' ) ) exit;
 function edd_admin_messages() {
 	global $typenow;
 
-	if( 'download' != $typenow )
+	if ( 'download' != $typenow )
 		return;
 
 	$edd_access_level = edd_get_menu_access_level();
-	if( isset( $_GET['edd-message'] ) && $_GET['edd-message'] == 'discount_updated' && current_user_can( $edd_access_level ) ) {
+
+	if ( isset( $_GET['edd-message'] ) && $_GET['edd-message'] == 'discount_updated' && current_user_can( $edd_access_level ) ) {
 		 add_settings_error( 'edd-notices', 'edd-discount-updated', __( 'Discount code updated.', 'edd' ), 'updated' );
 	}
-	if( isset( $_GET['edd-message'] ) && $_GET['edd-message'] == 'discount_update_failed' && current_user_can( $edd_access_level ) ) {
+
+	if ( isset( $_GET['edd-message'] ) && $_GET['edd-message'] == 'discount_update_failed' && current_user_can( $edd_access_level ) ) {
 		add_settings_error( 'edd-notices', 'edd-discount-updated-fail', __( 'There was a problem updating your discount code, please try again.', 'edd' ), 'error' );
 	}
-	if( isset( $_GET['edd-message'] ) && $_GET['edd-message'] == 'payment_deleted' && current_user_can( $edd_access_level ) ) {
+
+	if ( isset( $_GET['edd-message'] ) && $_GET['edd-message'] == 'payment_deleted' && current_user_can( $edd_access_level ) ) {
 		add_settings_error( 'edd-notices', 'edd-payment-deleted', __( 'The payment has been deleted.', 'edd' ), 'updated' );
 	}
-	if( isset( $_GET['edd-message']) && $_GET['edd-message'] == 'email_sent' && current_user_can( $edd_access_level ) ) {
+
+	if ( isset( $_GET['edd-message']) && $_GET['edd-message'] == 'email_sent' && current_user_can( $edd_access_level ) ) {
 		add_settings_error( 'edd-notices', 'edd-payment-sent', __( 'The purchase receipt has been resent.', 'edd' ), 'updated' );
 	}
+
 	settings_errors( 'edd-notices' );
 }
 add_action( 'admin_notices', 'edd_admin_messages' );
