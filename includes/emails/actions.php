8d544368d19980be13e44c99c6fe2796eb45e451
Pippin Williamson <pippin@pippinsplugins.com>
2012-12-27 16:47:54 -0600

    Fixed issue with purchase receipts not sending

--- includes/emails/actions.php
+++ includes/emails/actions.php
@@ -29,9 +29,9 @@ function edd_trigger_purchase_receipt( $payment_id, $new_status, $old_status ) {
 	if( $old_status == 'publish' || $old_status == 'complete')
 		return;
 
-	// make sure the purchase receipt is only sent if the new status is complete
-	if( $new_status != 'publish' && $new_status != 'complete' );
-		return;
+	// make sure the purchase receipt is only sent if the new status is complete -- No idea why, but this returns even when $new_status is 'publish'
+	//if( $new_status != 'publish' && $new_status != 'complete' );
+		//return;
 
 	// send email with secure download link
 	edd_email_purchase_receipt( $payment_id );
