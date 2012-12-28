e94a1285d6b3b6fd642bc49c02059603f66d458e
Chris Christoff <chriscct7@gmail.com>
2012-12-28 08:34:01 -0500

    Looks like the exit there is killing us. Literally.

--- easy-digital-downloads.php
+++ easy-digital-downloads.php
@@ -24,8 +24,9 @@ along with Easy Digital Downloads. If not, see <http://www.gnu.org/licenses/>.
 */
 
 // Exit if accessed directly
+echo "test1";
 if ( !defined( 'ABSPATH' ) ) exit;
-
+echo "test";
 /* PHP Hack to Get Plugin Headers in the .POT File */
 	$edd_plugin_header_translate = array(
 		__( 'Easy Digital Downloads', 'edd' ),
