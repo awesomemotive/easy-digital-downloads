e746c3539ac76c0ae2d98076961bf101cf3aacfb
Chris Christoff <chriscct7@gmail.com>
2012-12-27 21:48:12 -0500

    loader

--- travis/tests/bootstrap.php
+++ travis/tests/bootstrap.php
@@ -9,5 +9,10 @@ if( file_exists( $path ) ) {
 } else {
     exit( "Couldn't find wordpress-tests please run\n git submodule init && git submodule update\n" );
 }
-require_once './easy-digital-downloads.php';
+$loader= './easy-digital-downloads.php';
+if( file_exists( $loader ) ) {
+    require_once $loader;
+} else {
+    exit( "Couldn't find EDD plugin mainfile" );
+}
 $_SESSION['travis']='true';
