3b8ad93be2933ed2f349207ec10c45ecfa1b424f
Pippin Williamson <pippin@pippinsplugins.com>
2013-01-07 21:38:57 -0600

    Fixed pagination of download history for #723

--- templates/history-downloads.php
+++ templates/history-downloads.php
@@ -72,7 +72,7 @@ if( $purchases ) :
 			'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 			'format'  => '?paged=%#%',
 			'current' => max( 1, get_query_var( 'paged' ) ),
-			'total'   => edd_count_purchases_of_customer()
+			'total'   => ceil( edd_count_purchases_of_customer() / 20 ) // 20 items per page
 		) );
 		?>
 	</div>
