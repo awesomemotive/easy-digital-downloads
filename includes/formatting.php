f9f222471b47e159e3e2a94903d5a4c287409968
Chris Christoff <chriscct7@gmail.com>
2013-01-09 10:11:32 -0500

    721

--- includes/formatting.php
+++ includes/formatting.php
@@ -54,12 +54,17 @@ function edd_sanitize_amount( $amount ) {
  * @access      public
  * @since       1.0
  * @param       $amount string the price amount to format
- * @return      string - the newly formatted amount
+ * @return      string - the newly formatted amount or Price Not Available
 */
 
 function edd_format_amount( $amount ) {
 	global $edd_options;
-
+	// If no price was given for the downlaod
+	if (!isset($amount) || ($amount==null)){
+	$label = edd_get_label_singular();
+	$string = sprintf( __('%1$s Not Available', 'edd' ), $label )
+	return	apply_filters( 'edd_format_amount_decimals', $string )
+	}
 	$thousands_sep 	= isset( $edd_options['thousands_separator'] ) ? $edd_options['thousands_separator'] : ',';
 	$decimal_sep 	= isset( $edd_options['decimal_separator'] )   ? $edd_options['decimal_separator'] 	 : '.';
 
