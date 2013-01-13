0800c016befa36b486029cf5731ca2ee5ff9b00d
Matt <info@mgates.me>
2013-01-13 18:52:38 -0500

    Ajaxify the checkout, closes #755

--- assets/js/edd-checkout-global.js
+++ assets/js/edd-checkout-global.js
@@ -176,4 +176,18 @@ jQuery(document).ready(function($) {
         return false;
     });
 
+    $('#edd_purchase_form input[type=submit]').click(function(e) {
+
+        e.preventDefault();
+
+        $.post(edd_global_vars.ajaxurl, $('#edd_purchase_form').serialize() + '&action=edd_process_checkout', function(data) {
+            if ( data == 'success' ) {
+                $('#edd_purchase_form').submit();
+            } else {
+                $('#edd_purchase_form').before(data);
+            }
+        });
+
+    });
+
 });
