6344a707eb240b110fece25d71034b3c53b60d0f
Pippin Williamson <pippin@pippinsplugins.com>
2013-01-17 09:19:32 -0800

    Initial export classes

---
+++ includes/class-export-customers.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Customers Export Class
+ *
+ * This class handles customer export
+ *
+ * @package     Easy Digital Downloads
+ * @subpackage  Export Class
+ * @copyright   Copyright (c) 2012, Pippin Williamson
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.4.2
+*/
+
+class EDD_Customer_Export extends EDD_Export {
+
+
+	public function get_data() {
+
+	}
+
+
+}
