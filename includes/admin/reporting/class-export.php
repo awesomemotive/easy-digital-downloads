3f56371d4e952e9a0c8c24c68ca3cd058b6e0b13
Pippin Williamson <pippin@pippinsplugins.com>
2013-01-17 23:26:15 -0800

    Finished first version of EDD_Export class

--- includes/admin/reporting/class-export.php
+++ includes/admin/reporting/class-export.php
@@ -13,16 +13,36 @@
 
 class EDD_Export {
 
-	private $export_type;
 
-	public function __construct() {
-		$this->$export_type = 'default';
-	}
+	/**
+	 * Our export type. Used for export-type specific filters / actions
+	 *
+	 * @access      public
+	 * @since       1.4.2
+	 */
+
+	public $export_type = 'default';
+
+	/**
+	 * Can we export?
+	 *
+	 * @access      public
+	 * @since       1.4.2
+	 * @return      bool
+	 */
 
 	public function can_export() {
-		return apply_filters( 'edd_export_capability', current_user_can( 'manage_options' ) );
+		return (bool) apply_filters( 'edd_export_capability', current_user_can( 'manage_options' ) );
 	}
 
+
+	/**
+	 * Set the export headers
+	 *
+	 * @access      public
+	 * @since       1.4.2
+	 * @return      void
+	 */
 
 	public function headers() {
 
@@ -37,38 +57,93 @@ class EDD_Export {
 		header( "Expires: 0" );
 	}
 
+
+	/**
+	 * Set the CSV columns
+	 *
+	 * @access      public
+	 * @since       1.4.2
+	 * @return      array
+	 */
+
 	public function csv_cols() {
 		$cols = array(
 			'id'   => __( 'ID',   'edd' ),
 			'date' => __( 'Date', 'edd' )
 		);
+		return $cols;
 	}
+
+
+	/**
+	 * Retrieve CSV columns
+	 *
+	 * @access      public
+	 * @since       1.4.2
+	 * @return      array
+	 */
 
 	public function get_csv_cols() {
 		$cols = $this->csv_cols();
 		return apply_filters( 'edd_export_csv_cols_' . $this->export_type, $cols );
 	}
 
+
+	/**
+	 * Output the CSV columns
+	 *
+	 * @access      public
+	 * @since       1.4.2
+	 * @return      void
+	 */
+
 	public function csv_cols_out() {
 		$cols = $this->csv_cols();
+		$i = 1;
 		foreach( $cols as $col_id => $column ) {
-			echo '"' . $column . '",';
+			echo '"' . $column . '"';
+			echo $i == count( $cols ) ? '' : ',';
+			$i++;
 		}
 		echo "\r\n";
 	}
 
+
+	/**
+	 * Get the data being exported
+	 *
+	 * @access      public
+	 * @since       1.4.2
+	 * @return      array
+	 */
+
 	public function get_data() {
 
+		// Just a sample data array
 		$data = array(
-			'id'   => '',
-			'data' => date( 'F j, Y' )
+			0 => array(
+				'id'   => '',
+				'data' => date( 'F j, Y' )
+			),
+			1 => array(
+				'id'   => '',
+				'data' => date( 'F j, Y' )
+			)
 		);
 
 		$data = apply_filters( 'edd_export_get_data', $data );
 		$data = apply_filters( 'edd_export_get_data_' . $this->export_type, $data );
-
 		return $data;
 	}
+
+
+	/**
+	 * Output the CSV rows
+	 *
+	 * @access      public
+	 * @since       1.4.2
+	 * @return      void
+	 */
 
 	public function csv_rows_out() {
 
@@ -78,15 +153,17 @@ class EDD_Export {
 
 		// Output each row
 		foreach(  $data as $row ) {
-
+			$i = 1;
 			foreach( $row as $col_id => $column ) {
 
 				// Make sure the column is valid
-				if( in_array( $col_id, $cols ) ) {
+				if( array_key_exists( $col_id, $cols ) ) {
 
-					echo '"' . $column . '",';
+					echo '"' . $column . '"';
+					echo $i == count( $cols ) ? '' : ',';
 
 				}
+				$i++;
 
 			}
 
@@ -96,7 +173,15 @@ class EDD_Export {
 	}
 
 
-	public function send() {
+	/**
+	 * Perform the export
+	 *
+	 * @access      public
+	 * @since       1.4.2
+	 * @return      void
+	 */
+
+	public function export() {
 
 		if( ! $this->can_export() )
 			wp_die( __( 'You do not have permission to export data.', 'edd' ), __( 'Error', 'edd' ) );
@@ -110,7 +195,7 @@ class EDD_Export {
 		// Output CSV rows
 		$this->csv_rows_out();
 
-
+		exit;
 
 	}
 
