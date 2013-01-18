a5de47a44c7d3c3aa4a34abd7f4133b497cdf5ed
Pippin Williamson <pippin@pippinsplugins.com>
2013-01-17 23:26:54 -0800

    First pass at customer export

--- includes/admin/reporting/class-export-customers.php
+++ includes/admin/reporting/class-export-customers.php
@@ -11,12 +11,45 @@
  * @since       1.4.2
 */
 
-class EDD_Customer_Export extends EDD_Export {
+class EDD_Customers_Export extends EDD_Export {
 
+	public $export_type = 'customers';
+
+	public function csv_cols() {
+		$cols = array(
+			'name'      => __( 'Name',   'edd' ),
+			'email'     => __( 'Email', 'edd' ),
+			'purchases' => __( 'Total Purchases', 'edd' ),
+			'amount'    => __( 'Total Purchased', 'edd' )
+		);
+		return $cols;
+	}
 
 	public function get_data() {
 
-	}
+		global $wpdb;
 
+		$data = array();
+
+		$emails = $wpdb->get_col( "SELECT DISTINCT meta_value FROM $wpdb->postmeta WHERE meta_key = '_edd_payment_user_email' " );
+
+		foreach ( $emails as $email ) {
+
+			$wp_user = get_user_by( 'email', $email );
+
+			$data[] = array(
+				'name'      => $wp_user ? $wp_user->display_name : __( 'Guest', 'edd' ),
+				'email'     => $email,
+				'purchases' => edd_count_purchases_of_customer( $email ),
+				'amount'    => html_entity_decode( edd_currency_filter( edd_format_amount( edd_purchase_total_of_user( $email ) ) ) )
+			);
+
+		}
+
+		$data = apply_filters( 'edd_export_get_data', $data );
+		$data = apply_filters( 'edd_export_get_data_' . $this->export_type, $data );
+
+		return $data;
+	}
 
 }
