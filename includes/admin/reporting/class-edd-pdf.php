55a8d90ba655f9929afa93c264ab499e436254f8
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-02-02 09:12:22 +0000

    (OOP) function keywords

--- includes/admin/reporting/class-edd-pdf.php
+++ includes/admin/reporting/class-edd-pdf.php
@@ -8,7 +8,7 @@
 */
 
 class EDD_PDF extends TCPDF {
-	function Footer() {
+	public static function Footer() {
 		parent::SetY( -15 );
 		parent::SetFont( 'freesans', 'I', 8 );
 		parent::Cell( 0, 10, __( 'Page', 'edd' ) . ' ' . parent::getAliasNumPage() . ' ' . __( 'of', 'edd' ) . ' ' . parent::getAliasNbPages(), 0, 0, 'C');
