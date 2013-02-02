ee35a62d3ea95a2a18e33ee7bbed60b517f4e9f7
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-02-02 09:14:11 +0000

    Updated DocBlock and make footer extendable

--- includes/admin/reporting/class-edd-pdf.php
+++ includes/admin/reporting/class-edd-pdf.php
@@ -1,16 +1,27 @@
 <?php
 /**
- * PDF MultiCell Table Class
+ * EDD PDF Report Class
+ *
+ * Extends the TCPDF Class for the EDD Reports
  *
  * @package     Easy Digital Downloads
- * @subpackage  FPDF
- * @since       1.1.4.0
+ * @subpackage  Reporting
+ * @since       1.4.4
 */
 
+/**
+ * EDD_PDF Class
+ */
 class EDD_PDF extends TCPDF {
+	/**
+	 * Outputs the footer on each page
+	 *
+	 * @since 1.4.4
+	 */
 	public static function Footer() {
 		parent::SetY( -15 );
 		parent::SetFont( 'freesans', 'I', 8 );
 		parent::Cell( 0, 10, __( 'Page', 'edd' ) . ' ' . parent::getAliasNumPage() . ' ' . __( 'of', 'edd' ) . ' ' . parent::getAliasNbPages(), 0, 0, 'C');
+		do_action( 'edd_pdf_report_footer' );
 	}
 }
