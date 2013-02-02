821ccaaaac444826299484852b7ac6bfebf03531
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-02-02 07:16:57 +0000

    Load TCPDF and change font to Free Sans

--- includes/admin/reporting/pdf-reports.php
+++ includes/admin/reporting/pdf-reports.php
@@ -28,36 +28,39 @@ function edd_generate_pdf( $data ) {
 	$edd_pdf_reports_nonce = $_GET['_wpnonce'];
 
 	if ( wp_verify_nonce( $edd_pdf_reports_nonce, 'edd_generate_pdf' ) ) {
-		require_once EDD_PLUGIN_DIR . '/includes/libraries/fpdf/fpdf.php';
-		require_once EDD_PLUGIN_DIR . '/includes/libraries/fpdf/edd_pdf.php';
+		require_once EDD_PLUGIN_DIR . '/includes/libraries/tcpdf/tcpdf.php';
+		//require_once EDD_PLUGIN_DIR . '/includes/libraries/fpdf/edd_pdf.php';
 
 		$daterange = date_i18n( get_option( 'date_format' ), mktime( 0, 0, 0, 1, 1, date( 'Y' ) ) ) . ' ' . utf8_decode( __( 'to', 'edd' ) ) . ' ' . date_i18n( get_option( 'date_format' ) );
 
-		$pdf = new edd_pdf();
+		$pdf = new TCPDF( 'P', 'mm', 'A4', true, 'UTF-8', false );
+		$pdf->SetDisplayMode( 'real' );
+		$pdf->setJPEGQuality( 100 );
 		$pdf->AddPage( 'L', 'A4' );
 
-		$pdf->SetTitle( utf8_decode( __( 'Sales and earnings reports for the current year for all products', 'edd') ) );
-		$pdf->SetAuthor( utf8_decode( __( 'Easy Digital Downloads', 'edd' ) ) );
-		$pdf->SetCreator( utf8_decode( __( 'Easy Digital Downloads', 'edd' ) ) );
+
+		$pdf->SetTitle( __( 'Sales and earnings reports for the current year for all products', 'edd') );
+		$pdf->SetAuthor( __( 'Easy Digital Downloads', 'edd' ) );
+		$pdf->SetCreator( __( 'Easy Digital Downloads', 'edd' ) );
 
 		$pdf->Image( EDD_PLUGIN_URL . 'assets/images/edd-logo.png', 205, 10 );
 
 		$pdf->SetMargins( 8, 8, 8 );
 		$pdf->SetX( 8 );
 
-		$pdf->SetFont( 'Helvetica', '', 16 );
+		$pdf->SetFont( 'freesans', '', 16 );
 		$pdf->SetTextColor( 50, 50, 50 );
 		$pdf->Cell( 0, 3, utf8_decode( __( 'Sales and earnings reports for the current year for all products', 'edd' ) ), 0, 2, 'L', false );
 
-		$pdf->SetFont( 'Helvetica', '', 13 );
+		$pdf->SetFont( 'freesans', '', 13 );
 		$pdf->Ln();
 		$pdf->SetTextColor( 150, 150, 150 );
 		$pdf->Cell( 0, 6, utf8_decode( __( 'Date Range: ', 'edd' ) ) . $daterange, 0, 2, 'L', false );
 		$pdf->Ln();
 		$pdf->SetTextColor( 50, 50, 50 );
-		$pdf->SetFont( 'Helvetica', '', 14 );
+		$pdf->SetFont( 'freesans', '', 14 );
 		$pdf->Cell( 0, 10, utf8_decode( __( 'Table View', 'edd' ) ), 0, 2, 'L', false );
-		$pdf->SetFont( 'Helvetica', '', 12 );
+		$pdf->SetFont( 'freesans', '', 12 );
 
 		$pdf->SetFillColor( 238, 238, 238 );
 		$pdf->Cell( 70, 6, utf8_decode( __( 'Product Name', 'edd' ) ), 1, 0, 'L', true );
@@ -119,9 +122,9 @@ function edd_generate_pdf( $data ) {
 
 		$pdf->Ln();
 		$pdf->SetTextColor( 50, 50, 50 );
-		$pdf->SetFont( 'Helvetica', '', 14 );
+		$pdf->SetFont( 'freesans', '', 14 );
 		$pdf->Cell( 0, 10, utf8_decode( __('Graph View', 'edd') ), 0, 2, 'L', false );
-		$pdf->SetFont( 'Helvetica', '', 12 );
+		$pdf->SetFont( 'freesans', '', 12 );
 
 		$image = html_entity_decode( urldecode( edd_draw_chart_image() ) );
 		$image = str_replace( ' ', '%20', $image );
@@ -129,7 +132,7 @@ function edd_generate_pdf( $data ) {
 		$pdf->SetX( 25 );
 		$pdf->Image( $image .'&file=.png' );
 		$pdf->Ln( 7 );
-		$pdf->Output( 'edd-report-' . date_i18n('Y-m-d') . '.pdf', 'D' );
+		$pdf->Output( 'edd-report-' . date_i18n('Y-m-d') . '.pdf', 'I' );
 	}
 }
 add_action( 'edd_generate_pdf', 'edd_generate_pdf' );
