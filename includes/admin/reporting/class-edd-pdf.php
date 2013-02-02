9e9a43b8c7aae7eb603034f3088e2a77eb316a57
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-02-02 09:16:57 +0000

    Removed static keyword

--- includes/admin/reporting/class-edd-pdf.php
+++ includes/admin/reporting/class-edd-pdf.php
@@ -20,7 +20,7 @@ class EDD_PDF extends TCPDF {
 	 *
 	 * @since 1.4.4
 	 */
-	public static function Footer() {
+	public function Footer() {
 		parent::SetY( -15 );
 		parent::SetFont( 'freesans', 'I', 8 );
 		parent::Cell( 0, 10, __( 'Page', 'edd' ) . ' ' . parent::getAliasNumPage() . ' ' . __( 'of', 'edd' ) . ' ' . parent::getAliasNbPages(), 0, 0, 'C');
