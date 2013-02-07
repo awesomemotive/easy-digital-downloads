b3ab6d6a4ab6dd7cdcd1d7e3dfa70e63540dd972
Pippin Williamson <pippin@pippinsplugins.com>
2013-02-06 18:48:13 -0600

    Add new caps

--- includes/class-edd-roles.php
+++ includes/class-edd-roles.php
@@ -2,54 +2,81 @@
 
 class EDD_Roles {
 
-	function __construct() { }
+	function __construct() {
+		$this->add_roles();
+		$this->add_caps();
+	}
+
+
+	public function add_roles() {
+
+		add_role( 'shop_manager', __( 'Shop Manager', 'edd' ), array(
+		    'read'                   => true,
+		    'read_private_pages'     => true,
+		    'read_private_posts'     => true,
+		    'edit_users'             => true,
+		    'edit_posts'             => false,
+		    'delete_posts'           => false,
+		    'manage_categories'      => true,
+		    'manage_links'           => true,
+		    'moderate_comments'      => true,
+		    'unfiltered_html'        => true,
+		    'upload_files'           => true,
+		   	'export'                 => true,
+			'import'                 => true
+		) );
+
+		add_role( 'shop_accountant', __( 'Shop Accountant', 'edd' ), array(
+		    'read'                   => true,
+		    'edit_posts'             => false,
+		    'delete_posts'           => false
+		) );
+
+		add_role( 'shop_worker', __( 'Shop Worker', 'edd' ), array(
+		    'read'                   => true,
+		    'edit_posts'             => false,
+		    'delete_posts'           => false
+		) );
+
+		add_role( 'shop_vendor', __( 'Shop Vendor', 'edd' ), array(
+		    'read'                   => true,
+		    'edit_posts'             => false,
+		    'delete_posts'           => false
+		) );
+
+	}
 
 	/**
-	 * Get capabilities for EDD
+	 * Add new capabilities
 	 *
-	 * @since  1.4.2
+	 * @since  1.4.4
 	 * @return void
 	 */
 
-	public function get_core_capabilities() {
+	public function add_caps() {
+		global $wp_roles;
 
-		$capabilities = array();
+		if ( class_exists('WP_Roles') )
+			if ( ! isset( $wp_roles ) )
+				$wp_roles = new WP_Roles();
 
-		$capabilities['core'] = array(
-			"edd_manage",
-			"edd_view_reports"
-		);
+		if ( is_object( $wp_roles ) ) {
 
-		$capability_types = array( 'edd_product', 'edd_order', 'edd_discount' );
+			$wp_roles->add_cap( 'shop_manager', 'view_shop_reports' );
+			$wp_roles->add_cap( 'shop_manager', 'export_shop_reports' );
+			$wp_roles->add_cap( 'shop_manager', 'manage_shop_discounts' );
+			$wp_roles->add_cap( 'shop_manager', 'manage_shop_settings' );
 
-		foreach( $capability_types as $capability_type ) {
+			$wp_roles->add_cap( 'administrator', 'view_shop_reports' );
+			$wp_roles->add_cap( 'administrator', 'export_shop_reports' );
+			$wp_roles->add_cap( 'administrator', 'manage_shop_discounts' );
+			$wp_roles->add_cap( 'administrator', 'manage_shop_settings' );
 
-			$capabilities[ $capability_type ] = array(
+			$wp_roles->add_cap( 'shop_accountant', 'view_shop_reports' );
+			$wp_roles->add_cap( 'shop_accountant', 'export_shop_reports' );
 
-				// Post type
-				"edit_{$capability_type}",
-				"read_{$capability_type}",
-				"delete_{$capability_type}",
-				"edit_{$capability_type}s",
-				"edit_others_{$capability_type}s",
-				"publish_{$capability_type}s",
-				"read_private_{$capability_type}s",
-				"delete_{$capability_type}s",
-				"delete_private_{$capability_type}s",
-				"delete_published_{$capability_type}s",
-				"delete_others_{$capability_type}s",
-				"edit_private_{$capability_type}s",
-				"edit_published_{$capability_type}s",
-
-				// Terms
-				"manage_{$capability_type}_terms",
-				"edit_{$capability_type}_terms",
-				"delete_{$capability_type}_terms",
-				"assign_{$capability_type}_terms"
-			);
 		}
 
-		return apply_filters( 'edd_get_core_capabilities', $capabilities );
 	}
 
 }
