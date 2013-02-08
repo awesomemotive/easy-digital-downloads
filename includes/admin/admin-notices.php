0cc586aefe56520664cad8304df8e4b76b762d10
Pippin Williamson <pippin@pippinsplugins.com>
2013-02-08 16:51:18 -0600

    Alert user if checkout page doesn't exist. Fixes #754

--- includes/admin/admin-notices.php
+++ includes/admin/admin-notices.php
@@ -22,10 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 */
 
 function edd_admin_messages() {
-	global $typenow;
-
-	if ( 'download' != $typenow )
-		return;
+	global $typenow, $edd_options;
 
 	if ( isset( $_GET['edd-message'] ) && $_GET['edd-message'] == 'discount_updated' && current_user_can( 'manage_shop_discounts' ) ) {
 		 add_settings_error( 'edd-notices', 'edd-discount-updated', __( 'Discount code updated.', 'edd' ), 'updated' );
@@ -45,6 +42,10 @@ function edd_admin_messages() {
 
 	if ( isset( $_GET['page'] ) && $_GET['page'] == 'edd-payment-history' && current_user_can( 'view_shop_reports' ) && edd_is_test_mode() ) {
 		add_settings_error( 'edd-notices', 'edd-payment-sent', sprintf( __( 'Note: Test Mode is enabled, only test payments are shown below. %sSettings%s.', 'edd' ), '<a href="' . admin_url( 'edit.php?post_type=download&page=edd-settings' ) . '">', '</a>' ), 'updated' );
+	}
+
+	if ( ( empty( $edd_options['purchase_page'] ) || 'trash' == get_post_status( $edd_options['purchase_page'] ) ) && current_user_can( 'edit_pages' ) ) {
+		add_settings_error( 'edd-notices', 'set-checkout', sprintf( __( 'No checkout page has been configured. Visit <a href="%s">Settings</a> to set one.', 'edd' ), admin_url( 'edit.php?post_type=download&page=edd-settings' ) ) );
 	}
 
 	settings_errors( 'edd-notices' );
