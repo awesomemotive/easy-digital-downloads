3d6dd88faa0aab002d4d5ec8ec77b889fdecc5ea
Elliott Stocks <elliottstocks@hotmail.co.uk>
2013-02-09 14:51:14 +0000

    Add filter after price field
    If hooking into "edd_price_field" the container div is hidden when variable pricing is enabled. Allow for filtering after the price field.

--- includes/admin/downloads/metabox.php
+++ includes/admin/downloads/metabox.php
@@ -196,6 +196,8 @@ function edd_render_price_field( $post_id ) {
 
 		<?php do_action( 'edd_price_field', $post_id ); ?>
 	</div>
+	
+	<?php apply_filters( 'edd_after_price_field', $post_id ); ?>
 
 	<div id="edd_variable_price_fields" class="edd_pricing_fields" <?php echo $variable_display; ?>>
 		<input type="hidden" id="edd_variable_prices" class="edd_variable_prices_name_field" value=""/>
