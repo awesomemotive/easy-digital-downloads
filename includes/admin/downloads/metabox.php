483cb2a296c063bcc44f026c2e00fd66327b405e
Pippin Williamson <pippin@pippinsplugins.com>
2013-02-10 15:52:11 -0600

    Re-fixing 1.4.4.1

--- includes/admin/downloads/metabox.php
+++ includes/admin/downloads/metabox.php
@@ -171,6 +171,7 @@ function edd_render_price_field( $post_id ) {
 	$price 				= edd_get_download_price( $post_id );
 	$variable_pricing 	= edd_has_variable_prices( $post_id );
 	$prices 			= edd_get_variable_prices( $post_id );
+	$single_option_mode = edd_single_price_option_mode( $post_id );
 
 	$price_display    	= $variable_pricing ? ' style="display:none;"' : '';
 	$variable_display 	= $variable_pricing ? '' : ' style="display:none;"';
@@ -196,12 +197,13 @@ function edd_render_price_field( $post_id ) {
 
 		<?php do_action( 'edd_price_field', $post_id ); ?>
 	</div>
-	
-	<?php apply_filters( 'edd_after_price_field', $post_id ); ?>
 
 	<div id="edd_variable_price_fields" class="edd_pricing_fields" <?php echo $variable_display; ?>>
 		<input type="hidden" id="edd_variable_prices" class="edd_variable_prices_name_field" value=""/>
-
+		<p>
+			<input type="checkbox" name="_edd_price_options_mode" id="edd_price_options_mode"<?php checked( 1, $single_option_mode ); ?> />
+			<label for="edd_price_options_mode"><?php _e( 'Enable multi option purchase mode. Leave unchecked to only permit a single price option to be purchased', 'edd' ); ?></label>
+		</p>
 		<div id="edd_price_fields" class="edd_meta_table_wrap">
 			<table class="widefat" width="100%" cellpadding="0" cellspacing="0">
 				<thead>
@@ -221,14 +223,14 @@ function edd_render_price_field( $post_id ) {
 
 								$args = apply_filters( 'edd_price_row_args', compact( 'name', 'amount' ) );
 					?>
-						<tr class="edd_variable_prices_wrapper edd_repeatable_prices">
+						<tr class="edd_variable_prices_wrapper">
 							<?php do_action( 'edd_render_price_row', $key, $args, $post_id ); ?>
 						</tr>
 					<?php
 							endforeach;
 						else :
 					?>
-						<tr class="edd_variable_prices_wrapper edd_repeatable_prices">
+						<tr class="edd_variable_prices_wrapper">
 							<?php do_action( 'edd_render_price_row', 0, array(), $post_id ); ?>
 						</tr>
 					<?php endif; ?>
@@ -241,11 +243,10 @@ function edd_render_price_field( $post_id ) {
 				</tbody>
 			</table>
 		</div>
-	</div>
+	</div><!--end #edd_variable_price_fields-->
 <?php
 }
 add_action( 'edd_meta_box_fields', 'edd_render_price_field', 10 );
-
 
 /**
  * Individual price row.
