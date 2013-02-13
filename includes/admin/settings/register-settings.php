13b56080401af5365902d6f1d4d78d6511030d7c
Pippin Williamson <pippin@pippinsplugins.com>
2013-02-13 17:18:09 -0600

    Added new edd_license_key_callback

--- includes/admin/settings/register-settings.php
+++ includes/admin/settings/register-settings.php
@@ -859,6 +859,33 @@ function edd_upload_callback($args) {
 
 
 /**
+ * Registers the license field callback for Software Licensing
+ * *
+ * @access      private
+ * @since       1.5
+ * @return      void
+*/
+
+if( ! function_exists( 'edd_license_key_callback' ) ) {
+	function edd_license_key_callback( $args ) {
+		global $edd_options;
+
+		if( isset( $edd_options[ $args['id'] ] ) ) { $value = $edd_options[ $args['id'] ]; } else { $value = isset( $args['std'] ) ? $args['std'] : ''; }
+		$size = isset( $args['size'] ) && !is_null($args['size']) ? $args['size'] : 'regular';
+		$html = '<input type="text" class="' . $args['size'] . '-text" id="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" name="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" value="' . esc_attr( $value ) . '"/>';
+
+		if( 'valid' == get_option( $args['options']['is_valid_license_option'] ) ) {
+			$html .= wp_nonce_field( $args['id'] . '_nonce', $args['id'] . '_nonce', false );
+			$html .= '<input type="submit" class="button-secondary" name="' . $args['id'] . '_deactivate" value="' . __( 'Deactivate License',  'edd-recurring' ) . '"/>';
+		}
+		$html .= '<label for="edd_settings_' . $args['section'] . '[' . $args['id'] . ']"> '  . $args['desc'] . '</label>';
+
+		echo $html;
+	}
+}
+
+
+/**
  * Hook Callback
  *
  * Adds a do_action() hook in place of the field
