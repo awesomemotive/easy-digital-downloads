555117d4e716ea5273464d698903dcecb712858e
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-02-19 08:59:06 +0000

    class-export-customers.php #904

--- includes/admin/reporting/class-export-customers.php
+++ includes/admin/reporting/class-export-customers.php
@@ -12,17 +12,14 @@
 */
 
 class EDD_Customers_Export extends EDD_Export {
-
-
 	/**
 	 * Our export type. Used for export-type specific filters / actions
 	 *
 	 * @access      public
+	 * @var         string
 	 * @since       1.4.4
 	 */
-
 	public $export_type = 'customers';
-
 
 	/**
 	 * Set the export headers
@@ -31,9 +28,7 @@ class EDD_Customers_Export extends EDD_Export {
 	 * @since       1.4.4
 	 * @return      void
 	 */
-
 	public function headers() {
-
 		ignore_user_abort( true );
 
 		if ( ! edd_is_func_disabled( 'set_time_limit' ) && ! ini_get( 'safe_mode' ) )
@@ -41,7 +36,7 @@ class EDD_Customers_Export extends EDD_Export {
 
 		$extra = '';
 
-		if( ! empty( $_POST['edd_export_download'] ) ) {
+		if ( ! empty( $_POST['edd_export_download'] ) ) {
 			$extra = sanitize_title( get_the_title( absint( $_POST['edd_export_download'] ) ) ) . '-';
 		}
 
@@ -51,7 +46,6 @@ class EDD_Customers_Export extends EDD_Export {
 		header( "Expires: 0" );
 	}
 
-
 	/**
 	 * Set the CSV columns
 	 *
@@ -59,31 +53,24 @@ class EDD_Customers_Export extends EDD_Export {
 	 * @since       1.4.4
 	 * @return      array
 	 */
-
 	public function csv_cols() {
-
-		if( ! empty( $_POST['edd_export_download'] ) ) {
-
+		if ( ! empty( $_POST['edd_export_download'] ) ) {
 			$cols = array(
 				'name'      => __( 'Name',   'edd' ),
 				'email'     => __( 'Email', 'edd' ),
 				'date'      => __( 'Date Purchased', 'edd' )
 			);
-
 		} else {
-
 			$cols = array(
 				'name'      => __( 'Name',   'edd' ),
 				'email'     => __( 'Email', 'edd' ),
 				'purchases' => __( 'Total Purchases', 'edd' ),
 				'amount'    => __( 'Total Purchased', 'edd' )
 			);
-
 		}
 
 		return $cols;
 	}
-
 
 	/**
 	 * Get the data being exported
@@ -92,15 +79,12 @@ class EDD_Customers_Export extends EDD_Export {
 	 * @since       1.4.4
 	 * @return      array
 	 */
-
 	public function get_data() {
-
 		global $wpdb;
 
 		$data = array();
 
-		if( ! empty( $_POST['edd_export_download'] ) ) {
-
+		if ( ! empty( $_POST['edd_export_download'] ) ) {
 			// Export customers of a specific product
 			global $edd_logs;
 
@@ -110,11 +94,10 @@ class EDD_Customers_Export extends EDD_Export {
 				'no_paging'    => true
 			);
 
-
 			$logs = $edd_logs->get_connected_logs( $args );
 
-			if( $logs ) {
-				foreach( $logs as $log ) {
+			if ( $logs ) {
+				foreach ( $logs as $log ) {
 					$payment_id = get_post_meta( $log->ID, '_edd_log_payment_id', true );
 					$email      = edd_get_payment_user_email( $payment_id );
 
@@ -125,17 +108,13 @@ class EDD_Customers_Export extends EDD_Export {
 						'email'     => $email,
 						'date'      => $log->post_date
 					);
-
 				}
 			}
-
 		} else {
-
 			// Export all customers
 			$emails = $wpdb->get_col( "SELECT DISTINCT meta_value FROM $wpdb->postmeta WHERE meta_key = '_edd_payment_user_email' " );
 
 			foreach ( $emails as $email ) {
-
 				$wp_user = get_user_by( 'email', $email );
 
 				$data[] = array(
@@ -144,9 +123,7 @@ class EDD_Customers_Export extends EDD_Export {
 					'purchases' => edd_count_purchases_of_customer( $email ),
 					'amount'    => html_entity_decode( edd_currency_filter( edd_format_amount( edd_purchase_total_of_user( $email ) ) ) )
 				);
-
 			}
-
 		}
 
 		$data = apply_filters( 'edd_export_get_data', $data );
@@ -154,5 +131,4 @@ class EDD_Customers_Export extends EDD_Export {
 
 		return $data;
 	}
-
 }
