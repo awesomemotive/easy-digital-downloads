a807bc97417886eddc8ef03ac617103077097c70
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-02-19 09:56:58 +0000

    email/functions.php #904

--- includes/emails/functions.php
+++ includes/emails/functions.php
@@ -7,10 +7,10 @@
  * @copyright   Copyright (c) 2013, Pippin Williamson
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0
-*/
+ */
 
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
  * Email Download Purchase Receipt
@@ -20,27 +20,24 @@ if ( !defined( 'ABSPATH' ) ) exit;
  * @access      private
  * @since       1.0
  * @return      void
-*/
-
+ */
 function edd_email_purchase_receipt( $payment_id, $admin_notice = true ) {
 	global $edd_options;
 
 	$payment_data = edd_get_payment_meta( $payment_id );
 	$user_info = maybe_unserialize( $payment_data['user_info'] );
 
-	if(isset($user_info['id']) && $user_info['id'] > 0) {
+	if ( isset( $user_info['id'] ) && $user_info['id'] > 0 ) {
 		$user_data = get_userdata($user_info['id']);
 		$name = $user_data->display_name;
-	} elseif( isset( $user_info['first_name'] ) && isset($user_info['last_name'] ) ) {
+	} elseif ( isset( $user_info['first_name'] ) && isset( $user_info['last_name'] ) ) {
 		$name = $user_info['first_name'] . ' ' . $user_info['last_name'];
 	} else {
 		$name = $user_info['email'];
 	}
 
 	$message = edd_get_email_body_header();
-
 	$message .= edd_get_email_body_content( $payment_id, $payment_data );
-
 	$message .= edd_get_email_body_footer();
 
 	$from_name = isset( $edd_options['from_name'] ) ? $edd_options['from_name'] : get_bloginfo('name');
@@ -48,7 +45,7 @@ function edd_email_purchase_receipt( $payment_id, $admin_notice = true ) {
 
 	$subject = apply_filters( 'edd_purchase_subject', isset( $edd_options['purchase_subject'] )
 		? trim( $edd_options['purchase_subject'] )
-		: __('Purchase Receipt', 'edd'), $payment_id );
+		: __( 'Purchase Receipt', 'edd' ), $payment_id );
 
 	$subject = edd_email_template_tags( $subject, $payment_data, $payment_id );
 
@@ -62,11 +59,10 @@ function edd_email_purchase_receipt( $payment_id, $admin_notice = true ) {
 
 	wp_mail( $payment_data['email'], $subject, $message, $headers, $attachments );
 
-	if( $admin_notice ) {
+	if ( $admin_notice ) {
 		do_action( 'edd_admin_sale_notice', $payment_id, $payment_data );
 	}
 }
-
 
 /**
  * Email Test Download Purchase Receipt
@@ -76,8 +72,7 @@ function edd_email_purchase_receipt( $payment_id, $admin_notice = true ) {
  * @access      private
  * @since       1.5
  * @return      void
-*/
-
+ */
 function edd_email_test_purchase_receipt() {
 	global $edd_options;
 
@@ -97,7 +92,7 @@ function edd_email_test_purchase_receipt() {
 
 	$subject = apply_filters( 'edd_purchase_subject', isset( $edd_options['purchase_subject'] )
 		? trim( $edd_options['purchase_subject'] )
-		: __('Purchase Receipt', 'edd'), $payment_id );
+		: __( 'Purchase Receipt', 'edd' ), $payment_id );
 
 	$headers = "From: " . stripslashes_deep( html_entity_decode( $from_name, ENT_COMPAT, 'UTF-8' ) ) . " <$from_email>\r\n";
 	$headers .= "Reply-To: ". $from_email . "\r\n";
@@ -105,9 +100,7 @@ function edd_email_test_purchase_receipt() {
 	$headers .= "Content-Type: text/html; charset=utf-8\r\n";
 
 	wp_mail( edd_get_admin_notice_emails(), $subject, $message, $headers );
-
 }
-
 
 /**
  * Sends the admin sale notice
@@ -115,33 +108,32 @@ function edd_email_test_purchase_receipt() {
  * @access      private
  * @since       1.4.2
  * @return      void
-*/
-
+ */
 function edd_admin_email_notice( $payment_id = 0, $payment_data = array() ) {
-
-	/* send an email notification to the admin */
+	/* Send an email notification to the admin */
 	$admin_email   = edd_get_admin_notice_emails();
 
 	$user_info = maybe_unserialize( $payment_data['user_info'] );
 
-	if(isset($user_info['id']) && $user_info['id'] > 0) {
+	if ( isset( $user_info['id'] ) && $user_info['id'] > 0 ) {
 		$user_data = get_userdata($user_info['id']);
 		$name = $user_data->display_name;
-	} elseif( isset( $user_info['first_name'] ) && isset($user_info['last_name'] ) ) {
+	} elseif ( isset( $user_info['first_name'] ) && isset($user_info['last_name'] ) ) {
 		$name = $user_info['first_name'] . ' ' . $user_info['last_name'];
 	} else {
 		$name = $user_info['email'];
 	}
 
-	$admin_subject = apply_filters( 'edd_admin_purchase_notification_subject', __('New download purchase', 'edd'), $payment_id, $payment_data );
+	$admin_subject = apply_filters( 'edd_admin_purchase_notification_subject', __( 'New download purchase', 'edd' ), $payment_id, $payment_data );
 
-	$admin_message = __('Hello', 'edd') . "\n\n" . sprintf( __('A %s purchase has been made', 'edd'), edd_get_label_plural() ) . ".\n\n";
-	$admin_message .= sprintf( __('%s sold:', 'edd'), edd_get_label_plural() ) .  "\n\n";
+	$admin_message = __( 'Hello', 'edd' ) . "\n\n" . sprintf( __( 'A %s purchase has been made', 'edd' ), edd_get_label_plural() ) . ".\n\n";
+	$admin_message .= sprintf( __( '%s sold:', 'edd' ), edd_get_label_plural() ) .  "\n\n";
 
 	$download_list = '';
 	$downloads = maybe_unserialize( $payment_data['downloads'] );
-	if( is_array( $downloads ) ) {
-		foreach( $downloads as $download ) {
+
+	if ( is_array( $downloads ) ) {
+		foreach ( $downloads as $download ) {
 			$id    = isset( $payment_data['cart_details'] ) ? $download['id'] : $download;
 			$title = get_the_title( $id );
 			if ( isset( $download['options'] ) ) {
@@ -152,13 +144,14 @@ function edd_admin_email_notice( $payment_id = 0, $payment_data = array() ) {
 			$download_list .= html_entity_decode( $title, ENT_COMPAT, 'UTF-8' ) . "\n";
 		}
 	}
+
 	$gateway = edd_get_gateway_admin_label( get_post_meta( $payment_id, '_edd_payment_gateway', true ) );
 
 	$admin_message .= $download_list . "\n";
-	$admin_message .= __('Purchased by: ', 'edd')   . " " . html_entity_decode( $name, ENT_COMPAT, 'UTF-8' ) . "\n";
-	$admin_message .= __('Amount: ', 'edd')         . " " . html_entity_decode( edd_currency_filter( edd_format_amount( $payment_data['amount'] ) ), ENT_COMPAT, 'UTF-8' ) . "\n\n";
-	$admin_message .= __('Payment Method: ', 'edd') . " " . $gateway . "\n\n";
-	$admin_message .= __('Thank you', 'edd');
+	$admin_message .= __( 'Purchased by: ', 'edd' )   . " " . html_entity_decode( $name, ENT_COMPAT, 'UTF-8' ) . "\n";
+	$admin_message .= __( 'Amount: ', 'edd' )         . " " . html_entity_decode( edd_currency_filter( edd_format_amount( $payment_data['amount'] ) ), ENT_COMPAT, 'UTF-8' ) . "\n\n";
+	$admin_message .= __( 'Payment Method: ', 'edd' ) . " " . $gateway . "\n\n";
+	$admin_message .= __( 'Thank you', 'edd' );
 	$admin_message = apply_filters( 'edd_admin_purchase_notification', $admin_message, $payment_id, $payment_data );
 
 	$admin_headers = apply_filters( 'edd_admin_purchase_notification_headers', array(), $payment_id, $payment_data );
@@ -166,10 +159,8 @@ function edd_admin_email_notice( $payment_id = 0, $payment_data = array() ) {
 	$admin_attachments = apply_filters( 'edd_admin_purchase_notification_attachments', array(), $payment_id, $payment_data );
 
 	wp_mail( $admin_email, $admin_subject, $admin_message, $admin_headers, $admin_attachments );
-
 }
 add_action( 'edd_admin_sale_notice', 'edd_admin_email_notice', 10, 2 );
-
 
 /**
  * Retrieves the admin notice emails
@@ -179,10 +170,8 @@ add_action( 'edd_admin_sale_notice', 'edd_admin_email_notice', 10, 2 );
  * @access      private
  * @since       1.0
  * @return      void
-*/
-
+ */
 function edd_get_admin_notice_emails() {
-
 	global $edd_options;
 
 	$emails = isset( $edd_options['admin_notice_emails'] ) && strlen( trim( $edd_options['admin_notice_emails'] ) ) > 0 ? $edd_options['admin_notice_emails'] : get_bloginfo( 'admin_email' );
