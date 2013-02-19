12601f5cea2d1fc7ab1c91910af4afad78e125db
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-02-19 16:54:19 +0000

    Add Product Description Microdata #908

--- templates/shortcode-content-excerpt.php
+++ templates/shortcode-content-excerpt.php
@@ -1,4 +1,4 @@
-<div class="edd_download_excerpt">
+<div itemprop="description" class="edd_download_excerpt">
 	<?php
 		$excerpt_length = apply_filters('excerpt_length', 30);
 		echo apply_filters( 'edd_downloads_excerpt', wp_trim_words( get_post_field( 'post_content', get_the_ID() ), $excerpt_length ) );
