ce077105f401be9e12983d5e6de048422bd0c745
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-02-19 09:20:21 +0000

    admin-actions.php #904

--- includes/admin/admin-actions.php
+++ includes/admin/admin-actions.php
@@ -7,11 +7,10 @@
  * @copyright   Copyright (c) 2013, Pippin Williamson
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0
-*/
+ */
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
-
 
 /**
  * Process Actions
@@ -19,8 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * @access      private
  * @since       1.0
  * @return      void
-*/
-
+ */
 function edd_process_actions() {
 	if ( isset( $_POST['edd-action'] ) ) {
 		do_action( 'edd_' . $_POST['edd-action'], $_POST );
