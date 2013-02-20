7713ee2023370d7e11c076a69bfa49fa4383c7ca
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-02-20 15:46:39 +0000

    Remove loading animation

--- assets/js/admin-scripts.js
+++ assets/js/admin-scripts.js
@@ -282,6 +282,7 @@ jQuery(document).ready(function ($) {
 			$('.edd_add_download_to_purchase_waiting:last').removeClass('hidden');
 			$.post(ajaxurl, variable_price_check_ajax_data, function(response) {
 				$el.after(response);
+				$('.edd_add_download_to_purchase_waiting:last').addClass('hidden');
 			});
 		} else {
 			$el.next('select').remove();
