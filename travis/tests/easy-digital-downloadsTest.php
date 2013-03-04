ee67a5cd05f7abef08aad99d8e7a1be12f50e858
Chris Christoff <chriscct7@gmail.com>
2013-03-04 11:23:04 -0500

    Leggo!

---
+++ travis/tests/easy-digital-downloadsTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * ConfigTest Tests
+ * Some basic tests
+ */
+class Easy-Digital-DownloadsTest extends WP_UnitTestCase {
+    /**
+     * @var EDD_Roles
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new EDD_Digital_Downloads;
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @covers EDD_Digital_Downloads::setup_constants
+     */
+	 protected function setup_constants()
+	 {
+		// At this point, since plugin is loaded these should be defined
+		// Plugin version
+		$this->assertSame(EDD_VERSION, '1.4.2' );
+
+		// Plugin Folder URL
+		$this->assertSame(EDD_PLUGIN_URL, plugin_dir_url( __FILE__ ) );
+
+		// Plugin Folder Path
+		$this->assertSame(EDD_PLUGIN_DIR, plugin_dir_path( __FILE__ ) );
+
+		// Plugin Root File
+		$this->assertSame(EDD_PLUGIN_FILE, __FILE__ );
+	 }
+}
