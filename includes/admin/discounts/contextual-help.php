5fae73315536b6c627ae7647f9b82549c129426b
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-03-29 19:41:25 +0000

    Update @category for contextual help files

--- includes/admin/discounts/contextual-help.php
+++ includes/admin/discounts/contextual-help.php
@@ -3,7 +3,7 @@
  * Contextual Help
  *
  * @package     Easy Digital Downloads
- * @subpackage  Downloads
+ * @subpackage  Documentation
  * @copyright   Copyright (c) 2013, Pippin Williamson
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.2.3
