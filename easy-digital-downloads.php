57b90c434772883ebe41706ece56a610a2a32336
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-03-29 19:18:48 +0000

    DocBlock easy-digital-downloads.php

--- easy-digital-downloads.php
+++ easy-digital-downloads.php
@@ -24,61 +24,55 @@ along with Easy Digital Downloads. If not, see <http://www.gnu.org/licenses/>.
 */
 
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) exit;
 
-if ( !class_exists( 'Easy_Digital_Downloads' ) ) :
+if ( ! class_exists( 'Easy_Digital_Downloads' ) ) :
 
 /**
  * Main Easy_Digital_Downloads Class
  *
- * @since v1.4
+ * @since 1.4
  */
-
 final class Easy_Digital_Downloads {
 	/** Singleton *************************************************************/
 
 	/**
 	 * @var Easy_Digital_Downloads The one true Easy_Digital_Downloads
+	 * @since 1.4
 	 */
 	private static $instance;
 
 	/**
-	 * EDD user roles and capabilities object
+	 * @var object EDD User Roles and Capabilities Object
 	 * @since 1.4.4
-	 * @var object
 	 */
 	private $roles;
 
 	/**
-	 * EDD cart fees object
-	 * @var object
+	 * @var object EDD Cart Fees Object
 	 * @since 1.5
 	 */
 	public $fees;
 
 	/**
-	 * EDD API object
+	 * @var object EDD API Object
 	 * @since 1.5
 	 */
 	public $api;
 
-
 	/**
-	 * EDD HTML session object
-	 *
 	 * This holds cart items, purchase sessions, and anything else stored in the session
 	 *
+	 * @var object EDD HTML Session Object
 	 * @since 1.5
 	 */
 	public $session;
 
-
 	/**
-	 * EDD HTML Element helper object
+	 * @var object EDD HTML Element Helper Object
 	 * @since 1.5
 	 */
 	public $html;
-
 
 	/**
 	 * Main Easy_Digital_Downloads Instance
@@ -87,6 +81,7 @@ final class Easy_Digital_Downloads {
 	 * time. Also prevents needing to define globals all over the place.
 	 *
 	 * @since 1.4
+	 * @static
 	 * @staticvar array $instance
 	 * @uses Easy_Digital_Downloads::setup_globals() Setup the globals needed
 	 * @uses Easy_Digital_Downloads::includes() Include the required files
@@ -112,36 +107,34 @@ final class Easy_Digital_Downloads {
 	/**
 	 * Setup plugin constants
 	 *
-	 * @since 1.4
 	 * @access private
-	 * @uses plugin_dir_path() To generate EDD plugin path
-	 * @uses plugin_dir_url() To generate EDD plugin url
+	 * @since 1.4
+	 * @return void
 	 */
 	private function setup_constants() {
 		// Plugin version
-
-		if( ! defined( 'EDD_VERSION' ) )
+		if ( ! defined( 'EDD_VERSION' ) )
 			define( 'EDD_VERSION', '1.5.1' );
 
 		// Plugin Folder URL
-		if( ! defined( 'EDD_PLUGIN_URL' ) )
+		if ( ! defined( 'EDD_PLUGIN_URL' ) )
 			define( 'EDD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 		// Plugin Folder Path
-		if( ! defined( 'EDD_PLUGIN_DIR' ) )
+		if ( ! defined( 'EDD_PLUGIN_DIR' ) )
 			define( 'EDD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 		// Plugin Root File
-		if( ! defined( 'EDD_PLUGIN_FILE' ) )
+		if ( ! defined( 'EDD_PLUGIN_FILE' ) )
 			define( 'EDD_PLUGIN_FILE', __FILE__ );
 	}
 
 	/**
 	 * Include required files
 	 *
-	 * @since 1.4
 	 * @access private
-	 * @uses is_admin() If in WordPress admin, load additional file
+	 * @since 1.4
+	 * @return void
 	 */
 	private function includes() {
 		global $edd_options;
@@ -225,14 +218,9 @@ final class Easy_Digital_Downloads {
 	/**
 	 * Loads the plugin language files
 	 *
+	 * @access public
 	 * @since 1.4
-	 * @access private
-	 * @uses dirname()
-	 * @uses plugin_basename()
-	 * @uses apply_filters()
-	 * @uses load_textdomain()
-	 * @uses get_locale()
-	 * @uses load_plugin_textdomain()
+	 * @return void
 	 */
 	public function load_textdomain() {
 		// Set filter for plugin's languages directory
@@ -265,7 +253,7 @@ endif; // End if class_exists check
 
 /**
  * The main function responsible for returning the one true Easy_Digital_Downloads
- *  Instance to functions everywhere.
+ * Instance to functions everywhere.
  *
  * Use this function like you would a global variable, except without needing
  * to declare the global.
@@ -273,12 +261,11 @@ endif; // End if class_exists check
  * Example: <?php $edd = EDD(); ?>
  *
  * @since 1.4
- * @return The one true Easy_Digital_Downloads Instance
+ * @return object The one true Easy_Digital_Downloads Instance
  */
-
 function EDD() {
 	return Easy_Digital_Downloads::instance();
 }
 
-// Starts EDD running
+// Get EDD Running
 EDD();
