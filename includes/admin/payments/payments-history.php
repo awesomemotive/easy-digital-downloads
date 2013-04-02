1ba41961acf3a481e7c755acd68d78b8bdb3d7a0
Pippin Williamson <pippin@pippinsplugins.com>
2013-04-02 16:45:29 -0500

    Added iOS app link + icon. #1004

--- includes/admin/payments/payments-history.php
+++ includes/admin/payments/payments-history.php
@@ -45,6 +45,14 @@ function edd_payment_history_page() {
 			<?php $payments_table->display() ?>
 		</form>
 		<?php do_action( 'edd_payments_page_bottom' ); ?>
+
+		<p class="edd-mobile-link">
+			<a href="https://easydigitaldownloads.com/extension/ios-sales-earnings-tracker/" target="_blank">
+				<img src="<?php echo EDD_PLUGIN_URL . '/assets/images/icons/iphone.png'; ?>"/>
+				<?php _e( 'Get the EDD Sales / Earnings tracker for iOS', 'edd' ); ?>
+			</a>
+		</p>
+
 	</div>
 <?php
 	}
