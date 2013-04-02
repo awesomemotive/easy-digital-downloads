fb3683c6e12d1510e8b894c90b75561a6598087b
Pippin Williamson <pippin@pippinsplugins.com>
2013-04-02 16:52:57 -0500

    Fixed missing logic for install script. Closes #1024

--- includes/install.php
+++ includes/install.php
@@ -103,6 +103,7 @@ function edd_install() {
 
 	// Bail if activating from network, or bulk
 	if ( is_network_admin() || isset( $_GET['activate-multi'] ) )
+		return;
 
 	// Add the transient to redirect
     set_transient( '_edd_activation_redirect', true, 30 );
