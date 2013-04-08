00aa29d4971b5c92c5acdff9af4bd0dceea03e50
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-08 12:36:35 +0100

    Create a discount to work with

--- travis/tests/Discounts.Test.php
+++ travis/tests/Discounts.Test.php
@@ -1,10 +1,17 @@
 <?php
+require_once('./travis/vendor/wordpress-tests/lib/factory.php');
+
 /**
  * Test Discounts
  */
 class Test_Easy_Digital_Downloads_Discounts extends WP_UnitTestCase {
+	protected $_post = null;
+
 	public function setUp() {
 		parent::setUp();
+		$wp_factory = new WP_UnitTest_Factory;
+		$post_id = $wp_factory->post->create( array( 'post_type' => 'edd_discount', 'post_status' => 'draft' ) );
+		$this->_post = get_post( $post_id );
 	}
 
 	public function tearDown() {
