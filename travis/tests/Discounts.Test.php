59694a4957f6e140ed8fd7a3f3667fdcd1577ed7
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-08 13:33:42 +0100

    Finish up on discount tests

--- travis/tests/Discounts.Test.php
+++ travis/tests/Discounts.Test.php
@@ -17,7 +17,7 @@ class Test_Easy_Digital_Downloads_Discounts extends WP_UnitTestCase {
 			'amount' => '20',
 			'code' => '20OFF',
 			'product_condition' => 'all',
-			'start' => '12/12/2050 00:00:00',
+			'start' => '12/12/2000 00:00:00',
 			'expiration' => '12/31/2050 23:59:59',
 			'max_uses' => 10,
 			'uses' => 54,
@@ -77,7 +77,7 @@ class Test_Easy_Digital_Downloads_Discounts extends WP_UnitTestCase {
 	}
 
 	public function testDiscountStartDate() {
-		$this->assertSame('12/12/2050 00:00:00', edd_get_discount_start_date($this->_post->ID));
+		$this->assertSame('12/12/2000 00:00:00', edd_get_discount_start_date($this->_post->ID));
 	}
 
 	public function testDiscountExpirationDate() {
@@ -114,6 +114,63 @@ class Test_Easy_Digital_Downloads_Discounts extends WP_UnitTestCase {
 
 	public function testDiscountIsSingleUse() {
 		$this->assertTrue(edd_discount_is_single_use($this->_post->ID));
+	}
+
+	public function testDiscountIsStarted() {
+		$this->assertTrue(edd_is_discount_started($this->_post->ID));
+	}
+
+	public function testDiscountIsExpired() {
+		$this->assertFalse(edd_is_discount_expired($this->_post->ID));
+	}
+
+	public function testDiscountIsMaxedOut() {
+		$this->assertTrue(edd_is_discount_maxed_out($this->_post->ID));
+	}
+
+	public function testDiscountIsMinMet() {
+		$this->markTestIncomplete(
+		  'This test has not been implemented yet. The cart object needs to link with this test.'
+		);
+	}
+
+	public function testDiscountIsUsed() {
+		$this->markTestIncomplete(
+		  'This test has not been implemented yet. The user object needs to link with this test.'
+		);
+	}
+
+	public function testDiscountIsValidWhenPurchasing() {
+		$this->assertFalse(edd_is_discount_valid($this->_post->ID));
+	}
+
+	public function testDiscountIDByCode() {
+		$this->assertSame($this->_post->ID, edd_get_discount_id_by_code('20OFF'));
+	}
+
+	public function testGetDiscountedAmount() {
+		$this->assertSame(432.0, edd_get_discounted_amount('20OFF', '540'));
+	}
+
+	public function testDiscountIncreaseUsage() {
+		$uses = edd_increase_discount_usage('20OFF');
+		$this->assertSame(55, $uses);
+	}
+
+	public function testDiscountFormattedAmount() {
+		$this->assertSame('20%', edd_format_discount_rate('percent', get_post_meta($this->_post->ID, '_edd_discount_amount', true)));
+	}
+
+	public function testSetCartDiscount() {
+		$this->markTestIncomplete(
+			'setcookie generates an error about not being able to modify header information'
+		);
+	}
+
+	public function testUnsetCartDiscount() {
+		$this->markTestIncomplete(
+			'setcookie generates an error about not being able to modify header information'
+		);
 	}
 
 	public function testDeletionOfDiscount() {
