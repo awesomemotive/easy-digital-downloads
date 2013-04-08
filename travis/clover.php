1080a8e7f5d29a11774ac8a1ebbddc1b9fc0d911
Chris Christoff <chriscct7@gmail.com>
2013-04-08 17:23:38 -0300

    Update clover.php

--- travis/clover.php
+++ travis/clover.php
@@ -3,11 +3,11 @@ $inputFile  = $argv[1];
 $percentage = min(100, max(0, (int) $argv[2]));
  
 if (!file_exists($inputFile)) {
-    throw new InvalidArgumentException('Invalid input file provided');
+   return 'Invalid input file provided';
 }
  
 if (!$percentage) {
-    throw new InvalidArgumentException('An integer checked percentage must be given as second parameter');
+    return 'An integer checked percentage must be given as second parameter';
 }
  
 $xml             = new SimpleXMLElement(file_get_contents($inputFile));
