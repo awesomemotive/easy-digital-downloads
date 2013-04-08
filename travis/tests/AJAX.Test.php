991c5cdddebdbac699f7fe2904aea1cf6b282e51
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-08 15:35:05 +0100

    Add AJAX Tests

---
+++ travis/tests/AJAX.Test.php
@@ -0,0 +1,109 @@
+<?php
+require_once('./travis/vendor/wordpress/wp-admin/includes/screen.php');
+require_once('./travis/vendor/wordpress/wp-includes/plugin.php');
+require_once('./travis/vendor/wordpress-tests/lib/factory.php');
+
+class Test_Easy_Digital_Downloads_AJAX extends WP_UnitTestCase {
+	protected $_post = null;
+
+	protected $_last_response;
+
+	public function setUp() {
+		parent::setUp();
+
+		$wp_factory = new WP_UnitTest_Factory;
+
+		$_actions = array(
+			'edd_remove_from_cart', 'edd_add_to_cart', 'edd_apply_discount', 'checkout_login',
+			'checkout_register', 'get_download_title', 'edd_local_tax_opt_in', 'edd_local_tax_opt_out',
+			'edd_check_for_download_price_variations'
+		);
+
+		foreach ($_actions as $action) {
+			if ( function_exists( 'wp_ajax_' . $action ) )
+				add_action( 'wp_ajax_' . $action, $action, 1 );
+
+			add_filter( 'wp_die_ajax_handler', array( $this, 'getDieHandler' ), 1, 1 );
+
+			if (!defined('DOING_AJAX'))
+				define('DOING_AJAX', true);
+			set_current_screen( 'ajax' );
+
+			add_action( 'clear_auth_cookie', array( $this, 'logout' ) );
+
+			$wp_factory->post->create_many( 5 );
+		}
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+		$_POST = array();
+		remove_filter( 'wp_die_ajax_handler', array( $this, 'getDieHandler' ), 1, 1 );
+		remove_action( 'clear_auth_cookie', array( $this, 'logout' ) );
+		set_current_screen( 'front' );
+	}
+
+	public function logout() {
+		unset( $GLOBALS['current_user'] );
+		$cookies = array(AUTH_COOKIE, SECURE_AUTH_COOKIE, LOGGED_IN_COOKIE, USER_COOKIE, PASS_COOKIE);
+		foreach ( $cookies as $c )
+			unset( $_COOKIE[$c] );
+	}
+
+	public function getDieHandler() {
+		return array( $this, 'dieHandler' );
+	}
+
+	public function dieHandler( $message ) {
+		$this->_last_response .= ob_get_clean();
+		ob_end_clean();
+		if ( '' === $this->_last_response ) {
+			if ( is_scalar( $message) ) {
+					throw new Exception( (string) $message );
+			} else {
+					throw new Exception( '0' );
+			}
+		} else {
+				throw new Exception( $message );
+		}
+	}
+
+	protected function _setRole( $role ) {
+		$wp_factory = new WP_UnitTest_Factory;
+		$post = $_POST;
+		$user_id = $wp_factory->user->create( array( 'role' => $role ) );
+		wp_set_current_user( $user_id );
+		$_POST = array_merge($_POST, $post);
+	}
+
+	protected function _handleAjax($action) {
+		// Start output buffering
+		ini_set( 'implicit_flush', false );
+		ob_start();
+
+		// Build the request
+		$_POST['action'] = $action;
+
+		// Call the hooks
+		do_action( 'wp_ajax_' . $_POST['action'] );
+
+		// Save the output
+		$buffer = ob_get_clean();
+
+		if ( !empty( $buffer ) )
+			$this->_last_response = $buffer;
+	}
+
+	public function testRemoveItemFromCart() {
+		$this->_setRole( 'administrator' );
+
+		$_POST = array(
+			'cart_item' => 0,
+			'nonce' => wp_create_nonce( 'edd_ajax_nonce' ),
+		);
+
+		$this->_handleAjax('edd_remove_from_cart');
+
+		echo $this->_last_response;
+	}
+}
