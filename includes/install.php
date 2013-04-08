d2337d64eadb4fa32b20247d47eba9295db50d89
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-08 20:33:21 +0100

    Back into production, again...

--- includes/install.php
+++ includes/install.php
@@ -102,6 +102,10 @@ function edd_install() {
 		update_option( 'edd_version', EDD_VERSION );
 	}
 
+	// Bail if activating from network, or bulk
+	if ( is_network_admin() || isset( $_GET['activate-multi'] ) )
+		return;
+
 	// Add the transient to redirect
     set_transient( '_edd_activation_redirect', true, 30 );
 }
