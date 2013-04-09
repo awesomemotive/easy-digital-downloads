4ffe103c1e7e9d3aacb5c5050556f6d55e934c60
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-09 11:02:18 +0100

    Get wp_query

--- travis/tests/API.Test.php
+++ travis/tests/API.Test.php
@@ -6,16 +6,19 @@
 class Test_Easy_Digital_Downloads_API extends WP_UnitTestCase {
 	protected $_rewrite = null;
 
+	protected $query = null;
+
 	public function setUp() {
 		parent::setUp();
 
-		global $wp_rewrite;
+		global $wp_rewrite, $wp_query;
 		$GLOBALS['wp_rewrite']->init();
 		flush_rewrite_rules();
 
 		EDD()->api->add_endpoint($wp_rewrite);
 
 		$this->_rewrite = $wp_rewrite;
+		$this->_query = $wp_query;
 	}
 
 	public function testEndpoints() {
