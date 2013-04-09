0daa9041faabd1ead45dd06da5c1822cfaa0ed94
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-09 22:47:58 +0100

    Add assertion for formatting amount

--- travis/tests/Formatting.Test.php
+++ travis/tests/Formatting.Test.php
@@ -11,4 +11,8 @@ class Test_Easy_Digital_Downloads_Formatting extends WP_UnitTestCase {
 	public function testSanitizeAmount() {
 		$this->assertEquals('20,000.20', edd_sanitize_amount('20,000.20'));
 	}
+
+	public function testFormatAmount() {
+		$this->assertEquals('20,000.20', edd_format_amount('20,000.20'));
+	}
 }
