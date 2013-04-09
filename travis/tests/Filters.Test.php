18236249e1c675a4cc105ca745d49bc589c36491
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-09 15:18:01 +0100

    Added test for edd_email_template_default

--- travis/tests/Filters.Test.php
+++ travis/tests/Filters.Test.php
@@ -329,7 +329,10 @@ class Test_Easy_Digital_Downloads_Filters extends WP_UnitTestCase {
 		$this->assertArrayHasKey('edd_email_template_preview', $wp_filter['edd_email_settings'][10]);
 	}
 
-	public function testEmailTemplateDefaultFilters() { $this->markTestIncomplete('This test has not been implemented yet.'); }
+	public function testEmailTemplateDefaultFilters() {
+		global $wp_filter;
+		$this->assertArrayHasKey('edd_default_email_template', $wp_filter['edd_email_template_default'][10]);
+	}
 
 	public function testPurchaseReceiptDefaultFilters() { $this->markTestIncomplete('This test has not been implemented yet.'); }
 
