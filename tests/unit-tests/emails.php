77eaac37204681405d16fd6de447fcdcfaef01b1
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-10 07:32:49 +0100

    Add test for email templates

--- tests/unit-tests/emails.php
+++ tests/unit-tests/emails.php
@@ -12,4 +12,13 @@ class Tests_Emails extends WP_UnitTestCase {
 		$expected = array( 'admin@example.org' );
 		$this->assertEquals( $expected, edd_get_admin_notice_emails() );
 	}
+
+	public function test_email_templates() {
+		$expected = array(
+			'default' => 'Default Template',
+			'none' => 'No template, plain text only'
+		);
+
+		$this->assertEquals( $expected, edd_get_email_templates() );
+	}
 }
