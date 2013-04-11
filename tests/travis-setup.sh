b619efcb741a746d02f0f80d7dc31765124f3b2a
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-11 22:02:33 +0100

    Set exit overload

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -1,7 +1,11 @@
+echo -e "Installing PHPUnit Test Helpers..."
 pear channel-discover pear.phpunit.de
 pecl install phpunit/test_helpers
 export JPHP_INI=`php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 export JXDB=`php --ini | grep "xdebug" | sed -e "s/.*:\s*//" -e "s/,.*//"`
 if [ $JXDB -a -f $JXDB ]; then sed -i='' -e "s/^/;/g" $JXDB; fi
 if [[ ! `cat $JPHP_INI | grep 'test_helpers.so'` ]]; then echo "zend_extension=`php -r "echo ini_get('extension_dir');"`/test_helpers.so" >> $JPHP_INI; fi
+echo -e "Installed PHPUnit Test Helpers"
+echo -e "Refreshing path..."
 phpenv rehash
+echo -e "Refreshed path"
