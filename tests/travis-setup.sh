04f66fb39c48474291e47a2e71655d90c0e1cada
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-11 13:42:40 +0100

    Reload PHP

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/sebastianbergmann/php-test-helpers.git
 sh -c "cd php-test-helpers && phpize && ./configure && make && sudo make install"
 echo "zend_extension=`php -r "echo ini_get('extension_dir');"`/test_helpers.so" >> `php --ini | grep "Scan for additional .ini files in:" | sed -e "s|.*:\s*||"`/z_test_helpers.ini
+phpenv rehash
