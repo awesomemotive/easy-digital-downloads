867980af20ca9b3a828bc1cd2cf65edfb15d7fa0
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-11 12:09:48 +0100

    Add URL tests

--- tests/unit-tests/url.php
+++ tests/unit-tests/url.php
@@ -10,6 +10,8 @@ class Test_EDD_URL extends WP_UnitTestCase {
 
 	public function test_ajax_url() {
 		$_SERVER['SERVER_PORT'] = 80;
+		$_SERVER['HTTPS'] = 'off';
+
 		$this->assertEquals( edd_get_ajax_url(), get_site_url( null, '/wp-admin/admin-ajax.php', 'http' ) );
 	}
 
