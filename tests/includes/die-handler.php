4b8363568cbee55d04cbdb559679d49cb8636b09
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-12 21:06:21 +0100

    Add custom die handler

---
+++ tests/includes/die-handler.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Override the die handlers.
+ *
+ * Easy Digital Downloads uses the edd_die() function to die/exit the script
+ * which is a simple wrapper for calling the wp_die() function, this class simply
+ * overrides that die calls by returning false allowing us to make assertions when
+ * edd_die() is called, for example in the AJAX functions or the API functions.
+ *
+ * @package EDD_Unit_Tests
+ * @author Sunny Ratilal
+ */
+class EDD_Die_Handler {
+	public $die = false;
+
+	public function __construct() {
+		add_filter( 'wp_die_handler', array( $this, 'die_handler' ) );
+	}
+
+	public function die_handler() {
+		$this->die = true;
+	}
+
+	public function died() {
+		return $this->die;
+	}
+
+	public function reset() {
+		$this->die = false;
+	}
+}
