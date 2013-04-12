59ffb2a1b9c11030c3b0aec53a457e92ae0c5bbc
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-12 10:24:32 +0100

    No need to load twice

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -5,5 +5,4 @@ export JPHP_INI=`php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 export JXDB=`php --ini | grep "xdebug" | sed -e "s/.*:\s*//" -e "s/,.*//"`
 if [ $JXDB -a -f $JXDB ]; then sed -i='' -e "s/^/;/g" $JXDB; fi
 if [[ ! `cat $JPHP_INI | grep 'test_helpers.so'` ]]; then echo "zend_extension=`php -r "echo ini_get('extension_dir');"`/test_helpers.so" >> $JPHP_INI; fi
-echo "zend_extension=`php -r "echo ini_get('extension_dir');"`/test_helpers.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo -e "Installed PHPUnit Test Helpers"
