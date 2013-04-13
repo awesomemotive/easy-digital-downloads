3d2f8555dbc28b615a3c231cd5276f57af7bac94
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-13 10:28:01 +0100

    Finish up on AJAX

--- tests/unit-tests/ajax.php
+++ tests/unit-tests/ajax.php
@@ -307,4 +307,61 @@ DATA;
 	 	$this->assertEquals( $expected_html, $decoded['html'] );
 	 	$this->assertEquals( '$0.00', $decoded['total'] );
 	}
+
+	public function test_check_for_download_price_variations() {
+		$wp_factory = new WP_UnitTest_Factory;
+		$wp_factory = new WP_UnitTest_Factory;
+		$post_id = $wp_factory->post->create( array( 'post_title' => 'Test Download', 'post_type' => 'download', 'post_status' => 'draft' ) );
+
+		$_variable_pricing = array(
+			array(
+				'name' => 'Simple',
+				'amount' => 20
+			),
+			array(
+				'name' => 'Advanced',
+				'amount' => 100
+			)
+		);
+
+		$_download_files = array(
+			array(
+				'name' => 'File 1',
+				'file' => 'http://localhost/file1.jpg',
+				'condition' => 0
+			),
+			array(
+				'name' => 'File 2',
+				'file' => 'http://localhost/file2.jpg',
+				'condition' => 'all'
+			)
+		);
+
+		$meta = array(
+			'edd_price' => '0.00',
+			'_variable_pricing' => 1,
+			'_edd_price_options_mode' => 'on',
+			'edd_variable_prices' => array_values( $_variable_pricing ), 
+			'edd_download_files' => array_values( $_download_files ),
+			'_edd_download_limit' => 20,
+			'_edd_hide_purchase_link' => 1,
+			'edd_product_notes' => 'Purchase Notes',
+			'_edd_product_type' => 'default',
+			'_edd_download_earnings' => 129.43,
+			'_edd_download_sales' => 59,
+			'_edd_download_limit_override_1' => 1
+		);
+		foreach( $meta as $key => $value ) {
+			update_post_meta( $post_id, $key, $value );
+		}
+
+		$_POST = array(
+			'nonce' => wp_create_nonce( 'edd_add_downloads_to_purchase_nonce' ),
+			'download_id' => $post_id
+		);
+		$this->_handleAjax( 'edd_check_for_download_price_variations' );
+
+		$expected = '<select name="downloads[0][options][price_id]" class="edd-variable-prices-select"><option value="0">Simple</option><option value="1">Advanced</option></select>';
+		$this->assertEquals( $expected, $this->_last_response );
+	}
 }
