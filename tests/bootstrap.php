e73280aa03d2ed8ccbad60ae901f17f04acdda5e
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-17 19:29:38 +0100

    Remove reference to envs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,11 @@
 
 define( 'EDD_PLUGIN_DIR', dirname( dirname( __FILE__ ) ) . '/' );
 
-require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+require_once '../tmp/wordpress-tests/includes/functions.php';
 
 function _install_and_load_edd() {
 	require dirname( __FILE__ ) . '/includes/loader.php';
 }
 tests_add_filter( 'muplugins_loaded', '_install_and_load_edd' );
 
-require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+require '../tmp/wordpress-tests/includes/bootstrap.php';
