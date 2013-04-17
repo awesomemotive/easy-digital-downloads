9bff23d68d4230d0457eed4647986f2720b83f91
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-17 19:33:23 +0100

    Fix paths by using the dirname function

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,11 @@
 
 define( 'EDD_PLUGIN_DIR', dirname( dirname( __FILE__ ) ) . '/' );
 
-require_once '../tmp/wordpress-tests/includes/functions.php';
+require_once dirname( __FILE__ ) . '/../tmp/wordpress-tests/includes/functions.php';
 
 function _install_and_load_edd() {
 	require dirname( __FILE__ ) . '/includes/loader.php';
 }
 tests_add_filter( 'muplugins_loaded', '_install_and_load_edd' );
 
-require '../tmp/wordpress-tests/includes/bootstrap.php';
+require dirname( __FILE__ ) . '/../tmp/wordpress-tests/includes/bootstrap.php';
