114603caf0a47664964553eb885fa6b6b097a128
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-26 20:08:22 +0100

    Disable cloning and unserialising instances of the class

--- easy-digital-downloads.php
+++ easy-digital-downloads.php
@@ -106,7 +106,7 @@ final class Easy_Digital_Downloads {
 	 * @return The one true Easy_Digital_Downloads
 	 */
 	public static function instance() {
-		if ( ! isset( self::$instance ) ) {
+		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Easy_Digital_Downloads ) ) {
 			self::$instance = new Easy_Digital_Downloads;
 			self::$instance->setup_constants();
 			self::$instance->includes();
@@ -118,6 +118,33 @@ final class Easy_Digital_Downloads {
 			self::$instance->html = new EDD_HTML_Elements();
 		}
 		return self::$instance;
+	}
+
+	/**
+	 * Throw error on object clone
+	 *
+	 * The whole idea of the singleton design pattern is that there is a single
+	 * object therefore, we don't want the object to be cloned.
+	 *
+	 * @since 1.6
+	 * @access protected
+	 * @return void
+	 */
+	public function __clone() {
+		// Cloning instances of the class is forbidden
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'edd' ), '1.6' );
+	}
+
+	/**
+	 * Disable unserializing of the class
+	 *
+	 * @since 1.6
+	 * @access protected
+	 * @return void
+	 */
+	public function __wakeup() {
+		// Unserializing instances of the class is forbidden
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'edd' ), '1.6' );
 	}
 
 	/**
