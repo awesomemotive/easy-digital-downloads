ba40348ea7cc46cd7584c6ff7441a353157040c3
Sunny Ratilal <ratilal.sunny@gmail.com>
2013-04-28 11:21:42 +0100

    Update @since

--- includes/admin/payments/view-order-details.php
+++ includes/admin/payments/view-order-details.php
@@ -6,7 +6,7 @@
  * @subpackage  Admin/Payments
  * @copyright   Copyright (c) 2013, Pippin Williamson
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
- * @since       1.0
+ * @since       1.6
 */
 
 // Exit if accessed directly
