566e9ffc9845efe3c42df71112063db52d1a999b
Pippin Williamson <pippin@pippinsplugins.com>
2013-04-30 23:53:45 -0500

    Move Payment #xx into metabox title. #1138

--- includes/admin/payments/view-order-details.php
+++ includes/admin/payments/view-order-details.php
@@ -24,17 +24,18 @@ function edd_view_order_details_screen() {
 	}
 
 	// Setup the variables
-	$item = get_post( $_GET['id'] );
-	$payment_meta = edd_get_payment_meta( $_GET['id'] );
+	$payment_id = absint( $_GET['id'] );
+	$item = get_post( $payment_id );
+	$payment_meta = edd_get_payment_meta( $payment_id );
 	$cart_items   = isset( $payment_meta['cart_details'] ) ? maybe_unserialize( $payment_meta['cart_details'] ) : false;
 	if ( empty( $cart_items ) || ! $cart_items ) {
 		$cart_items = maybe_unserialize( $payment_meta['downloads'] );
 	}
-	$user_info = edd_get_payment_meta_user_info( $_GET['id'] );
+	$user_info = edd_get_payment_meta_user_info( $payment_id );
 	$payment_date = strtotime( $item->post_date );
 	?>
 	<div class="wrap">
-		<h2><?php _e( 'View Order Details', 'edd' ); ?> <a class="add-new-h2" href="<?php echo admin_url( 'edit.php?post_type=download&amp;page=edd-payment-history&amp;edd-action=edit-payment&amp;purchase_id=' . $_GET['id'] ); ?>"><?php _e( 'Edit Order', 'edd' ); ?></a></h2>
+		<h2><?php _e( 'View Order Details', 'edd' ); ?> <a class="add-new-h2" href="<?php echo admin_url( 'edit.php?post_type=download&amp;page=edd-payment-history&amp;edd-action=edit-payment&amp;purchase_id=' . $payment_id ); ?>"><?php _e( 'Edit Order', 'edd' ); ?></a></h2>
 		<?php do_action( 'edd_view_order_details_before' ); ?>
 		<div id="poststuff">
 			<div id="post-body" class="metabox-holder columns-2">
@@ -52,11 +53,11 @@ function edd_view_order_details_screen() {
 									$taxes = edd_use_taxes();
 									if ( $taxes ) : ?>
 									<div class="edd-order-taxes edd-admin-box-inside">
-										<p><span class="label"><?php _e( 'Tax', 'edd' ); ?></span> <span class="right"><?php echo edd_currency_filter( edd_format_amount( edd_get_payment_tax( $_GET['id'] ) ) ); ?></span></p>
+										<p><span class="label"><?php _e( 'Tax', 'edd' ); ?></span> <span class="right"><?php echo edd_currency_filter( edd_format_amount( edd_get_payment_tax( $payment_id ) ) ); ?></span></p>
 									</div>
 									<?php endif; ?>
 									<?php
-									$fees = edd_get_payment_fees( $_GET['id'] );
+									$fees = edd_get_payment_fees( $payment_id );
 									if ( ! empty( $fees ) ) : ?>
 									<div class="edd-order-fees edd-admin-box-inside">
 										<p class="strong"><?php _e( 'Fees', 'edd' ); ?></p>
@@ -68,7 +69,7 @@ function edd_view_order_details_screen() {
 									</div>
 									<?php endif; ?>
 									<div class="edd-order-payment edd-admin-box-inside">
-										<p><span class="label"><?php _e( 'Total Price', 'edd' ); ?></span> <span class="right"><?php echo edd_currency_filter( edd_format_amount( edd_get_payment_amount( $_GET['id'] ) ) ); ?></span></p>
+										<p><span class="label"><?php _e( 'Total Price', 'edd' ); ?></span> <span class="right"><?php echo edd_currency_filter( edd_format_amount( edd_get_payment_amount( $payment_id ) ) ); ?></span></p>
 									</div>
 								</div><!-- /.edd-order-totals-box -->
 							</div><!-- /.inside -->
@@ -78,7 +79,7 @@ function edd_view_order_details_screen() {
 							<h3 class="hndle"><span>Payment Notes</span></h3>
 							<div class="inside">
 								<?php
-								$notes = edd_get_payment_notes( $_GET['id'] );
+								$notes = edd_get_payment_notes( $payment_id );
 								if ( ! empty( $notes ) ) :
 									foreach ( $notes as $note ) :
 										if ( ! empty( $note->user_id ) ) {
@@ -108,13 +109,12 @@ function edd_view_order_details_screen() {
 					<div id="normal-sortables" class="meta-box-sortables ui-sortable">
 						<?php do_action( 'edd_view_order_details_main_before' ); ?>
 						<div id="edd-order-data" class="postbox">
-							<h3 class="hndle"><?php _e( 'Order Details', 'edd' ); ?></h3>
+							<h3 class="hndle"><?php printf( __( 'Order Details - Payment #%d', 'edd' ), $payment_id ); ?></h3>
 							<div class="inside">
-								<p class="order-id"><?php _e( 'Payment', 'edd' ); ?> <?php echo '#' . $_GET['id']; ?></p>
 								<div class="column-container">
 									<div class="order-data-column">
 										<h4><?php _e( 'General Details' ); ?></h4>
-										<p class="data"><span><?php _e( 'Status:', 'edd' ); ?></span> <?php echo edd_get_payment_status( get_post( $_GET['id'] ), true ) ?></p>
+										<p class="data"><span><?php _e( 'Status:', 'edd' ); ?></span> <?php echo edd_get_payment_status( get_post( $payment_id ), true ) ?></p>
 										<p class="data"><span><?php _e( 'Date:', 'edd' ); ?></span> <?php echo date_i18n( get_option( 'date_format' ), $payment_date ) ?></p>
 										<p class="data"><span><?php _e( 'Time:', 'edd' ); ?></span> <?php echo date_i18n( get_option( 'time_format' ), $payment_date ); ?></p>
 									</div>
@@ -128,7 +128,7 @@ function edd_view_order_details_screen() {
 									<div class="order-data-column">
 										<h4><?php _e( 'Payment Details', 'edd' ); ?></h4>
 										<?php 
-										$gateway = edd_get_payment_gateway( $_GET['id'] );
+										$gateway = edd_get_payment_gateway( $payment_id );
 										if ( $gateway ) {
 										?>
 										<p class="data"><span><?php _e( 'Gateway:', 'edd' ); ?></span> <?php echo edd_get_gateway_admin_label( $gateway ); ?></p>
@@ -161,7 +161,7 @@ function edd_view_order_details_screen() {
 															$price_options = $cart_items[ $key ]['item_number']['options'];
 
 															if ( isset( $price_options['price_id'] ) ) {
-																echo ' - ' . edd_get_price_option_name( $id, $price_options['price_id'], $_GET['id'] );
+																echo ' - ' . edd_get_price_option_name( $id, $price_options['price_id'], $payment_id );
 															}
 														}
 														?>
