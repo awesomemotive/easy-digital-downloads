c36a505d0776a3c3fe39855f7d6d93437116eb76
itsmattadams <itismattadams@gmail.com>
2013-06-04 20:15:11 -0500

    Fix empty [download_discounts] invalid argument warning.

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -153,21 +153,21 @@ function edd_discounts_shortcode( $atts, $content = null ) {
 
 	$discounts_list = '<ul id="edd_discounts_list">';
 
-	/**
-	 * Move this outside of the foreach() construct, otherwise an invalid
-	 * argument supplied warning is thrown when there are no discounts.
-	 */
 	if ( edd_has_active_discounts( $discount->ID ) ) {
 
 		foreach ( $discounts as $discount ) {
 
-			$discounts_list .= '<li class="edd_discount">';
+			if ( edd_is_discount_active( $discount->ID ) ) {
 
-				$discounts_list .= '<span class="edd_discount_name">' . edd_get_discount_code( $discount->ID ) . '</span>';
-				$discounts_list .= '<span class="edd_discount_separator"> - </span>';
-				$discounts_list .= '<span class="edd_discount_amount">' . edd_format_discount_rate( edd_get_discount_type( $discount->ID ), edd_get_discount_amount( $discount->ID ) ) . '</span>';
+				$discounts_list .= '<li class="edd_discount">';
 
-			$discounts_list .= '</li>';
+					$discounts_list .= '<span class="edd_discount_name">' . edd_get_discount_code( $discount->ID ) . '</span>';
+					$discounts_list .= '<span class="edd_discount_separator"> - </span>';
+					$discounts_list .= '<span class="edd_discount_amount">' . edd_format_discount_rate( edd_get_discount_type( $discount->ID ), edd_get_discount_amount( $discount->ID ) ) . '</span>';
+
+				$discounts_list .= '</li>';
+
+			}
 
 		}
 
