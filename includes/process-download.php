5a4651d066581f52ca9a79973d43abf5e0665107
Pippin Williamson <pippin@pippinsplugins.com>
2013-06-06 18:43:57 -0500

    Restore headers for #1245

--- includes/process-download.php
+++ includes/process-download.php
@@ -74,14 +74,14 @@ function edd_process_download() {
 		@session_write_close();
 		if( function_exists( 'apache_setenv' ) ) @apache_setenv('no-gzip', 1);
 		@ini_set( 'zlib.output_compression', 'Off' );
-		/*
+
 		nocache_headers();
 		header("Robots: none");
 		header("Content-Type: " . $ctype . "");
 		header("Content-Description: File Transfer");
 		header("Content-Disposition: attachment; filename=\"" . apply_filters( 'edd_requested_file_name', basename( $requested_file ) ) . "\";");
 		header("Content-Transfer-Encoding: binary");
-		*/
+
 		$method = edd_get_file_download_method();
 		if( 'x_sendfile' == $method && ( ! function_exists( 'apache_get_modules' ) || ! in_array( 'mod_xsendfile', apache_get_modules() ) ) ) {
 			// If X-Sendfile is selected but is not supported, fallback to Direct
