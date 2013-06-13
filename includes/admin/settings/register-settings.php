4a9e6dd6f929a9724cb1a41efe400e229d77f942
Chris Christoff <chriscct7@gmail.com>
2013-06-13 10:31:49 -0300

    Update register-settings.php

--- includes/admin/settings/register-settings.php
+++ includes/admin/settings/register-settings.php
@@ -1094,7 +1094,7 @@ function edd_tax_rates_callback($args) {
 					<input type="checkbox" name="tax_rates[<?php echo $key; ?>][global]" id="tax_rates[<?php echo $key; ?>][global]" value="1"<?php checked( true, ! empty( $rate['global'] ) ); ?>/>
 					<label for="tax_rates[<?php echo $key; ?>][global]"><?php _e( 'Apply to whole country', 'edd' ); ?></a>
 				</td>
-				<td><input type="number" class="small-text" step="0.1" min="0.0" max="99" name="tax_rates[<?php echo $key; ?>][rate]" value="<?php echo $rate['rate']; ?>"/></td>
+				<td><input type="number" class="small-text" step="0.01" min="0.0" max="99" name="tax_rates[<?php echo $key; ?>][rate]" value="<?php echo $rate['rate']; ?>"/></td>
 				<td><span class="edd_remove_tax_rate button-secondary"><?php _e( 'Remove Rate', 'edd' ); ?></span></td>
 			</tr>
 			<?php endforeach; ?>
