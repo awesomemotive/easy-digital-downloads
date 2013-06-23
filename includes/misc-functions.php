2bbd681f61952c869c8bc0c69920f3021d4517bd
Pippin Williamson <pippin@pippinsplugins.com>
2013-06-23 14:41:45 -0500

    Clean up indention for edd_get_ip()

--- includes/misc-functions.php
+++ includes/misc-functions.php
@@ -193,15 +193,16 @@ function edd_string_is_image_url( $str ) {
 function edd_get_ip() {
 	if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
 		//check ip from share internet
-	  $ip = $_SERVER['HTTP_CLIENT_IP'];
+		$ip = $_SERVER['HTTP_CLIENT_IP'];
 	} elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
 		//to check ip is pass from proxy
-	  $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 	} else {
-	  $ip = $_SERVER['REMOTE_ADDR'];
+		$ip = $_SERVER['REMOTE_ADDR'];
 	}
 	return apply_filters( 'edd_get_ip', $ip );
 }
+
 
 /**
  * Get Currencies
