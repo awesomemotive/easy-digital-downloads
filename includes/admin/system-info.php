2824537441b1d8597c7a867848846f406f0be029
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-06-29 18:36:45 -0500

    Add new line

--- includes/admin/system-info.php
+++ includes/admin/system-info.php
@@ -51,7 +51,7 @@ HOME_URL:                 <?php echo home_url() . "\n"; ?>
 EDD Version:              <?php echo EDD_VERSION . "\n"; ?>
 Upgraded From:            <?php echo get_option( 'edd_version_upgraded_from', 'None' ) . "\n"; ?>
 WordPress Version:        <?php echo get_bloginfo( 'version' ) . "\n"; ?>
-Permalink Structure:      <?php echo get_option( 'permalink_structure' ); ?>
+Permalink Structure:      <?php echo get_option( 'permalink_structure' ) . "\n"; ?>
 
 Test Mode Enabled:        <?php echo edd_is_test_mode() ? "Yes\n" : "No\n"; ?>
 Ajax Enabled:             <?php echo edd_is_ajax_enabled() ? "Yes\n" : "No\n"; ?>
