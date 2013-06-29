c76944bb5e4e7483c1985c22b36b9815c732d1d8
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-06-29 12:27:54 -0500

    Bump up the padding. See #1327.

--- includes/emails/template.php
+++ includes/emails/template.php
@@ -368,8 +368,8 @@ add_filter( 'edd_purchase_receipt', 'edd_apply_email_template', 20, 3 );
  * @since 1.0.8.2
  */
 function edd_default_email_template() {
-	echo '<div style="width: 550px; border: 1px solid #ccc; background: #f0f0f0; padding: 8px 10px; margin: 0 auto;">';
-	echo '<div id="edd-email-content" style="background: #fff; border: 1px solid #ccc; padding: 10px;">';
+	echo '<div style="width: 550px; border: 1px solid #ccc; background: #f0f0f0; padding: 12px; margin: 0 auto;">';
+	echo '<div id="edd-email-content" style="background: #fff; border: 1px solid #ccc; padding: 15px;">';
 	echo '{email}'; // This tag is required in order for the contents of the email to be shown
 	echo '</div>';
 	echo '</div>';
