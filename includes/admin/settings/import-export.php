5bc71c574e66764613b973de55834f7991df8668
Pippin Williamson <pippin@pippinsplugins.com>
2013-06-30 16:26:14 -0500

    Finalize import / export of settings. #1133

--- includes/admin/settings/import-export.php
+++ includes/admin/settings/import-export.php
@@ -41,13 +41,13 @@ function edd_settings_export_import() {
 				<h3><span><?php _e( 'Import Settings', 'edd' ); ?></span></h3>
 				<div class="inside">
 					<p><?php _e( 'Import the Easy Digital Downloads settings from .json file. This file can be obtained by exporting the settings on another user using the form above.', 'edd' ); ?></p>
-					<form method="post" action="<?php echo admin_url( 'tools.php?page=edd-settings-export-import' ); ?>">
+					<form method="post" enctype="multipart/form-data" action="<?php echo admin_url( 'tools.php?page=edd-settings-export-import' ); ?>">
 						<p>
 							<input type="file" name="import_file"/>
 						</p>
 						<p>
 							<input type="hidden" name="edd_action" value="import_settings" />
-							<?php wp_nonce_field( 'edd_settings_export_nonce', 'edd_settings_export_nonce' ); ?>
+							<?php wp_nonce_field( 'edd_settings_import_nonce', 'edd_settings_import_nonce' ); ?>
 							<?php submit_button( __( 'Import', 'edd' ), 'secondary', 'submit', false ); ?>
 						</p>
 					</form>
@@ -58,3 +58,81 @@ function edd_settings_export_import() {
 	<?php
 
 }
+
+
+/**
+ * Process a settings export that generates a .json file of the shop settings
+ *
+ * @since 1.7
+ * @return void
+ */
+function edd_process_settings_export() {
+
+	if( empty( $_POST['edd_settings_export_nonce'] ) )
+		return;
+
+	if( ! wp_verify_nonce( $_POST['edd_settings_export_nonce'], 'edd_settings_export_nonce' ) )
+		return;
+
+	if( ! current_user_can( 'manage_shop_settings' ) )
+		return;
+
+	$settings = array();
+	$settings['general']    = get_option( 'edd_settings_general' );
+	$settings['gateways']   = get_option( 'edd_settings_gateways' );
+	$settings['emails']     = get_option( 'edd_settings_emails' );
+	$settings['styles']     = get_option( 'edd_settings_styles' );
+	$settings['taxes']      = get_option( 'edd_settings_taxes' );
+	$settings['extensions'] = get_option( 'edd_settings_extensions' );
+	$settings['misc']       = get_option( 'edd_settings_misc' );
+
+	ignore_user_abort( true );
+
+	if ( ! edd_is_func_disabled( 'set_time_limit' ) && ! ini_get( 'safe_mode' ) )
+		set_time_limit( 0 );
+
+	nocache_headers();
+	header( 'Content-Type: application/json; charset=utf-8' );
+	header( 'Content-Disposition: attachment; filename=edd-settings-export-' . date( 'm-d-Y' ) . '.json' );
+	header( "Expires: 0" );
+
+	echo json_encode( $settings );
+	exit;
+
+}
+add_action( 'edd_export_settings', 'edd_process_settings_export' );
+
+/**
+ * Process a settings import from a json file
+ *
+ * @since 1.7
+ * @return void
+ */
+function edd_process_settings_import() {
+
+	if( empty( $_POST['edd_settings_import_nonce'] ) )
+		return;
+
+	if( ! wp_verify_nonce( $_POST['edd_settings_import_nonce'], 'edd_settings_import_nonce' ) )
+		return;
+
+	if( ! current_user_can( 'manage_shop_settings' ) )
+		return;
+
+	$import_file = $_FILES['import_file']['tmp_name'];
+
+	// Retrieve the settings from the file and convert the json object to an array
+	$settings = edd_object_to_array( json_decode( file_get_contents( $import_file ) ) );
+
+	update_option( 'edd_settings_general'   , $settings['general']    );
+	update_option( 'edd_settings_gateways'  , $settings['gateways']   );
+	update_option( 'edd_settings_emails'    , $settings['emails']     );
+	update_option( 'edd_settings_styles'    , $settings['styles']     );
+	update_option( 'edd_settings_taxes'     , $settings['taxes']      );
+	update_option( 'edd_settings_extensions', $settings['extensions'] );
+	update_option( 'edd_settings_misc'      , $settings['misc']       );
+
+	wp_safe_redirect( admin_url( 'tools.php?page=edd-settings-export-import&edd-message=settings-imported' ) ); exit;
+
+}
+add_action( 'edd_import_settings', 'edd_process_settings_import' );
