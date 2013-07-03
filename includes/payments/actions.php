00ea178c6160c6761bb0a2063e2214b7e2f1aaba
Pippin Williamson <pippin@pippinsplugins.com>
2013-07-03 11:15:50 -0500

    Fix #1337

--- includes/payments/actions.php
+++ includes/payments/actions.php
@@ -155,6 +155,7 @@ function edd_update_edited_purchase( $data ) {
 		$user_info['email']        = strip_tags( $_POST['edd-buyer-email'] );
 		$user_info['user_id']      = strip_tags( intval( $_POST['edd-buyer-user-id'] ) );
 		$payment_data['user_info'] = serialize( $user_info );
+		$payment_data['email']     = strip_tags( $_POST['edd-buyer-email'] );
 
 		update_post_meta( $payment_id, '_edd_payment_meta', $payment_data );
 		update_post_meta( $payment_id, '_edd_payment_user_email', strip_tags( $_POST['edd-buyer-email'] ) );
