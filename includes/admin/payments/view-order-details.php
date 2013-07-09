6634a9641fb95d55dd4cfa1368df70e1c7f7c525
Pippin Williamson <pippin@pippinsplugins.com>
2013-07-09 11:49:55 -0500

    Use edd_get_payment_user_id(). #1350

--- includes/admin/payments/view-order-details.php
+++ includes/admin/payments/view-order-details.php
@@ -31,6 +31,7 @@ if ( empty( $cart_items ) || ! $cart_items ) {
 	$cart_items = maybe_unserialize( $payment_meta['downloads'] );
 }
 $user_info    = edd_get_payment_meta_user_info( $payment_id );
+$user_id      = edd_get_payment_user_id( $payment_id );
 $payment_date = strtotime( $item->post_date );
 ?>
 <div class="wrap">
@@ -145,9 +146,9 @@ $payment_date = strtotime( $item->post_date );
 								<div class="order-data-column">
 									<h4><?php _e( 'Buyer\'s Personal Details', 'edd' ); ?></h4>
 									<p class="data"><span><?php _e( 'Name:', 'edd' ); ?></span> <?php echo $user_info['first_name'] . ' ' . $user_info['last_name']; ?></p>
-									<p class="data"><span><?php _e( 'User Status:', 'edd' ); ?></span> <?php echo $user_info['id'] > 0 ? __( 'Registered User', 'edd' ) : __( 'Guest', 'edd' ); ?></p>
-									<?php if( $user_info['id'] > 0 ) : ?>
-										<p class="data"><span><?php _e( 'User ID:', 'edd' ); ?></span> <?php echo $user_info['id']; ?></p>
+									<p class="data"><span><?php _e( 'User Status:', 'edd' ); ?></span> <?php echo $user_id > 0 ? __( 'Registered User', 'edd' ) : __( 'Guest', 'edd' ); ?></p>
+									<?php if( $user_id > 0 ) : ?>
+										<p class="data"><span><?php _e( 'User ID:', 'edd' ); ?></span> <?php echo $user_id; ?></p>
 									<?php endif; ?>
 									<p class="data"><span><?php _e( 'Email:', 'edd' ); ?></span> <a href="mailto:<?php echo $payment_meta['email']; ?>"><?php echo $payment_meta['email']; ?></a></p>
 									<ul><?php do_action( 'edd_payment_personal_details_list', $payment_meta, $user_info ); ?></ul>
