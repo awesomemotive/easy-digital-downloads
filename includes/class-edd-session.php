e06b04ddd6992cc1c7c2719e05a7dd57e70f8626
Pippin Williamson <pippin@pippinsplugins.com>
2013-07-18 11:48:31 -0500

    Fix Last Year report dates error

--- includes/class-edd-session.php
+++ includes/class-edd-session.php
@@ -62,7 +62,7 @@ class EDD_Session {
 			// Use PHP SESSION (must be enabled via the EDD_USE_PHP_SESSIONS constant)
 
 			if( ! session_id() )
-				add_action( 'init', 'session_start', -1 );
+				add_action( 'init', 'session_start', -2 );
 
 		} else {
 
