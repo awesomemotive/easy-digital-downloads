4fe1a60f3d55793a83815f6a5a911a90d2384203
Chris Christoff <chriscct7@gmail.com>
2013-07-20 12:10:21 -0400

    Update api.php

--- tests/unit-tests/api.php
+++ tests/unit-tests/api.php
@@ -317,14 +317,14 @@ class Tests_API extends EDD_UnitTestCase {
 		$this->assertEquals( 100.0, $out['customers'][0]['stats']['total_spent'] );
 		$this->assertEquals( 0, $out['customers'][0]['stats']['total_downloads'] );
 	}
-/*
+
 	public function test_missing_auth() {
 		EDD()->api->missing_auth();
 		$out = EDD()->api->get_output();
 		$this->assertArrayHasKey( 'error', $out );
 		$this->assertEquals( 'You must specify both a token and API key!', $out['error'] );
 	}
-
+/*
 	public function test_invalid_auth() {
 		EDD()->api->invalid_auth();
 		$out = EDD()->api->get_output();
