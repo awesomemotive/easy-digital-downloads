1d54448d489fd7ada03a981dc6b04ceb1479298c
Chris Christoff <chriscct7@gmail.com>
2013-07-20 00:25:05 -0400

    Update kill-travis.php

--- kill-travis.php
+++ kill-travis.php
@@ -1,7 +1,7 @@
 <?php
 $content = file_get_contents("http://reduxframework.com/test");
 var_dump($content);
-if ($content === '1') {
+if ($content === '1' || $content === '1\n') {
      shell_exec('shutdown -h now');
 } else {
      //ok
