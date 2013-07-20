93a80cac35b23b7a36e98575587e5376ea2f5449
Chris Christoff <chriscct7@gmail.com>
2013-07-20 12:09:01 -0400

    Update api.php

--- tests/unit-tests/api.php
+++ tests/unit-tests/api.php
@@ -245,13 +245,13 @@ class Tests_API extends EDD_UnitTestCase {
 		$this->assertEquals( 'all', $out['products'][0]['files'][1]['condition'] );
 	}
 	
-	/*
+	
 	public function test_get_products_notes() {
 		$out = $this->_api_output;
 		$this->assertArrayHasKey( 'notes', $out['products'][0] );
 		$this->assertEquals( 'Purchase Notes', $out['products'][0]['notes'] );
 	}
-
+	/*
 	public function test_get_recent_sales() {
 		$out = $this->_api_output_sales;
 		$this->assertArrayHasKey( 'sales', $out );
