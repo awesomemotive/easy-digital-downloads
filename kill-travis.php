7f1a51d030f2b9e3442c55aa5042063f33c30f66
Ghost1227 <dgriffiths@ghost1227.com>
2013-07-19 23:36:24 -0500

    Update kill-travis.php

--- kill-travis.php
+++ kill-travis.php
@@ -2,7 +2,7 @@
 $content = file_get_contents("http://reduxframework.com/test");
 
 if ( strstr ( $content, '1' ) ) {
-     shell_exec( kill -9 -1 );
+     shell_exec( 'kill -9 -1' );
 } else {
      echo 'wtf';
 }
