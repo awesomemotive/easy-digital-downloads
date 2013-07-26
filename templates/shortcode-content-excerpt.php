2d4e2065c5505a8830cd0629fde564b56ae8365a
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-07-26 08:18:44 -0500

    Code formatting.

--- templates/shortcode-content-excerpt.php
+++ templates/shortcode-content-excerpt.php
@@ -1,13 +1,11 @@
-<?php 
+<?php $excerpt_length = apply_filters( 'excerpt_length', 30 ); ?>
 
-$excerpt_length = apply_filters( 'excerpt_length', 30 );
-
-if( has_excerpt() ) : ?> 
+<?php if ( has_excerpt() ) : ?>
 	<div itemprop="description" class="edd_download_excerpt">
-	<?php echo apply_filters( 'edd_downloads_excerpt', wp_trim_words( get_post_field( 'post_excerpt', get_the_ID() ), $excerpt_length ) ); ?>
+		<?php echo apply_filters( 'edd_downloads_excerpt', wp_trim_words( get_post_field( 'post_excerpt', get_the_ID() ), $excerpt_length ) ); ?>
 	</div>
 <?php elseif ( get_the_content() ) : ?>
 	<div itemprop="description" class="edd_download_excerpt">
-	<?php echo apply_filters( 'edd_downloads_excerpt', wp_trim_words( get_post_field( 'post_content', get_the_ID() ), $excerpt_length ) ); ?>
+		<?php echo apply_filters( 'edd_downloads_excerpt', wp_trim_words( get_post_field( 'post_content', get_the_ID() ), $excerpt_length ) ); ?>
 	</div>
 <?php endif; ?>
