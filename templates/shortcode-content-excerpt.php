1f0e72f3e9e9659149d8e14052386b913a2d91fe
Sumobi <andrew@sumobi.com>
2013-07-26 17:09:31 +1200

    changed class and apply filter name

--- templates/shortcode-content-excerpt.php
+++ templates/shortcode-content-excerpt.php
@@ -7,7 +7,7 @@ if( has_excerpt() ) : ?>
 	<?php echo apply_filters( 'edd_downloads_excerpt', wp_trim_words( get_post_field( 'post_excerpt', get_the_ID() ), $excerpt_length ) ); ?>
 	</div>
 <?php elseif ( get_the_content() ) : ?>
-	<div itemprop="description" class="edd_download_content">
-	<?php echo apply_filters( 'edd_downloads_content', wp_trim_words( get_post_field( 'post_content', get_the_ID() ), $excerpt_length ) ); ?>
+	<div itemprop="description" class="edd_download_excerpt">
+	<?php echo apply_filters( 'edd_downloads_excerpt', wp_trim_words( get_post_field( 'post_content', get_the_ID() ), $excerpt_length ) ); ?>
 	</div>
 <?php endif; ?>
