dcebf41038ec6e38077862c0003d4a499c927cb3
Michael Cannon <mc@aihr.us>
2013-07-29 19:17:36 +0700

    Plugin links for #1404 #1406

---
+++ includes/admin/plugins.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Admin Plugins
+ *
+ * @package     EDD
+ * @subpackage  Admin/Plugins
+ * @copyright   Copyright (c) 2013, Pippin Williamson
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.8
+ */
+
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+
+/**
+ * Plugins row action links
+ *
+ * @author Michael Cannon <mc@aihr.us>
+ * @since 1.8
+ * @return void
+ */
+function edd_plugin_action_links( $links, $file ) {
+	$settings_link = '<a href="' . admin_url( 'edit.php?post_type=download&page=edd-settings' ) . '">' . esc_html__( 'General Settings', 'edd' ) . '</a>';
+	if ( $file == 'easy-digital-downloads/easy-digital-downloads.php' )
+		array_unshift( $links, $settings_link );
+
+	return $links;
+}
+
+
+add_filter( 'plugin_action_links', 'edd_plugin_action_links', 10, 2 );
+
+
+/**
+ * Plugin row meta links
+ *
+ * @author Michael Cannon <mc@aihr.us>
+ * @since 1.8
+ * @return void
+ */
+function edd_plugin_row_meta( $input, $file ) {
+	if ( $file != 'easy-digital-downloads/easy-digital-downloads.php' )
+		return $input;
+
+	$links = array(
+		'<a href="https://easydigitaldownloads.com/extensions/">' . esc_html__( 'Buy Extensions', 'edd' ) . '</a>',
+	);
+
+	$input = array_merge( $input, $links );
+
+	return $input;
+}
+
+
+add_filter( 'plugin_row_meta', 'edd_plugin_row_meta', 10, 2 );
+
+?>
