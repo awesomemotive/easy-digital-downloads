a35cde4264f88b4c79bfc5e7142deb300f57d6e2
Pippin Williamson <pippin@pippinsplugins.com>
2013-08-09 11:25:09 -0500

    First pass at complete EDD_Payments_Query class. #1446

--- includes/payments/class-payments-query.php
+++ includes/payments/class-payments-query.php
@@ -23,26 +23,43 @@ class EDD_Payments_Query extends EDD_Stats_Base {
 
 
 	/**
+	 * The args to pass to the edd_get_payments() query
+	 *
+	 * @var array
+	 * @access public
+	 * @since 1.8
+	 */
+	public $args = array();
+
+
+	/**
 	 *
 	 * @access public
 	 * @since 1.8
 	 * @return void
 	 */
-	public function __construct() { /* nothing here. Call get_sales() and get_earnings() directly */ }
+	public function __construct( $_args = array() ) {
+
+		$this->args = $_args;
+
+	}
 
 
 	/**
-	 * Retrieve sale stats
+	 * Retrieve payments
 	 *
 	 * @access public
 	 * @since 1.8
-	 * @param $download_id INT The download product to retrieve stats for. If false, gets stats for all products
+	 * @param $start_date string|int The starting date to get payments for
+	 * @param $start_date string|int The ending date to get payments for
 	 * @param $status string The sale status to count. Only valid when retrieving global stats
 	 * @return float|int
 	 */
-	public function get_sales( $download_id = 0, $start_date = false, $end_date = false, $status = 'publish' ) {
+	public function get_payments( $start_date = false, $end_date = false ) {
 
-		$this->setup_dates( $start_date, $end_date );
+		if( $start_date || $end_date ) {
+			$this->setup_dates( $start_date, $end_date );
+		}
 
 		// Make sure start date is valid
 		if( is_wp_error( $this->start_date ) )
@@ -52,15 +69,40 @@ class EDD_Payments_Query extends EDD_Stats_Base {
 		if( is_wp_error( $this->end_date ) )
 			return $this->end_date;
 
+		// Only filter the posts_where if a start or end date is specified
+		if( $start_date || $end_date ) {
+			add_filter( 'posts_where', array( $this, 'payments_where' ) );
+		}
 
-		add_filter( 'posts_where', array( $this, 'payments_where' ) );
+		$payments = array();
+		$query    = edd_get_payments( $this->args );
 
+		if( $query ) {
+			foreach( $query as $payment ) {
+				$details = new stdClass;
 
+				$details->ID           = $payment->ID;
+				$details->date         = $payment->post_date;
+				$details->status       = edd_get_payment_status( $payment );
+				$details->total        = edd_get_payment_amount( $payment->ID );
+				$details->subtotal     = edd_get_payment_subtotal( $payment->ID );
+				$details->tax          = edd_get_payment_tax( $payment->ID );
+				$details->fees         = edd_get_payment_fees( $payment->ID );
+				$details->key          = edd_get_payment_key( $payment->ID );
+				$details->gateway      = edd_get_payment_gateway( $payment->ID );
+				$details->user_info    = edd_get_payment_meta_user_info( $payment->ID );
+				$details->cart_details = edd_get_payment_meta_cart_details( $payment->ID, true );
 
-		remove_filter( 'posts_where', array( $this, 'payments_where' ) );
+				$payments[] = $details;
+			}
+		}
 
+		// Remove the posts_where filter, if it was added
+		if( $start_date || $end_date ) {
+			remove_filter( 'posts_where', array( $this, 'payments_where' ) );
+		}
 
-		return $count;
+		return $payments;
 
 	}
 
