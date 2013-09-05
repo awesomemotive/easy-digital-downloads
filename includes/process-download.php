60a27dfd7cc62298704d788a8b70dcdd5642d71e
Pippin Williamson <pippin@pippinsplugins.com>
2013-09-05 17:32:31 -0500

    Remove flush() and ob_flush(). Closes #1515

--- includes/process-download.php
+++ includes/process-download.php
@@ -547,8 +547,8 @@ function edd_readfile_chunked( $file, $retbytes = TRUE ) {
 	while ( ! feof( $handle ) ) :
 	   $buffer = fread( $handle, $chunksize );
 	   echo $buffer;
-	   ob_flush();
-	   flush();
+	   //ob_flush();
+	   //flush();
 
 	   if ( $retbytes ) $cnt += strlen( $buffer );
 	endwhile;
