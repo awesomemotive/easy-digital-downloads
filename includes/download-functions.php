82890674425912936a09f25dd2d8f88bee4ad4fc
Pippin Williamson <pippin@pippinsplugins.com>
2013-09-10 11:42:11 -0500

    Add index.php to download URLs. #1527

--- includes/download-functions.php
+++ includes/download-functions.php
@@ -796,7 +796,7 @@ function edd_get_download_file_url( $key, $email, $filekey, $download_id, $price
 
 	$params = apply_filters( 'edd_download_file_url_args', $params );
 
-	$download_url = add_query_arg( $params, home_url() );
+	$download_url = add_query_arg( $params, home_url( 'index.php' ) );
 
 	return $download_url;
 }
