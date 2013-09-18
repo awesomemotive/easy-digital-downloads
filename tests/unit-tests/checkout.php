0759034e6493d489e03561921eb848ff8f03f2ef
Pippin Williamson <pippin@pippinsplugins.com>
2013-09-17 23:33:00 -0500

    Add test_checkout_actions() test. #600

--- tests/unit-tests/checkout.php
+++ tests/unit-tests/checkout.php
@@ -39,6 +39,26 @@ class Tests_Checkout extends EDD_UnitTestCase {
 		edd_add_to_cart( $this->_post->ID, $options );
 	}
 
+	public function test_checkout_actions() {
+		global $wp_filter;
+		$this->markTestIncomplete( 'This test kills PHP unit for some reason.' );
+		$this->assertarrayHasKey( 'edd_show_purchase_form',        $wp_filter['edd_purchase_form'][10]                 );
+		$this->assertarrayHasKey( 'edd_user_info_fields',          $wp_filter['edd_purchase_form_after_user_info'][10] );
+		$this->assertarrayHasKey( 'edd_get_cc_form',               $wp_filter['edd_cc_form'][10]                       );
+		$this->assertarrayHasKey( 'edd_default_cc_address_fields', $wp_filter['edd_after_cc_fields'][10]               );
+		$this->assertarrayHasKey( 'edd_checkout_tax_fields',       $wp_filter['edd_purchase_form_after_cc_form'][999]  );
+		$this->assertarrayHasKey( 'edd_get_register_fields',       $wp_filter['edd_purchase_form_register_fields'][10] );
+		$this->assertarrayHasKey( 'edd_get_login_fields',          $wp_filter['edd_purchase_form_login_fields'][10]    );
+		$this->assertarrayHasKey( 'edd_payment_mode_select',       $wp_filter['edd_payment_mode_select'][10]           );
+		$this->assertarrayHasKey( 'edd_show_payment_icons',        $wp_filter['edd_payment_mode_top'][10]              );
+		$this->assertarrayHasKey( 'edd_show_payment_icons',        $wp_filter['edd_checkout_form_top'][10]             );
+		$this->assertarrayHasKey( 'edd_discount_field',            $wp_filter['edd_checkout_form_top'][-1]             );
+		$this->assertarrayHasKey( 'edd_terms_agreement',           $wp_filter['edd_purchase_form_before_submit'][10]   );
+		$this->assertarrayHasKey( 'edd_checkout_final_total',      $wp_filter['edd_purchase_form_before_submit'][999]  );
+		$this->assertarrayHasKey( 'edd_checkout_submit',           $wp_filter['edd_purchase_form_after_cc_form'][9999] );
+		$this->assertarrayHasKey( 'edd_agree_to_terms_js',         $wp_filter['edd_checkout_form_top'][9999]           );
+	}
+
 	public function test_can_checkout() {
 		$this->assertTrue( edd_can_checkout() );
 	}
