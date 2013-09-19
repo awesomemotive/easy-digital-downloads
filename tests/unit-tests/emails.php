df6bd4e41a181eea6c9ff82455971f697a2e6f9c
Pippin Williamson <pippin@pippinsplugins.com>
2013-09-19 11:09:26 -0500

    Add new test for email actions. #600

--- tests/unit-tests/emails.php
+++ tests/unit-tests/emails.php
@@ -13,6 +13,17 @@ class Tests_Emails extends EDD_UnitTestCase {
 		parent::tearDown();
 	}
 
+	/**
+     * Test that each of the actions are added and each hooked in with the right priority
+     */
+	public function test_email_actions() {
+		global $wp_filter;
+		$this->assertarrayHasKey( 'edd_admin_email_notice',       $wp_filter['edd_admin_sale_notice'][10]  );
+		$this->assertarrayHasKey( 'edd_trigger_purchase_receipt', $wp_filter['edd_complete_purchase'][999] );
+		$this->assertarrayHasKey( 'edd_resend_purchase_receipt',  $wp_filter['edd_email_links'][10]        );
+		$this->assertarrayHasKey( 'edd_send_test_email',          $wp_filter['edd_send_test_email'][10]    );
+	}
+
 	public function test_admin_notice_emails() {
 		$expected = array( 'admin@example.org' );
 		$this->assertEquals( $expected, edd_get_admin_notice_emails() );
