d8a0323c1258051b72ab62981809e4393f3f29d1
Chris Christoff <chriscct7@gmail.com>
2013-09-20 23:49:59 -0400

    Update loader.php

--- tests/includes/loader.php
+++ tests/includes/loader.php
@@ -3,7 +3,9 @@
 // Install EDD
 $config_file_path = dirname( __FILE__ ) . '/../../tmp/wordpress-tests/wp-tests-config.php';
 $multisite = (int) ( defined( 'WP_TESTS_MULTISITE') && WP_TESTS_MULTISITE );
+ob_start();
 system( WP_PHP_BINARY . ' ' . escapeshellarg( dirname( __FILE__ ) . '/install.php' ) . ' ' . escapeshellarg( $config_file_path ) . ' ' . $multisite );
+ob_get_clean();
 
 // Bootstrap EDD
 require dirname( __FILE__ ) . '/../../easy-digital-downloads.php';
