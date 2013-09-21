abacbd4cd03649ed76534030f945dd170969681e
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-09-21 07:16:32 +0100

    Simplify edd_is_cart_saving_disabled(). See #1257.

--- includes/cart/functions.php
+++ includes/cart/functions.php
@@ -856,10 +856,7 @@ function edd_get_purchase_session() {
 function edd_is_cart_saving_disabled() {
 	global $edd_options;
 
-	if ( isset( $edd_options['disable_cart_saving'] ) )
-		return true;
-
-	return false;
+	return apply_filters( 'edd_cart_saving_disabled', isset( $edd_options['disable_cart_saving'] ) );
 }
 
 /**
