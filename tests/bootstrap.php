f675d9117980e743337f2257176d8cb2c00d6e7f
Chris Christoff <chriscct7@gmail.com>
2013-09-20 23:56:41 -0400

    ob_start();

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,9 @@ ob_start();
 require_once dirname( __FILE__ ) . '/../tmp/wordpress-tests/includes/functions.php';
 ob_get_clean();
 function _install_and_load_edd() {
+	ob_start();
 	require dirname( __FILE__ ) . '/includes/loader.php';
+	ob_get_clean();
 }
 tests_add_filter( 'muplugins_loaded', '_install_and_load_edd' );
 ob_start();
