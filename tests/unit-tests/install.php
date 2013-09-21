4d8abc76ac6f43a784bef331706099b6c0f3e482
Chris Christoff <chriscct7@gmail.com>
2013-09-20 20:00:30 -0400

    Update install.php

--- tests/unit-tests/install.php
+++ tests/unit-tests/install.php
@@ -10,6 +10,7 @@ class Tests_Activation extends EDD_UnitTestCase {
 	}
 
 	public function test_settings_general() {
+		$this->markTestIncomplete('This test needs to be fixed');
 	//	$this->assertArrayHasKey( 'purchase_page', get_option( 'edd_settings_general' ) );
 	//	$this->assertArrayHasKey( 'success_page', get_option( 'edd_settings_general' ) );
 	//	$this->assertArrayHasKey( 'failure_page', get_option( 'edd_settings_general' ) );
