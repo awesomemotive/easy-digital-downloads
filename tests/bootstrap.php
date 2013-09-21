11d01f5d6aa55c835f3e720e5d25c2ada97b261e
Chris Christoff <chriscct7@gmail.com>
2013-09-21 00:56:57 -0400

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,13 @@
 <?php
 define( 'EDD_PLUGIN_DIR', dirname( dirname( __FILE__ ) ) . '/'  );
-ob_start();
+
 require_once dirname( __FILE__ ) . '/../tmp/wordpress-tests/includes/functions.php';
-ob_get_clean();
+
 function _install_and_load_edd() {
-	ob_start();
 	require dirname( __FILE__ ) . '/includes/loader.php';
-	ob_get_clean();
 }
 tests_add_filter( 'muplugins_loaded', '_install_and_load_edd' );
-ob_start();
+
 require dirname( __FILE__ ) . '/../tmp/wordpress-tests/includes/bootstrap.php';
-ob_get_clean();
+
 require dirname( __FILE__ ) . '/framework/testcase.php';
