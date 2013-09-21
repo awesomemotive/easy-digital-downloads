2c4589c212fea97faf98c5032a62e863e669c785
Pippin Williamson <pippin@pippinsplugins.com>
2013-09-21 13:13:52 -0500

    Fix EDD_Payment_Stats tests. #600

--- tests/unit-tests/stats.php
+++ tests/unit-tests/stats.php
@@ -7,6 +7,7 @@ use \EDD_Payment_Stats;
  */
 class Tests_Stats extends EDD_UnitTestCase {
 
+	protected $_post;
 	protected $_stats;
 	protected $_payment_stats;
 
@@ -15,6 +16,94 @@ class Tests_Stats extends EDD_UnitTestCase {
 
 		$this->_stats = new EDD_Stats();
 		$this->_payment_stats = new EDD_Payment_Stats;
+
+		$post_id = $this->factory->post->create( array( 'post_title' => 'Test Download', 'post_type' => 'download', 'post_status' => 'publish' ) );
+
+		$_variable_pricing = array(
+			array(
+				'name' => 'Simple',
+				'amount' => 20
+			),
+			array(
+				'name' => 'Advanced',
+				'amount' => 100
+			)
+		);
+
+		$meta = array(
+			'edd_price' => '0.00',
+			'_variable_pricing' => 1,
+			'_edd_price_options_mode' => 'on',
+			'edd_variable_prices' => array_values( $_variable_pricing ),
+			'_edd_download_earnings' => 129.43,
+			'_edd_download_sales' => 59
+		);
+		foreach( $meta as $key => $value ) {
+			update_post_meta( $post_id, $key, $value );
+		}
+
+		$this->_post = get_post( $post_id );
+
+		/** Generate some sales */
+		$user = get_userdata(1);
+
+		$user_info = array(
+			'id' => $user->ID,
+			'email' => $user->user_email,
+			'first_name' => $user->first_name,
+			'last_name' => $user->last_name,
+			'discount' => 'none'
+		);
+
+		$download_details = array(
+			array(
+				'id' => $this->_post->ID,
+				'options' => array(
+					'price_id' => 1
+				)
+			)
+		);
+
+		$price      = '100.00';
+		$total      = 0;
+		$prices     = edd_get_variable_prices( $this->_post->ID );
+		$item_price = $prices[1]['amount'];
+
+		$total += $item_price;
+
+		$cart_details = array(
+			array(
+				'name'        => 'Test Download',
+				'id'          => $this->_post->ID,
+				'price'       =>  100,
+				'quantity'    => 1,
+				'item_number' => array(
+					'id'      => $this->_post->ID,
+					'options' => array(
+						'price_id' => 1
+					)
+				),
+			)
+		);
+
+		$purchase_data     = array(
+			'price'        => number_format( (float) $total, 2 ),
+			'date'         => date( 'Y-m-d H:i:s', strtotime( '-1 day' ) ),
+			'purchase_key' => strtolower( md5( uniqid() ) ),
+			'user_email'   => $user_info['email'],
+			'user_info'    => $user_info,
+			'currency'     => 'USD',
+			'downloads'    => $download_details,
+			'cart_details' => $cart_details,
+			'status'       => 'pending'
+		);
+
+		$_SERVER['REMOTE_ADDR'] = '10.0.0.0';
+		$_SERVER['SERVER_NAME'] = 'edd_virtual';
+
+		$payment_id = edd_insert_payment( $purchase_data );
+		edd_update_payment_status( $payment_id );
+
 	}
 
 	/*
@@ -43,6 +132,7 @@ class Tests_Stats extends EDD_UnitTestCase {
 		$this->assertEquals( $expected, $out );
 
 	}
+
 
 	/*
 	 *
