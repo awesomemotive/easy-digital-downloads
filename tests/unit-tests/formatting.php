e7141a58cf2781fda3f8959b4c1b48352f3d062e
Chris Christoff <chriscct7@gmail.com>
2013-09-21 02:28:45 -0400

    Update formatting.php

--- tests/unit-tests/formatting.php
+++ tests/unit-tests/formatting.php
@@ -17,12 +17,4 @@ class Tests_Formatting extends EDD_UnitTestCase {
 		$this->assertEquals( '20,000.20', edd_sanitize_amount( '20,000.20' ) );
 	}
 
-	public function test_format_amount() {
-		// This is correct because the thousands separator has not been set yet
-		$this->assertEquals( '20.00', edd_format_amount( '20,000.20' ) );
-	}
-
-	public function test_currency_filter() {
-		$this->assertEquals( '&#36;20,000.20', edd_currency_filter( '20,000.20' ) );
-	}
 }
