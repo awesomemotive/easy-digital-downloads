ec4c0382cd10664a7c67e8b4c899be99691e0601
Ghost1227 <dgriffiths@ghost1227.com>
2013-10-03 21:43:06 +0000

    Pippin likes tabs!

--- includes/admin/downloads/dashboard-columns.php
+++ includes/admin/downloads/dashboard-columns.php
@@ -71,19 +71,19 @@ function edd_render_download_columns( $column_name, $post_id ) {
 					echo '<input type="hidden" class="downloadprice-' . $post_id . '" value="' . edd_get_download_price( $post_id ) . '" />';
 				}
 				break;
-            case 'sales':
-                if ( current_user_can( 'edit_product', $post_id ) ) {
-    				echo edd_get_download_sales_stats( $post_id );
-                } else {
-                    echo '-';
-                }
+			case 'sales':
+				if ( current_user_can( 'edit_product', $post_id ) ) {
+					echo edd_get_download_sales_stats( $post_id );
+				} else {
+					echo '-';
+				}
 				break;
-            case 'earnings':
-                if ( current_user_can( 'edit_product', $post_id ) ) {
-    				echo edd_currency_filter( edd_format_amount( edd_get_download_earnings_stats( $post_id ) ) );
-                } else {
-                    echo '-';
-                }
+			case 'earnings':
+				if ( current_user_can( 'edit_product', $post_id ) ) {
+					echo edd_currency_filter( edd_format_amount( edd_get_download_earnings_stats( $post_id ) ) );
+				} else {
+					echo '-';
+				}
 				break;
 			case 'shortcode':
 				echo '[purchase_link id="' . absint( $post_id ) . '" text="' . esc_html( $purchase_text ) . '" style="' . $style . '" color="' . esc_attr( $color ) . '"]';
