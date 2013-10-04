d1131a0a1226221fd9e977822eeec4bbaa966b48
Pippin Williamson <pippin@pippinsplugins.com>
2013-10-04 10:50:07 -0500

    Add _edd_payment_user_id on past purchase look up. #1583

--- includes/user-functions.php
+++ includes/user-functions.php
@@ -307,6 +307,7 @@ function edd_add_past_purchases_to_new_user( $user_id ) {
 
 			// Store the updated user ID in the payment meta
 			update_post_meta( $payment->ID, '_edd_payment_meta', $meta );
+			update_post_meta( $payment->ID, '_edd_payment_user_id', $user_id );
 		}
 	}
 
