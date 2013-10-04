a8d9783c36bbf11420cc221ed7fadddfe8f00e20
Ghost1227 <dgriffiths@ghost1227.com>
2013-10-04 00:55:36 +0000

    System info overhaul

--- includes/admin/system-info.php
+++ includes/admin/system-info.php
@@ -41,165 +41,236 @@ function edd_system_info() {
 		$theme_data = wp_get_theme();
 		$theme      = $theme_data->Name . ' ' . $theme_data->Version;
 	}
+
+	$return  = '### Begin System Info ###' . "\n\n";
+	$return .= '## Please include this information when posting support requests ##' . "\n\n";
+
+	do_action( 'edd_system_info_before' );
+
+	// Site basics
+	$return .= '-- Site Info' . "\n\n";
+
+	$return .= 'Site URL:                 ' . site_url() . "\n";
+	$return .= 'Home URL:                 ' . home_url() . "\n";
+	$return .= 'Multisite:                ' . ( is_multisite() ? 'Yes' : 'No' ) . "\n";
+
+	if( has_filter( 'edd_after_site_info' ) )
+		$return .= apply_filters( 'edd_after_site_info', $return );
+
+	// Browser info, handled by the Browser class
+	$return .= "\n" . '-- User Browser' . "\n\n";
+
+	$return .= $browser;
+
+	if( has_filter( 'edd_after_user_browser' ) )
+		$return .= apply_filters( 'edd_after_user_browser', $return );
+
+	// WordPress configuration
+	$return .= "\n" . '-- WordPress Configuration' . "\n\n";
+
+	$return .= 'Version:                  ' . get_bloginfo( 'version' ) . "\n";
+	$return .= 'Permalink Structure:      ' . ( get_option( 'permalink_structure' ) ? get_option( 'permalink_structure' ) : 'Default' ) . "\n";
+	$return .= 'Active Theme:             ' . $theme . "\n";
+	$return .= 'Show On Front:            ' . get_option( 'show_on_front' ) . "\n";
+
+	// Only show page specs if frontpage is set to 'page'
+	if( get_option( 'show_on_front' ) == 'page' ) {
+		$front_page_id = get_option( 'page_on_front' );
+		$blog_page_id = get_option( 'page_for_posts' );
+
+		$return .= 'Page On Front:            ' . ( $front_page_id != 0 ? get_the_title( $front_page_id ) . ' (#' . $front_page_id . ')' : 'Unset' ) . "\n";
+		$return .= 'Page For Posts:           ' . ( $blog_page_id != 0 ? get_the_title( $blog_page_id ) . ' (#' . $blog_page_id . ')' : 'Unset' ) . "\n";
+	}
+
+	// Make sure wp_remote_post() is working
+	$request['cmd'] = '_notify-validate';
+
+	$params = array(
+		'sslverify'     => false,
+		'timeout'       => 60,
+		'user-agent'	=> 'EDD/' . EDD_VERSION,
+		'body'          => $request
+	);
+
+	$response = wp_remote_post( 'https://www.paypal.com/cgi-bin/webscr', $params );
+
+	if( !is_wp_error( $response ) && $response['response']['code'] >= 200 && $response['response']['code'] < 300 ) {
+		$WP_REMOTE_POST = 'wp_remote_post() works';
+	} else {
+		$WP_REMOTE_POST = 'wp_remote_post() does not work';
+	}
+
+	$return .= 'Remote Post:              ' . $WP_REMOTE_POST . "\n";
+	$return .= 'Table Prefix:             ' . 'Length: ' . strlen( $wpdb->prefix ) . '   Status: ' . ( strlen( $wpdb->prefix ) > 16 ? 'ERROR: Too long' : 'Acceptable' ) . "\n";
+	$return .= 'WP_DEBUG:                 ' . ( defined( 'WP_DEBUG' ) ? WP_DEBUG ? 'Enabled' : 'Disabled' : 'Not set' ) . "\n";
+	$return .= 'Memory Limit:             ' . WP_MEMORY_LIMIT . "\n";
+	$return .= 'Registered Post Stati:    ' . implode( ', ', get_post_stati() ) . "\n";
+
+	if( has_filter( 'edd_after_wordpress_config' ) )
+		$return .= apply_filters( 'edd_after_wordpress_config', $return );
+
+	// EDD Config
+	$return .= "\n" . '-- EDD Configuration' . "\n\n";
+
+	$return .= 'EDD Version:              ' . EDD_VERSION . "\n";
+	$return .= 'Upgraded From:            ' . get_option( 'edd_version_upgraded_from', 'None' ) . "\n\n";
+
+	$return .= 'Test Mode Enabled:        ' . ( edd_is_test_mode() ? 'Yes' : 'No' ) . "\n";
+	$return .= 'Ajax Enabled:             ' . ( edd_is_ajax_enabled() ? 'Yes' : 'No' ) . "\n";
+	$return .= 'Guest Checkout Enabled:   ' . ( edd_no_guest_checkout() ? 'No' : 'Yes' ) . "\n";
+	$return .= 'Symlinks Enabled:         ' . ( apply_filters( 'edd_symlink_file_downloads', isset( $edd_options['symlink_file_downloads'] ) ) && function_exists( 'symlink' ) ? 'Yes' : 'No' ) . "\n\n";
+
+	$return .= 'Checkout is:              ' . ( ! empty( $edd_options['purchase_page'] ) ? 'Valid' : 'Invalid' ) . "\n";
+	$return .= 'Checkout Page:            ' . ( ! empty( $edd_options['purchase_page'] ) ? get_permalink( $edd_options['purchase_page'] ) : '' ) . "\n";
+	$return .= 'Success Page:             ' . ( ! empty( $edd_options['success_page'] ) ? get_permalink( $edd_options['success_page'] ) : '' ) . "\n";
+	$return .= 'Failure Page:             ' . ( ! empty( $edd_options['failure_page'] ) ? get_permalink( $edd_options['failure_page'] ) : '' ) . "\n";
+	$return .= 'Downloads slug:           ' . ( defined( 'EDD_SLUG' ) ? '/' . EDD_SLUG : '/downloads' ) . "\n\n";
+
+	$return .= 'Taxes Enabled:            ' . ( edd_use_taxes() ? 'Yes' : 'No' ) . "\n";
+	$return .= 'Taxes After Discounts:    ' . ( edd_taxes_after_discounts() ? 'Yes' : 'No' ) . "\n";
+	$return .= 'Tax Rate:                 ' . edd_get_tax_rate() * 100 . '%' . "\n";
+
+	$rates = edd_get_tax_rates();
+	if( ! empty( $rates ) ) {
+		$return .= 'Country / State Rates:    ' . "\n";
+
+		foreach( $rates as $rate ) {
+			$return .= '                          Country: ' . $rate['country'] . ', State: ' . $rate['state'] . ', Rate: ' . $rate['rate'] . "\n";
+		}
+	}
+
+	if( has_filter( 'edd_after_edd_config' ) )
+		$return .= apply_filters( 'edd_after_edd_config', $return );
+
+	// EDD Template Overrides
+	$dir = get_stylesheet_directory() . '/edd_templates/';
+	if( is_dir( $dir ) && ( count( glob( "$dir/*" ) ) !== 0 ) ) {
+		$return .= "\n" . '-- EDD Template Overrides' . "\n\n";
+
+		foreach( glob( "$dir/*" ) as $file ) {
+			$return .= 'Filename:                 ' . basename( $file ) . "\n";
+		}
+	}
+
+	if( has_filter( 'edd_after_edd_templates' ) )
+		$return .= apply_filters( 'edd_after_edd_templates', $return );
+
+	// WordPress active plugins
+	$return .= "\n" . '-- WordPress Active Plugins' . "\n\n";
+
+	$plugins = get_plugins();
+	$active_plugins = get_option( 'active_plugins', array() );
+
+	foreach( $plugins as $plugin_path => $plugin ) {
+		if( !in_array( $plugin_path, $active_plugins ) )
+			continue;
+
+		$return .= $plugin['Name'] . ': ' . $plugin['Version'] . "\n";
+	}
+
+	// WordPress inactive plugins
+	$return .= "\n" . '-- WordPress Inactive Plugins' . "\n\n";
+
+	foreach( $plugins as $plugin_path => $plugin ) {
+		if( in_array( $plugin_path, $active_plugins ) )
+			continue;
+
+		$return .= $plugin['Name'] . ': ' . $plugin['Version'] . "\n";
+	}
+
+	if( is_multisite() ) {
+		// WordPress Multisite active plugins
+		$return .= "\n" . '-- Network Active Plugins' . "\n\n";
+
+		$plugins = wp_get_active_network_plugins();
+		$active_plugins = get_site_option( 'active_sitewide_plugins', array() );
+
+		foreach( $plugins as $plugin_path ) {
+			$plugin_base = plugin_basename( $plugin_path );
+
+			if( !array_key_exists( $plugin_base, $active_plugins ) )
+				continue;
+
+			$return .= $plugin['Name'] . ': ' . $plugin['Version'] . "\n";
+		}
+
+		// WordPress Multisite inactive plugins
+		$return .= "\n" . '-- Network Inactive Plugins' . "\n\n";
+
+		foreach( $plugins as $plugin_path ) {
+			$plugin_base = plugin_basename( $plugin_path );
+
+			if( array_key_exists( $plugin_base, $active_plugins ) )
+				continue;
+
+			$return .= $plugin['Name'] . ': ' . $plugin['Version'] . "\n";
+		}
+	}
+
+	if( has_filter( 'edd_after_wordpress_plugins' ) )
+		$return .= apply_filters( 'edd_after_wordpress_plugins', $return );
+
+	// Server configuration
+	$return .= "\n" . '-- Webserver Configuration' . "\n\n";
+
+	$return .= 'PHP Version:              ' . PHP_VERSION . "\n";
+	$return .= 'MySQL Version:            ' . mysql_get_server_info() . "\n";
+	$return .= 'Webserver Info:           ' . $_SERVER['SERVER_SOFTWARE'] . "\n";
+
+	if( has_filter( 'edd_after_webserver_config' ) )
+		$return .= apply_filters( 'edd_after_webserver_config', $return );
+
+	// PHP configs
+	$return .= "\n" . '-- PHP Configuration' . "\n\n";
+
+	$return .= 'Safe Mode:                ' . ( ini_get( 'safe_mode' ) ? 'Yes' : 'No' ) . "\n";
+	$return .= 'Memory Limit:             ' . ini_get( 'memory_limit' ) . "\n";
+	$return .= 'Upload Max Size:          ' . ini_get( 'upload_max_filesize' ) . "\n";
+	$return .= 'Post Max Size:            ' . ini_get( 'post_max_size' ) . "\n";
+	$return .= 'Upload Max Filesize:      ' . ini_get( 'upload_max_filesize' ) . "\n";
+	$return .= 'Time Limit:               ' . ini_get( 'max_execution_time' ) . "\n";
+	$return .= 'Max Input Vars:           ' . ini_get( 'max_input_vars' ) . "\n";
+	$return .= 'Display Errors:           ' . ( ini_get( 'display_errors' ) ? 'On (' . ini_get( 'display_errors' ) . ')' : 'N/A' ) . "\n";
+
+	if( has_filter( 'edd_after_php_config' ) )
+		$return .= apply_filters( 'edd_after_php_config', $return );
+
+	// PHP extensions
+	$return .= "\n" . '-- PHP Extensions' . "\n\n";
+
+	$return .= 'cURL:                     ' . ( function_exists( 'curl_init' ) ? 'Supported' : 'Not Supported' ) . "\n";
+	$return .= 'fsockopen:                ' . ( function_exists( 'fsockopen' ) ? 'Supported' : 'Not Supported' ) . "\n";
+	$return .= 'SOAP Client:              ' . ( class_exists( 'SoapClient' ) ? 'Installed' : 'Not Installed' ) . "\n";
+	$return .= 'Suhosin:                  ' . ( extension_loaded( 'suhosin' ) ? 'Installed' : 'Not Installed' ) . "\n";
+
+	if( has_filter( 'edd_after_php_ext' ) )
+		$return .= apply_filters( 'edd_after_php_ext', $return );
+
+	// Session information
+	$return .= "\n" . '-- Session Configuration' . "\n\n";
+
+	$return .= 'Session:                  ' . ( isset( $_SESSION ) ? 'Enabled' : 'Disabled' ) . "\n";
+
+	if( isset( $_SESSION ) ) {
+		$return .= 'Session Name:             ' . esc_html( ini_get( 'session.name' ) ) . "\n";
+		$return .= 'Cookie Path:              ' . esc_html( ini_get( 'session.cookie_path' ) ) . "\n";
+		$return .= 'Save Path:                ' . esc_html( ini_get( 'session.save_path' ) ) . "\n";
+		$return .= 'Use Cookies:              ' . ( ini_get( 'session.use_cookies' ) ? 'On' : 'Off' ) . "\n";
+		$return .= 'Use Only Cookies:         ' . ( ini_get( 'session.use_only_cookies' ) ? 'On' : 'Off' ) . "\n";
+	}
+
+	if( has_filter( 'edd_after_session_info' ) )
+		$return .= apply_filters( 'edd_after_session_info', $return );
+
+	do_action( 'edd_system_info_after' );
+
+	$return .= "\n" . '### End System Info ###';
+
 ?>
 	<div class="wrap">
 		<h2><?php _e( 'System Information', 'edd' ) ?></h2><br/>
 		<form action="<?php echo esc_url( admin_url( 'edit.php?post_type=download&page=edd-system-info' ) ); ?>" method="post" dir="ltr">
-			<textarea readonly="readonly" onclick="this.focus();this.select()" id="system-info-textarea" name="edd-sysinfo" title="<?php _e( 'To copy the system info, click below then press Ctrl + C (PC) or Cmd + C (Mac).', 'edd' ); ?>">
-### Begin System Info ###
-
-## Please include this information when posting support requests ##
-
-<?php do_action( 'edd_system_info_before' ); ?>
-
-Multisite:                <?php echo is_multisite() ? 'Yes' . "\n" : 'No' . "\n" ?>
-
-SITE_URL:                 <?php echo site_url() . "\n"; ?>
-HOME_URL:                 <?php echo home_url() . "\n"; ?>
-
-EDD Version:              <?php echo EDD_VERSION . "\n"; ?>
-Upgraded From:            <?php echo get_option( 'edd_version_upgraded_from', 'None' ) . "\n"; ?>
-WordPress Version:        <?php echo get_bloginfo( 'version' ) . "\n"; ?>
-Permalink Structure:      <?php echo get_option( 'permalink_structure' ) . "\n"; ?>
-Active Theme:             <?php echo $theme . "\n"; ?>
-
-Test Mode Enabled:        <?php echo edd_is_test_mode() ? "Yes\n" : "No\n"; ?>
-Ajax Enabled:             <?php echo edd_is_ajax_enabled() ? "Yes\n" : "No\n"; ?>
-Guest Checkout Enabled:   <?php echo edd_no_guest_checkout() ? "No\n" : "Yes\n"; ?>
-Symlinks Enabled:         <?php echo apply_filters( 'edd_symlink_file_downloads', isset( $edd_options['symlink_file_downloads'] ) ) && function_exists( 'symlink' ) ? "Yes\n" : "No\n"; ?>
-
-Checkout is:              <?php echo ! empty( $edd_options['purchase_page'] ) ? "Valid\n" : "Invalid\n"; ?>
-Checkout Page:            <?php echo ! empty( $edd_options['purchase_page'] ) ? get_permalink( $edd_options['purchase_page'] ) . "\n" : "\n" ?>
-Success Page:             <?php echo ! empty( $edd_options['success_page'] ) ? get_permalink( $edd_options['success_page'] ) . "\n" : "\n" ?>
-Failure Page:             <?php echo ! empty( $edd_options['failure_page'] ) ? get_permalink( $edd_options['failure_page'] ) . "\n" : "\n" ?>
-Downloads slug:           <?php echo defined( 'EDD_SLUG' ) ? '/' . EDD_SLUG . "\n" : "/downloads\n"; ?>
-
-Taxes Enabled:            <?php echo edd_use_taxes() ? "Yes\n" : "No\n"; ?>
-Taxes After Discounts:    <?php echo edd_taxes_after_discounts() ? "Yes\n" : "No\n"; ?>
-Tax Rate:                 <?php echo edd_get_tax_rate() * 100; ?>%
-Country / State Rates:    <?php
-
-$rates = edd_get_tax_rates();
-if( ! empty( $rates ) ) {
-	foreach( $rates as $rate ) {
-		echo 'Country: ' . $rate['country'] . ', State: ' . $rate['state'] . ', Rate: ' . $rate['rate'] . ' | ';
-	}
-}
-?>
-
-Registered Post Stati:    <?php echo implode( ', ', get_post_stati() ) . "\n\n"; ?>
-
-<?php echo $browser ; ?>
-
-PHP Version:              <?php echo PHP_VERSION . "\n"; ?>
-MySQL Version:            <?php echo mysql_get_server_info() . "\n"; ?>
-Web Server Info:          <?php echo $_SERVER['SERVER_SOFTWARE'] . "\n"; ?>
-
-PHP Safe Mode:            <?php echo ini_get( 'safe_mode' ) ? "Yes" : "No\n"; ?>
-PHP Memory Limit:         <?php echo ini_get( 'memory_limit' ) . "\n"; ?>
-PHP Upload Max Size:      <?php echo ini_get( 'upload_max_filesize' ) . "\n"; ?>
-PHP Post Max Size:        <?php echo ini_get( 'post_max_size' ) . "\n"; ?>
-PHP Upload Max Filesize:  <?php echo ini_get( 'upload_max_filesize' ) . "\n"; ?>
-PHP Time Limit:           <?php echo ini_get( 'max_execution_time' ) . "\n"; ?>
-PHP Max Input Vars:       <?php echo ini_get( 'max_input_vars' ) . "\n"; ?>
-
-WP_DEBUG:                 <?php echo defined( 'WP_DEBUG' ) ? WP_DEBUG ? 'Enabled' . "\n" : 'Disabled' . "\n" : 'Not set' . "\n" ?>
-
-WP Table Prefix:          <?php echo "Length: ". strlen( $wpdb->prefix ); echo " Status:"; if ( strlen( $wpdb->prefix )>16 ) {echo " ERROR: Too Long";} else {echo " Acceptable";} echo "\n"; ?>
-
-Show On Front:            <?php echo get_option( 'show_on_front' ) . "\n" ?>
-Page On Front:            <?php $id = get_option( 'page_on_front' ); echo get_the_title( $id ) . ' (#' . $id . ')' . "\n" ?>
-Page For Posts:           <?php $id = get_option( 'page_for_posts' ); echo get_the_title( $id ) . ' (#' . $id . ')' . "\n" ?>
-
-<?php
-$request['cmd'] = '_notify-validate';
-
-$params = array(
-	'sslverify'		=> false,
-	'timeout'		=> 60,
-	'user-agent'	=> 'EDD/' . EDD_VERSION,
-	'body'			=> $request
-);
-
-$response = wp_remote_post( 'https://www.paypal.com/cgi-bin/webscr', $params );
-
-if ( ! is_wp_error( $response ) && $response['response']['code'] >= 200 && $response['response']['code'] < 300 ) {
-	$WP_REMOTE_POST =  __( 'wp_remote_post() works', 'edd' ) . "\n";
-} else {
-	$WP_REMOTE_POST =  __( 'wp_remote_post() does not work', 'edd' ) . "\n";
-}
-?>
-WP Remote Post:           <?php echo $WP_REMOTE_POST; ?>
-
-Session:                  <?php echo isset( $_SESSION ) ? 'Enabled' : 'Disabled'; ?><?php echo "\n"; ?>
-Session Name:             <?php echo esc_html( ini_get( 'session.name' ) ); ?><?php echo "\n"; ?>
-Cookie Path:              <?php echo esc_html( ini_get( 'session.cookie_path' ) ); ?><?php echo "\n"; ?>
-Save Path:                <?php echo esc_html( ini_get( 'session.save_path' ) ); ?><?php echo "\n"; ?>
-Use Cookies:              <?php echo ini_get( 'session.use_cookies' ) ? 'On' : 'Off'; ?><?php echo "\n"; ?>
-Use Only Cookies:         <?php echo ini_get( 'session.use_only_cookies' ) ? 'On' : 'Off'; ?><?php echo "\n"; ?>
-
-WordPress Memory Limit:   <?php echo ( edd_let_to_num( WP_MEMORY_LIMIT )/( 1024 ) )."MB"; ?><?php echo "\n"; ?>
-DISPLAY ERRORS:           <?php echo ( ini_get( 'display_errors' ) ) ? 'On (' . ini_get( 'display_errors' ) . ')' : 'N/A'; ?><?php echo "\n"; ?>
-FSOCKOPEN:                <?php echo ( function_exists( 'fsockopen' ) ) ? __( 'Your server supports fsockopen.', 'edd' ) : __( 'Your server does not support fsockopen.', 'edd' ); ?><?php echo "\n"; ?>
-cURL:                     <?php echo ( function_exists( 'curl_init' ) ) ? __( 'Your server supports cURL.', 'edd' ) : __( 'Your server does not support cURL.', 'edd' ); ?><?php echo "\n"; ?>
-SOAP Client:              <?php echo ( class_exists( 'SoapClient' ) ) ? __( 'Your server has the SOAP Client enabled.', 'edd' ) : __( 'Your server does not have the SOAP Client enabled.', 'edd' ); ?><?php echo "\n"; ?>
-SUHOSIN:                  <?php echo ( extension_loaded( 'suhosin' ) ) ? __( 'Your server has SUHOSIN installed.', 'edd' ) : __( 'Your server does not have SUHOSIN installed.', 'edd' ); ?><?php echo "\n"; ?>
-
-TEMPLATES:
-
-<?php
-// Show templates that have been copied to the theme's edd_templates dir
-$dir = get_stylesheet_directory() . '/edd_templates/*';
-if (!empty($dir)){
-	foreach ( glob( $dir ) as $file ) {
-		echo "Filename: " . basename( $file ) . "\n";
-	}
-}
-else {
-	echo 'No overrides found';
-}
-?>
-
-ACTIVE PLUGINS:
-
-<?php
-$plugins = get_plugins();
-$active_plugins = get_option( 'active_plugins', array() );
-
-foreach ( $plugins as $plugin_path => $plugin ) {
-	// If the plugin isn't active, don't show it.
-	if ( ! in_array( $plugin_path, $active_plugins ) )
-		continue;
-
-	echo $plugin['Name'] . ': ' . $plugin['Version'] ."\n";
-}
-
-if ( is_multisite() ) :
-?>
-
-NETWORK ACTIVE PLUGINS:
-
-<?php
-$plugins = wp_get_active_network_plugins();
-$active_plugins = get_site_option( 'active_sitewide_plugins', array() );
-
-foreach ( $plugins as $plugin_path ) {
-	$plugin_base = plugin_basename( $plugin_path );
-
-	// If the plugin isn't active, don't show it.
-	if ( ! array_key_exists( $plugin_base, $active_plugins ) )
-		continue;
-
-	$plugin = get_plugin_data( $plugin_path );
-
-	echo $plugin['Name'] . ' :' . $plugin['Version'] ."\n";
-}
-
-endif;
-
-do_action( 'edd_system_info_after' );
-?>
-### End System Info ###</textarea>
+			<textarea readonly="readonly" onclick="this.focus();this.select()" id="system-info-textarea" name="edd-sysinfo" title="<?php _e( 'To copy the system info, click below then press Ctrl + C (PC) or Cmd + C (Mac).', 'edd' ); ?>"><?php echo $return; ?></textarea>
 			<p class="submit">
 				<input type="hidden" name="edd-action" value="download_sysinfo" />
 				<?php submit_button( __( 'Download System Info File', 'edd' ), 'primary', 'edd-download-sysinfo', false ); ?>
