2171b18267e7c9d1e08c516b18027ce9f0d588c2
Pippin Williamson <pippin@pippinsplugins.com>
2013-10-10 18:30:38 -0500

    Fix syntax error

--- includes/admin/payments/class-payments-table.php
+++ includes/admin/payments/class-payments-table.php
@@ -309,7 +309,7 @@ class EDD_Payment_History_Table extends WP_List_Table {
 	 */
 	public function column_user( $payment ) {
 
-		$user_id = edd_get_payment_user_id( $payment->ID )
+		$user_id = edd_get_payment_user_id( $payment->ID );
 
 		if ( $user_id && $user_id > 0 ) {
 			$user = get_userdata( $user_id ) ;
