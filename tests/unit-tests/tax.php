2866046be3cc8f03c40be268f59893cfdcaecdc0
Pippin Williamson <pippin@pippinsplugins.com>
2013-12-03 23:06:46 -0600

    Improved tax unit tests. #1744 #1558

--- tests/unit-tests/tax.php
+++ tests/unit-tests/tax.php
@@ -13,6 +13,8 @@ class Tests_Taxes extends EDD_UnitTestCase {
 	public function setUp() {
 		parent::setUp();
 
+		global $edd_options;
+
 		$post_id = $this->factory->post->create( array( 'post_title' => 'Test Download', 'post_type' => 'download', 'post_status' => 'publish' ) );
 
 		$meta = array(
@@ -46,32 +48,31 @@ class Tests_Taxes extends EDD_UnitTestCase {
 
 		$total += $item_price;
 
-		$cart_details = array(
-			array(
-				'name' => 'Test Download',
+		$cart_details = array();
+		$cart_details[] = array(
+			'name' => 'Test Download',
+			'id' => $this->_post->ID,
+			'item_number' => array(
 				'id' => $this->_post->ID,
-				'item_number' => array(
-					'id' => $this->_post->ID,
-					'options' => array()
-				),
-				'subtotal' => '10',
-				'discount' => '0',
-				'tax'      => '0.36',
-				'price'    => '10.36',
-				'quantity' => 1
-			)
+				'options' => array()
+			),
+			'subtotal' => '10',
+			'discount' => '0',
+			'tax'      => '0.36',
+			'price'    => '10.36',
+			'quantity' => 1
 		);
 
 		$purchase_data = array(
 			'price' => '10.36',
-			'date' => date( 'Y-m-d H:i:s', strtotime( '-1 day' ) ),
+			'date' => date( 'Y-m-d H:i:s', current_time( 'timestamp' ) ),
 			'purchase_key' => strtolower( md5( uniqid() ) ),
 			'user_email' => $user_info['email'],
 			'user_info' => $user_info,
 			'currency' => 'USD',
 			'downloads' => $download_details,
 			'cart_details' => $cart_details,
-			'status' => 'complete'
+			'status' => 'publish'
 		);
 
 		$_SERVER['REMOTE_ADDR'] = '10.0.0.0';
@@ -80,6 +81,17 @@ class Tests_Taxes extends EDD_UnitTestCase {
 		$payment_id = edd_insert_payment( $purchase_data );
 
 		$this->_payment_id = $payment_id;
+
+		// Setup global tax rate
+		$edd_options['enable_taxes'] = '1';
+		$edd_options['tax_rate']     = '3.6';
+
+		// Setup country / state tax rates
+		$tax_rates = array();
+		$tax_rates[] = array( 'country' => 'US', 'state' => 'AL', 'rate' => 15 );
+
+		update_option( 'edd_options', $edd_options );
+		update_option( 'edd_tax_rates', $tax_rates );
 	}
 
 	public function tearDown() {
@@ -87,11 +99,7 @@ class Tests_Taxes extends EDD_UnitTestCase {
 	}
 
 	public function test_use_taxes() {
-		$this->assertFalse( edd_use_taxes() );
-		$options = array();
-		$options['edd_use_taxes'] = '1';
-		$edd_options = array_merge( $options, $edd_options );
-		update_option( 'edd_options', $edd_options );
+		global $edd_options;
 		$this->assertTrue( edd_use_taxes() );
 	}
 
@@ -100,39 +108,11 @@ class Tests_Taxes extends EDD_UnitTestCase {
 	}
 
 	public function test_get_tax_rate() {
-		global $edd_options;
-
-		// Setup global tax rate
-		$options = array();
-		$options['tax_rate'] = '3.6';
-		$edd_options = array_merge( $options, $edd_options );
-
-		// Setup country / state tax rates
-		$tax_rates = array();
-		$tax_rates[] = array( 'country' => 'US', 'state' => 'AL', 'rate' => 15 );
-
-		update_option( 'edd_options', $edd_options );
-		update_option( 'edd_tax_rates', $tax_rates );
-
 		$this->assertInternalType( 'float', edd_get_tax_rate( 'US', 'AL' ) );
 		$this->assertEquals( '0.15', edd_get_tax_rate( 'US', 'AL' ) );
 	}
 
 	public function test_get_global_tax_rate() {
-		global $edd_options;
-
-		// Setup global tax rate
-		$options = array();
-		$options['tax_rate'] = '3.6';
-		$edd_options = array_merge( $options, $edd_options );
-
-		// Setup country / state tax rates
-		$tax_rates = array();
-		$tax_rates[] = array( 'country' => 'US', 'state' => 'AL', 'rate' => 15 );
-
-		update_option( 'edd_options', $edd_options );
-		update_option( 'edd_tax_rates', $tax_rates );
-
 		$this->assertInternalType( 'float', edd_get_tax_rate( 'CA', 'AB' ) );
 		$this->assertEquals( '0.036', edd_get_tax_rate( 'CA', 'AB' ) );
 
@@ -141,22 +121,15 @@ class Tests_Taxes extends EDD_UnitTestCase {
 	}
 
 	public function test_calculate_tax() {
-		global $edd_options;
-
-		// Calculate with taxes disabled
-		$this->assertEquals( 0.0, edd_calculate_tax( 54 ) );
-
-		// Enable taxes
-		$edd_options['edd_use_taxes'] = '1';
-
-		// Calculate with taxes enabled
-		$this->assertEquals( '1.944', edd_calculate_tax( 54 ) );
+		$this->assertEquals( '1.94', edd_calculate_tax( 54 ) );
+		$this->assertEquals( '1.97', edd_calculate_tax( 54.7 ) );
+		$this->assertEquals( '5.54', edd_calculate_tax( 153.85 ) );
+		$this->assertEquals( '9.30', edd_calculate_tax( 258.31 ) );
+		$this->assertEquals( '37.42', edd_calculate_tax( 1039.32 ) );
+		$this->assertEquals( '361.59', edd_calculate_tax( 10044.09 ) );
 	}
 
 	public function test_get_sales_tax_for_year() {
-
-		// This needs to test with a payment created
-
 		$this->assertEquals( '0.36', edd_get_sales_tax_for_year( date( 'Y' ) ) );
 	}
 
@@ -169,30 +142,20 @@ class Tests_Taxes extends EDD_UnitTestCase {
 	}
 
 	public function test_is_cart_taxed() {
-		global $edd_options;
-
-		$this->assertFalse( edd_is_cart_taxed() );
-
-		$edd_options['edd_use_taxes'] = '1';
-
 		$this->assertTrue( edd_is_cart_taxed() );
 
 	}
 
 	public function test_display_tax_rates() {
-		global $edd_options;
-
 		$this->assertFalse( edd_display_tax_rate() );
-
-		$edd_options['edd_use_taxes'] = '1';
-		$edd_options['display_tax_rate'] = '1';
-
-		$this->assertTrue( edd_is_cart_taxed() );
-
 	}
 
 	public function test_download_is_exclusive_of_tax() {
 		$this->assertFalse( edd_download_is_tax_exclusive( $this->_post->ID ) );
+	}
+
+	public function test_get_payment_tax() {
+		$this->assertEquals( '0.36', edd_get_payment_tax( $this->_payment_id ) );
 	}
 
 }
