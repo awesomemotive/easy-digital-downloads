4ee78a82bb0e460bf75a74d9a9913dbb0578d942
Ghost1227 <dgriffiths@ghost1227.com>
2013-12-09 19:01:18 +0000

    Really Pippin? If you delete it, of course it's never set

--- includes/admin/tracking.php
+++ includes/admin/tracking.php
@@ -222,8 +222,7 @@ class EDD_Tracking {
 	public function admin_notice() {
 
 		global $edd_options;
-		delete_option( 'edd_tracking_notice' );
-		if( ! get_option( 'edd_tracking_notice' ) && ! isset( $edd_options['allow_tracking'] ) ) {
+		if( ! get_option( 'edd_tracking_notice' ) && ! isset( $edd_options['allow_tracking'] ) && current_user_can( 'manage_options' ) ) {
 			if( 
 				stristr( network_site_url( '/' ), 'dev'       ) !== false ||
 				stristr( network_site_url( '/' ), 'localhost' ) !== false ||
