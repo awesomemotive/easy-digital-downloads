6bdc00e257474290527775cfffd114ec84485b13
Barry Kooij <git@barrykooij.nl>
2013-12-18 09:59:34 +0100

    The tags on the settings screen are missing their {}

--- includes/emails/email-tags.php
+++ includes/emails/email-tags.php
@@ -158,7 +158,7 @@ function edd_get_emails_tags_list() {
 		foreach ( $email_tags as $email_tag ) {
 
 			// Add email tag to list
-			$list .= $email_tag['tag'] . ' - ' . $email_tag['description'] . '<br/>';
+			$list .= '{' . $email_tag['tag'] . '} - ' . $email_tag['description'] . '<br/>';
 
 		}
 
