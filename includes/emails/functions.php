f45b65d424fd640629f5d63dac1efda264d60e7e
Pippin Williamson <pippin@pippinsplugins.com>
2013-12-19 17:34:22 -0600

    Move edd_get_email_names() to functions.php #1451

--- includes/emails/functions.php
+++ includes/emails/functions.php
@@ -214,3 +214,33 @@ function edd_get_default_sale_notification_email() {
 
 	return $message;
 }
+
+/**
+ * Get various correctly formatted names used in emails
+ *
+ * @since 1.9
+ * @param $user_info
+ *
+ * @return array $email_names
+ */
+function edd_get_email_names( $user_info ) {
+	$email_names = array();
+	$user_info 	= maybe_unserialize( $user_info );
+
+	$email_names[ 'fullname' ] = '';
+	if ( isset( $user_info['id'] ) && $user_info['id'] > 0 && isset( $user_info['first_name'] ) ) {
+		$user_data = get_userdata( $user_info['id'] );
+		$email_names[ 'name' ]      = $user_info['first_name'];
+		$email_names[ 'fullname' ]  = $user_info['first_name'] . ' ' . $user_info['last_name'];
+		$email_names[ 'username' ]  = $user_data->user_login;
+	} elseif ( isset( $user_info['first_name'] ) ) {
+		$email_names[ 'name' ]     = $user_info['first_name'];
+		$email_names[ 'fullname' ] = $user_info['first_name'] . ' ' . $user_info['last_name'];
+		$email_names[ 'username' ] = $user_info['first_name'];
+	} else {
+		$email_names[ 'name' ]     = $user_info['email'];
+		$email_names[ 'username' ] = $user_info['email'];
+	}
+
+	return $email_names;
+}
