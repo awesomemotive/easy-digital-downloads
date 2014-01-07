b6b5ceb7ed73da39082305d42a6ffbeb3adf61b0
Pippin Williamson <pippin@pippinsplugins.com>
2014-01-07 12:07:10 -0600

    mark test as incomplete

--- tests/unit-tests/api.php
+++ tests/unit-tests/api.php
@@ -208,8 +208,9 @@ class Tests_API extends EDD_UnitTestCase {
 	
 	public function test_get_product_stats() {
 		$out = $this->_api_output;		
-		// This one failes and haven't figured out why
-		//$this->assertArrayHasKey( 'stats', $out['products'][0] );
+		// This one fails and haven't figured out why
+		$this->markTestIncomplete( 'This test needs to be fixed. The stats key doesn\'t exist due to not being able to correctly check the user\'s permissions' );
+		$this->assertArrayHasKey( 'stats', $out['products'][0] );
 		$this->assertArrayHasKey( 'total', $out['products'][0]['stats'] );
 		$this->assertArrayHasKey( 'sales', $out['products'][0]['stats']['total'] );
 		$this->assertArrayHasKey( 'earnings', $out['products'][0]['stats']['total'] );
