2722ce4a41de86548c6b03799d3372ff5647e8d2
Pippin Williamson <pippin@pippinsplugins.com>
2014-02-02 13:22:56 -0600

    Move edd_get_cart_item_discount_amount() into discount-functions.php and refactor to work properly with flat rate and percentage discounts. #1917

--- includes/cart/functions.php
+++ includes/cart/functions.php
@@ -385,59 +385,6 @@ function edd_get_cart_item_price( $download_id = 0, $options = array(), $include
 }
 
 /**
- * Get the discounted amount on a price
- *
- * @since 1.9
- * @param array $item Cart item array
- * @return float The discounted amount
- */
-function edd_get_cart_item_discount_amount( $item = array() ) {
-
-	$amount           = 0;
-	$price            = edd_get_cart_item_price( $item['id'], $item['options'], edd_prices_include_tax() );
-	$discounted_price = $price;
-
-	// Retrieve all discounts applied to the cart
-	$discounts = edd_get_cart_discounts();
-
-	if( $discounts ) {
-
-		foreach ( $discounts as $discount ) {
-
-			$code_id           = edd_get_discount_id_by_code( $discount );
-			$reqs              = edd_get_discount_product_reqs( $code_id );
-			$excluded_products = edd_get_discount_excluded_products( $code_id );
-
-			// Make sure requirements are set and that this discount shouldn't apply to the whole cart
-			if ( ! empty( $reqs ) && edd_is_discount_not_global( $code_id ) ) {
-
-				// This is a product(s) specific discount
-
-				foreach ( $reqs as $download_id ) {
-					
-					if ( $download_id == $item['id'] && ! in_array( $item['id'], $excluded_products ) && 'flat' !== edd_get_discount_type( $code_id ) ) {
-						$discounted_price = edd_get_discounted_amount( $discount, $price );
-					}
-					
-				}
-
-			} else {
-
-				// This is a global cart discount
-				if( ! in_array( $item['id'], $excluded_products ) && 'flat' === edd_get_discount_type( $discount ) ) {
-					$discounted_price = edd_get_discounted_amount( $discount, $price );
-				}
-			}
-		}
-
-		$amount = round( $price - $discounted_price, 2 );
-	}
-
-	return $amount;
-
-}
-
-/**
  * Get cart item's final price
  *
  * Gets the amount after taxes and discounts
