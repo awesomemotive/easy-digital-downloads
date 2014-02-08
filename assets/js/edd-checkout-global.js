e14b971ac751aa104babbd101338f4125808a358
Chris Klosowski <cklosowski@gmail.com>
2014-02-08 09:16:59 -0700

    Fixing a whitespace issue after commiting #1916

--- assets/js/edd-checkout-global.js
+++ assets/js/edd-checkout-global.js
@@ -156,7 +156,7 @@ jQuery(document).ready(function($) {
                         });
                         $('#edd-discount', $checkout_form_wrap ).val('');
                         recalculate_taxes();
-                        $('body').trigger('edd_discount_applied', [ discount_response ]);
+						$('body').trigger('edd_discount_applied', [ discount_response ]);
                     } else {
                         $('#edd-discount-error-wrap').html(discount_response.msg).show();
                     }
@@ -205,7 +205,7 @@ jQuery(document).ready(function($) {
                     $(this).text(discount_response.total);
                 });
                 recalculate_taxes();
-                $('body').trigger('edd_discount_removed', [ discount_response ]);
+				$('body').trigger('edd_discount_removed', [ discount_response ]);
             }
         }).fail(function (data) {
             if ( window.console && window.console.log ) {
