1381d933940bac46dfda0aa7a4daf8edb522f6e8
Pippin Williamson <pippin@pippinsplugins.com>
2014-02-24 10:57:38 -0600

    add missing payment_id param

--- easy-digital-downloads.php
+++ easy-digital-downloads.php
@@ -5,7 +5,7 @@
  * Description: Serve Digital Downloads Through WordPress
  * Author: Pippin Williamson
  * Author URI: http://pippinsplugins.com
- * Version: 1.9.5
+ * Version: 1.9.6
  * Text Domain: edd
  * Domain Path: languages
  *
@@ -25,7 +25,7 @@
  * @package EDD
  * @category Core
  * @author Pippin Williamson
- * @version 1.9.5
+ * @version 1.9.6
  */
 
 // Exit if accessed directly
@@ -159,7 +159,7 @@ final class Easy_Digital_Downloads {
 		
 		// Plugin version
 		if ( ! defined( 'EDD_VERSION' ) ) {
-			define( 'EDD_VERSION', '1.9.5' );
+			define( 'EDD_VERSION', '1.9.6' );
 		}
 
 		// Plugin Folder Path
