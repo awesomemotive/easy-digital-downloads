b16c36ae59ecf19440489e627857749343122bdf
Ghost1227 <dgriffiths@ghost1227.com>
2014-03-01 23:28:26 +0200

    Monthly sales is NOT a dollar value

--- includes/admin/reporting/graphing.php
+++ includes/admin/reporting/graphing.php
@@ -173,7 +173,7 @@ function edd_reports_graph() {
 				<p class="edd_graph_totals"><strong><?php _e( 'Total earnings for period shown: ', 'edd' ); echo edd_currency_filter( edd_format_amount( $earnings_totals ) ); ?></strong></p>
 				<p class="edd_graph_totals"><strong><?php _e( 'Total sales for period shown: ', 'edd' ); echo edd_format_amount( $sales_totals, false ); ?></strong></p>
 				<p class="edd_graph_totals"><strong><?php _e( 'Estimated monthly earnings: ', 'edd' ); echo edd_currency_filter( edd_format_amount( $estimated['earnings'] ) ); ?></strong></p>
-				<p class="edd_graph_totals"><strong><?php _e( 'Estimated monthly sales: ', 'edd' ); echo edd_currency_filter( edd_format_amount( $estimated['sales'] ) ); ?></strong></p>
+				<p class="edd_graph_totals"><strong><?php _e( 'Estimated monthly sales: ', 'edd' ); echo edd_format_amount( $estimated['sales'], false ); ?></strong></p>
 			</div>
 		</div>
 	</div>
