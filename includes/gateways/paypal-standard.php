c9e684ca5b4478ecbbc39bfc96bb4c7a6a9ab8dd
Pippin Williamson <pippin@pippinsplugins.com>
2014-03-20 15:52:19 -0500

    Fix extra slash in PayPal IPN URL. Closes #2135

--- includes/gateways/paypal-standard.php
+++ includes/gateways/paypal-standard.php
@@ -55,7 +55,7 @@ function edd_process_paypal_purchase( $purchase_data ) {
         edd_send_back_to_checkout( '?payment-mode=' . $purchase_data['post_data']['edd-gateway'] );
     } else {
         // Only send to PayPal if the pending payment is created successfully
-        $listener_url = trailingslashit( home_url( 'index.php' ) ) . '?edd-listener=IPN';
+        $listener_url = add_query_arg( 'edd-listener', 'IPN', home_url( 'index.php' ) );
 
          // Get the success url
         $return_url = add_query_arg( array(
