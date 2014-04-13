279ca6aa6f6076fac24c93d8f3e38bb09982a657
Pippin Williamson <pippin@pippinsplugins.com>
2014-04-13 17:02:24 -0500

    Add limit to ajax download search. @2134

--- includes/ajax-functions.php
+++ includes/ajax-functions.php
@@ -329,9 +329,9 @@ function edd_ajax_download_search() {
 	$search  = esc_sql( sanitize_text_field( $_GET['s'] ) );
 	$results = array();
 	if ( current_user_can( 'manage_shop_products' ) ) {
-		$items   = $wpdb->get_results( "SELECT ID,post_title FROM $wpdb->posts WHERE `post_type` = 'download' AND `post_title` LIKE '%$search%'" );
+		$items   = $wpdb->get_results( "SELECT ID,post_title FROM $wpdb->posts WHERE `post_type` = 'download' AND `post_title` LIKE '%$search%' LIMIT 50" );
 	} else {
-		$items   = $wpdb->get_results( "SELECT ID,post_title FROM $wpdb->posts WHERE `post_type` = 'download' AND `post_status` = 'publish' AND `post_title` LIKE '%$search%'" );
+		$items   = $wpdb->get_results( "SELECT ID,post_title FROM $wpdb->posts WHERE `post_type` = 'download' AND `post_status` = 'publish' AND `post_title` LIKE '%$search%' LIMIT 50" );
 	}
 
 	if( $items ) {
