5ad136e5e32cd8094c017e555f601060598efda5
Pippin Williamson <pippin@pippinsplugins.com>
2014-04-18 11:10:09 -0500

    Introduce edd_is_email_banned() and unit tests. #2081

--- includes/process-purchase.php
+++ includes/process-purchase.php
@@ -978,30 +978,38 @@ function edd_check_purchase_email( $valid_data, $posted ) {
 	$is_banned = false;
 	$banned    = edd_get_banned_emails();
 
-	if( empty( $banned ) )
+	if( empty( $banned ) ) {
 		return;
+	}
 
 	if( is_user_logged_in() ) {
+
 		// The user is logged in, check that their account email is not banned
 		$user_data = get_userdata( get_current_user_id() );
-		if( in_array( $user_data->user_email, $banned ) ) {
+		if( edd_is_email_banned( $user_data->user_email ) ) {
+
 			$is_banned = true;
 		}
 
-		if( in_array( $posted['edd_email'], $banned ) ) {
+		if( edd_is_email_banned( $posted['edd_email'] ) ) {
 			$is_banned = true;
 		}
+
 	} elseif( isset( $posted['edd-purchase-var'] ) && $posted['edd-purchase-var'] == 'needs-to-login' ) {
+		
 		// The user is logging in, check that their email is not banned
 		$user_data = get_user_by( 'login', $posted['edd_user_login'] );
-		if( $user_data && in_array( $user_data->user_email, $banned ) ) {
+		if( $user_data && edd_is_email_banned( $user_data->user_email ) ) {
 			$is_banned = true;
 		}
+
 	} else {
+		
 		// Guest purchase, check that the email is not banned
-		if( in_array( $posted['edd_email'], $banned ) ) {
+		if( edd_is_email_banned( $posted['edd_email'] ) ) {
 			$is_banned = true;
 		}
+
 	}
 
 	if( $is_banned ) {
