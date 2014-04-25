ab98c082a5ecbb90e3b44ef89aa7c3ec1f1e180c
Pippin Williamson <pippin@pippinsplugins.com>
2014-04-25 12:06:05 -0500

    Fix fatal error from missing function name change #2229

--- includes/admin/payments/view-order-details.php
+++ includes/admin/payments/view-order-details.php
@@ -35,7 +35,7 @@ $payment_meta = edd_get_payment_meta( $payment_id );
 $cart_items   = edd_get_payment_meta_cart_details( $payment_id );
 $user_id      = edd_get_payment_user_id( $payment_id );
 $payment_date = strtotime( $item->post_date );
-$unlimited    = edd_get_payment_has_unlimited_downloads( $payment_id );
+$unlimited    = edd_payment_has_unlimited_downloads( $payment_id );
 $user_info    = edd_get_payment_meta_user_info( $payment_id );
 $address      = ! empty( $user_info['address'] ) ? $user_info['address'] : array( 'line1' => '', 'line2' => '', 'city' => '', 'country' => '', 'state' => '', 'zip' => '' );
 ?>
