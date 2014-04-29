d76ee756ce55db55b9714f5fe33f796a31aaa4f6
Elliott Stocks <elliottstocks@hotmail.co.uk>
2014-04-29 10:37:18 +0100

    Add $payment_id to download title filter
    There may be info within the payment meta to add to the title. Passing the $payment_id makes this possible.

--- includes/emails/email-tags.php
+++ includes/emails/email-tags.php
@@ -396,7 +396,7 @@ function edd_email_tag_download_list( $payment_id ) {
 					$title .= "&nbsp;&ndash;&nbsp;" . edd_get_price_option_name( $item['id'], $price_id );
 				}
 
-				$download_list .= '<li>' . apply_filters( 'edd_email_receipt_download_title', $title, $item, $price_id ) . '<br/>';
+				$download_list .= '<li>' . apply_filters( 'edd_email_receipt_download_title', $title, $item, $price_id, $payment_id ) . '<br/>';
 				$download_list .= '<ul>';
 			}
 
