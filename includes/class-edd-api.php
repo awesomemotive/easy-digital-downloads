8865d1f0cd0d4277cf7777ff31553ef0e82852fc
Ghost1227 <dgriffiths@ghost1227.com>
2014-05-05 18:03:43 -0500

    Escape URLs

--- includes/class-edd-api.php
+++ includes/class-edd-api.php
@@ -1391,15 +1391,15 @@ class EDD_API {
 		switch( $process ) {
 			case 'generate':
 				$this->generate_api_key( $user_id );
-				wp_redirect( add_query_arg( 'edd-message', 'api-key-generated', 'edit.php?post_type=download&page=edd-tools&tab=api_keys' ) ); exit();
+				wp_redirect( esc_url( add_query_arg( 'edd-message', 'api-key-generated', 'edit.php?post_type=download&page=edd-tools&tab=api_keys' ) ) ); exit();
 				break;
 			case 'regenerate':
 				$this->generate_api_key( $user_id, true );
-				wp_redirect( add_query_arg( 'edd-message', 'api-key-regenerated', 'edit.php?post_type=download&page=edd-tools&tab=api_keys' ) ); exit();
+				wp_redirect( esc_url( add_query_arg( 'edd-message', 'api-key-regenerated', 'edit.php?post_type=download&page=edd-tools&tab=api_keys' ) ) ); exit();
 				break;
 			case 'revoke':
 				$this->revoke_api_key( $user_id );
-				wp_redirect( add_query_arg( 'edd-message', 'api-key-revoked', 'edit.php?post_type=download&page=edd-tools&tab=api_keys' ) ); exit();
+				wp_redirect( esc_url( add_query_arg( 'edd-message', 'api-key-revoked', 'edit.php?post_type=download&page=edd-tools&tab=api_keys' ) ) ); exit();
 				break;
 			default;
 				break;
