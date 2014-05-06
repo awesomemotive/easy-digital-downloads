4adb2db535d909e894f768c38556a65cf197f476
Pippin Williamson <pippin@pippinsplugins.com>
2014-05-05 19:04:50 -0500

    Set per_page to 30

--- includes/admin/class-api-keys-table.php
+++ includes/admin/class-api-keys-table.php
@@ -30,7 +30,7 @@ class EDD_API_Keys_Table extends WP_List_Table {
 	 * @var int Number of items per page
 	 * @since 2.0
 	 */
-	public $per_page = 1;
+	public $per_page = 30;
 
 	/**
 	 * @var object Query results
