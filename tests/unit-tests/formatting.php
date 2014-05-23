ca222093f22562b1fb01f05a1a63225bc2cba5b4
Ghost1227 <dgriffiths@ghost1227.com>
2014-05-23 17:01:17 -0500

    Add unit test for issue 2316

--- tests/unit-tests/formatting.php
+++ tests/unit-tests/formatting.php
@@ -19,6 +19,7 @@ class Tests_Formatting extends EDD_UnitTestCase {
 		$this->assertEquals( '20000.20', edd_sanitize_amount( '22 000.20' ) );
 		$this->assertEquals( '20.20', edd_sanitize_amount( '20.2' ) );
 		$this->assertEquals( '25.42', edd_sanitize_amount( '25.42221112993' ) );
+		$this->assertEquals( '20.20', edd_sanitize_amount( '$20.2' ) );
 
 	}
 
