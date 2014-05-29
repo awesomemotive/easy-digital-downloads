9224ca37b11e8d1a0c70542059d6b2af9ecc3700
Pippin Williamson <pippin@pippinsplugins.com>
2014-05-29 16:30:01 -0500

    Re-eanble discount amount checks. #2302

--- tests/unit-tests/discounts.php
+++ tests/unit-tests/discounts.php
@@ -201,18 +201,18 @@ class Tests_Discounts extends EDD_UnitTestCase {
 
 
 	public function test_get_discounted_amount() {
-		$this->markTestIncomplete('Fix this per #2302');
-		//$this->assertEquals( 432.0, edd_get_discounted_amount( '20OFF', '540' ) );
+		//$this->markTestIncomplete('Fix this per #2302');
+		$this->assertEquals( 432.0, edd_get_discounted_amount( '20OFF', '540' ) );
 	}
 
 	public function test_get_discounted_amount_negative() {
-		$this->markTestIncomplete('Fix this per #2302');
-		//$this->assertEqual( 150.0, edd_get_discounted_amount( 'DOUBLE', '75' ) );
+		//$this->markTestIncomplete('Fix this per #2302');
+		$this->assertEqual( 150.0, edd_get_discounted_amount( 'DOUBLE', '75' ) );
 	}
 
 	public function test_get_discounted_amount_flat() {
-		$this->markTestIncomplete('Fix this per #2302');
-		//$this->assertEqual( 9.0, edd_get_discounted_amount( 'FLAT', '1' ) );
+		//$this->markTestIncomplete('Fix this per #2302');
+		$this->assertEqual( 9.0, edd_get_discounted_amount( 'FLAT', '1' ) );
 	}
 
 	public function test_increase_discount_usage() {
