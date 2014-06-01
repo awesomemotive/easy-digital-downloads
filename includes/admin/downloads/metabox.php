262d8c4d71a48ac238e25f04b4f2f041f08ef78c
Ghost1227 <dgriffiths@ghost1227.com>
2014-05-31 20:53:34 -0500

    Add file empty check

--- includes/admin/downloads/metabox.php
+++ includes/admin/downloads/metabox.php
@@ -234,7 +234,9 @@ add_filter( 'edd_metabox_save__edd_bundled_products', 'edd_sanitize_bundled_prod
 function edd_sanitize_files_save( $files ) {
 	// Clean up filenames to ensure whitespaces are stripped
 	foreach( $files as $id => $file ) {
-		$files[$id]['file'] = trim( $file['file'] );
+		if( !empty( $files[$id]['file'] ) ) {
+			$files[$id]['file'] = trim( $file['file'] );
+		}
 	}
 
 	// Make sure all files are rekeyed starting at 0
