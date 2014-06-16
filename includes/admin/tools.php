038e4304f9b3e45c1047c18a9164a9b3a6ffee0f
Ghost1227 <dgriffiths@ghost1227.com>
2014-06-15 19:14:48 -0500

    Add check for mysqli_get_client_version

--- includes/admin/tools.php
+++ includes/admin/tools.php
@@ -523,7 +523,9 @@ function edd_tools_sysinfo_get() {
 	// Server configuration (really just versioning)
 	$return .= "\n" . '-- Webserver Configuration' . "\n\n";
 	$return .= 'PHP Version:              ' . PHP_VERSION . "\n";
-	$return .= 'MySQL Version:            ' . mysqli_get_client_version() . "\n";
+	if ( function_exists( 'mysqli_get_client_version' ) ) {
+		$return .= 'MySQL Version:            ' . mysqli_get_client_version() . "\n";
+	}
 	$return .= 'Webserver Info:           ' . $_SERVER['SERVER_SOFTWARE'] . "\n";
 
 	$return  = apply_filters( 'edd_sysinfo_after_webserver_config', $return );
