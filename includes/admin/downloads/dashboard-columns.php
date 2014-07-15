72b099cb4f30051a1b98b0aa72125d3e0974e76c
Ghost1227 <dgriffiths@ghost1227.com>
2014-07-14 23:48:32 -0500

    Remove date filter on Download CPT

--- includes/admin/downloads/dashboard-columns.php
+++ includes/admin/downloads/dashboard-columns.php
@@ -213,6 +213,20 @@ function edd_add_download_filters() {
 add_action( 'restrict_manage_posts', 'edd_add_download_filters', 100 );
 
 /**
+ * Remove Download Month Filter
+ *
+ * Removes the drop down filter for downloads by date.
+ *
+ * @author Daniel J Griffiths
+ * @since 2.1
+ * @return array Empty array disables the dropdown
+ */
+function edd_remove_month_filter( $date ) {
+	return array();
+}
+add_filter( 'months_dropdown_results', 'edd_remove_month_filter', 99 );
+
+/**
  * Adds price field to Quick Edit options
  *
  * @since 1.1.3.4
