8eecdd84fe5b914a0b5d95073034f175efd474b4
Andrew Munro <andrew@sumobi.com>
2014-07-15 21:23:27 +1200

    Hide cart on checkout option
    - fix undefined variables in download categories/tags widget
    - removed unnecessary quantity instance
    - various formatting improvements

--- includes/widgets.php
+++ includes/widgets.php
@@ -38,8 +38,11 @@ class edd_cart_widget extends WP_Widget {
 
 	/** @see WP_Widget::widget */
 	function widget( $args, $instance ) {
-	
-		if( ! isset( $args['id'] ) ) {
+		if ( $instance['hide_on_checkout'] && edd_is_checkout() ) {
+			return;
+		}
+
+		if ( ! isset( $args['id'] ) ) {
 			$args['id'] = 'edd_cart_widget';
 		}		
 
@@ -50,32 +53,50 @@ class edd_cart_widget extends WP_Widget {
 		global $post, $edd_options;
 
 		echo $before_widget;
+
 		if ( $title ) {
 			echo $before_title . $title . $after_title;
 		}
+
 		do_action( 'edd_before_cart_widget' );
+
 		edd_shopping_cart( true );
+
 		do_action( 'edd_after_cart_widget' );
+
 		echo $after_widget;
 	}
 
 	/** @see WP_Widget::update */
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$instance['title'] = strip_tags( $new_instance['title'] );
-		$instance['quantity'] = isset( $new_instance['quantity'] ) ? strip_tags( $new_instance['quantity'] ) : '';
+
+		$instance['title']            = strip_tags( $new_instance['title'] );
+		$instance['hide_on_checkout'] = isset( $new_instance['hide_on_checkout'] ) ? $new_instance['hide_on_checkout'] : '';
+
 		return $instance;
 	}
 
 	/** @see WP_Widget::form */
 	function form( $instance ) {
-		$title = isset( $instance[ 'title' ] ) ? esc_attr( $instance[ 'title' ] ) : '';
-		?>
+
+        $defaults = array(
+            'title'            => '',
+            'hide_on_checkout' => ''
+        );
+
+        $instance = wp_parse_args( (array) $instance, $defaults ); ?>
 		<p>
        		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title:', 'edd' ); ?></label>
-     		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo $title; ?>"/>
+     		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo $instance['title']; ?>"/>
     	</p>
-    
+    	
+	    <!-- Hide on Checkout Page -->
+        <p>
+            <input <?php checked( $instance['hide_on_checkout'], 'on' ); ?> id="<?php echo esc_attr( $this->get_field_id( 'hide_on_checkout' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'hide_on_checkout' ) ); ?>" type="checkbox" />
+            <label for="<?php echo esc_attr( $this->get_field_id( 'hide_on_checkout' ) ); ?>"><?php _e( 'Hide on Checkout Page', 'edd' ); ?></label>
+        </p>
+
    		 <?php
 	}
 }
@@ -149,14 +170,13 @@ class edd_categories_tags_widget extends WP_Widget {
 		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
 		<p>
 			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title:', 'edd' ); ?></label>
-			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"
-				   name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo $title; ?>"/>
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo $instance['title']; ?>"/>
 		</p>
 		<p>
 			<label for="<?php echo esc_attr( $this->get_field_id( 'taxonomy' ) ); ?>"><?php _e( 'Taxonomy:', 'edd' ); ?></label>
 			<select name="<?php echo esc_attr( $this->get_field_name( 'taxonomy' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'taxonomy' ) ); ?>">
-				<option value="download_category" <?php selected( 'download_category', $taxonomy ); ?>><?php _e( 'Categories', 'edd' ); ?></option>
-				<option value="download_tag" <?php selected( 'download_tag', $taxonomy ); ?>><?php _e( 'Tags', 'edd' ); ?></option>
+				<option value="download_category" <?php selected( 'download_category', $instance['taxonomy'] ); ?>><?php _e( 'Categories', 'edd' ); ?></option>
+				<option value="download_tag" <?php selected( 'download_tag', $instance['taxonomy'] ); ?>><?php _e( 'Tags', 'edd' ); ?></option>
 			</select>
 		</p>
 		<p>
@@ -335,14 +355,14 @@ class EDD_Product_Details_Widget extends WP_Widget {
     public function update( $new_instance, $old_instance ) {
         $instance = $old_instance;
 
-        $instance['title'] 				= strip_tags( $new_instance['title'] );
-        $instance['download_id']		= strip_tags( $new_instance['download_id'] );
-    	$instance['download_title']		= isset( $new_instance['download_title'] )    ? $new_instance['download_title']  : '';
-        $instance['purchase_button'] 	= isset( $new_instance['purchase_button'] )   ? $new_instance['purchase_button'] : '';
-        $instance['categories'] 		= isset( $new_instance['categories'] )        ? $new_instance['categories']      : '';
-        $instance['tags'] 				= isset( $new_instance['tags'] )              ? $new_instance['tags']            : '';
+        $instance['title']           = strip_tags( $new_instance['title'] );
+        $instance['download_id']     = strip_tags( $new_instance['download_id'] );
+        $instance['download_title']  = isset( $new_instance['download_title'] )  ? $new_instance['download_title']  : '';
+        $instance['purchase_button'] = isset( $new_instance['purchase_button'] ) ? $new_instance['purchase_button'] : '';
+        $instance['categories']      = isset( $new_instance['categories'] )      ? $new_instance['categories']      : '';
+        $instance['tags']            = isset( $new_instance['tags'] )            ? $new_instance['tags']            : '';
 
-        do_action( 'edd_product_details_widget_update' , $instance );
+        do_action( 'edd_product_details_widget_update', $instance );
         
         return $instance;
     } 
