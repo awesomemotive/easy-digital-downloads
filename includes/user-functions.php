aff759ccd3718dbf5ddc2f3eb9a1328c98b64248
Pippin Williamson <pippin@pippinsplugins.com>
2014-07-17 16:50:29 -0500

    Rework edd_get_purchase_stats_by_user() to use the new  customer API. #2256

--- includes/user-functions.php
+++ includes/user-functions.php
@@ -213,47 +213,20 @@ function edd_has_purchases( $user_id = null ) {
  */
 function edd_get_purchase_stats_by_user( $user = '' ) {
 
-	global $wpdb;
-
-	$stats = array(
-		'purchases'   => 0,
-		'total_spent' => 0
-	);
-
-	if( is_email( $user ) )
+	if ( is_email( $user ) ) {
+	
 		$field = 'email';
-	elseif( is_numeric( $user ) )
-		$field = 'id';
-	else
-		return $stats;
-
-	$stats = wp_cache_get( $user, 'customers' );
-
-	if( false == $stats ) {
-
-		$query = "SELECT {$wpdb->prefix}meta_1.meta_value AS payment_total
-			FROM {$wpdb->prefix}postmeta {$wpdb->prefix}meta_1
-			LEFT JOIN {$wpdb->prefix}postmeta {$wpdb->prefix}meta_2
-				ON {$wpdb->prefix}meta_1.post_id = {$wpdb->prefix}meta_2.post_id
-				AND {$wpdb->prefix}meta_1.meta_key = '_edd_payment_total'
-			INNER JOIN {$wpdb->prefix}posts
-				ON {$wpdb->prefix}posts.ID = {$wpdb->prefix}meta_2.post_id
-				AND {$wpdb->prefix}posts.post_status = 'publish'
-			WHERE {$wpdb->prefix}meta_2.meta_key = '_edd_payment_user_{$field}'
-			AND {$wpdb->prefix}meta_2.meta_value = '%s'";
-
-		$purchases = $wpdb->get_col( $wpdb->prepare( $query, $user ) );
-
-		$purchases = array_filter( $purchases );
-
-		if( $purchases ) {
-			$stats['purchases']   = count( $purchases );
-			$stats['total_spent'] = round( array_sum( $purchases ), 2 );
-		}
-
-		wp_cache_set( $user, $stats, 'customers' );
-
+	
+	} elseif ( is_numeric( $user ) ) {
+	
+		$field = 'user_id';
+	
 	}
+
+	$customer = EDD()->customers->get_by( $field, $user );
+
+	$stats['purchases']   = absint( $customer->purchase_count );
+	$stats['total_spent'] = edd_sanitize_amount( $customer->purchase_value );
 
 	return (array) apply_filters( 'edd_purchase_stats_by_user', $stats, $user );
 }
