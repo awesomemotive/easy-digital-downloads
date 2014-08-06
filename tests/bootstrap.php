e83c053f54d88759c3f0eb3ce32206bb2fb6f970
Pippin Williamson <pippin@pippinsplugins.com>
2014-08-05 23:35:29 -0500

    Disable PHP sessions in unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,7 @@
 ini_set('display_errors','on');
 error_reporting(E_ALL);
 define( 'EDD_PLUGIN_DIR', dirname( dirname( __FILE__ ) ) . '/'  );
+define( 'EDD_USE_PHP_SESSIONS', false );
 
 require_once dirname( __FILE__ ) . '/../tmp/wordpress-tests/includes/functions.php';
 
