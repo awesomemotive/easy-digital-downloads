072c68dc9eb634c0975f56d61cfddc1223c978ac
Pippin Williamson <pippin@pippinsplugins.com>
2014-08-06 22:18:30 -0500

    Remove index for email in customer table. INdex is created automatically when the unique key is set. #2256

--- includes/class-edd-db-customers.php
+++ includes/class-edd-db-customers.php
@@ -424,8 +424,7 @@ class EDD_DB_Customers extends EDD_DB  {
 		`date_created` datetime NOT NULL,
 		PRIMARY KEY  (id),
 		UNIQUE KEY email (email),
-		INDEX user_id (user_id),
-		INDEX email (email)
+		INDEX user_id (user_id)
 		) CHARACTER SET utf8 COLLATE utf8_general_ci;";
 
 		dbDelta( $sql );
