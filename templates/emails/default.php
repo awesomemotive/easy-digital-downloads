709fa121b849d0363b36c511b4f68d9f1a085264
Pippin Williamson <pippin@pippinsplugins.com>
2014-08-13 20:12:08 -0500

    Add default template

---
+++ templates/emails/default.php
@@ -0,0 +1,10 @@
+<?php $text_align = is_rtl() ? 'right' : 'left' ?>
+<div style="margin: 0; background-color: #fafafa; width: auto; padding: 30px;">
+	<center>
+		<div style="border: 1px solid #ddd; width: 660px; background: #f0f0f0; padding: 8px; margin: 0;">
+			<div id="edd-email-content" style="background: #fff; border: 1px solid #ddd; padding: 15px; text-align: <?php echo $text_align; ?>!important;">
+				{email}
+			</div>
+		</div>
+	</center>
+</div>
