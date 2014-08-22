416ca95354736566ba867411e044f15372267923
Pippin Williamson <pippin@pippinsplugins.com>
2014-08-22 13:29:01 -0500

    Minor CSS for email template base. #1810

--- templates/emails/footer.php
+++ templates/emails/footer.php
@@ -13,13 +13,13 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 // For gmail compatibility, including CSS styles in head/body are stripped out therefore styles need to be inline. These variables contain rules which are added to the template inline.
 $template_footer = "
 	border-top:0;
-	-webkit-border-radius:6px;
+	-webkit-border-radius:3px;
 ";
 
 $credit = "
 	border:0;
 	color: #000000;
-	font-family: Arial;
+	font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
 	font-size:12px;
 	line-height:125%;
 	text-align:center;
