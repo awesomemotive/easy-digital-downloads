ce3f562db9a56c4429e52716ed17a12e1c4c84ac
Pippin Williamson <pippin@pippinsplugins.com>
2014-08-22 13:29:01 -0500

    Minor CSS for email template base. #1810

--- templates/emails/header.php
+++ templates/emails/header.php
@@ -10,39 +10,42 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 // For gmail compatibility, including CSS styles in head/body are stripped out therefore styles need to be inline. These variables contain rules which are added to the template inline. !important; is a gmail hack to prevent styles being stripped if it doesn't like something.
+$body = "
+	background-color: #f6f6f6;
+	font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
+";
 $wrapper = "
-	background-color: #f0f0f0;
 	width:100%;
 	-webkit-text-size-adjust:none !important;
 	margin:0;
 	padding: 70px 0 70px 0;
 ";
 $template_container = "
-	box-shadow:0 0 0 3px rgba(0,0,0,0.025) !important;
-	border-radius:6px !important;
+	box-shadow:0 0 0 1px #f3f3f3 !important;
+	border-radius:3px !important;
 	background-color: #ffffff;
-	border: 1px solid #000;
-	border-radius:6px !important;
+	border: 1px solid #e9e9e9;
+	border-radius:3px !important;
+	padding: 20px;
 ";
 $template_header = "
-	background-color: #cccccc;
 	color: #00000;
-	border-top-left-radius:6px !important;
-	border-top-right-radius:6px !important;
+	border-top-left-radius:3px !important;
+	border-top-right-radius:3px !important;
 	border-bottom: 0;
-	font-family:Arial;
 	font-weight:bold;
 	line-height:100%;
+	text-align: center;
 	vertical-align:middle;
 ";
 $body_content = "
-	background-color: #dddddd;
-	border-radius:6px !important;
+	border-radius:3px !important;
+	font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
 ";
 $body_content_inner = "
 	color: #000000;
-	font-family:Arial;
 	font-size:14px;
+	font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
 	line-height:150%;
 	text-align:left;
 ";
@@ -50,13 +53,11 @@ $header_content_h1 = "
 	color: #000000;
 	margin:0;
 	padding: 28px 24px;
-	text-shadow: 0 1px 0 #000000;
 	display:block;
-	font-family:Arial;
-	font-size:30px;
-	font-weight:bold;
-	text-align:left;
-	line-height: 150%;
+	font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
+	font-size:32px;
+	font-weight: 500;
+	line-height: 1.2;
 ";
 ?>
 <!DOCTYPE html>
@@ -65,7 +66,7 @@ $header_content_h1 = "
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <title><?php echo get_bloginfo( 'name' ); ?></title>
 	</head>
-    <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+    <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="<?php echo $body; ?>">
     	<div style="<?php echo $wrapper; ?>">
         	<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
             	<tr>
@@ -75,11 +76,11 @@ $header_content_h1 = "
 	                			//echo '<p style="margin-top:0;"><img src="' . esc_url( $img ) . '" alt="' . get_bloginfo( 'name' ) . '" /></p>';
 	                		?>
 						</div>
-                    	<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_container" style="<?php echo $template_container; ?>">
+                    	<table border="0" cellpadding="0" cellspacing="0" width="520" id="template_container" style="<?php echo $template_container; ?>">
                         	<tr>
                             	<td align="center" valign="top">
                                     <!-- Header -->
-                                	<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_header" style="<?php echo $template_header; ?>" bgcolor="#ffffff">
+                                	<table border="0" cellpadding="0" cellspacing="0" width="520" id="template_header" style="<?php echo $template_header; ?>" bgcolor="#ffffff">
                                         <tr>
                                             <td>
                                             	<h1 style="<?php echo $header_content_h1; ?>"><?php _e( 'Purchase Receipt', 'edd' ); ?></h1>
@@ -92,7 +93,7 @@ $header_content_h1 = "
                         	<tr>
                             	<td align="center" valign="top">
                                     <!-- Body -->
-                                	<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_body">
+                                	<table border="0" cellpadding="0" cellspacing="0" width="520" id="template_body">
                                     	<tr>
                                             <td valign="top" style="<?php echo $body_content; ?>">
                                                 <!-- Content -->
