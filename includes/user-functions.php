19f16f37e8b8175daeb29b83e4b58ff11a271256
Pippin Williamson <pippin@pippinsplugins.com>
2014-08-24 18:37:08 -0500

    Properly pass  to get_column_by(). #2256

--- includes/user-functions.php
+++ includes/user-functions.php
@@ -68,7 +68,7 @@ function edd_get_users_purchases( $user = 0, $number = 20, $pagination = false, 
 		
 	}
 	
-	$payment_ids = EDD()->customers->get_column_by( 'payment_ids', 'user_id', $user );
+	$payment_ids = EDD()->customers->get_column_by( 'payment_ids', $field, $user );
 
 	if( ! empty( $payment_ids ) ) {
 		unset( $args['user'] );
