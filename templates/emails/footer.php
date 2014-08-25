c0f584188e22e771a5589a3ee31a459e0c10c421
Pippin Williamson <pippin@pippinsplugins.com>
2014-08-25 16:17:16 -0500

    Reformat header and footer HTML. #1810

--- templates/emails/footer.php
+++ templates/emails/footer.php
@@ -36,16 +36,16 @@ $credit = "
                                     <!-- End Body -->
                                 </td>
                             </tr>
-                        	<tr>
-                            	<td align="center" valign="top">
+                            <tr>
+                                <td align="center" valign="top">
                                     <!-- Footer -->
-                                	<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer" style="<?php echo $template_footer; ?>">
-                                    	<tr>
-                                        	<td valign="top">
+                                    <table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer" style="<?php echo $template_footer; ?>">
+                                        <tr>
+                                            <td valign="top">
                                                 <table border="0" cellpadding="10" cellspacing="0" width="100%">
                                                     <tr>
                                                         <td colspan="2" valign="middle" id="credit" style="<?php echo $credit; ?>">
-                                                        	<?php echo wpautop( wp_kses_post( wptexturize( apply_filters( 'edd_email_footer_text', '<a href="' . esc_url( home_url() ) . '">' . get_bloginfo( 'name' ) . '</a>' ) ) ) ); ?>
+                                                           <?php echo wpautop( wp_kses_post( wptexturize( apply_filters( 'edd_email_footer_text', '<a href="' . esc_url( home_url() ) . '">' . get_bloginfo( 'name' ) . '</a>' ) ) ) ); ?>
                                                         </td>
                                                     </tr>
                                                 </table>
