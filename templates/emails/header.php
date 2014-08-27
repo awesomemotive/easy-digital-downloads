e6cce64b26b2bb0f8f91d2120b71dbb3c9b01cdc
Pippin Williamson <pippin@pippinsplugins.com>
2014-08-26 21:22:29 -0500

    minor tweaks for full backwards compatibility with Email Templates extension. #1810

--- templates/emails/header.php
+++ templates/emails/header.php
@@ -9,12 +9,6 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 // This is the footer used if no others are available
-
-$body = "
-	max-width: 600px;
-	margin: 0 auto;
-";
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,4 +16,4 @@ $body = "
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title><?php echo get_bloginfo( 'name' ); ?></title>
 	</head>
-	<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="<?php echo $body; ?>">
+	<body>
