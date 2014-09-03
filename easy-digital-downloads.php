223c1dc97146849052d4ecf9c407e85ae87a6905
Pippin Williamson <pippin@pippinsplugins.com>
2014-09-03 09:20:51 -0500

    Version bump to 2.1

--- easy-digital-downloads.php
+++ easy-digital-downloads.php
@@ -5,7 +5,7 @@
  * Description: Serve Digital Downloads Through WordPress
  * Author: Pippin Williamson
  * Author URI: http://pippinsplugins.com
- * Version: 2.0.4
+ * Version: 2.1
  * Text Domain: edd
  * Domain Path: languages
  *
@@ -25,7 +25,7 @@
  * @package EDD
  * @category Core
  * @author Pippin Williamson
- * @version 2.0.4
+ * @version 2.1
  */
 
 // Exit if accessed directly
@@ -185,7 +185,7 @@ final class Easy_Digital_Downloads {
 
 		// Plugin version
 		if ( ! defined( 'EDD_VERSION' ) ) {
-			define( 'EDD_VERSION', '2.0.4' );
+			define( 'EDD_VERSION', '2.1' );
 		}
 
 		// Plugin Folder Path
