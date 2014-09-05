dc6879ec28eb12dfbb367ba19b2124aabdd20bf2
Pippin Williamson <pippin@pippinsplugins.com>
2014-09-04 19:34:53 -0500

    Remove var dump from checkout. oops

--- templates/checkout_cart.php
+++ templates/checkout_cart.php
@@ -1,4 +1,3 @@
-<?php echo '<pre>'; print_r( edd_get_cart_content_details() ); echo '</pre>'; ?>
 <?php global $post; ?>
 <table id="edd_checkout_cart" <?php if ( ! edd_is_ajax_disabled() ) { echo 'class="ajaxed"'; } ?>>
 	<thead>
