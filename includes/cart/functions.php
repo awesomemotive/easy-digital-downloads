aa7ccbe67af799b5e2a48f534adf98462567bde6
Pippin Williamson <pippin@pippinsplugins.com>
2014-09-08 14:51:10 -0500

    Force cart item quantities to one if they are not enabled. #2598

--- includes/cart/functions.php
+++ includes/cart/functions.php
@@ -44,6 +44,8 @@ function edd_get_cart_content_details() {
 
 	foreach( $cart_items as $key => $item ) {
 
+		$item['quantity'] = edd_item_quantities_enabled() ? absint( $item['quantity'] ) : 1;
+
 		$item_price = edd_get_cart_item_price( $item['id'], $item['options'] );
 		$discount   = apply_filters( 'edd_get_cart_content_details_item_discount_amount', edd_get_cart_item_discount_amount( $item ), $item );
 		$quantity   = edd_get_cart_item_quantity( $item['id'], $item['options'] );
