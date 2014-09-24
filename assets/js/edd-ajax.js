91ca17b7a65768ff5bb563474d8218ca84b00d8c
Chris Klosowski <cklosowski@gmail.com>
2014-09-23 23:25:28 -0700

    #2585 - Making variable price JS logic more isolated

--- assets/js/edd-ajax.js
+++ assets/js/edd-ajax.js
@@ -104,15 +104,16 @@ jQuery(document).ready(function ($) {
 
         if( variable_price == 'yes' ) {
 
-            if( ! $('.edd_price_option_' + download + ':checked, input:radio, input:checkbox', form).length ) {
-                 // hide the spinner
-                $this.removeAttr( 'data-edd-loading' );
-                alert( edd_scripts.select_option );
-                return;
-            }
             if ( $('.edd_price_option_' + download).is('input:hidden') ) {
             	item_price_ids[0] = $('.edd_price_option_' + download).val();
             } else {
+	            if( ! $('.edd_price_option_' + download + ':checked', form).length ) {
+	                 // hide the spinner
+	                $this.removeAttr( 'data-edd-loading' );
+	                alert( edd_scripts.select_option );
+	                return;
+	            }
+
 	            $('.edd_price_option_' + download + ':checked', form).each(function( index ) {
 	                item_price_ids[ index ] = $(this).val();
 	            });
