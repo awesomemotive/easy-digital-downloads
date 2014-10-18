ed434423738e9261dd1a383421cc1f8e7bacb9ea
Pippin Williamson <pippin@pippinsplugins.com>
2014-10-18 11:18:36 -0500

    Add classes to export postboxes. #2658

--- includes/admin/reporting/reports.php
+++ includes/admin/reporting/reports.php
@@ -295,7 +295,7 @@ function edd_reports_tab_export() {
 	
 					<?php do_action( 'edd_reports_tab_export_content_top' ); ?>
 	
-					<div class="postbox">
+					<div class="postbox edd-export-pdf-sales-earnings">
 						<h3><span><?php _e( 'Export PDF of Sales and Earnings', 'edd' ); ?></span></h3>
 						<div class="inside">
 							<p><?php _e( 'Download a PDF of Sales and Earnings reports for all products for the current year.', 'edd' ); ?> <?php _e( 'Date range reports will be coming soon.', 'edd' ); ?></p>
@@ -303,7 +303,7 @@ function edd_reports_tab_export() {
 						</div><!-- .inside -->
 					</div><!-- .postbox -->
 	
-					<div class="postbox">
+					<div class="postbox edd-export-sales-earnings">
 						<h3><span><?php _e( 'Export Earnings and Sales Stats', 'edd' ); ?></span></h3>
 						<div class="inside">
 							<p><?php _e( 'Download a CSV of earnings and sales over time.', 'edd' ); ?></p>
@@ -321,7 +321,7 @@ function edd_reports_tab_export() {
 						</div><!-- .inside -->
 					</div><!-- .postbox -->
 
-					<div class="postbox">
+					<div class="postbox edd-export-payment-history">
 						<h3><span><?php _e('Export Payment History', 'edd'); ?></span></h3>
 						<div class="inside">
 							<p><?php _e( 'Download a CSV of all payments recorded.', 'edd' ); ?></p>
@@ -345,7 +345,7 @@ function edd_reports_tab_export() {
 						</div><!-- .inside -->
 					</div><!-- .postbox -->
 	
-					<div class="postbox">
+					<div class="postbox edd-export-customers">
 						<h3><span><?php _e('Export Customers in CSV', 'edd'); ?></span></h3>
 						<div class="inside">
 							<p><?php _e( 'Download a CSV of all customer emails. Optionally export only customers that have purchased a particular product. Note, if you have a large number of customers, exporting the purchase stats may fail.', 'edd' ); ?></p>
