2e6a6322be9fe978df8ed0f0f08fb28b2404f903
Pippin Williamson <pippin@pippinsplugins.com>
2014-10-20 10:55:01 -0500

    Simplify filter

--- includes/cart/functions.php
+++ includes/cart/functions.php
@@ -47,7 +47,8 @@ function edd_get_cart_content_details() {
 		$item['quantity'] = edd_item_quantities_enabled() ? absint( $item['quantity'] ) : 1;
 
 		$item_price = edd_get_cart_item_price( $item['id'], $item['options'] );
-		$discount   = apply_filters( 'edd_get_cart_content_details_item_discount_amount', edd_get_cart_item_discount_amount( $item ), $item );
+		$discount   = edd_get_cart_item_discount_amount( $item );
+		$discount   = apply_filters( 'edd_get_cart_content_details_item_discount_amount', $discount, $item );
 		$quantity   = edd_get_cart_item_quantity( $item['id'], $item['options'] );
 		$subtotal   = $item_price * $quantity;
 		$tax        = edd_get_cart_item_tax( $item['id'], $item['options'], $subtotal - $discount );
