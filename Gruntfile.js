620ad89750367af6ff0c2a25dae839f2dec2e172
fxbenard <fxbenard@gmail.com>
2014-10-21 13:11:52 +0200

    [Grunt]> bump wp-i18n to 0.4.9 Edit gruntfile to include pot headers based on that

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,20 +14,17 @@ grunt.initConfig({
           exclude: ['build/.*'],
           mainFile: 'easy-digital-downloads.php',    // Main project file.
           potFilename: 'edd.pot',    // Name of the POT file.
+          potHeaders: {
+                    poedit: true,                 // Includes common Poedit headers.
+                    'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
+                },
           type: 'wp-plugin',    // Type of project (wp-plugin or wp-theme).
           updateTimestamp: true,    // Whether the POT-Creation-Date should be updated without other changes.
           processPot: function( pot, options ) {
             pot.headers['report-msgid-bugs-to'] = 'https://easydigitaldownloads.com/';
-            pot.headers['plural-forms'] = 'nplurals=2; plural=n != 1;';
-            pot.headers['last-translator'] = 'WP-Translations (http://wp-translations.org/)\n';
-            pot.headers['language-team'] = 'WP-Translations (http://www.transifex.com/projects/p/wp-translations/)\n';
-            pot.headers['x-poedit-basepath'] = '..\n';
-            pot.headers['x-poedit-language'] = 'English\n';
-            pot.headers['x-poedit-country'] = 'UNITED STATES\n';
-            pot.headers['x-poedit-sourcecharset'] = 'utf-8\n';
-            pot.headers['x-poedit-searchpath-0'] = '.\n';
-            pot.headers['x-poedit-keywordslist'] = '__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n';
-            pot.headers['x-textdomain-support'] = 'yes\n';
+            pot.headers['last-translator'] = 'WP-Translations (http://wp-translations.org/)';
+            pot.headers['language-team'] = 'WP-Translations <wpt@wp-translations.org>';
+            pot.headers['language'] = 'en_US';
             return pot;
           }
         }
