0d7c6d5f2721456d74f61e6d084c390563d28c84
Sean Davis <sdavis2702@gmail.com>
2014-10-27 22:05:15 -0500

    remove icon HTML from translation string

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -134,7 +134,7 @@ function edd_get_purchase_link( $args = array() ) {
 				<span class="edd-cart-ajax-alert">
 					<span class="edd-cart-added-alert" style="display: none;">
 						<?php printf(
-								__( '<i class="edd-icon-ok"></i> Added to cart', 'edd' ),
+								'<i class="edd-icon-ok"></i> ' . __( 'Added to cart', 'edd' ),
 								'<a href="' . esc_url( edd_get_checkout_uri() ) . '" title="' . __( 'Go to Checkout', 'edd' ) . '">',
 								'</a>'
 							);
