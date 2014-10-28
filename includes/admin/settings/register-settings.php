dd71bba7ffc5280de0847383c2d420edd8a3cf6a
Pippin Williamson <pippin@pippinsplugins.com>
2014-10-28 13:27:14 -0500

    Change label of default tax rate to Fallback Tax Rate. #2690

--- includes/admin/settings/register-settings.php
+++ includes/admin/settings/register-settings.php
@@ -441,7 +441,7 @@ function edd_get_registered_settings() {
 				),
 				'tax_rate' => array(
 					'id' => 'tax_rate',
-					'name' => __( 'Default Tax Rate', 'edd' ),
+					'name' => __( 'Fallback Tax Rate', 'edd' ),
 					'desc' => __( 'Enter a percentage, such as 6.5. Customers not in a specific rate below will be charged this rate.', 'edd' ),
 					'type' => 'text',
 					'size' => 'small'
