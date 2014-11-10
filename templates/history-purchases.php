31000b1136c7cd8d3be89de30a8c55c8b932a014
Nikhil Vimal <techvoltz@gmail.com>
2014-11-09 19:19:00 -0600

    Fixing template doc for history-purchases.php
    Small wording fix.

--- templates/history-purchases.php
+++ templates/history-purchases.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template of Purchase history for the current user.
+ * This template is used to display the purchase history of the current user.
  */
 $purchases = edd_get_users_purchases( get_current_user_id(), 20, true, 'any' );
 if ( $purchases ) : ?>
