aceca07e3b12618a926cf3fb4fc69ddb54950617
Chris Klosowski <cklosowski@gmail.com>
2015-01-01 09:55:48 -0700

    Fixing hardcoded years in unit tests #2906

--- tests/tests-html-elements.php
+++ tests/tests-html-elements.php
@@ -40,7 +40,7 @@ class Test_HTML_Elements extends WP_UnitTestCase {
 	}
 
 	public function test_year_dropdown() {
-		$expected = '<select name="year" id="" class="edd-select " data-placeholder=""><option value="2009">2009</option><option value="2010">2010</option><option value="2011">2011</option><option value="2012">2012</option><option value="2013">2013</option><option value="2014" selected=\'selected\'>2014</option></select>';
+		$expected = '<select name="year" id="" class="edd-select " data-placeholder=""><option value="2010">2010</option><option value="2011">2011</option><option value="2012">2012</option><option value="2013">2013</option><option value="2014">2014</option><option value="2015" selected=\'selected\'>2015</option></select>';
 		$this->assertEquals( $expected, EDD()->html->year_dropdown() );
 	}
 
