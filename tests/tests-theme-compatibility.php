9645b6c9101bb162cab778d3454e1795b21ed900
JeroenSormani <jeroen.sormani@gmail.com>
2015-01-25 21:47:28 +0100

    Fix theme compatibility test

--- tests/tests-theme-compatibility.php
+++ tests/tests-theme-compatibility.php
@@ -33,7 +33,7 @@ class Tests_Theme_Compatibility extends WP_UnitTestCase {
 		$this->go_to( get_permalink( $post_id ) );
 
 		// Test some regular values in a post (should be unaffected)
-		$this->assertContains( 'post', get_post_class() );
+		$this->assertContains( 'post-' . $post_id, get_post_class() );
 		$this->assertContains( 'type-post', get_post_class() );
 
 		// Reset to origin
