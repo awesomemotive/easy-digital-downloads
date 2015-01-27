d0798e9b84f052aeeed5f3ff648c04290b767265
Ghost1227 <dgriffiths@section214.com>
2015-01-26 19:02:55 -0600

    Remove unnecessary global

--- includes/admin/reporting/class-export-payments.php
+++ includes/admin/reporting/class-export-payments.php
@@ -57,8 +57,6 @@ class EDD_Payments_Export extends EDD_Export {
 	 * @return array $cols All the columns
 	 */
 	public function csv_cols() {
-		global $edd_options;
-
 		$cols = array(
 			'id'       => __( 'ID',   'edd' ), // unaltered payment ID (use for querying)
 			'seq_id'   => __( 'Payment Number',   'edd' ), // sequential payment ID
@@ -104,7 +102,7 @@ class EDD_Payments_Export extends EDD_Export {
 	 * @return array $data The data for the CSV file
 	 */
 	public function get_data() {
-		global $wpdb, $edd_options;
+		global $wpdb;
 
 		$data = array();
 
