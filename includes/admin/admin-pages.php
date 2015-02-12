49006a7b1104a34abacfcafe3b146325a9e9c473
Ghost1227 <dgriffiths@section214.com>
2015-02-11 23:38:16 -0600

    Fix indentation

--- includes/admin/admin-pages.php
+++ includes/admin/admin-pages.php
@@ -34,10 +34,10 @@ function edd_add_options_link() {
 	$edd_store_menu         = add_menu_page( __( 'Payment History', 'edd' ), __( 'Store', 'edd' ), 'manage_shop_settings', 'edd-store', 'edd_payment_history_page', 'dashicons-store', 25.544 );
 	$edd_payments_page      = add_submenu_page( 'edd-store', $edd_payment->labels->name, $edd_payment->labels->menu_name, 'edit_shop_payments', 'edd-store', 'edd_payment_history_page' );
 	$edd_discounts_page     = add_submenu_page( 'edd-store', __( 'Discount Codes', 'edd' ), __( 'Discount Codes', 'edd' ), 'manage_shop_discounts', 'edd-discounts', 'edd_discounts_page' );
-	$edd_reports_page 	    = add_submenu_page( 'edd-store', __( 'Earnings and Sales Reports', 'edd' ), __( 'Reports', 'edd' ), 'view_shop_reports', 'edd-reports', 'edd_reports_page' );
-	$edd_settings_page 	    = add_submenu_page( 'edd-store', __( 'Easy Digital Download Settings', 'edd' ), __( 'Settings', 'edd' ), 'manage_shop_settings', 'edd-settings', 'edd_options_page' );
+	$edd_reports_page       = add_submenu_page( 'edd-store', __( 'Earnings and Sales Reports', 'edd' ), __( 'Reports', 'edd' ), 'view_shop_reports', 'edd-reports', 'edd_reports_page' );
+	$edd_settings_page      = add_submenu_page( 'edd-store', __( 'Easy Digital Download Settings', 'edd' ), __( 'Settings', 'edd' ), 'manage_shop_settings', 'edd-settings', 'edd_options_page' );
 	$edd_tools_page         = add_submenu_page( 'edd-store', __( 'Easy Digital Download Info and Tools', 'edd' ), __( 'Tools', 'edd' ), 'install_plugins', 'edd-tools', 'edd_tools_page' );
-	$edd_add_ons_page 	    = add_submenu_page( 'edd-store', __( 'Easy Digital Download Add Ons', 'edd' ), __( 'Add Ons', 'edd' ), 'install_plugins', 'edd-addons', 'edd_add_ons_page' );
+	$edd_add_ons_page       = add_submenu_page( 'edd-store', __( 'Easy Digital Download Add Ons', 'edd' ), __( 'Add Ons', 'edd' ), 'install_plugins', 'edd-addons', 'edd_add_ons_page' );
 	$edd_upgrades_screen    = add_submenu_page( null, __( 'EDD Upgrades', 'edd' ), __( 'EDD Upgrades', 'edd' ), 'install_plugins', 'edd-upgrades', 'edd_upgrades_screen' );
 }
 add_action( 'admin_menu', 'edd_add_options_link', 10 );
