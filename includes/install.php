17c4c4361e269582ced762ead08c81b72d29967f
Chris Klosowski <cklosowski@gmail.com>
2015-03-11 14:20:14 -0700

    Set upgrades to copmlete on fresh install #3248

--- includes/install.php
+++ includes/install.php
@@ -151,6 +151,18 @@ function edd_install() {
 		return;
 	}
 
+	if ( ! $current_version ) {
+		// When new upgrade routines are added, mark them as complete on fresh install
+		$upgrade_routines = array(
+			'upgrade_payment_taxes',
+			'upgrade_customer_payments_association'
+		);
+
+		foreach ( $upgrade_routines as $upgrade ) {
+			edd_set_upgrade_complete( $upgrade );
+		}
+	}
+
 	// Add the transient to redirect
 	set_transient( '_edd_activation_redirect', true, 30 );
 }
