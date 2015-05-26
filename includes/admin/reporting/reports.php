ba3604064ad9fef6b2ce462705870624faa14f28
Pippin Williamson <pippin@pippinsplugins.com>
2015-05-26 13:31:49 -0500

    First pass at upgrading custom export for batch processing. #2999. Also standardizes columns for #3434

--- includes/admin/reporting/reports.php
+++ includes/admin/reporting/reports.php
@@ -356,8 +356,8 @@ function edd_reports_tab_export() {
 						<div class="inside">
 							<p><?php _e( 'Download a CSV of all customer emails. Optionally export only customers that have purchased a particular product. Note, if you have a large number of customers, exporting the purchase stats may fail.', 'edd' ); ?></p>
 							<p>
-								<form method="post" id="edd_customer_export">
-									<select name="edd_export_download" id="edd_customer_export_download">
+								<form id="edd-export-customers" class="edd-export-form" method="post">
+									<select name="download" id="edd_customer_export_download">
 										<option value="0"><?php printf( __( 'All %s', 'edd' ), edd_get_label_plural() ); ?></option>
 										<?php
 										$downloads = get_posts( array( 'post_type' => 'download', 'posts_per_page' => -1 ) );
@@ -368,12 +368,7 @@ function edd_reports_tab_export() {
 										}
 										?>
 									</select>
-									<select name="edd_export_option" id="edd_customer_export_option">
-										<option value="emails"><?php _e( 'Emails', 'edd' ); ?></option>
-										<option value="emails_and_names"><?php _e( 'Emails and Names', 'edd' ); ?></option>
-										<option value="full"><?php _e( 'Emails, Names, and Purchase Stats', 'edd' ); ?></option>
-									</select>
-									<input type="hidden" name="edd-action" value="email_export"/>
+									<input type="hidden" name="edd-export-class" value="EDD_Batch_Customers_Export"/>
 									<input type="submit" value="<?php _e( 'Generate CSV', 'edd' ); ?>" class="button-secondary"/>
 								</form>
 							</p>
