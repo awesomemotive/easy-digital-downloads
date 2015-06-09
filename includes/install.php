5213d354ca10a4641504509208dea9c440c03450
Pippin Williamson <pippin@pippinsplugins.com>
2015-06-09 14:50:32 -0500

    Set default version with v. #3337

--- includes/install.php
+++ includes/install.php
@@ -138,7 +138,7 @@ function edd_install() {
 	$roles->add_caps();
 
 	$api = new EDD_API;
-	update_option( 'edd_default_api_version', $api->get_version() );
+	update_option( 'edd_default_api_version', 'v' . $api->get_version() );
 
 	// Create the customers database
 	@EDD()->customers->create_table();
