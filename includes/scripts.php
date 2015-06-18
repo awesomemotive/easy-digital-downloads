0daaa267e8ea68f1865fe0b131a8983865962677
Chris Klosowski <cklosowski@gmail.com>
2015-06-18 08:37:38 -0700

    Fixing AJAX of the cart widget #2506

--- includes/scripts.php
+++ includes/scripts.php
@@ -79,7 +79,8 @@ function edd_load_scripts() {
 			'redirect_to_checkout'    => ( edd_straight_to_checkout() || edd_is_checkout() ) ? '1' : '0',
 			'checkout_page'           => edd_get_checkout_uri(),
 			'permalinks'              => get_option( 'permalink_structure' ) ? '1' : '0',
-			'quantities_enabled'      => edd_item_quantities_enabled()
+			'quantities_enabled'      => edd_item_quantities_enabled(),
+			'taxes_enabled'           => edd_use_taxes() ? '1' : '0', // Adding here for widget, but leaving in checkout vars for backcompat
 		) ) );
 	}
 }
--- templates/widget-cart-checkout.php
+++ templates/widget-cart-checkout.php
@@ -1,6 +1,5 @@
 <?php if ( edd_use_taxes() ) : ?>
 <li class="cart_item edd-cart-meta edd_subtotal"><?php echo __( 'Subtotal:', 'edd' ). " <span class='subtotal'>" . edd_currency_filter( edd_format_amount( edd_get_cart_subtotal() ) ); ?></span></li>
-<li class="cart_item edd-cart-meta edd_purchase_tax_rate"><?php _e( 'Estimated Tax:', 'edd' ); ?> <?php echo edd_currency_filter( edd_format_amount( edd_get_cart_tax() ) ); ?></li>
+<li class="cart_item edd-cart-meta edd_cart_tax"><?php _e( 'Estimated Tax:', 'edd' ); ?> <span class="cart-tax"><?php echo edd_currency_filter( edd_format_amount( edd_get_cart_tax() ) ); ?></span></li>
 <?php endif; ?>
-<li class="cart_item edd-cart-meta edd_total"><?php _e( 'Total:', 'edd' ); ?> <?php echo edd_currency_filter( edd_format_amount( edd_get_cart_total() ) ); ?></li>
-
+<li class="cart_item edd-cart-meta edd_total"><?php _e( 'Total:', 'edd' ); ?> <span class="cart-total"><?php echo edd_currency_filter( edd_format_amount( edd_get_cart_total() ) ); ?></span></li>
