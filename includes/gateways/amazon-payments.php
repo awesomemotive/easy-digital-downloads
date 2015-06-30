a6bb82b092a71cf4fe87d1a28371963d6ba75fdd
Pippin Williamson <pippin@pippinsplugins.com>
2015-06-30 09:05:25 -0500

    print_client via wp_head not wp_enqueue_scripts. #3223

--- includes/gateways/amazon-payments.php
+++ includes/gateways/amazon-payments.php
@@ -144,7 +144,7 @@ final class EDD_Amazon_Payments {
 	 */
 	private function actions() {
 
-		add_action( 'wp_enqueue_scripts',                      array( $this, 'print_client' ), 10 );
+		add_action( 'wp_head',                                 array( $this, 'print_client' ), 10 );
 		add_action( 'wp_enqueue_scripts',                      array( $this, 'load_scripts' ), 11 );
 		add_action( 'init',                                    array( $this, 'capture_oauth' ), 9 );
 		add_action( 'init',                                    array( $this, 'signin_redirect' ) );
