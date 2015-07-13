027476f967c838214ed64f1c658907098828eee0
Justin Sainton <justinsainton@gmail.com>
2015-07-13 15:51:06 -0700

    Signup is a noun.  Sign up is a verb.

--- includes/admin/welcome.php
+++ includes/admin/welcome.php
@@ -400,10 +400,10 @@ class EDD_Welcome {
 				<div class="feature-section">
 
 					<h4><?php _e( 'Get Notified of Extension Releases','edd' );?></h4>
-					<p><?php _e( 'New extensions that make Easy Digital Downloads even more powerful are released nearly every single week. Subscribe to the newsletter to stay up to date with our latest releases. <a href="http://eepurl.com/kaerz" target="_blank">Signup now</a> to ensure you do not miss a release!', 'edd' );?></p>
+					<p><?php _e( 'New extensions that make Easy Digital Downloads even more powerful are released nearly every single week. Subscribe to the newsletter to stay up to date with our latest releases. <a href="http://eepurl.com/kaerz" target="_blank">Sign up now</a> to ensure you do not miss a release!', 'edd' );?></p>
 
 					<h4><?php _e( 'Get Alerted About New Tutorials', 'edd' );?></h4>
-					<p><?php _e( '<a href="http://eepurl.com/kaerz" target="_blank">Signup now</a> to hear about the latest tutorial releases that explain how to take Easy Digital Downloads further.', 'edd' );?></p>
+					<p><?php _e( '<a href="http://eepurl.com/kaerz" target="_blank">Sign up now</a> to hear about the latest tutorial releases that explain how to take Easy Digital Downloads further.', 'edd' );?></p>
 
 				</div>
 			</div>
