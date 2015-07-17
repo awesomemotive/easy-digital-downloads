79277ed9c91956212f740ef42e9c9128b07cdb67
wolfkang <wolfkang@gmail.com>
2015-07-17 17:50:57 +0900

    Get all the purchase of the logged-in user.

--- includes/user-functions.php
+++ includes/user-functions.php
@@ -67,14 +67,13 @@ function edd_get_users_purchases( $user = 0, $number = 20, $pagination = false, 
 	}
 
 	$by_user_id = is_numeric( $user ) ? true : false;
-	$customer   = new EDD_Customer( $user, $by_user_id );
-
-	if( ! empty( $customer->payment_ids ) ) {
-
-		unset( $args['user'] );
-		$args['post__in'] = array_map( 'absint', explode( ',', $customer->payment_ids ) );
-
-	}
+        if (!$by_user_id) {
+                $customer   = new EDD_Customer( $user, $by_user_id );
+                if( ! empty( $customer->payment_ids ) ) {
+                        unset( $args['user'] );
+                        $args['post__in'] = array_map( 'absint', explode( ',', $customer->payment_ids ) );
+                }
+        }
 
 	$purchases = edd_get_payments( apply_filters( 'edd_get_users_purchases_args', $args ) );
 
