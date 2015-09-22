8047f3c179a9d3258d9c0d70f0667c882db1366f
Pippin Williamson <pippin@pippinsplugins.com>
2015-09-22 12:22:10 -0500

    Properly account for Last Week when it crosses a month boundary. #3482

--- includes/admin/reporting/graphing.php
+++ includes/admin/reporting/graphing.php
@@ -640,7 +640,7 @@ function edd_get_report_dates() {
 			$dates['m_start']   = $month;
 			$dates['m_end']     = $month;
 			$dates['year']      = $year;
-			$dates['year_end']      = $year;
+			$dates['year_end']  = $year;
 		break;
 
 		case 'this_week' :
@@ -656,18 +656,18 @@ function edd_get_report_dates() {
 			$dates['day']       = date( 'd', $current_time - ( date( 'w' ) - 1 ) *60*60*24 ) - 8;
 			$dates['day']      += get_option( 'start_of_week' );
 			$dates['day_end']   = $dates['day'] + 6;
-			$dates['year']		= date( 'Y' );
+			$dates['year']      = date( 'Y' );
 
 			if( date( 'j', $current_time ) <= 7 ) {
-				$dates['m_start'] 	= date( 'n', $current_time ) - 1;
-				$dates['m_end']		= date( 'n', $current_time ) - 1;
+				$dates['m_start']   = date( 'n', $current_time );
+				$dates['m_end']     = date( 'n', $current_time );
 				if( $dates['m_start'] <= 1 ) {
 					$dates['year'] = date( 'Y', $current_time ) - 1;
 					$dates['year_end'] = date( 'Y', $current_time ) - 1;
 				}
 			} else {
-				$dates['m_start'] 	= date( 'n', $current_time );
-				$dates['m_end']		= date( 'n', $current_time );
+				$dates['m_start']   = date( 'n', $current_time );
+				$dates['m_end']     = date( 'n', $current_time );
 			}
 		break;
 
