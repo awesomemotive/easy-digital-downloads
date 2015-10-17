92691ab4a355adc38be35cd5d72b1b8ae742dbf5
Chris <labontychris@gmail.com>
2015-10-16 22:02:10 -0700

    fix spacing issue

--- templates/shortcode-receipt.php
+++ templates/shortcode-receipt.php
@@ -155,7 +155,7 @@ $status    = edd_get_payment_status( $payment, true );
 						</div>
 
 						<?php if ( $edd_receipt_args['notes'] ) : ?>
-							<div class="edd_purchase_receipt_product_notes"><?php echo wp_kses_post(wpautop( edd_get_product_notes( $item['id']) ) ); ?></div>
+							<div class="edd_purchase_receipt_product_notes"><?php echo wp_kses_post(wpautop( edd_get_product_notes( $item['id'] ) ) ); ?></div>
 						<?php endif; ?>
 
 						<?php
