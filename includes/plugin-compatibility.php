43985c4c8b9f22fa37f973b3af113d08b4673709
Pippin Williamson <pippin@pippinsplugins.com>
2015-11-03 12:36:04 -0600

    Introduce edd_qtranslate_prevent_redirect() to disable the language redirect in qTranslate. #3635

--- includes/plugin-compatibility.php
+++ includes/plugin-compatibility.php
@@ -81,7 +81,7 @@ add_filter( 'edd_settings_misc', 'edd_append_no_cache_param', -1 );
 
 /**
  * Show the correct language on the [downloads] short code if qTranslate is active
-  *
+ *
  * @since 1.7
  * @param string $content Download content
  * @return string $content Download content
@@ -93,6 +93,23 @@ function edd_qtranslate_content( $content ) {
 }
 add_filter( 'edd_downloads_content', 'edd_qtranslate_content' );
 add_filter( 'edd_downloads_excerpt', 'edd_qtranslate_content' );
+
+/**
+ * Prevents qTranslate from redirecting to language-specific URL when downloading purchased files
+ *
+ * @since 2.5
+ * @param string       $target Target URL
+ * @return string|bool $target Target URL. False if redirect is disabled
+ */
+function edd_qtranslate_prevent_redirect( $target ) {
+
+	if( strpos( $target, 'eddfile' ) ) {
+		$target = false;
+	}
+
+	return $target;
+}
+add_filter( 'qtranslate_language_detect_redirect', 'edd_qtranslate_prevent_redirect' );
 
 /**
  * Disable the WooCommerce 'Un-force SSL when leaving checkout' option on EDD checkout
