0c2ed6cfc042c6cf24efc11a96172f6cb13f266f
Chris Klosowski <cklosowski@gmail.com>
2015-11-18 11:08:38 -0700

    Move our santization functions in metabox.php to the class #2480

--- includes/class-edd-register-meta.php
+++ includes/class-edd-register-meta.php
@@ -68,8 +68,8 @@ class EDD_Register_Meta {
 		register_meta( 'post', '_edd_download_earnings' , 'edd_sanitize_amount' );
 		register_meta( 'post', '_edd_download_sales'    , array( $this, 'intval_wrapper' ) );
 		register_meta( 'post', 'edd_price'              , 'edd_sanitize_amount' );
-		register_meta( 'post', 'edd_variable_prices'    , array( $this, 'sanitize_array' ) );
-		register_meta( 'post', 'edd_download_files'     , array( $this, 'sanitize_array' ) );
+		register_meta( 'post', 'edd_variable_prices'    , array( $this, 'sanitize_variable_prices' ) );
+		register_meta( 'post', 'edd_download_files'     , array( $this, 'sanitize_files' ) );
 		register_meta( 'post', '_edd_bundled_products'  , array( $this, 'sanitize_array' ) );
 		register_meta( 'post', '_edd_button_behavior'   , 'sanitize_text_field' );
 		register_meta( 'post', '_edd_default_price_id'  , array( $this, 'intval_wrapper' ) );
@@ -129,6 +129,67 @@ class EDD_Register_Meta {
 		}
 
 		return $value;
+	}
+
+	/**
+	 * Sanitize the variable prices
+	 *
+	 * Ensures prices are correctly mapped to an array starting with an index of 0
+	 *
+	 * @since 2.5
+	 * @param array $prices Variable prices
+	 * @return array $prices Array of the remapped variable prices
+	 */
+	function sanitize_variable_prices( $prices = array() ) {
+		if ( ! is_array( $prices ) ) {
+			return $prices;
+		}
+
+		foreach( $prices as $id => $price ) {
+
+			if( empty( $price['amount'] ) && empty( $price['name'] ) ) {
+
+				unset( $prices[ $id ] );
+				continue;
+
+			} elseif( empty( $price['amount'] ) ) {
+
+				$price['amount'] = 0;
+
+			}
+
+			$prices[ $id ]['amount'] = edd_sanitize_amount( $price['amount'] );
+
+		}
+
+		return $prices;
+	}
+
+	/**
+	 * Sanitize the file downloads
+	 *
+	 * Ensures files are correctly mapped to an array starting with an index of 0
+	 *
+	 * @since 2.5
+	 * @param array $files Array of all the file downloads
+	 * @return array $files Array of the remapped file downloads
+	 */
+	function sanitize_files( $files = array() ) {
+
+		// Clean up filenames to ensure whitespaces are stripped
+		foreach( $files as $id => $file ) {
+
+			if( ! empty( $files[ $id ]['file'] ) ) {
+				$files[ $id ]['file'] = trim( $file['file'] );
+			}
+
+			if( ! empty( $files[ $id ]['name'] ) ) {
+				$files[ $id ]['name'] = trim( $file['name'] );
+			}
+		}
+
+		// Make sure all files are rekeyed starting at 0
+		return array_values( $files );
 	}
 
 }
