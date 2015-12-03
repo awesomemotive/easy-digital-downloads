c3f2b73dc3435501a1099466ac2f57c2daec4b0f
Pippin Williamson <pippin@pippinsplugins.com>
2015-12-03 00:29:58 -0500

    Array of arrays for proper meta_query. #3633

--- includes/admin/tools/class-edd-tools-recount-single-customer-stats.php
+++ includes/admin/tools/class-edd-tools-recount-single-customer-stats.php
@@ -225,8 +225,10 @@ class EDD_Tools_Recount_Single_Customer_Stats extends EDD_Batch_Export {
 				'post__not_in' => $attached_payment_ids,
 				'number'       => -1,
 				'meta_query'   => array(
-					'key'     => '_edd_payment_email',
-					'value'   => $customer->email,
+					array(
+						'key'   => '_edd_payment_email',
+						'value' => $customer->email,
+					)
 				),
 			);
 
