7fcb7a2691fbf51e825076ac2dc525d95994d6a3
katanyan <closure28@gmail.com>
2016-01-26 14:42:43 +0900

    Uninstall function for multisite.

--- uninstall.php
+++ uninstall.php
@@ -25,69 +25,102 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit;
 // Load EDD file.
 include_once( 'easy-digital-downloads.php' );
 
-global $wpdb, $wp_roles;
+function edd_uninstall() {
+	global $wpdb, $edd_options;
 
-if( edd_get_option( 'uninstall_on_delete' ) ) {
+	if ( is_multisite() ) {
+		foreach ( $wpdb->get_col( "SELECT blog_id FROM {$wpdb->blogs}" ) as $blog_id ) {
+			switch_to_blog( $blog_id );
 
-	/** Delete All the Custom Post Types */
-	$edd_taxonomies = array( 'download_category', 'download_tag', 'edd_log_type', );
-	$edd_post_types = array( 'download', 'edd_payment', 'edd_discount', 'edd_log' );
-	foreach ( $edd_post_types as $post_type ) {
+			/**
+			 * Load `edd_settings` and update global variable for switched site.
+			 *
+			 * @pending  Generally, best way is probably below.
+			 *
+			 * Update global option variable at `switch_blog` action.
+			 *
+			 *	add_action( 'switch_blog', 'update_function_name', 10, 2 );
+			 *
+			 */
+			$edd_options = edd_get_settings();
 
-		$edd_taxonomies = array_merge( $edd_taxonomies, get_object_taxonomies( $post_type ) );
-		$items = get_posts( array( 'post_type' => $post_type, 'post_status' => 'any', 'numberposts' => -1, 'fields' => 'ids' ) );
+			edd_cleanup();
 
-		if ( $items ) {
-			foreach ( $items as $item ) {
-				wp_delete_post( $item, true);
-			}
+			restore_current_blog();
 		}
+	} else {
+		edd_cleanup();
 	}
-
-	/** Delete All the Terms & Taxonomies */
-	foreach ( array_unique( array_filter( $edd_taxonomies ) ) as $taxonomy ) {
-
-		$terms = $wpdb->get_results( $wpdb->prepare( "SELECT t.*, tt.* FROM $wpdb->terms AS t INNER JOIN $wpdb->term_taxonomy AS tt ON t.term_id = tt.term_id WHERE tt.taxonomy IN ('%s') ORDER BY t.name ASC", $taxonomy ) );
-
-		// Delete Terms.
-		if ( $terms ) {
-			foreach ( $terms as $term ) {
-				$wpdb->delete( $wpdb->term_taxonomy, array( 'term_taxonomy_id' => $term->term_taxonomy_id ) );
-				$wpdb->delete( $wpdb->terms, array( 'term_id' => $term->term_id ) );
-			}
-		}
-
-		// Delete Taxonomies.
-		$wpdb->delete( $wpdb->term_taxonomy, array( 'taxonomy' => $taxonomy ), array( '%s' ) );
-	}
-
-	/** Delete the Plugin Pages */
-	$edd_created_pages = array( 'purchase_page', 'success_page', 'failure_page', 'purchase_history_page' );
-	foreach ( $edd_created_pages as $p ) {
-		$page = edd_get_option( $p, false );
-		if ( $page ) {
-			wp_delete_post( $page, true );
-		}
-	}
-
-	/** Delete all the Plugin Options */
-	delete_option( 'edd_settings' );
-	delete_option( 'edd_version' );
-
-	/** Delete Capabilities */
-	EDD()->roles->remove_caps();
-
-	/** Delete the Roles */
-	$edd_roles = array( 'shop_manager', 'shop_accountant', 'shop_worker', 'shop_vendor' );
-	foreach ( $edd_roles as $role ) {
-		remove_role( $role );
-	}
-
-	// Remove all database tables
-	$wpdb->query( "DROP TABLE IF EXISTS " . $wpdb->prefix . "edd_customers" );
-
-	/** Cleanup Cron Events */
-	wp_clear_scheduled_hook( 'edd_daily_scheduled_events' );
-	wp_clear_scheduled_hook( 'edd_daily_cron' );
-	wp_clear_scheduled_hook( 'edd_weekly_cron' );
 }
+
+function edd_cleanup() {
+	global $wpdb, $wp_roles;
+
+	if ( edd_get_option( 'uninstall_on_delete' ) ) {
+
+		/** Delete All the Custom Post Types */
+		$edd_taxonomies = array( 'download_category', 'download_tag', 'edd_log_type', );
+		$edd_post_types = array( 'download', 'edd_payment', 'edd_discount', 'edd_log' );
+		foreach ( $edd_post_types as $post_type ) {
+
+			$edd_taxonomies = array_merge( $edd_taxonomies, get_object_taxonomies( $post_type ) );
+			$items = get_posts( array( 'post_type' => $post_type, 'post_status' => 'any', 'numberposts' => -1, 'fields' => 'ids' ) );
+
+			if ( $items ) {
+				foreach ( $items as $item ) {
+					wp_delete_post( $item, true);
+				}
+			}
+		}
+
+		/** Delete All the Terms & Taxonomies */
+		foreach ( array_unique( array_filter( $edd_taxonomies ) ) as $taxonomy ) {
+
+			$terms = $wpdb->get_results( $wpdb->prepare( "SELECT t.*, tt.* FROM $wpdb->terms AS t INNER JOIN $wpdb->term_taxonomy AS tt ON t.term_id = tt.term_id WHERE tt.taxonomy IN ('%s') ORDER BY t.name ASC", $taxonomy ) );
+
+			// Delete Terms.
+			if ( $terms ) {
+				foreach ( $terms as $term ) {
+					$wpdb->delete( $wpdb->term_taxonomy, array( 'term_taxonomy_id' => $term->term_taxonomy_id ) );
+					$wpdb->delete( $wpdb->term_relationships, array( 'term_taxonomy_id' => $term->term_taxonomy_id ) );
+					$wpdb->delete( $wpdb->terms, array( 'term_id' => $term->term_id ) );
+				}
+			}
+
+			// Delete Taxonomies.
+			$wpdb->delete( $wpdb->term_taxonomy, array( 'taxonomy' => $taxonomy ), array( '%s' ) );
+		}
+
+		/** Delete the Plugin Pages */
+		$edd_created_pages = array( 'purchase_page', 'success_page', 'failure_page', 'purchase_history_page' );
+		foreach ( $edd_created_pages as $p ) {
+			$page = edd_get_option( $p, false );
+			if ( $page ) {
+				wp_delete_post( $page, true );
+			}
+		}
+
+		/** Delete all the Plugin Options */
+		delete_option( 'edd_settings' );
+		delete_option( 'edd_version' );
+
+		/** Delete Capabilities */
+		EDD()->roles->remove_caps();
+
+		/** Delete the Roles */
+		$edd_roles = array( 'shop_manager', 'shop_accountant', 'shop_worker', 'shop_vendor' );
+		foreach ( $edd_roles as $role ) {
+			remove_role( $role );
+		}
+
+		// Remove all database tables
+		$wpdb->query( "DROP TABLE IF EXISTS " . $wpdb->prefix . "edd_customers" );
+
+		/** Cleanup Cron Events */
+		wp_clear_scheduled_hook( 'edd_daily_scheduled_events' );
+		wp_clear_scheduled_hook( 'edd_daily_cron' );
+		wp_clear_scheduled_hook( 'edd_weekly_cron' );
+	}
+}
+
+edd_uninstall();
