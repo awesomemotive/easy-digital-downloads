46ab81c47cf77d82c57ecf3ac8045a532e65cd01
Pippin Williamson <pippin@pippinsplugins.com>
2016-05-04 16:37:00 -0500

    Correct status return values in get_payment_status tests. #4041

--- tests/tests-payments.php
+++ tests/tests-payments.php
@@ -123,18 +123,18 @@ class Tests_Payments extends WP_UnitTestCase {
 	}
 
 	public function test_get_payment_status() {
-		$this->assertEquals( 'publish', edd_get_payment_status( $this->_payment_id ) );
-		$this->assertEquals( 'publish', edd_get_payment_status( get_post( $this->_payment_id ) ) );
+		$this->assertEquals( 'pending', edd_get_payment_status( $this->_payment_id ) );
+		$this->assertEquals( 'pending', edd_get_payment_status( get_post( $this->_payment_id ) ) );
 		$payment = new EDD_Payment( $this->_payment_id );
-		$this->assertEquals( 'publish', edd_get_payment_status( $payment ) );
+		$this->assertEquals( 'pending', edd_get_payment_status( $payment ) );
 		$this->assertFalse( edd_get_payment_status( 1212121212121 ) );
 	}
 
 	public function test_get_payment_status_label() {
-		$this->assertEquals( 'Complete', edd_get_payment_status( $this->_payment_id, true ) );
-		$this->assertEquals( 'Complete', edd_get_payment_status( get_post( $this->_payment_id, true ) ) );
+		$this->assertEquals( 'Pending', edd_get_payment_status( $this->_payment_id, true ) );
+		$this->assertEquals( 'Pending', edd_get_payment_status( get_post( $this->_payment_id, true ) ) );
 		$payment = new EDD_Payment( $this->_payment_id );
-		$this->assertEquals( 'Complete', edd_get_payment_status( $payment, true ) );
+		$this->assertEquals( 'Pending', edd_get_payment_status( $payment, true ) );
 	}
 
 	public function test_get_payment_statuses() {
