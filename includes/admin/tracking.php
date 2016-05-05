f25c8cae63a3ff6a3c77665d5e25a9e551268041
Pippin Williamson <pippin@pippinsplugins.com>
2016-05-05 11:04:44 -0500

    Add locale to tracking info. #4520

--- includes/admin/tracking.php
+++ includes/admin/tracking.php
@@ -102,6 +102,7 @@ class EDD_Tracking {
 		$data['inactive_plugins'] = $plugins;
 		$data['products']         = wp_count_posts( 'download' )->publish;
 		$data['download_label']   = edd_get_label_singular( true );
+		$data['locale']           = get_locale();
 
 		$this->data = $data;
 	}
