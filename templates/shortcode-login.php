9bcc3a8e4146fa0b9b943b27544eca118409e8c0
Tareq Hasan <tareq1988@gmail.com>
2016-05-05 20:44:16 +0600

    Remember me option added on login form

--- templates/shortcode-login.php
+++ templates/shortcode-login.php
@@ -11,15 +11,18 @@ if ( ! is_user_logged_in() ) :
 		<fieldset>
 			<span><legend><?php _e( 'Log into Your Account', 'easy-digital-downloads' ); ?></legend></span>
 			<?php do_action( 'edd_login_fields_before' ); ?>
-			<p>
+			<p class="edd-login-username">
 				<label for="edd_user_login"><?php _e( 'Username or Email', 'easy-digital-downloads' ); ?></label>
 				<input name="edd_user_login" id="edd_user_login" class="required edd-input" type="text" title="<?php _e( 'Username or Email', 'easy-digital-downloads' ); ?>"/>
 			</p>
-			<p>
+			<p class="edd-login-password">
 				<label for="edd_user_pass"><?php _e( 'Password', 'easy-digital-downloads' ); ?></label>
 				<input name="edd_user_pass" id="edd_user_pass" class="password required edd-input" type="password"/>
 			</p>
-			<p>
+			<p class="edd-login-remember">
+				<label><input name="rememberme" type="checkbox" id="rememberme" value="forever" /> <?php _e( 'Remember Me', 'easy-digital-downloads' ); ?></label>
+			</p>
+			<p class="edd-login-submit">
 				<input type="hidden" name="edd_redirect" value="<?php echo esc_url( $edd_login_redirect ); ?>"/>
 				<input type="hidden" name="edd_login_nonce" value="<?php echo wp_create_nonce( 'edd-login-nonce' ); ?>"/>
 				<input type="hidden" name="edd_action" value="user_login"/>
