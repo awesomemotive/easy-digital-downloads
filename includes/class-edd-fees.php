6bfaef7256a2c04b8e394d4d55d7d1b7239bff32
Diego Zanella <businessdad75@gmail.com>
2016-05-13 18:22:16 +0100

    Restored EDD_Fees class from EDD 2.5.x

--- includes/class-edd-fees.php
+++ includes/class-edd-fees.php
@@ -97,10 +97,7 @@ class EDD_Fees {
 		$args['amount'] = edd_sanitize_amount( $args['amount'] );
 
 		// Set the fee
-		$fees[ $key ] = apply_filters( 'edd_fees_add_fee', $args, $this );
-
-		// Allow 3rd parties to process the fees before storing them in the session
-		$fees = apply_filters( 'edd_fees_set_fees', $fees, $this );
+		$fees[ $key ] = $args;
 
 		// Update fees
 		EDD()->session->set( 'edd_cart_fees', $fees );
@@ -219,8 +216,7 @@ class EDD_Fees {
 
 		}
 
-		// Allow 3rd parties to process the fees before returning them
-		return apply_filters( 'edd_fees_get_fees', ! empty( $fees ) ? $fees : array(), $this );
+		return ! empty( $fees ) ? $fees : array();
 	}
 
 	/**
