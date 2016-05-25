a3f83cd122ff4ab07e5572f4c0635806a0227679
Pippin Williamson <pippin@pippinsplugins.com>
2016-05-24 21:22:40 -0500

    Some initial handling for payment importing. #3620

--- includes/admin/import/class-batch-import-payments.php
+++ includes/admin/import/class-batch-import-payments.php
@@ -21,6 +21,15 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 class EDD_Batch_Payments_Import extends EDD_Batch_Import {
 
+	public function init() {
+
+		// Set up default field map values
+		$payment = new EDD_Paymet;
+
+		$this->field_mapping = $payment->array_convert();
+
+	}
+
 	/**
 	 * Process a step
 	 *
@@ -35,30 +44,34 @@ class EDD_Batch_Payments_Import extends EDD_Batch_Import {
 			wp_die( __( 'You do not have permission to import data.', 'easy-digital-downloads' ), __( 'Error', 'easy-digital-downloads' ), array( 'response' => 403 ) );
 		}
 
-		$csv = new parseCSV();
-		$csv->auto( $this->file );
+		$i      = 1;
+		$offset = $this->step > 1 ? ( $this->per_step * $this->step ) : 0;
 
-		if( $csv->data ) {
+		if( $offset > $this->total ) {
+			$this->done = true;
+		}
 
-			$i    = 0;
+		if( ! $this->done && $this->csv->data ) {
+
 			$more = true;
 
-			foreach( $csv->data as $key => $row ) {
+			foreach( $this->csv->data as $key => $row ) {
+
+				// Skip all rows until we reach pass our offset
+				if( $key + 1 < $offset ) {
+					continue;
+				}
 
 				// Done with this batch
-				if( $i >= 19 ) {
+				if( $i >= $this->per_step ) {
 					break;
 				}
 
 				// Import payment
 				$this->create_payment( $row );
 
-				// Once payment is imported, remove row
-				unset( $csv->data[ $key ] );
 				$i++;
 			}
-
-			$csv->save();
 
 		}
 
@@ -67,10 +80,177 @@ class EDD_Batch_Payments_Import extends EDD_Batch_Import {
 
 	public function create_payment( $row = array() ) {
 
-		$payment = new EDD_Payment;
+		$payment = new EDD_Payment;		
 
-		
+		if( ! empty( $this->field_mapping['total'] ) && ! empty( $row[ $this->field_mapping['total'] ] ) ) {
 
+			$payment->total = edd_sanitize_amount( $row[ $this->field_mapping['total'] ] );
+
+		}
+
+		if( ! empty( $this->field_mapping['subtotal'] ) && ! empty( $row[ $this->field_mapping['subtotal'] ] ) ) {
+
+			$payment->subtotal = edd_sanitize_amount( $row[ $this->field_mapping['subtotal'] ] );
+
+		}
+
+		if( ! empty( $this->field_mapping['tax'] ) && ! empty( $row[ $this->field_mapping['tax'] ] ) ) {
+
+			$payment->tax = edd_sanitize_amount( $row[ $this->field_mapping['tax'] ] );
+
+		}
+
+		if( ! empty( $this->field_mapping['number'] ) && ! empty( $row[ $this->field_mapping['number'] ] ) ) {
+
+			$payment->number = sanitize_text_field( $row[ $this->field_mapping['number'] ] );
+
+		}
+
+		if( ! empty( $this->field_mapping['mode'] ) && ! empty( $row[ $this->field_mapping['mode'] ] ) ) {
+
+			$mode = strtolower( sanitize_text_field( $row[ $this->field_mapping['mode'] ] ) );
+			$mode = 'test' != $mode && 'live' != $mode ? false : $mode;
+			if( ! $mode ) {
+				$mode = edd_is_test_mode() ? 'test' : 'live';
+			}
+
+			$payment->mode = $mode;
+
+		}
+
+		if( ! empty( $this->field_mapping['date'] ) && ! empty( $row[ $this->field_mapping['date'] ] ) ) {
+
+			$date = sanitize_text_field( $row[ $this->field_mapping['date'] ] );
+
+			if( ! strtotime( $date ) ) {
+
+				$date = date( 'Y-n-d H:i:s', current_time( 'timestamp' ) );
+
+			}
+
+			$payment->date = $date;
+
+		}
+
+		if( ! empty( $this->field_mapping['completed_date'] ) && ! empty( $row[ $this->field_mapping['completed_date'] ] ) ) {
+
+			$completed_date = sanitize_text_field( $row[ $this->field_mapping['completed_date'] ] );
+
+			if( ! strtotime( $completed_date ) ) {
+
+				$completed_date = date( 'Y-n-d H:i:s', current_time( 'timestamp' ) );
+
+			}
+
+			$payment->completed_date = $completed_date;
+
+		}
+
+		if( ! empty( $this->field_mapping['status'] ) && ! empty( $row[ $this->field_mapping['status'] ] ) ) {
+
+			$status   = sanitize_text_field( $row[ $this->field_mapping['status'] ] );
+			$statuses = edd_get_payment_statuses();
+
+			if( ! array_key_exists( $status, $statuses ) ) {
+
+				$status = 'pending';
+	
+			}
+
+			$payment->status = $status;
+
+		}
+
+		if( ! empty( $this->field_mapping['customer_id'] ) && ! empty( $row[ $this->field_mapping['customer_id'] ] ) ) {
+
+			$customer_id = absint( $row[ $this->field_mapping['customer_id'] ] );
+
+			$customer = new EDD_Customer( $customer_id );
+
+			if( $customer->id > 0 ) {
+
+				$payment->customer_id = $customer_id;
+	
+			}
+
+		}
+
+		if( ! empty( $this->field_mapping['email'] ) && ! empty( $row[ $this->field_mapping['email'] ] ) ) {
+
+			$payment->email = sanitize_text_field( $row[ $this->field_mapping['email'] ] );
+
+		}
+
+		if( ! empty( $this->field_mapping['first_name'] ) && ! empty( $row[ $this->field_mapping['first_name'] ] ) ) {
+
+			$payment->first_name = sanitize_text_field( $row[ $this->field_mapping['first_name'] ] );
+
+		}
+
+		if( ! empty( $this->field_mapping['last_name'] ) && ! empty( $row[ $this->field_mapping['last_name'] ] ) ) {
+
+			$payment->last_name = sanitize_text_field( $row[ $this->field_mapping['last_name'] ] );
+
+		}
+
+		if( ! empty( $this->field_mapping['user_id'] ) && ! empty( $row[ $this->field_mapping['user_id'] ] ) ) {
+
+			$user_id = absint( $row[ $this->field_mapping['user_id'] ] );
+
+			$user = get_userdata( $user_id );
+
+			if( $user ) {
+
+				$payment->user_id = $user->ID;
+	
+			}
+
+		}
+
+		if( ! empty( $this->field_mapping['transaction_id'] ) && ! empty( $row[ $this->field_mapping['transaction_id'] ] ) ) {
+
+			$payment->transaction_id = sanitize_text_field( $row[ $this->field_mapping['transaction_id'] ] );
+
+		}
+
+		if( ! empty( $this->field_mapping['ip'] ) && ! empty( $row[ $this->field_mapping['ip'] ] ) ) {
+
+			$payment->ip = sanitize_text_field( $row[ $this->field_mapping['ip'] ] );
+
+		}
+
+		if( ! empty( $this->field_mapping['gateway'] ) && ! empty( $row[ $this->field_mapping['gateway'] ] ) ) {
+
+			$payment->gateway = sanitize_text_field( $row[ $this->field_mapping['gateway'] ] );
+
+		}
+
+		if( ! empty( $this->field_mapping['currency'] ) && ! empty( $row[ $this->field_mapping['currency'] ] ) ) {
+
+			$payment->currency = strtoupper( sanitize_text_field( $row[ $this->field_mapping['currency'] ] ) );
+
+		}
+
+		if( ! empty( $this->field_mapping['parent_payment_id'] ) && ! empty( $row[ $this->field_mapping['parent_payment_id'] ] ) ) {
+
+			$payment->parent_payment_id = absint( $row[ $this->field_mapping['parent_payment_id'] ] );
+
+		}
+
+		if( ! empty( $this->field_mapping['downloads'] ) && ! empty( $row[ $this->field_mapping['downloads'] ] ) ) {
+
+			$downloads = $this->str_to_array( $row[ $this->field_mapping['downloads'] ] );
+			if( is_array( $downloads ) ) {
+
+				foreach( $downloads as $download ) {
+
+					
+
+				}
+				
+			}
+
+		}
 
 	}
 
