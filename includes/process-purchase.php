df08854906378a481dd14a404bb9bcb8829f3d69
Pippin Williamson <pippin@pippinsplugins.com>
2016-05-27 12:07:27 -0500

    Instantiate customer with user ID. #3755

--- includes/process-purchase.php
+++ includes/process-purchase.php
@@ -145,10 +145,10 @@ function edd_checkout_check_existing_email( $valid_data, $post ) {
 	if ( is_user_logged_in() ) {
 
 		$email    = $valid_data['logged_in_user']['user_email'];
-		$customer = new EDD_Customer( get_current_user_id() );
+		$customer = new EDD_Customer( get_current_user_id(), true );
 
 		// If this email address is not registered with this customer, see if it belongs to any other customer
-		if ( $email !== $customer->email && ( is_array( $customer->emails ) && ! in_array( $email, $customer->emails ) ) ) {
+		if ( $email != $customer->email && ( is_array( $customer->emails ) && ! in_array( $email, $customer->emails ) ) ) {
 			$found_customer = new EDD_Customer( $email );
 			if ( $found_customer->id > 0 ) {
 				edd_set_error( 'edd-customer-email-exists', __( sprintf( 'The email address %s is already in use.', $email ), 'easy-digital-downloads' ) );
