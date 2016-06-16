4a09c535ebe6a6b21c69b2d3e9081c7c75436193
Chris Klosowski <cklosowski@gmail.com>
2016-06-16 14:39:59 -0700

    Adding grunt task to download translations from .org

--- Gruntfile.js
+++ Gruntfile.js
@@ -152,6 +152,17 @@ grunt.initConfig({
 		}
 	},
 
+	glotpress_download: {
+		core: {
+			options: {
+				domainPath: 'languages',
+				url: 'https://translate.wordpress.org',
+				slug: 'wp-plugins/easy-digital-downloads/dev',
+				textdomain: 'easy-digital-downloads',
+			}
+		},
+	},
+
 });
 
 // Default task. - grunt makepot
