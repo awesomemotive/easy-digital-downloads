7f4dfdee539a24fe8002a9a32b8e40cf4001dfbf
Pippin Williamson <pippin@pippinsplugins.com>
2016-08-12 12:47:13 -0500

    Change gif to png. #4238

--- includes/checkout/template.php
+++ includes/checkout/template.php
@@ -657,7 +657,7 @@ function edd_show_payment_icons() {
 
 			} else {
 
-				$image       = edd_locate_template( 'images' . DIRECTORY_SEPARATOR . 'icons' . DIRECTORY_SEPARATOR . $card . '.gif', false );
+				$image       = edd_locate_template( 'images' . DIRECTORY_SEPARATOR . 'icons' . DIRECTORY_SEPARATOR . $card . '.png', false );
 				$content_dir = WP_CONTENT_DIR;
 
 				if( function_exists( 'wp_normalize_path' ) ) {
