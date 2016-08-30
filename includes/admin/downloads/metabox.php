a51f685659b3b2ec30108e1111ce1e46fbabbc95
Phil Johnston <support@mintplugins.com>
2016-08-30 18:37:40 -0400

    Capitalize Buy Now (the letter N)

--- includes/admin/downloads/metabox.php
+++ includes/admin/downloads/metabox.php
@@ -936,7 +936,7 @@ function edd_render_disable_button( $post_id ) {
 			<?php if ( $supports_buy_now ) : ?>
 				<span alt="f223" class="edd-help-tip dashicons dashicons-editor-help" title="<?php _e( '<strong>Button Behavior</strong>: Add to Cart buttons follow a traditional eCommerce flow. A Buy Now button bypasses most of the process, taking the customer directly from button click to payment, greatly speeding up the process of buying the product.', 'easy-digital-downloads' ); ?>"></span>
 			<?php else: ?>
-				<span alt="f223" class="edd-help-tip dashicons dashicons-editor-help" title="<?php _e( '<strong>Button Behavior</strong>: Add to Cart buttons follow a traditional eCommerce flow. Buy now buttons are only available for supported gateways and stores that do not use taxes.', 'easy-digital-downloads' ); ?>"></span>
+				<span alt="f223" class="edd-help-tip dashicons dashicons-editor-help" title="<?php _e( '<strong>Button Behavior</strong>: Add to Cart buttons follow a traditional eCommerce flow. Buy Now buttons are only available for supported gateways and stores that do not use taxes.', 'easy-digital-downloads' ); ?>"></span>
 			<?php endif; ?>
 
 		</label>
