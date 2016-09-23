5e99fe47ebdba2eb3e95a088a209e7125deda874
Chris Klosowski <cklosowski@gmail.com>
2016-09-23 15:46:28 -0700

    Add support for variable price file conditions to ex/import #4969 (#4972)
    * Add support for variable price file conditions to ex/import #4969
    
    * Use 1 based indexes for price IDs #4969

--- includes/admin/import/class-batch-import-downloads.php
+++ includes/admin/import/class-batch-import-downloads.php
@@ -264,6 +264,7 @@ class EDD_Batch_Downloads_Import extends EDD_Batch_Import {
 			if( ! empty( $prices ) ) {
 
 				$variable_prices = array();
+				$price_id        = 1;
 				foreach( $prices as $price ) {
 
 					// See if this matches the EDD Download export for variable prices
@@ -271,7 +272,8 @@ class EDD_Batch_Downloads_Import extends EDD_Batch_Import {
 
 						$price = array_map( 'trim', explode( ':', $price ) );
 
-						$variable_prices[] = array( 'name' => $price[0], 'amount' => $price[1] );
+						$variable_prices[ $price_id ] = array( 'name' => $price[ 0 ], 'amount' => $price[ 1 ] );
+						$price_id++;
 
 					}
 
@@ -299,7 +301,24 @@ class EDD_Batch_Downloads_Import extends EDD_Batch_Import {
 			$download_files = array();
 			foreach( $files as $file ) {
 
-				$download_files[] = array( 'file' => $file, 'name' => basename( $file ) );
+				$condition = '';
+
+				if ( strpos( $file, ';' ) ) {
+					$split_on  = strpos( $file, ';' );
+					$file_url  = substr( $file, 0, $split_on );
+					$condition = substr( $file, $split_on + 1 );
+				}
+
+				$download_file_args = array(
+					'file' => $file_url,
+					'name' => basename( $file_url ),
+				);
+
+				if ( ! empty( $condition ) ) {
+					$download_file_args['condition'] = $condition;
+				}
+
+				$download_files[] = $download_file_args;
 
 			}
 
