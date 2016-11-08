0eef1a3b6db7873028d5f90580a55f65a3c9fe35
Michael Beil <michaelbeil@me.com>
2016-11-08 13:13:58 +0100

    Add edd_profile_editor_before_name actionhook
    #5148

--- templates/shortcode-profile-editor.php
+++ templates/shortcode-profile-editor.php
@@ -28,6 +28,9 @@ if ( is_user_logged_in() ):
 
 	<form id="edd_profile_editor_form" class="edd_form" action="<?php echo edd_get_current_page_url(); ?>" method="post">
 		<fieldset>
+			
+			<?php do_action( 'edd_profile_editor_before_name' ); ?>
+			
 			<span id="edd_profile_name_label"><legend><?php _e( 'Change your Name', 'easy-digital-downloads' ); ?></legend></span>
 			<p id="edd_profile_name_wrap">
 				<label for="edd_first_name"><?php _e( 'First Name', 'easy-digital-downloads' ); ?></label>
