531bbe69789c90a24889fba85f7d7290a2e2f073
Chris Klosowski <cklosowski@gmail.com>
2016-11-11 15:27:10 -0700

    Use the cached resposne if we can #4854

--- includes/EDD_SL_Plugin_Updater.php
+++ includes/EDD_SL_Plugin_Updater.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * Allows plugins to use their own update API.
  *
  * @author Pippin Williamson
- * @version 1.6.5
+ * @version 1.6.6
  */
 class EDD_SL_Plugin_Updater {
 
@@ -58,7 +58,7 @@ class EDD_SL_Plugin_Updater {
 	 */
 	public function init() {
 
-		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'check_update' ) );
+		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'check_update' ), 10 );
 		add_filter( 'plugins_api', array( $this, 'plugins_api_filter' ), 10, 3 );
 		remove_action( 'after_plugin_row_' . $this->name, 'wp_plugin_update_row', 10, 2 );
 		add_action( 'after_plugin_row_' . $this->name, array( $this, 'show_update_notification' ), 10, 2 );
@@ -94,8 +94,15 @@ class EDD_SL_Plugin_Updater {
 		if ( ! empty( $_transient_data->response ) && ! empty( $_transient_data->response[ $this->name ] ) && false === $this->wp_override ) {
 			return $_transient_data;
 		}
+		$cache_key    = md5( 'edd_plugin_' . sanitize_key( $this->name ) . '_version_info' );
+		$version_info = get_transient( $cache_key );
 
-		$version_info = $this->api_request( 'plugin_latest_version', array( 'slug' => $this->slug ) );
+		if ( false === $version_info ) {
+
+			$version_info = $this->api_request( 'plugin_latest_version', array( 'slug' => $this->slug ) );
+
+			set_transient( $cache_key, $version_info, 3600 );
+		}
 
 		if ( false !== $version_info && is_object( $version_info ) && isset( $version_info->new_version ) ) {
 
@@ -149,18 +156,17 @@ class EDD_SL_Plugin_Updater {
 			$cache_key    = md5( 'edd_plugin_' . sanitize_key( $this->name ) . '_version_info' );
 			$version_info = get_transient( $cache_key );
 
-			if( false === $version_info ) {
-
+			if ( false === $version_info ) {
 				$version_info = $this->api_request( 'plugin_latest_version', array( 'slug' => $this->slug ) );
 
 				set_transient( $cache_key, $version_info, 3600 );
 			}
 
-			if( ! is_object( $version_info ) ) {
+			if ( ! is_object( $version_info ) ) {
 				return;
 			}
 
-			if( version_compare( $this->version, $version_info->new_version, '<' ) ) {
+			if ( version_compare( $this->version, $version_info->new_version, '<' ) ) {
 
 				$update_cache->response[ $this->name ] = $version_info;
 
