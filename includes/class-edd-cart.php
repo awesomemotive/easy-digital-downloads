8ed6d4a0a55a327f2cf0ad539c25e31766d15c0b
Sunny Ratilal <sunny@sunnyratilal.com>
2016-12-15 17:32:08 +0000

    Introduce base EDD_Cart wrapper class #2283

---
+++ includes/class-edd-cart.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Cart Object
+ *
+ * @package     EDD
+ * @subpackage  Classes/Download
+ * @copyright   Copyright (c) 2016, Sunny Ratilal
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       2.7
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * EDD_Cart Class
+ *
+ * @since 2.7
+ */
+class EDD_Cart {
+
+}
