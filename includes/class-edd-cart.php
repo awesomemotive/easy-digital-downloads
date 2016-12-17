d64ab03a950e0d8c38e520b5605575a905ea5e21
Sunny Ratilal <sunny@sunnyratilal.com>
2016-12-17 14:15:15 +0000

    Follow singleton pattern for the EDD_Cart class #2283

--- includes/class-edd-cart.php
+++ includes/class-edd-cart.php
@@ -19,6 +19,12 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 final class EDD_Cart {
 	/**
+	 * @var EDD_Cart
+	 * @since 1.4
+	 */
+	private static $instance;
+
+	/**
 	 * Cart contents
 	 *
 	 * @var array
@@ -73,4 +79,58 @@ final class EDD_Cart {
 	 * @var array
 	 */
 	private $session;
+
+	/**
+	 * EDD_Cart Instance.
+	 *
+	 * Insures that only one instance of EDD_Cart exists in memory.
+	 *
+	 * @since 2.7
+	 * @static
+	 * @staticvar array $instance
+	 * @return object|EDD_Cart Instance of EDD_Cart
+	 */
+	public static function instance() {
+		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof EDD_Cart ) ) {
+			self::$instance = new EDD_Cart;
+			self::$instance->setup_cart();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Throw error on object clone.
+	 *
+	 * The whole idea of the singleton design pattern is that there is a single
+	 * object therefore, we don't want the object to be cloned.
+	 *
+	 * @since 2.7
+	 * @access protected
+	 * @return void
+	 */
+	public function __clone() {
+		// Cloning instances of the class is forbidden.
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'easy-digital-downloads' ), '2.7' );
+	}
+
+	/**
+	 * Disable unserializing of the class.
+	 *
+	 * @since 2.7
+	 * @access protected
+	 * @return void
+	 */
+	public function __wakeup() {
+		// Unserializing instances of the class is forbidden.
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'easy-digital-downloads' ), '2.7' );
+	}
+
+	/**
+	 * Setup cart
+	 *
+	 * @since 2.7
+	 * @access private
+	 * @return void
+	 */
 }
