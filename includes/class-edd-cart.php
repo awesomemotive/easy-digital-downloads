ed5393d90abb8d2b47eab9623d357aff373c627f
Sunny Ratilal <sunny@sunnyratilal.com>
2016-12-17 14:11:12 +0000

    Mark class as final and add attributes #2283

--- includes/class-edd-cart.php
+++ includes/class-edd-cart.php
@@ -17,6 +17,60 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  *
  * @since 2.7
  */
-class EDD_Cart {
+final class EDD_Cart {
+	/**
+	 * Cart contents
+	 *
+	 * @var array
+	 */
+	private $cart;
 
+	/**
+	 * Details of the cart contents
+	 *
+	 * @var array
+	 */
+	private $details;
+
+	/**
+	 * Cart Quantity
+	 *
+	 * @var int
+	 */
+	private $quantity;
+
+	/**
+	 * Subtotal
+	 *
+	 * @var double
+	 */
+	private $subtotal;
+
+	/**
+	 * Total
+	 *
+	 * @var double
+	 */
+	private $total;
+
+	/**
+	 * Fees
+	 *
+	 * @var array
+	 */
+	private $fees;
+
+	/**
+	 * Tax
+	 *
+	 * @var double
+	 */
+	private $tax;
+
+	/**
+	 * Purchase Session
+	 *
+	 * @var array
+	 */
+	private $session;
 }
