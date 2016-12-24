61de340ac282b0d4639a177a9a78d2af8ac0c035
Sunny Ratilal <sunny@sunnyratilal.com>
2016-12-24 23:32:33 +0530

    Code cleanup

--- includes/payments/class-payment-stats.php
+++ includes/payments/class-payment-stats.php
@@ -22,39 +22,39 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * @since 1.8
  */
 class EDD_Payment_Stats extends EDD_Stats {
-
-
 	/**
 	 * Retrieve sale stats
 	 *
-	 * @access public
 	 * @since 1.8
-	 * @param INT $download_id The download product to retrieve stats for. If false, gets stats for all products
-	 * @param string|bool $start_date The starting date for which we'd like to filter our sale stats. If false, we'll use the default start date of `this_month`
-	 * @param string|bool $end_date The end date for which we'd like to filter our sale stats. If false, we'll use the default end date of `this_month`
-	 * @param string|array $status The sale status(es) to count. Only valid when retrieving global stats
+	 * @access public
+	 *
+	 * @param int          $download_id The download product to retrieve stats for. If false, gets stats for all products
+	 * @param string|bool  $start_date  The starting date for which we'd like to filter our sale stats. If false, we'll use the default start date of `this_month`
+	 * @param string|bool  $end_date    The end date for which we'd like to filter our sale stats. If false, we'll use the default end date of `this_month`
+	 * @param string|array $status      The sale status(es) to count. Only valid when retrieving global stats
+	 *
 	 * @return float|int Total amount of sales based on the passed arguments.
 	 */
 	public function get_sales( $download_id = 0, $start_date = false, $end_date = false, $status = 'publish' ) {
-
 		$this->setup_dates( $start_date, $end_date );
 
 		// Make sure start date is valid
-		if( is_wp_error( $this->start_date ) )
+		if ( is_wp_error( $this->start_date ) ) {
 			return $this->start_date;
+		}
 
 		// Make sure end date is valid
-		if( is_wp_error( $this->end_date ) )
+		if ( is_wp_error( $this->end_date ) ) {
 			return $this->end_date;
+		}
 
-		if( empty( $download_id ) ) {
-
+		if ( empty( $download_id ) ) {
 			// Global sale stats
 			add_filter( 'edd_count_payments_where', array( $this, 'count_where' ) );
 
-			if( is_array( $status ) ) {
+			if ( is_array( $status ) ) {
 				$count = 0;
-				foreach( $status as $payment_status ) {
+				foreach ( $status as $payment_status ) {
 					$count += edd_count_payments()->$payment_status;
 				}
 			} else {
@@ -62,57 +62,52 @@ class EDD_Payment_Stats extends EDD_Stats {
 			}
 
 			remove_filter( 'edd_count_payments_where', array( $this, 'count_where' ) );
-
 		} else {
-
 			$this->timestamp = false;
 
 			// Product specific stats
 			global $edd_logs;
 
 			add_filter( 'posts_where', array( $this, 'payments_where' ) );
-
 			$count = $edd_logs->get_log_count( $download_id, 'sale' );
-
 			remove_filter( 'posts_where', array( $this, 'payments_where' ) );
-
 		}
 
 		return $count;
-
 	}
-
 
 	/**
 	 * Retrieve earning stats
 	 *
-	 * @access public
 	 * @since 1.8
-	 * @param INT $download_id The download product to retrieve stats for. If false, gets stats for all products
-	 * @param string|bool $start_date The starting date for which we'd like to filter our sale stats. If false, we'll use the default start date of `this_month`
-	 * @param string|bool $end_date The end date for which we'd like to filter our sale stats. If false, we'll use the default end date of `this_month`
-	 * @param bool $include_taxes If taxes should be included in the earnings graphs
+	 * @access public
+	 *
+	 * @param int         $download_id   The download product to retrieve stats for. If false, gets stats for all products
+	 * @param string|bool $start_date    The starting date for which we'd like to filter our sale stats. If false, we'll use the default start date of `this_month`
+	 * @param string|bool $end_date      The end date for which we'd like to filter our sale stats. If false, we'll use the default end date of `this_month`
+	 * @param bool        $include_taxes If taxes should be included in the earnings graphs
+	 *
 	 * @return float|int Total amount of sales based on the passed arguments.
 	 */
 	public function get_earnings( $download_id = 0, $start_date = false, $end_date = false, $include_taxes = true ) {
-
 		global $wpdb;
+
 		$this->setup_dates( $start_date, $end_date );
 
 		// Make sure start date is valid
-		if( is_wp_error( $this->start_date ) )
+		if ( is_wp_error( $this->start_date ) ) {
 			return $this->start_date;
+		}
 
 		// Make sure end date is valid
-		if( is_wp_error( $this->end_date ) )
+		if ( is_wp_error( $this->end_date ) ) {
 			return $this->end_date;
+		}
 
 		add_filter( 'posts_where', array( $this, 'payments_where' ) );
 
 		if ( empty( $download_id ) ) {
-
 			// Global earning stats
-
 			$args = array(
 				'post_type'              => 'edd_payment',
 				'nopaging'               => true,
@@ -126,16 +121,15 @@ class EDD_Payment_Stats extends EDD_Stats {
 				'include_taxes'          => $include_taxes,
 			);
 
-			$args = apply_filters( 'edd_stats_earnings_args', $args );
-			$cached   = get_transient( 'edd_stats_earnings' );
-			$key      = md5( json_encode( $args ) );
+			$args   = apply_filters( 'edd_stats_earnings_args', $args );
+			$cached = get_transient( 'edd_stats_earnings' );
+			$key    = md5( json_encode( $args ) );
 
 			if ( ! isset( $cached[ $key ] ) ) {
 				$sales    = get_posts( $args );
 				$earnings = 0;
 
 				if ( $sales ) {
-
 					$sales = implode( ',', array_map('intval', $sales ) );
 
 					$total_earnings = $wpdb->get_var( "SELECT SUM(meta_value) FROM $wpdb->postmeta WHERE meta_key = '_edd_payment_total' AND post_id IN ({$sales})" );
@@ -148,15 +142,13 @@ class EDD_Payment_Stats extends EDD_Stats {
 					$total_earnings = apply_filters( 'edd_payment_stats_earnings_total', $total_earnings, $sales, $args );
 
 					$earnings += ( $total_earnings - $total_tax );
-
 				}
+
 				// Cache the results for one hour
 				$cached[ $key ] = $earnings;
 				set_transient( 'edd_stats_earnings', $cached, HOUR_IN_SECONDS );
 			}
-
 		} else {
-
 			// Download specific earning stats
 			global $edd_logs, $wpdb;
 
@@ -172,9 +164,9 @@ class EDD_Payment_Stats extends EDD_Stats {
 				'include_taxes'      => $include_taxes,
 			);
 
-			$args     = apply_filters( 'edd_stats_earnings_args', $args );
-			$cached   = get_transient( 'edd_stats_earnings' );
-			$key      = md5( json_encode( $args ) );
+			$args   = apply_filters( 'edd_stats_earnings_args', $args );
+			$cached = get_transient( 'edd_stats_earnings' );
+			$key    = md5( json_encode( $args ) );
 
 			if ( ! isset( $cached[ $key ] ) ) {
 				$this->timestamp = false;
@@ -182,16 +174,15 @@ class EDD_Payment_Stats extends EDD_Stats {
 
 				$earnings = 0;
 
-				if( $log_ids ) {
+				if ( $log_ids ) {
 					$log_ids     = implode( ',', array_map('intval', $log_ids ) );
 					$payment_ids = $wpdb->get_col( "SELECT DISTINCT meta_value FROM $wpdb->postmeta WHERE meta_key = '_edd_log_payment_id' AND post_id IN ($log_ids);" );
 
-					foreach( $payment_ids as $payment_id ) {
+					foreach ( $payment_ids as $payment_id ) {
 						$items = edd_get_payment_meta_cart_details( $payment_id );
 
-						foreach( $items as $cart_key => $item ) {
-
-							if( $item['id'] != $download_id ) {
+						foreach ( $items as $cart_key => $item ) {
+							if ( $item['id'] != $download_id ) {
 								continue;
 							}
 
@@ -209,9 +200,7 @@ class EDD_Payment_Stats extends EDD_Stats {
 							if ( ! $include_taxes ) {
 								$earnings -= edd_get_payment_item_tax( $payment_id, $cart_key );
 							}
-
 						}
-
 					}
 				}
 
@@ -226,26 +215,27 @@ class EDD_Payment_Stats extends EDD_Stats {
 		$result = $cached[ $key ];
 
 		return round( $result, edd_currency_decimal_filter() );
-
 	}
 
 	/**
 	 * Get the best selling products
 	 *
-	 * @access public
 	 * @since 1.8
+	 * @access public
+	 *
 	 * @param int $number The number of results to retrieve with the default set to 10.
+	 *
 	 * @return array List of download IDs that are best selling
 	 */
 	public function get_best_selling( $number = 10 ) {
-
 		global $wpdb;
 
 		$downloads = $wpdb->get_results( $wpdb->prepare(
 			"SELECT post_id as download_id, max(meta_value) as sales
-				FROM $wpdb->postmeta WHERE meta_key='_edd_download_sales' AND meta_value > 0
-				GROUP BY meta_value+0
-				DESC LIMIT %d;", $number
+			 FROM $wpdb->postmeta
+			 WHERE meta_key='_edd_download_sales' AND meta_value > 0
+			 GROUP BY meta_value+0
+			 DESC LIMIT %d;", $number
 		) );
 
 		return $downloads;
@@ -419,5 +409,4 @@ class EDD_Payment_Stats extends EDD_Stats {
 
 		return in_array( $date_range, $cacheable_ranges );
 	}
-
 }
