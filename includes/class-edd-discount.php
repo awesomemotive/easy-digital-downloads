6c8cd1e546e69d307f442c3207374396392bccd5
Sunny Ratilal <sunny@sunnyratilal.com>
2016-12-25 23:17:28 +0530

    Setup discount object vars #5217

--- includes/class-edd-discount.php
+++ includes/class-edd-discount.php
@@ -19,6 +19,149 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 class EDD_Discount {
 	/**
+	 * Discount ID.
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var int
+	 */
+	public $ID = 0;
+
+	/**
+	 * Discount Name.
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var string
+	 */
+	public $name;
+
+	/**
+	 * Discount Code.
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var string
+	 */
+	public $code;
+
+	/**
+	 * Discount Status (Active or Inactive).
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var string
+	 */
+	public $status;
+
+	/**
+	 * Discount Type (Percentage or Flat Amount).
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var string
+	 */
+	public $type;
+
+	/**
+	 * Discount Amount.
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var mixed float|int
+	 */
+	public $amount = 0;
+
+	/**
+	 * Download Requirements.
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var array
+	 */
+	public $download_requirements;
+
+	/**
+	 * Excluded Downloads.
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var array
+	 */
+	public $excluded_downloads;
+
+	/**
+	 * Start Date.
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var string
+	 */
+	public $start_date;
+
+	/**
+	 * End Date.
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var string
+	 */
+	public $end_date;
+
+	/**
+	 * Maximum Uses.
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var int
+	 */
+	public $max_uses;
+
+	/**
+	 * Minimum Amount.
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var mixed int|float
+	 */
+	public $min_amount;
+
+	/**
+	 * Is Single Use?
+	 *
+	 * @since 2.7
+	 * @access public
+	 * @var bool
+	 */
+	public $is_single_use;
+
+	/**
+	 * Declare the default properties in WP_Post as we can't extend it
+	 */
+	public $post_author = 0;
+	public $post_date = '0000-00-00 00:00:00';
+	public $post_date_gmt = '0000-00-00 00:00:00';
+	public $post_content = '';
+	public $post_title = '';
+	public $post_excerpt = '';
+	public $post_status = 'publish';
+	public $comment_status = 'open';
+	public $ping_status = 'open';
+	public $post_password = '';
+	public $post_name = '';
+	public $to_ping = '';
+	public $pinged = '';
+	public $post_modified = '0000-00-00 00:00:00';
+	public $post_modified_gmt = '0000-00-00 00:00:00';
+	public $post_content_filtered = '';
+	public $post_parent = 0;
+	public $guid = '';
+	public $menu_order = 0;
+	public $post_mime_type = '';
+	public $comment_count = 0;
+	public $filter;
+
+	/**
 	 * Constructor.
 	 *
 	 * @since 2.7
@@ -74,8 +217,11 @@ class EDD_Discount {
 			return false;
 		}
 
+		/**
+		 * Setup discount object vars with WP_Post vars
+		 */
 		foreach ( $discount as $key => $value ) {
-			$this->$key = $value;
+			$this->{$key} = $value;
 		}
 
 		return true;
