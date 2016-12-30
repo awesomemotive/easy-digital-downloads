6c8d2ab66f11ba4ee0d5e46187b4af0c522a2b43
Sunny Ratilal <sunny@sunnyratilal.com>
2016-12-30 21:31:08 +0530

    Create base EDD_Earnings_Report_Export class #3055

---
+++ includes/admin/reporting/class-export-earnings-report.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Earnings Report Export Class
+ *
+ * This class handles earnings report export.
+ *
+ * @package     EDD
+ * @subpackage  Admin/Reports
+ * @copyright   Copyright (c) 2016, Sunny Ratilal
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       2.7
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * EDD_Earnings_Report_Export Class
+ *
+ * @since 2.7
+ */
+class EDD_Earnings_Report_Export extends EDD_Export {
+
+}
