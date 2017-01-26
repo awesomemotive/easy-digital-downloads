14d2e6af8c8d8353c9c190b8b57baafced7f21d4
Sunny Ratilal <sunny@sunnyratilal.com>
2017-01-26 20:00:58 +0000

    Fix selected option #2612

--- includes/admin/downloads/metabox.php
+++ includes/admin/downloads/metabox.php
@@ -555,12 +555,18 @@ function edd_render_products_field( $post_id ) {
 									$price_assignments = edd_get_bundle_pricing_variations( $post_id );
 									$price_assignments = $price_assignments[0];
 
+									if ( in_array( 'all', $price_assignments ) ) {
+										$selected = isset( $price_assignments[ $key + 1 ] ) ? $price_assignments[ $key + 1 ] : null;
+									} else {
+										$selected = isset( $price_assignments[ $key ] ) ? $price_assignments[ $key ] : null;
+									}
+
 									echo EDD()->html->select( array(
 										'name'             => '_edd_bundled_products_conditions['. $key .']',
 										'class'            => 'edd_repeatable_condition_field',
 										'options'          => $options,
 										'show_option_none' => false,
-										'selected'         => isset( $price_assignments[ $key + 1 ] ) ? $price_assignments[ $key + 1 ] : null,
+										'selected'         => $selected
 									) );
 								?>
 							</td>
