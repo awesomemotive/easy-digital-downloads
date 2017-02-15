19b6338a2672cde372acab5547e4a151e3d165f5
Pippin Williamson <pippin@pippinsplugins.com>
2017-02-15 12:46:48 -0600

    Register the sales export batch process. Fixes #5475 (#5476)

--- includes/admin/reporting/export/export-actions.php
+++ includes/admin/reporting/export/export-actions.php
@@ -177,6 +177,29 @@ function edd_include_file_downloads_batch_processer( $class ) {
 }
 
 /**
+ * Register the sales batch exporter
+ *
+ * @since  2.7
+ */
+function edd_register_sales_export_batch_export() {
+	add_action( 'edd_batch_export_class_include', 'edd_include_sales_export_batch_processer', 10, 1 );
+}
+add_action( 'edd_register_batch_exporter', 'edd_register_sales_export_batch_export', 10 );
+
+/**
+ * Loads the sales export batch process if needed
+ *
+ * @since  2.7
+ * @param  string $class The class being requested to run for the batch export
+ * @return void
+ */
+function edd_include_sales_export_batch_processer( $class ) {
+	if ( 'EDD_Batch_Sales_Export' === $class ) {
+		require_once EDD_PLUGIN_DIR . 'includes/admin/reporting/export/class-batch-export-sales.php';
+	}
+}
+
+/**
  * Register the earnings report batch exporter
  *
  * @since  2.7
@@ -198,6 +221,7 @@ function edd_include_earnings_report_batch_processer( $class ) {
 		require_once EDD_PLUGIN_DIR . 'includes/admin/reporting/export/class-batch-export-earnings-report.php';
 	}
 }
+
 /**
  * Register the API requests batch exporter
  *
