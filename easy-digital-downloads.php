3ba29225867dc7df1d54c42523b3785fd5913ed5
Chris Klosowski <cklosowski@gmail.com>
2017-05-24 07:24:22 -0700

    Remove extra include of includes/misc-functions.php #5762

--- easy-digital-downloads.php
+++ easy-digital-downloads.php
@@ -277,7 +277,6 @@ final class Easy_Digital_Downloads {
 		require_once EDD_PLUGIN_DIR . 'includes/country-functions.php';
 		require_once EDD_PLUGIN_DIR . 'includes/formatting.php';
 		require_once EDD_PLUGIN_DIR . 'includes/widgets.php';
-		require_once EDD_PLUGIN_DIR . 'includes/misc-functions.php';
 		require_once EDD_PLUGIN_DIR . 'includes/mime-types.php';
 		require_once EDD_PLUGIN_DIR . 'includes/gateways/actions.php';
 		require_once EDD_PLUGIN_DIR . 'includes/gateways/functions.php';
