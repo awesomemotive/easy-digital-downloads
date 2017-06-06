9d4660569e1d3d095a327710b6e8a60c6e438fd3
Chris Klosowski <cklosowski@gmail.com>
2017-06-06 13:07:43 -0700

    Fix incomplete unit tests (props @devinwalker) (#5786)
    * 1. Added edd_test_api_return_helper() to prevent edd_die() failing unit tests
    2. Fixed test_get_queried_version()
    
    * Return false after authorization failures to return prevent looping within unit test checks
    
    * Fixed test_missing_auth() and test_invalid_key()
    
    * Completed the test_info() test
    
    * Code reformatted
    
    * Testing breaking travis return
    
    * Added more caps to fix failing unit test check
    
    * Added caps so user can view stats API data
    
    * More elegant way of setting user caps
    
    * Checking value in travis
    
    * Remove var_dump
    
    * Resolve failing test due to permissions
    
    * Updated caps

--- includes/api/class-edd-api.php
+++ includes/api/class-edd-api.php
@@ -301,7 +301,7 @@ class EDD_API {
 	 * @uses EDD_API::invalid_key()
 	 * @uses EDD_API::invalid_auth()
 	 * @since 1.5
-	 * @return void
+	 * @return bool
 	 */
 	private function validate_request() {
 		global $wp_query;
@@ -313,6 +313,7 @@ class EDD_API {
 
 			if ( empty( $wp_query->query_vars['token'] ) || empty( $wp_query->query_vars['key'] ) ) {
 				$this->missing_auth();
+				return  false;
 			}
 
 			// Auth was provided, include the upgrade routine so we can use the fallback api checks
@@ -322,6 +323,7 @@ class EDD_API {
 			if ( ! ( $user = $this->get_user( $wp_query->query_vars['key'] ) ) ) {
 
 				$this->invalid_key();
+				return  false;
 
 			} else {
 
@@ -333,6 +335,7 @@ class EDD_API {
 					$this->is_valid_request = true;
 				} else {
 					$this->invalid_auth();
+					return  false;
 				}
 			}
 		} elseif ( ! empty( $wp_query->query_vars['edd-api'] ) && $this->is_public_query() ) {
