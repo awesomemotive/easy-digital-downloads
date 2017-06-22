a49074dfd0c598b4782d5f32bd665d5a4984f670
Sunny Ratilal <sunny@sunnyratilal.com>
2017-06-22 18:55:44 +0100

    Change from _x to __ #5404

--- includes/admin/settings/register-settings.php
+++ includes/admin/settings/register-settings.php
@@ -1229,7 +1229,7 @@ function edd_get_registered_settings_sections() {
 
 	$sections = array(
 		'general'    => apply_filters( 'edd_settings_sections_general', array(
-			'main'               => _x( 'General', 'easy-digital-downloads' ),
+			'main'               => __( 'General', 'easy-digital-downloads' ),
 			'currency'           => __( 'Currency', 'easy-digital-downloads' ),
 			'api'                => __( 'API', 'easy-digital-downloads' ),
 		) ),
