3b9fb5d6a289e7cc9e4784aa935c205acad8a52d
Sunny Ratilal <sunny@sunnyratilal.com>
2017-06-23 10:28:27 +0100

    Allow placeholder to be passed in via data attribute #5357

--- includes/admin/payments/view-order-details.php
+++ includes/admin/payments/view-order-details.php
@@ -718,7 +718,10 @@ $customer       = new EDD_Customer( $payment->customer_id );
 															'show_option_none' => false,
 															'chosen'           => true,
 															'placeholder'      => __( 'Select a country', 'easy-digital-downloads' ),
-															'data'             => array( 'search-type' => 'no_ajax' ),
+															'data'             => array(
+																'search-type'        => 'no_ajax',
+																'search-placeholder' => __( 'Type to search all Countries', 'easy-digital-downloads' ),
+															),
 														) );
 														?>
 													</p>
@@ -736,7 +739,10 @@ $customer       = new EDD_Customer( $payment->customer_id );
 																'show_option_none' => false,
 																'chosen'           => true,
 																'placeholder'      => __( 'Select a state', 'easy-digital-downloads' ),
-																'data'             => array( 'search-type' => 'no_ajax' ),
+																'data'             => array(
+																	'search-type'        => 'no_ajax',
+																	'search-placeholder' => __( 'Type to search all States/Provinces', 'easy-digital-downloads' ),
+																),
 															) );
 														} else { ?>
 															<input type="text" name="edd-payment-address[0][state]" value="<?php echo esc_attr( $address['state'] ); ?>" class="medium-text"/>
--- includes/scripts.php
+++ includes/scripts.php
@@ -220,10 +220,6 @@ function edd_load_admin_scripts( $hook ) {
 		'batch_export_no_class'       => __( 'You must choose a method.', 'easy-digital-downloads' ),
 		'batch_export_no_reqs'        => __( 'Required fields not completed.', 'easy-digital-downloads' ),
 		'reset_stats_warn'            => __( 'Are you sure you want to reset your store? This process is <strong><em>not reversible</em></strong>. Please be sure you have a recent backup.', 'easy-digital-downloads' ),
-		'search_placeholder'          => sprintf( __( 'Type to search all %s', 'easy-digital-downloads' ), edd_get_label_plural() ),
-		'search_placeholder_customer' => __( 'Type to search all Customers', 'easy-digital-downloads' ),
-		'search_placeholder_country'  => __( 'Type to search all Countries', 'easy-digital-downloads' ),
-		'search_placeholder_state'    => __( 'Type to search all States/Provinces', 'easy-digital-downloads' ),
 		'unsupported_browser'         => __( 'We are sorry but your browser is not compatible with this kind of file upload. Please upgrade your browser.', 'easy-digital-downloads' ),
 	));
 
