6ee183ce355072f16d59156b9b2c88ff58260704
Sunny Ratilal <sunny@sunnyratilal.com>
2017-06-27 22:35:29 +0100

    Update variable #5401

--- templates/shortcode-download.php
+++ templates/shortcode-download.php
@@ -10,14 +10,14 @@
  * @version 1.0.0
  */
 
-global $edd_download_shortcode_item_atts, $edd_download_shortcode_item_i;
+global $edd_download_shortcode_item_atts, $edd_download_shortcode_item_$i;
 ?>
 
 <?php $schema = edd_add_schema_microdata() ? 'itemscope itemtype="http://schema.org/Product" ' : ''; ?>
 
-<div <?php echo $schema; ?>class="<?php echo esc_attr( apply_filters( 'edd_download_class', 'edd_download', get_the_ID(), $edd_download_shortcode_item_atts, $edd_download_shortcode_item_i ) ); ?>" id="edd_download_<?php the_ID(); ?>">
+<div <?php echo $schema; ?>class="<?php echo esc_attr( apply_filters( 'edd_download_class', 'edd_download', get_the_ID(), $edd_download_shortcode_item_atts, $edd_download_shortcode_item_$i ) ); ?>" id="edd_download_<?php the_ID(); ?>">
 
-	<div class="<?php echo esc_attr( apply_filters( 'edd_download_inner_class', 'edd_download_inner', get_the_ID(), $edd_download_shortcode_item_atts, $edd_download_shortcode_item_i ) ); ?>">
+	<div class="<?php echo esc_attr( apply_filters( 'edd_download_inner_class', 'edd_download_inner', get_the_ID(), $edd_download_shortcode_item_atts, $edd_download_shortcode_item_$i ) ); ?>">
 
 		<?php
 			do_action( 'edd_download_before' );
