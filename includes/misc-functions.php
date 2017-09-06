271bfd25f0ddbab12d3253dcb2fbe1f21f12623f
Eric Daams <eric@164a.com>
2017-09-06 13:44:21 +0930

    getallheaders should always return an array

--- includes/misc-functions.php
+++ includes/misc-functions.php
@@ -885,7 +885,7 @@ if ( ! function_exists( 'getallheaders' ) ) :
 	 * @return array
 	 */
 	function getallheaders() {
-		$headers = '';
+		$headers = array();
 		foreach ( $_SERVER as $name => $value ) {
 			if ( substr( $name, 0, 5 ) == 'HTTP_' ) {
 				$headers[ str_replace( ' ', '-', ucwords( strtolower( str_replace( '_', ' ', substr( $name, 5 ) ) ) ) ) ] = $value;
