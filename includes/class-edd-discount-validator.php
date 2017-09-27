84ac4e027f75984093910d199a72436f892d6793
Sunny Ratilal <sunny@sunnyratilal.com>
2017-09-27 12:07:03 +0100

    Add validation method by @Shelob9 #5618

--- includes/class-edd-discount-validator.php
+++ includes/class-edd-discount-validator.php
@@ -38,22 +38,91 @@ class EDD_Discount_Validator {
 	/**
 	 * EDD_Discount_Validator constructor.
 	 *
-	 * @param EDD_Discount $discount  Discount object.
-	 * @param array        $downloads Download IDs.
+	 * @param int|EDD_Discount $discount  Discount object or discount ID.
+	 * @param array            $downloads Download IDs.
 	 */
-	public function __construct( $discount = null, $downloads = array() ) {
+	public function __construct( $discount = 0, $downloads = array() ) {
 		$this->discount  = $discount;
 		$this->downloads = $downloads;
+
+		$this->setup_object();
+	}
+
+	/**
+	 * Setup discount object.
+	 *
+	 * @access private
+	 */
+	private function setup_object() {
+		if ( ! $this->discount instanceof EDD_Discount ) {
+			$this->discount = new EDD_Discount( $this->discount );
+		}
 	}
 
 	/**
 	 * Parent method that calls other validation methods.
 	 *
+	 * @access public
 	 * @since 2.8.7
+	 *
+	 * @return bool $validity If the discount is valid or not.
 	 */
 	public function is_valid() {
-		if ( ! $this->discount instanceof EDD_Discount ) {
-			return new WP_Error( 'invalid-arg', __( 'Discount object not valid.', 'easy-digital-downloads' ) );
+		if ( empty( $this->downloads ) ) {
+			return new WP_Error( 'invalid-arg', __( 'Download IDs not supplied.', 'easy-digital-downloads' ) );
 		}
+
+		$product_requirements = $this->discount->get_product_reqs();
+		$excluded_products    = $this->discount->get_excluded_products();
+
+		// Initially the discount is invalid
+		$validity = false;
+
+		// If there are no requirements/excluded products set, the discount is valid
+		if ( empty( $product_requirements ) && empty( $excluded_products ) ) {
+			$validity = true;
+		}
+
+		$product_requirements = array_map( 'absint', $product_requirements );
+		asort( $product_requirements );
+		$product_requirements = array_values( $product_requirements );
+
+		$excluded_products = array_map( 'absint', $excluded_products );
+		asort( $excluded_products );
+		$excluded_products = array_values( $excluded_products );
+
+		if ( ! $validity && ! empty( $product_requirements ) ) {
+			switch ( $this->discount->get_product_condition() ) {
+				case 'all' :
+					$validity = true;
+					foreach ( $product_requirements as $download_id ) {
+						if ( ! in_array( $download_id, $this->downloads ) ) {
+							$validity = false;
+							break;
+						}
+					}
+					break;
+				default:
+					foreach ( $product_requirements as $download_id ) {
+						if ( in_array( $download_id, $this->downloads ) ) {
+							$validity = true;
+							break;
+						}
+					}
+					break;
+			}
+		} else {
+			$validity = true;
+		}
+
+		if ( ! empty( $excluded_products ) ) {
+			foreach ( $excluded_products as $download_id ) {
+				if ( in_array( $download_id, $this->downloads ) ) {
+					$validity = false;
+				}
+			}
+		}
+
+		return $validity;
 	}
 }
