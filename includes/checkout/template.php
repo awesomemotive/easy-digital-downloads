f2b9df1209761293d9a5b0a9774a20870f31a9c7
Andrew Munro <andrew@amdrew.com>
2017-09-28 20:52:35 +1300

    Formatting #6041

--- includes/checkout/template.php
+++ includes/checkout/template.php
@@ -745,7 +745,6 @@ function edd_discount_field() {
 					<?php _e( 'Discount', 'easy-digital-downloads' ); ?>
 				</label>
 				<span class="edd-description"><?php _e( 'Enter a coupon code if you have one.', 'easy-digital-downloads' ); ?></span>
-				
 				<span class="edd-discount-code-field-wrap">
 					<input class="edd-input" type="text" id="edd-discount" name="edd-discount" placeholder="<?php _e( 'Enter discount', 'easy-digital-downloads' ); ?>"/>
 					<input type="submit" class="edd-apply-discount edd-submit button <?php echo $color . ' ' . $style; ?>" value="<?php echo _x( 'Apply', 'Apply discount at checkout', 'easy-digital-downloads' ); ?>"/>
