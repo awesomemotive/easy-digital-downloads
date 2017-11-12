7bc73f556b5dd78e71b4b5f7ca8a587944e499a6
Sunny Ratilal <sunny@sunnyratilal.com>
2017-11-12 13:02:35 +0000

    Add support to resume downloads of large files #5087

--- includes/process-download.php
+++ includes/process-download.php
@@ -733,11 +733,18 @@ function edd_get_file_ctype( $extension ) {
  * See http://codeigniter.com/wiki/Download_helper_for_large_files/
  *
  * @access   public
- * @param    string  $file      The file
- * @param    boolean $retbytes  Return the bytes of file
+ *
+ * @param    string  $file     The file
+ * @param    boolean $retbytes Return the bytes of file
+ *
  * @return   bool|string        If string, $status || $cnt
  */
 function edd_readfile_chunked( $file, $retbytes = true ) {
+	while ( ob_get_level() > 0 ) {
+		ob_end_clean();
+	}
+
+	ob_start();
 
 	$chunksize = 1024 * 1024;
 	$buffer    = '';
@@ -745,21 +752,68 @@ function edd_readfile_chunked( $file, $retbytes = true ) {
 	$handle    = @fopen( $file, 'rb' );
 
 	if ( $size = @filesize( $file ) ) {
-		header("Content-Length: " . $size );
+		header( "Content-Length: " . $size );
 	}
 
 	if ( false === $handle ) {
 		return false;
 	}
 
+	if ( isset( $_SERVER['HTTP_RANGE'] ) ) {
+		list( $size_unit, $range ) = explode( '=', $_SERVER['HTTP_RANGE'], 2 );
+		if ( 'bytes' === $size_unit ) {
+			if ( strpos( ',', $range ) ) {
+				list( $range ) = explode( ',', $range, 1 );
+			}
+		} else {
+			$range = '';
+			header( 'HTTP/1.1 416 Requested Range Not Satisfiable' );
+			exit;
+		}
+	} else {
+		$range = '';
+	}
+
+	if ( empty( $range ) ) {
+		$seek_start = null;
+		$seek_end   = null;
+	} else {
+		list( $seek_start, $seek_end ) = explode( '-', $range, 2 );
+	}
+
+	$seek_end   = ( empty( $seek_end ) ) ? ( $size - 1 ) : min( abs( intval( $seek_end ) ), ( $size - 1 ) );
+	$seek_start = ( empty( $seek_start ) || $seek_end < abs( intval( $seek_start ) ) ) ? 0 : max( abs( intval( $seek_start ) ), 0 );
+
+	// Only send partial content header if downloading a piece of the file (IE workaround)
+	if ( $seek_start > 0 || $seek_end < ( $size - 1 ) ) {
+		header( 'HTTP/1.1 206 Partial Content' );
+		header( 'Content-Range: bytes ' . $seek_start . '-' . $seek_end . '/' . $size );
+		header( 'Content-Length: ' . ( $seek_end - $seek_start + 1 ) );
+	} else {
+		header( "Content-Length: $size" );
+	}
+
+	header( 'Accept-Ranges: bytes' );
+
+	set_time_limit( 0 );
+	fseek( $handle, $seek_start );
+
 	while ( ! @feof( $handle ) ) {
 		$buffer = @fread( $handle, $chunksize );
 		echo $buffer;
+		ob_flush();
 
 		if ( $retbytes ) {
 			$cnt += strlen( $buffer );
 		}
+
+		if ( connection_status() != 0 ) {
+			@fclose( $handle );
+			exit;
+		}
 	}
+
+	ob_flush();
 
 	$status = @fclose( $handle );
 
