3781f513a9091b1110fa69bf8bc694cd92413530
Sunny Ratilal <sunny@sunnyratilal.com>
2017-11-16 15:31:05 +0000

    Check the payment is an object #6185

--- includes/user-functions.php
+++ includes/user-functions.php
@@ -483,7 +483,7 @@ function edd_add_past_purchases_to_new_user( $user_id ) {
 		edd_send_user_verification_email( $user_id );
 
 		foreach( $payments as $payment ) {
-			if ( $payment instanceof EDD_Payment ) {
+			if ( is_object( $payment ) && $payment instanceof EDD_Payment ) {
 				if ( intval( edd_get_payment_user_id( $payment->ID ) ) > 0 ) {
 					continue; // This payment already associated with an account
 				}
