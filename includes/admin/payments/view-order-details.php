397be4dcb1947a4f777d15ef6f7b743a0c993348
David Sherlock <info@creativenevada.com>
2017-12-01 17:32:59 +0000

    Add functionality/filter to allow IP addresses to link to ipinfo.io

--- includes/admin/payments/view-order-details.php
+++ includes/admin/payments/view-order-details.php
@@ -272,7 +272,7 @@ $customer       = new EDD_Customer( $payment->customer_id );
 										<div class="edd-order-ip edd-admin-box-inside">
 											<p>
 												<span class="label"><?php _e( 'IP:', 'easy-digital-downloads' ); ?></span>&nbsp;
-												<span><?php echo esc_attr( $payment->ip ); ?></span>
+												<span><?php echo apply_filters( 'edd_payment_details_ip_address', $payment->ip ); ?></span>
 											</p>
 										</div>
 
