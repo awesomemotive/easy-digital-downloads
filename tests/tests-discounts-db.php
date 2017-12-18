61896d7507d7352ba9fd0a774c48db28a2c9a66f
Pippin Williamson <pippin@pippinsplugins.com>
2017-12-18 09:02:02 -0600

    Add shell for discounts DB test. #5277

---
+++ tests/tests-discounts-db.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * @group edd_discounts_db
+ */
+class Tests_Discounts_DB extends EDD_UnitTestCase {
+
+
+	public function setUp() {
+
+	}
+
+	public function tearDown() {
+		self::_delete_all_data();
+		edd_empty_cart();
+		parent::tearDown();
+	}
+
+	public function test_get_columns() {
+
+	}
+
+	public function test_get_column_defaults() {
+		
+	}
+
+	public function test_insert() {
+
+	}
+
+	public function test_update() {
+
+	}
+
+	public function test_delete() {
+
+	}
+
+	public function test_get_discounts() {
+
+	}
+
+	public function test_get_discounts_with_number() {
+
+	}
+
+	public function test_get_discounts_with_offset() {
+		
+	}
+
+	public function test_get_discounts_with_search() {
+		
+	}
+
+	public function test_get_discounts_with_order_asc() {
+		
+	}
+
+	public function test_get_discounts_with_order_desc() {
+		
+	}
+
+	public function test_get_discounts_with_orderby() {
+		
+	}
+
+	public function test_get_discounts_with_type() {
+		
+	}
+
+	public function test_get_discounts_with_status() {
+		
+	}
+
+	public function test_get_discounts_with_created_date() {
+		
+	}
+
+	public function test_get_discounts_with_end_date() {
+		
+	}
+
+	public function test_get_discounts_with_start_date() {
+		
+	}
+
+	public function test_count() {
+		
+	}
+
+	public function test_count_with_search() {
+		
+	}
+
+	public function test_count_with_status() {
+		
+	}
+
+	public function test_count_with_type() {
+		
+	}
+
+	public function test_count_with_created_date() {
+		
+	}
+
+	public function test_count_with_end_date() {
+		
+	}
+
+	public function test_count_with_start_date() {
+		
+	}
+
+}
