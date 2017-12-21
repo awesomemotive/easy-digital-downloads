898d6f11e78659be659a03902754125863b4e834
Sunny Ratilal <sunny@sunnyratilal.com>
2017-12-21 22:13:20 +0000

    Clean up and add more unit tests #5277

--- tests/tests-discounts-db.php
+++ tests/tests-discounts-db.php
@@ -1,7 +1,12 @@
 <?php
 
 /**
+ * Discount DB Tests
+ *
+ * @covers EDD_DB_Discounts
  * @group edd_discounts_db
+ * @group database
+ * @group edd_discounts
  */
 class Tests_Discounts_DB extends EDD_UnitTestCase {
 
@@ -23,226 +28,267 @@ class Tests_Discounts_DB extends EDD_UnitTestCase {
 		parent::tearDown();
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_columns()
+	 */
 	public function test_get_columns() {
-
 		$expected = array(
-			'id'                  => '%d',
-			'name'                => '%s',
-			'code'                => '%s',
-			'status'              => '%s',
-			'type'                => '%s',
-			'amount'              => '%s',
-			'description'         => '%s',
-			'max_uses'            => '%d',
-			'use_count'           => '%d',
-			'min_cart_price'      => '%f',
-			'once_per_customer'   => '%d',
-			'product_reqs'        => '%s',
-			'product_condition'   => '%s',
-			'scope'               => '%s',
-			'created_date'        => '%s',
-			'start_date'          => '%s',
-			'end_date'            => '%s',
-			'notes'               => '%s',
+			'id'                => '%d',
+			'name'              => '%s',
+			'code'              => '%s',
+			'status'            => '%s',
+			'type'              => '%s',
+			'amount'            => '%s',
+			'description'       => '%s',
+			'created_date'      => '%s',
+			'start_date'        => '%s',
+			'end_date'          => '%s',
+			'max_uses'          => '%d',
+			'use_count'         => '%d',
+			'min_cart_price'    => '%f',
+			'once_per_customer' => '%d',
+			'product_condition' => '%s',
+			'scope'             => '%s',
+			'notes'             => '%s',
 		);
 
 		$this->assertSame( $expected, self::$db->get_columns() );
-
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_column_defaults()
+	 */
 	public function test_get_column_defaults() {
-
 		$expected = array(
-			'id'                  => 0,
-			'name'                => '',
-			'code'                => '',
-			'status'              => '',
-			'type'                => '',
-			'amount'              => '',
-			'description'         => '',
-			'max_uses'            => 0,
-			'use_count'           => 0,
-			'min_cart_price'      => 0.00,
-			'once_per_customer'   => 0,
-			'product_reqs'        => '',
-			'product_condition'   => '',
-			'scope'               => 'global',
-			'created_date'        => date( 'Y-m-d H:i:s' ),
-			'start_date'          => '0000-00-00 00:00:00',
-			'end_date'            => '0000-00-00 00:00:00',
-			'notes'               => '',
+			'id'                => 0,
+			'name'              => '',
+			'code'              => '',
+			'status'            => '',
+			'type'              => '',
+			'amount'            => '',
+			'description'       => '',
+			'max_uses'          => 0,
+			'use_count'         => 0,
+			'min_cart_price'    => 0.00,
+			'once_per_customer' => 0,
+			'product_condition' => '',
+			'scope'             => 'global',
+			'created_date'      => date( 'Y-m-d H:i:s' ),
+			'start_date'        => '0000-00-00 00:00:00',
+			'end_date'          => '0000-00-00 00:00:00',
+			'notes'             => '',
 		);
 
 		$this->assertSame( $expected, self::$db->get_column_defaults() );
-		
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::insert()
+	 */
 	public function test_insert() {
-
 		$id = self::$db->insert( array(
-		 'code' => 'TEST',
-		 'status' => 'active'
+			'code'   => 'TEST',
+			'status' => 'active',
 		) );
 
 		$this->assertInternalType( 'int', $id );
 		$this->assertTrue( $id > 0 );
-
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::update()
+	 */
 	public function test_update() {
-
 		$success = self::$db->update( $this->discount_id, array( 'code' => 'NEWCODE' ) );
-
 		$this->assertTrue( $success );
-
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::update()
+	 */
 	public function test_update_without_id_should_fail() {
-
 		$success = self::$db->update( null, array( 'code' => 'NEWCODE' ) );
-
 		$this->assertFalse( $success );
-
 	}
 
-
+	/**
+	 * @covers EDD_DB_Discounts::delete()
+	 */
 	public function test_delete() {
-
 		$success = self::$db->delete( $this->discount_id );
 		$this->assertTrue( $success );
-
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::delete()
+	 */
 	public function test_delete_without_id_should_fail() {
-
 		$success = self::$db->delete( '' );
 		$this->assertFalse( $success );
-
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_discounts()
+	 */
 	public function test_get_discounts() {
-
 		$discounts = self::$db->get_discounts();
-		$this->assertTrue( count( $discounts ) == 1 );
-
+		$this->assertTrue( count( $discounts ) === 1 );
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_discounts()
+	 */
 	public function test_get_discounts_returns_expected_discount() {
-
 		$discounts = self::$db->get_discounts();
-		$this->assertTrue( $discounts[0]->code == '20OFF' );
-
+		$this->assertTrue( '20OFF' === $discounts[0]->code );
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_discounts()
+	 */
 	public function test_get_discounts_with_number() {
-
-		$d2 = EDD_Helper_Discount::create_simple_percent_discount();
+		EDD_Helper_Discount::create_simple_percent_discount();
 
 		$discounts = self::$db->get_discounts( array( 'number' => 1 ) );
-
-		$this->assertTrue( count( $discounts ) == 1 );
+		$this->assertTrue( count( $discounts ) === 1 );
 
 		$discounts = self::$db->get_discounts( array( 'number' => 2 ) );
-	
-		$this->assertTrue( count( $discounts ) == 2 );
-	
+		$this->assertTrue( count( $discounts ) === 2 );
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_discounts()
+	 */
 	public function test_get_discounts_with_offset() {
-
-		$d2 = EDD_Helper_Discount::create_simple_percent_discount();
-		$d3 = EDD_Helper_Discount::create_simple_percent_discount();
+		EDD_Helper_Discount::create_simple_percent_discount();
+		EDD_Helper_Discount::create_simple_percent_discount();
 
 		$discounts = self::$db->get_discounts( array( 'offset' => 1 ) );
-		$this->assertTrue( count( $discounts ) == 2 );
-	
+		$this->assertTrue( count( $discounts ) === 2 );
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_discounts()
+	 */
 	public function test_get_discounts_with_offset_order_asc() {
-
-		$d2 = EDD_Helper_Discount::create_simple_percent_discount();
-		$d3 = EDD_Helper_Discount::create_simple_percent_discount();
+		EDD_Helper_Discount::create_simple_percent_discount();
+		EDD_Helper_Discount::create_simple_percent_discount();
 
 		$discounts = self::$db->get_discounts( array( 'offset' => 1, 'order' => 'asc' ) );
-
-		$this->assertTrue( count( $discounts ) == 2 );
-	
+		$this->assertTrue( 2 === count( $discounts ) );
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_discounts()
+	 */
 	public function test_get_discounts_with_search_by_code() {
-		$flat = EDD_Helper_Discount::create_simple_flat_discount();
+		EDD_Helper_Discount::create_simple_flat_discount();
 		$discounts = self::$db->get_discounts( array( 'search' => '10FLAT' ) );
 
-		$this->assertTrue( count( $discounts ) == 1 );
-		$this->assertTrue( $discounts[0]->code == '10FLAT' );
+		$this->assertTrue( 2 === count( $discounts ) );
+		$this->assertTrue( '10FLAT' === $discounts[0]->code );
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_discounts()
+	 */
 	public function test_get_discounts_with_search_by_name() {
-		$flat = EDD_Helper_Discount::create_simple_flat_discount();
+		EDD_Helper_Discount::create_simple_flat_discount();
 		$discounts = self::$db->get_discounts( array( 'search' => '$10 Off' ) );
 
-		$this->assertTrue( count( $discounts ) == 1 );
-		$this->assertTrue( $discounts[0]->code == '10FLAT' );
+		$this->assertTrue( 2 === count( $discounts ) );
+		$this->assertTrue( '10FLAT' === $discounts[0]->code );
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_discounts()
+	 */
 	public function test_get_discounts_with_order_asc() {
-		
+		EDD_Helper_Discount::create_simple_flat_discount();
+		$discounts = self::$db->get_discounts( array( 'order' => 'asc' ) );
+
+		$this->assertTrue( 2 === count( $discounts ) );
+		$this->assertTrue( $discounts[0]->id < $discounts[1]->id );
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_discounts()
+	 */
 	public function test_get_discounts_with_order_desc() {
-		
+		EDD_Helper_Discount::create_simple_flat_discount();
+		$discounts = self::$db->get_discounts( array( 'order' => 'desc' ) );
+
+		$this->assertTrue( 2 === count( $discounts ) );
+		$this->assertTrue( $discounts[0]->id > $discounts[1]->id );
 	}
 
+	/**
+	 * @covers EDD_DB_Discounts::get_discounts()
+	 */
 	public function test_get_discounts_with_orderby() {
-		
+		EDD_Helper_Discount::create_simple_flat_discount();
+		$discounts = self::$db->get_discounts( array( 'orderby' => 'code', 'order' => 'asc' ) );
+
+		$this->assertTrue( strcmp( $discounts[0]->code, $discounts[1]->code ) < 0 );
 	}
 
 	public function test_get_discounts_with_type() {
-		
+		$discounts = self::$db->get_discounts( array( 'type' => 'percent' ) );
+		$this->assertTrue( 1 === count( $discounts ) );
+
+		EDD_Helper_Discount::create_simple_flat_discount();
+		EDD_Helper_Discount::create_simple_flat_discount();
+
+		$discounts = self::$db->get_discounts( array( 'type' => 'flat' ) );
+		$this->assertTrue( 2 === count( $discounts ) );
+
+		$discounts = self::$db->get_discounts( array( 'type' => array( 'percent', 'flat' ) ) );
+		$this->assertTrue( 3 === count( $discounts ) );
 	}
 
 	public function test_get_discounts_with_status() {
-		
+		$discounts = self::$db->get_discounts( array( 'status' => 'active' ) );
+		$this->assertTrue( 1 === count( $discounts ) );
 	}
 
 	public function test_get_discounts_with_created_date() {
-		
+
 	}
 
 	public function test_get_discounts_with_end_date() {
-		
+
 	}
 
 	public function test_get_discounts_with_start_date() {
-		
+
 	}
 
 	public function test_count() {
-		
+
 	}
 
 	public function test_count_with_search() {
-		
+
 	}
 
 	public function test_count_with_status() {
-		
+
 	}
 
 	public function test_count_with_type() {
-		
+
 	}
 
 	public function test_count_with_created_date() {
-		
+
 	}
 
 	public function test_count_with_end_date() {
-		
+
 	}
 
 	public function test_count_with_start_date() {
-		
+
 	}
 
 }
