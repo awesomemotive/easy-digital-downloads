9d6e08eaa530439c2ea705a15f494275130319e2
Sunny Ratilal <sunny@sunnyratilal.com>
2017-12-28 20:28:11 +0000

    DocBlock changes #5277 [skip ci]

--- includes/class-edd-db-discount-meta.php
+++ includes/class-edd-db-discount-meta.php
@@ -5,7 +5,7 @@
  * This class is for interacting with the discount meta database table
  *
  * @package     EDD
- * @subpackage  Classes/DB Discount Meta
+ * @subpackage  Classes/Discounts
  * @copyright   Copyright (c) 2018, Pippin Williamson
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       3.0
