50680cb9f8dd517b3bca5b5add129d0a56d0d30f
DrewAPicture <info@drewapicture.com>
2017-12-29 09:48:26 -0700

    Remove the Logs tab from Reports.

--- includes/admin/reporting/reports.php
+++ includes/admin/reporting/reports.php
@@ -31,7 +31,6 @@ function edd_reports_page() {
 			<?php if ( current_user_can( 'export_shop_reports' ) ) { ?>
 				<a href="<?php echo add_query_arg( array( 'tab' => 'export', 'settings-updated' => false ), $current_page ); ?>" class="nav-tab <?php echo $active_tab == 'export' ? 'nav-tab-active' : ''; ?>"><?php _e( 'Export', 'easy-digital-downloads' ); ?></a>
 			<?php } ?>
-			<a href="<?php echo add_query_arg( array( 'tab' => 'logs', 'settings-updated' => false ), $current_page ); ?>" class="nav-tab <?php echo $active_tab == 'logs' ? 'nav-tab-active' : ''; ?>"><?php _e( 'Logs', 'easy-digital-downloads' ); ?></a>
 			<?php do_action( 'edd_reports_tabs' ); ?>
 		</h2>
 
