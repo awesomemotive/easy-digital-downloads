5018ce37266bb2a67a36513e7cfb1820721b8b70
DrewAPicture <info@drewapicture.com>
2018-01-02 15:37:24 -0700

    Instantiate a top-level EDD_Utilities class and use it to bootstrap the registry and exception classes.

---
+++ includes/class-edd-utilities.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * EDD Utilities Bootstrap
+ *
+ * @package     EDD
+ * @subpackage  Utilities
+ * @copyright   Copyright (c) 2018, Pippin Williamson
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0
+ */
+
+/**
+ * Class that bootstraps various utilities leveraged in EDD core.
+ *
+ * @since 3.0
+ */
+class EDD_Utilities {
+
+	/**
+	 * Sets up instantiating core utilities.
+	 *
+	 * @since 3.0
+	 */
+	public function __construct() {
+		$this->includes();
+	}
+
+	/**
+	 * Loads needed files for core utilities.
+	 *
+	 * @since 3.0
+	 */
+	private function includes() {
+		$utils_dir = EDD_PLUGIN_DIR . 'includes/utilities/';
+
+		// Exceptions.
+		require_once $utils_dir . 'exceptions/interface-edd-exception.php';
+		require_once $utils_dir . 'exceptions/class-edd-exception.php';
+		require_once $utils_dir . 'exceptions/class-attribute-not-found.php';
+
+		// Registry.
+		require_once $utils_dir . 'class-edd-registry.php';
+	}
+
+	/**
+	 * Retrieves a given registry instance by name.
+	 *
+	 * @since 3.0
+	 *
+	 * @param string $name Registry name.
+	 * @return \EDD\Utils\Registry|null Null if the registry doesn't exist, otherwise the object instance.
+	 */
+	public function get_registry( $name ) {
+		switch( $name ) {
+			case 'reports:tabs':
+				if ( class_exists( '\EDD\Admin\Reports\Tabs_Registry' ) ) {
+					$registry = \EDD\Admin\Reports\Tabs_Registry::instance();
+				}
+				break;
+
+			default:
+				$registry = null;
+				break;
+		}
+
+		return $registry;
+	}
+}
