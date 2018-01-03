a34b458f08665e0bce1db844f372ac723e6e1c78
DrewAPicture <info@drewapicture.com>
2018-01-02 19:44:20 -0700

    Remove now-unused alias from misc-functions.php.

--- includes/misc-functions.php
+++ includes/misc-functions.php
@@ -8,7 +8,6 @@
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0
  */
-use EDD\Admin\Reports;
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
