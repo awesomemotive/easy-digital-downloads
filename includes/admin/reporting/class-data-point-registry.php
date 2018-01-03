89e47935d767d1245d4c9582ecd15a7b466e9c49
DrewAPicture <info@drewapicture.com>
2018-01-03 13:32:43 -0700

    Add a stub for Data_Point_Registry, extending Registry and implementing Static_Registry.

---
+++ includes/admin/reporting/class-data-point-registry.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Reports API Data Points Registry
+ *
+ * @package     EDD
+ * @subpackage  Admin/Reports
+ * @copyright   Copyright (c) 2018, Pippin Williamson
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0
+ */
+namespace EDD\Admin\Reports;
+
+use EDD\Utils;
+
+/**
+ * Implements a singleton registry for registering reports data points.
+ *
+ * @since 3.0
+ */
+class Data_Point_Registry extends Utils\Registry implements Utils\Static_Registry {
+
+}
