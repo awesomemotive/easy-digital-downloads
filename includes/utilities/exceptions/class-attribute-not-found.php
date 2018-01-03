e20700a6c77e45b1cf21a60baea00e6d3f25d31b
DrewAPicture <info@drewapicture.com>
2018-01-02 23:08:41 -0700

    Add a $collection param to Attribute_Not_Found::from_attr().

--- includes/utilities/exceptions/class-attribute-not-found.php
+++ includes/utilities/exceptions/class-attribute-not-found.php
@@ -27,13 +27,17 @@ class Attribute_Not_Found extends \OutOfBoundsException implements \EDD_Exceptio
 	 * @since 3.0
 	 *
 	 * @param string     $attribute_name Attribute resulting in the exception.
+	 * @param string     $collection     Collection the attribute belongs to.
 	 * @param int        $code           Optional. Exception code. Default null.
 	 * @param \Exception $previous       Optional. Previous exception (used for chaining).
 	 *                                   Default null.
 	 * @return \EDD\Utils\Exceptions\Attribute_Not_Found Exception instance.
 	 */
-	public static function from_attr( $attribute_name, $code = null, $previous = null ) {
-		$message = sprintf( 'The \'%1$s\' attribute does not exist.', $attribute_name );
+	public static function from_attr( $attribute_name, $collection, $code = null, $previous = null ) {
+		$message = sprintf( 'The \'%1$s\' attribute does not exist for \'%2$s\'.',
+			$attribute_name,
+			$collection
+		);
 
 		return new static( $message, $code, $previous);
 	}
