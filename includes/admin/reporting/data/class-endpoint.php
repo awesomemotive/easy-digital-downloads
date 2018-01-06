a92e22704b9b3e1d733fe14344f44932721046ea
DrewAPicture <info@drewapicture.com>
2018-01-06 12:00:36 -0700

    Bring over Endpoint API stubs from #6254.

---
+++ includes/admin/reporting/data/class-endpoint.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Reports API - Endpoint View object
+ *
+ * @package     EDD
+ * @subpackage  Admin/Reports
+ * @copyright   Copyright (c) 2018, Pippin Williamson
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0
+ */
+namespace EDD\Admin\Reports\Data;
+
+/**
+ * Represents a data endpoint for the Reports API.
+ *
+ * @since 3.0
+ */
+class Endpoint {
+
+	/**
+	 * Represents filters available to the endpoint.
+	 *
+	 * @since 3.0
+	 */
+	public $filters = array();
+
+	/**
+	 * Endpoint label.
+	 *
+	 * @since 3.0
+	 * @var   string
+	 */
+	public $label;
+
+	/**
+	 * Endpoint type.
+	 *
+	 * @since 3.0
+	 * @var   string
+	 */
+	public $type;
+
+	/**
+	 * Sets the endpoint type.
+	 *
+	 * @since 3.0
+	 *
+	 * @param string $type Endpoint type.
+	 */
+	public function set_type( $type ) {
+		$types = edd_get_reports_endpoint_views();
+
+		if ( in_array( $type, $types, true ) ) {
+			$this->type = $type;
+		}
+	}
+
+	/**
+	 * Constructs the endpoint object.
+	 *
+	 * @since 3.0
+	 *
+	 * @param string $endpoint_id Endpoint ID.
+	 * @param string $type        Endpoint view type. Determines which view attribute to
+	 *                            retrieve from the corresponding endpoint registry entry.
+	 */
+	public function __construct( $endpoint_id, $type ) {
+		$this->set_type( $type );
+	}
+
+}
--- includes/class-edd-reports.php
+++ includes/class-edd-reports.php
@@ -53,6 +53,7 @@ final class Reports {
 
 		// Registries.
 		require_once $reports_dir . '/class-registry.php';
+		require_once $reports_dir . '/data/class-endpoint-registry.php';
 	}
 
 	/**
