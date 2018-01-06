502e84953d4950fb321cf97045d6e3769303a24d
DrewAPicture <info@drewapicture.com>
2018-01-05 20:03:23 -0700

    Bring over changes to EDD\Utils\Exception from #6254.

--- includes/utilities/class-edd-exception.php
+++ includes/utilities/class-edd-exception.php
@@ -1,11 +1,9 @@
 <?php
 /**
- * Exception object for EDD
- *
- * This class handles connecting exception handling to the debug log (as needed).
+ * Namespaced exception object for EDD
  *
  * @package     EDD
- * @subpackage  Classes/Utilities
+ * @subpackage  Classes/Utilities/Exceptions
  * @copyright   Copyright (c) 2018, Pippin Williamson
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       3.0
@@ -13,43 +11,14 @@
 namespace EDD\Utils;
 
 /**
- * Implements some EDD-specific logic for exception handling.
+ * Implements a namespaced EDD-specific exception object.
+ *
+ * Implements the EDD_Exception marker interface to make it easier to catch
+ * EDD-specific exceptions under one umbrella.
  *
  * @since 3.0
  *
  * @see \Exception
+ * @see \EDD_Exception
  */
-class Exception extends \Exception {
-
-	/**
-	 * Logs an exception to the EDD debug log.
-	 *
-	 * @since 3.0
-	 *
-	 * @param string $extra Any extra information to include in the log entry
-	 *                      alongside the exception message.
-	 */
-	public function log( $extra = '' ) {
-		if ( $this->getCode() ) {
-
-			$message = sprintf( 'Exception Code: %1$s – Message: %2$s',
-				$this->getCode(),
-				$this->getMessage()
-			);
-
-		} else {
-
-			$message = sprintf( 'Exception Message: %1$s',
-				$this->getMessage()
-			);
-
-		}
-
-		if ( ! empty( $extra ) ) {
-			$message = sprintf( "{$message} %s", $extra );
-		}
-
-		edd_debug_log( $message );
-	}
-
-}
+class Exception extends \Exception implements \EDD_Exception {}
