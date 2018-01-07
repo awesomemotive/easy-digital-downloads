0bf862ed08f56cc048b5ca081e333f5cd14730b1
John James Jacoby <johnjamesjacoby@me.com>
2018-01-06 23:47:17 -0600

    First pass `wp_edd_payments` & `wp_edd_paymentmeta` db table schema classes.
    * Schema not done-done; changes likely
    * Individual item-to-payment schema still todo
    * May rename to `sale` - maybe not? :)
    
    See #4576, #6277.

---
+++ includes/database/class-edd-db-table-paymentmeta.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Payment Meta Table: EDD_DB_Table_Paymentmeta class
+ *
+ * @package Plugins/EDD/Database/Object/Meta
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+if ( class_exists( 'WP_DB_Table' ) ) :
+/**
+ * Setup the global "edd_paymentmeta" database table
+ *
+ * @since 3.0.0
+ */
+final class EDD_DB_Table_Paymentmeta extends WP_DB_Table {
+
+	/**
+	 * @var string Table name
+	 */
+	protected $name = 'edd_paymentmeta';
+
+	/**
+	 * @var string Database version
+	 */
+	protected $version = 201801060001;
+
+	/**
+	 * @var boolean This is not a global table
+	 */
+	protected $global = false;
+
+	/**
+	 * Setup the database schema
+	 *
+	 * @since 3.0.0
+	 */
+	protected function set_schema() {
+		$max_index_length = 191;
+		$this->schema     = "meta_id bigint(20) unsigned NOT NULL auto_increment,
+			payment_id bigint(20) unsigned NOT NULL default 0,
+			meta_key varchar(255) DEFAULT NULL,
+			meta_value longtext DEFAULT NULL,
+			PRIMARY KEY payment_id (payment_id),
+			KEY meta_key (meta_key({$max_index_length}))";
+	}
+
+	/**
+	 * Handle schema changes
+	 *
+	 * @since 3.0.0
+	 */
+	protected function upgrade() {
+
+	}
+}
+endif;
---
+++ includes/database/class-edd-db-table-payments.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Payments Table: EDD_DB_Table_Payments class
+ *
+ * @package Plugins/EDD/Database/Object
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+if ( class_exists( 'WP_DB_Table' ) ) :
+/**
+ * Setup the global "edd_payments" database table
+ *
+ * @since 3.0.0
+ */
+final class EDD_DB_Table_Payments extends WP_DB_Table {
+
+	/**
+	 * @var string Table name
+	 */
+	protected $name = 'edd_payments';
+
+	/**
+	 * @var string Database version
+	 */
+	protected $version = 201801060001;
+
+	/**
+	 * @var boolean This is not a global table
+	 */
+	protected $global = false;
+
+	/**
+	 * Setup the database schema
+	 *
+	 * @since 3.0.0
+	 */
+	protected function set_schema() {
+		$this->schema = "id bigint(20) unsigned NOT NULL auto_increment,
+			number tinytext NOT NULL default '',
+			status varchar(20) NOT NULL default 'pending',
+			date_created datetime NOT NULL default '0000-00-00 00:00:00',
+			date_completed datetime NOT NULL default '0000-00-00 00:00:00',
+			user_id bigint(20) unsigned NOT NULL default '0',
+			customer_id bigint(20) unsigned NOT NULL default '0',
+			email varchar(100) NOT NULL default '',
+			ip varchar(60) NOT NULL default '',
+			gateway varchar(20) NOT NULL default '',
+			payment_key varchar(64) NOT NULL default '',
+			subtotal double NOT NULL default '0',
+			tax double NOT NULL default '0',
+			discounts double NOT NULL default '0',
+			total double NOT NULL default '0',
+			PRIMARY KEY (id),
+			KEY number (number),
+			KEY status (status(20)),
+			KEY user_id (user_id),
+			KEY customer_id (customer_id),
+			KEY email (email(100)),
+			KEY ip (ip(60)),
+			KEY payment_key (payment_key(64)),
+			KEY date_created_completed (date_created,date_completed)";
+	}
+
+	/**
+	 * Handle schema changes
+	 *
+	 * @since 3.0.0
+	 */
+	protected function upgrade() {
+
+	}
+}
+endif;
