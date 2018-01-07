8810915998dd66aa1e519fe4c5e5c641e2764fcf
John James Jacoby <johnjamesjacoby@me.com>
2018-01-06 23:00:20 -0600

    First pass `wp_edd_notes` & `wp_edd_notemeta` db table schema classes.
    See #5277, #6277.

---
+++ includes/database/class-edd-db-table-notemeta.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Note Meta Table: EDD_DB_Table_Notemeta class
+ *
+ * @package Plugins/EDD/Database/Object/Meta
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+if ( class_exists( 'WP_DB_Table' ) ) :
+/**
+ * Setup the global "edd_notemeta" database table
+ *
+ * @since 3.0.0
+ */
+final class EDD_DB_Table_Notemeta extends WP_DB_Table {
+
+	/**
+	 * @var string Table name
+	 */
+	protected $name = 'edd_notemeta';
+
+	/**
+	 * @var string Database version
+	 */
+	protected $version = 201801060001;
+
+	/**
+	 * @var boolean This is not a global table
+	 */
+	protected $global = false;
+
+	/**
+	 * Setup the database schema
+	 *
+	 * @since 3.0.0
+	 */
+	protected function set_schema() {
+		$max_index_length = 191;
+		$this->schema     = "meta_id bigint(20) unsigned NOT NULL auto_increment,
+			note_id bigint(20) unsigned NOT NULL default 0,
+			meta_key varchar(255) DEFAULT NULL,
+			meta_value longtext DEFAULT NULL,
+			PRIMARY KEY note_id (note_id),
+			KEY meta_key (meta_key({$max_index_length}))";
+	}
+
+	/**
+	 * Handle schema changes
+	 *
+	 * @since 3.0.0
+	 */
+	protected function upgrade() {
+
+	}
+}
+endif;
---
+++ includes/database/class-edd-db-table-notes.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Notes Table: EDD_DB_Table_Notes class
+ *
+ * @package Plugins/EDD/Database/Object
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+if ( class_exists( 'WP_DB_Table' ) ) :
+/**
+ * Setup the global "edd_notes" database table
+ *
+ * @since 3.0.0
+ */
+final class EDD_DB_Table_Notes extends WP_DB_Table {
+
+	/**
+	 * @var string Table name
+	 */
+	protected $name = 'edd_notes';
+
+	/**
+	 * @var string Database version
+	 */
+	protected $version = 201801060001;
+
+	/**
+	 * @var boolean This is not a global table
+	 */
+	protected $global = false;
+
+	/**
+	 * Setup the database schema
+	 *
+	 * @since 3.0.0
+	 */
+	protected function set_schema() {
+		$this->schema = "id bigint(20) unsigned NOT NULL auto_increment,
+			object_id bigint(20) unsigned NOT NULL default '0',
+			object_type varchar(20) NOT NULL default '',
+			author_id bigint(20) unsigned NOT NULL default '0',
+			author_ip varchar(60) unsigned NOT NULL default '',
+			content longtext NOT NULL default '',
+			date_created datetime NOT NULL default '0000-00-00 00:00:00',
+			PRIMARY KEY (id),
+			KEY object_id_type (object_id,object_type(20)),
+			KEY author (author_id,author_ip(60)),
+			KEY date_created (date_created)";
+	}
+
+	/**
+	 * Handle schema changes
+	 *
+	 * @since 3.0.0
+	 */
+	protected function upgrade() {
+
+	}
+}
+endif;
