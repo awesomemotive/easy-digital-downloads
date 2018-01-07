78daa23bb2a5d339e7fcaec44eae1701fdbc3765
John James Jacoby <johnjamesjacoby@me.com>
2018-01-06 22:12:50 -0600

    First pass `wp_edd_logs` & `wp_edd_logmeta` db table schema classes.
    (Subject to further index scrutinization.)
    
    See #3841, #6277.

---
+++ includes/database/class-edd-db-table-logmeta.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Log Meta Table: EDD_DB_Table_Logmeta class
+ *
+ * @package Plugins/EDD/Database/Object/Meta
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+if ( class_exists( 'WP_DB_Table' ) ) :
+/**
+ * Setup the global "edd_logmeta" database table
+ *
+ * @since 3.0.0
+ */
+final class EDD_DB_Table_Logmeta extends WP_DB_Table {
+
+	/**
+	 * @var string Table name
+	 */
+	protected $name = 'edd_logmeta';
+
+	/**
+	 * @var string Database version
+	 */
+	protected $version = 201801060001;
+
+	/**
+	 * @var boolean This is a global table
+	 */
+	protected $global = false;
+
+	/**
+	 * Setup the database schema
+	 *
+	 * @since 3.0.0
+	 */
+	protected function set_schema() {
+		$max_index_length = 191;
+		$this->schema     = "meta_id bigint(20) unsigned NOT NULL auto_increment,
+			log_id bigint(20) unsigned NOT NULL default 0,
+			meta_key varchar(255) DEFAULT NULL,
+			meta_value longtext DEFAULT NULL,
+			PRIMARY KEY log_id (log_id),
+			KEY meta_key (meta_key({$max_index_length}))";
+	}
+
+	/**
+	 * Handle schema changes
+	 *
+	 * @since 3.0.0
+	 */
+	protected function upgrade() {
+
+	}
+}
+endif;
