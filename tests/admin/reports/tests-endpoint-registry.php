cc9081ffb5c02526525c25da72db28f2338d95f2
DrewAPicture <info@drewapicture.com>
2018-01-08 16:24:51 -0700

    Stub out a tests class for Endpoint_Registry.

---
+++ tests/admin/reports/tests-endpoint-registry.php
@@ -0,0 +1,74 @@
+<?php
+namespace EDD\Admin\Reports\Data;
+
+if ( ! class_exists( '\EDD\Admin\Reports' ) ) {
+	require_once( EDD_PLUGIN_DIR . 'includes/class-edd-reports.php' );
+}
+
+/**
+ * Tests for the Reports registry API.
+ *
+ * @group edd_registry
+ * @group edd_reports
+ * @group edd_reports_endpoints
+ */
+class Endpoint_Registry_Tests extends \EDD_UnitTestCase {
+
+	/**
+	 * Reports fixture.
+	 *
+	 * @var \EDD\Admin\Reports
+	 * @static
+	 */
+	protected static $reports;
+
+	/**
+	 * Endpoint registry fixture.
+	 *
+	 * @access protected
+	 * @var    \EDD\Admin\Reports\Data\Endpoint_Registry
+	 */
+	protected $registry;
+
+	/**
+	 * Set up fixtures once.
+	 */
+	public static function wpSetUpBeforeClass() {
+		self::$reports = new \EDD\Admin\Reports();
+	}
+
+	/**
+	 * Set up fixtures once.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->registry = new \EDD\Admin\Reports\Data\Endpoint_Registry();
+	}
+
+	/**
+	 * Runs after each test to reset the items array.
+	 *
+	 * @access public
+	 */
+	public function tearDown() {
+		$this->registry->exchangeArray( array() );
+
+		parent::tearDown();
+	}
+
+	/**
+	 * @covers \EDD\Admin\Reports\Reports_Registry::$item_error_label
+	 */
+	public function test_item_error_label_should_be_report() {
+		$this->assertSame( 'reports endpoint', $this->registry->item_error_label );
+	}
+
+	/**
+	 * @covers \EDD\Admin\Reports\Reports_Registry::instance()
+	 */
+	public function test_static_registry_should_have_instance_method() {
+		$this->assertTrue( method_exists( $this->registry, 'instance' ) );
+	}
+
+}
