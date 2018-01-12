9ae16773e7a5d834b0d4d64e88ce7eb0ebfe6cd9
DrewAPicture <info@drewapicture.com>
2018-01-12 09:56:33 -0700

    Type hint $registry in the top-level endpoint getter and setter.

--- includes/admin/reporting/reports.php
+++ includes/admin/reporting/reports.php
@@ -596,6 +596,7 @@ function edd_get_reports_endpoint_views() {
  * @return bool True if the endpoint was successfully registered, otherwise false.
  */
 function edd_reports_register_endpoint( $endpoint_id, $attributes ) {
+	/** @var \EDD\Admin\Reports\Data\Endpoint_Registry $registry */
 	$registry = EDD()->utils->get_registry( 'reports:endpoints' );
 
 	if ( is_wp_error( $registry ) ) {
@@ -627,6 +628,7 @@ function edd_reports_register_endpoint( $endpoint_id, $attributes ) {
  * @return \EDD\Admin\Reports\Data\Endpoint|\WP_Error Endpoint object on success, otherwise a WP_Error object.
  */
 function edd_reports_get_endpoint( $endpoint_id, $view_type ) {
+	/** @var \EDD\Admin\Reports\Data\Endpoint_Registry $registry */
 	$registry = EDD()->utils->get_registry( 'reports:endpoints' );
 
 	if ( is_wp_error( $registry ) ) {
