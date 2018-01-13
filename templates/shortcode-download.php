a81cd2987448c220a0ac09c4179bc3c93a1bae8e
Sunny Ratilal <sunny@sunnyratilal.com>
2018-01-13 15:24:38 +0000

    Remove all references to microdata and deprecate edd_purchase_link_single_pricing_schema() #5240

--- templates/shortcode-download.php
+++ templates/shortcode-download.php
@@ -13,9 +13,7 @@
 global $edd_download_shortcode_item_atts, $edd_download_shortcode_item_i;
 ?>
 
-<?php $schema = edd_add_schema_microdata() ? 'itemscope itemtype="http://schema.org/Product" ' : ''; ?>
-
-<div <?php echo $schema; ?>class="<?php echo esc_attr( apply_filters( 'edd_download_class', 'edd_download', get_the_ID(), $edd_download_shortcode_item_atts, $edd_download_shortcode_item_i ) ); ?>" id="edd_download_<?php the_ID(); ?>">
+<div class="<?php echo esc_attr( apply_filters( 'edd_download_class', 'edd_download', get_the_ID(), $edd_download_shortcode_item_atts, $edd_download_shortcode_item_i ) ); ?>" id="edd_download_<?php the_ID(); ?>">
 
 	<div class="<?php echo esc_attr( apply_filters( 'edd_download_inner_class', 'edd_download_inner', get_the_ID(), $edd_download_shortcode_item_atts, $edd_download_shortcode_item_i ) ); ?>">
 
