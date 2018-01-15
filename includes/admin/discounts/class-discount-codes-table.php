b97489d5b5edc604e7baaf9bf141b2c67ccacfb3
Sunny Ratilal <sunny@sunnyratilal.com>
2018-01-15 23:35:38 +0000

    Sanitise search fields. #5277

--- includes/admin/discounts/class-discount-codes-table.php
+++ includes/admin/discounts/class-discount-codes-table.php
@@ -111,9 +111,9 @@ class EDD_Discount_Codes_Table extends WP_List_Table {
 		}
 		?>
 		<p class="search-box">
-            <label class="screen-reader-text" for="<?php echo $input_id ?>"><?php echo $text; ?>:</label>
-            <input type="search" id="<?php echo $input_id ?>" name="s" value="<?php _admin_search_query(); ?>"/>
-			<?php submit_button( $text, 'button', false, false, array( 'ID' => 'search-submit' ) ); ?>
+            <label class="screen-reader-text" for="<?php echo esc_attr( $input_id ) ?>"><?php echo sanitize_text_field( $text ); ?>:</label>
+            <input type="search" id="<?php echo esc_attr( $input_id ); ?>" name="s" value="<?php _admin_search_query(); ?>"/>
+			<?php submit_button( sanitize_text_field( $text ), 'button', false, false, array( 'ID' => 'search-submit' ) ); ?>
 		</p>
 	<?php
 	}
