6f2992711c8a8cce4b1e33754f66a06b23434b0d
DrewAPicture <info@drewapicture.com>
2018-01-15 13:46:26 -0700

    Add tests coverage for build_endpoints(), parse_view_groups(), and validate_endpoint() Report methods.

--- tests/admin/reports/tests-report.php
+++ tests/admin/reports/tests-report.php
@@ -162,6 +162,111 @@ class Report_Tests extends \EDD_UnitTestCase {
 	}
 
 	/**
+	 * @covers \EDD\Admin\Reports\Report::build_endpoints()
+	 * @throws \EDD_Exception
+	 */
+	public function test_build_endpoints_with_empty_array_should_add_no_new_endpoints() {
+		$report = new Report( array() );
+
+		$report->build_endpoints( array() );
+
+		$this->assertEqualSets( array(), $report->get_endpoints() );
+	}
+
+	/**
+	 * @covers \EDD\Admin\Reports\Report::build_endpoints()
+	 * @throws \EDD_Exception
+	 */
+	public function test_build_endpoints_with_invalid_view_group_should_throw_exception() {
+		$this->setExpectedException( '\EDD_Exception', "The 'fake' view group does not correspond to a known endpoint view type." );
+
+		$report = new Report( array(
+			'id'    => 'foo',
+			'label' => 'Foo',
+		) );
+
+		$report->build_endpoints( array( 'fake' => array() ) );
+	}
+
+	/**
+	 * @covers \EDD\Admin\Reports\Report::parse_view_groups()
+	 */
+	public function test_parse_view_groups_should_return_group_view_key_value_pairs() {
+		$report = new Report( array(
+			'id'    => 'foo',
+			'label' => 'Foo',
+		) );
+
+		$expected = array(
+			'tiles'  => 'tile',
+			'charts' => 'chart',
+			'tables' => 'table',
+			'graphs' => 'graph',
+		);
+
+		$this->assertEqualSetsWithIndex( $expected, $report->parse_view_groups() );
+	}
+
+	/**
+	 * @covers \EDD\Admin\Reports\Report::validate_endpoint()
+	 */
+	public function test_validate_endpoint_passed_a_WP_Error_object_should_add_a_new_error_to_errors() {
+		$report = new Report( array(
+			'id'    => 'foo',
+			'label' => 'Foo',
+		) );
+
+		$report->validate_endpoint( 'tiles', new \WP_Error( 'foo' ) );
+
+		$errors = $report->get_errors();
+
+		$this->assertContains( 'foo', $report->get_errors()->get_error_codes() );
+	}
+
+	/**
+	 * @covers \EDD\Admin\Reports\Report::validate_endpoint()
+	 */
+	public function test_validate_endpoint_passed_an_endpoint_with_errors_should_add_that_error() {
+		$report = new Report( array(
+			'id'    => 'foo',
+			'label' => 'Foo',
+		) );
+
+		$endpoint = new Data\Endpoint( 'tile', array() );
+
+		$report->validate_endpoint( 'tiles', $endpoint );
+
+		$this->assertContains( 'invalid_endpoint', $report->get_errors()->get_error_codes() );
+	}
+
+	/**
+	 * @covers \EDD\Admin\Reports\Report::validate_endpoint()
+	 */
+	public function test_validate_endpoint_passed_a_legitimate_endpoint_should_add_it_to_the_endpoints_array() {
+		$endpoint = new Data\Endpoint( 'tile', array(
+			'id'    => 'foo',
+			'label' => 'Foo',
+			'views' => array(
+				'tile' => array(
+					'display_callback' => '__return_false',
+					'data_callback'    => '__return_false',
+				),
+			),
+		) );
+
+		// Add a completely valid endpoint.
+		$report = new Report( array(
+			'id'        => 'foo',
+			'label'     => 'Foo',
+			'endpoints' => array(),
+		) );
+
+		$report->validate_endpoint( 'tiles', $endpoint );
+
+		$this->assertArrayHasKey( 'foo', $report->get_endpoints( 'tiles' ) );
+	}
+
+	/**
 	 * @covers \EDD\Admin\Reports\Report::has_errors()
 	 */
 	public function test_Report_has_errors_if_no_errors_should_return_false() {
