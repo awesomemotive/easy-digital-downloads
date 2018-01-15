b7c8734d30b626520e1923c9c8376f95a959915b
John James Jacoby <johnjamesjacoby@me.com>
2018-01-15 15:23:09 -0600

    Add `product_condition` column back (accidentally missed.)
    See #6308, #6277.

--- includes/database/class-edd-db-table-discounts.php
+++ includes/database/class-edd-db-table-discounts.php
@@ -50,6 +50,7 @@ final class EDD_DB_Table_Discounts extends WP_DB_Table {
 			use_count bigint(20) unsigned NOT NULL default '0',
 			once_per_customer int(1) NOT NULL default '0',
 			min_cart_price mediumtext NOT NULL default '',
+			product_condition varchar(3) NOT NULL DEFAULT 'all',
 			date_created datetime NOT NULL default '0000-00-00 00:00:00',
 			start_date datetime NOT NULL default '0000-00-00 00:00:00',
 			end_date datetime NOT NULL default '0000-00-00 00:00:00',
