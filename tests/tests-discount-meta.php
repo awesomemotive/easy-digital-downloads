c2e4ef0dea65dec68298e9dc64c0dd733877066c
Sunny Ratilal <sunny@sunnyratilal.com>
2018-01-23 09:07:27 +0000

    Switch to static test fixture. One assertion per unit test. #5277

--- tests/tests-discount-meta.php
+++ tests/tests-discount-meta.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Discount Meta DB Tests
+ * Test for the discount meta table.
  *
  * @covers EDD_DB_Discount_Meta
  * @group edd_discounts_db
@@ -16,78 +16,105 @@ class Tests_Discount_Meta extends EDD_UnitTestCase {
 	 * @access protected
 	 * @var    EDD_Discount
 	 */
-	protected $_discount;
-
-	/**
-	 * Discount ID.
-	 *
-	 * @access protected
-	 * @var    int
-	 */
-	protected $_discount_id;
+	protected static $discount;
 
 	/**
 	 * Set up fixtures.
 	 *
 	 * @access public
 	 */
-	public function setUp() {
-		parent::setUp();
-
-		$this->_discount_id = EDD_Helper_Discount::create_simple_percent_discount();
-		$this->_discount    = edd_get_discount( $this->_discount_id );
-	}
-
-	/**
-	 * Run after each test to empty the cart and reset the test store.
-	 *
-	 * @access public
-	 */
-	public function tearDown() {
-		self::_delete_all_data();
-
-		parent::tearDown();
+	public static function wpSetUpBeforeClass() {
+		$discount_id    = EDD_Helper_Discount::create_simple_percent_discount();
+		self::$discount = new EDD_Discount( $discount_id );
 	}
 
 	/**
 	 * @covers EDD_DB_Discount_Meta::add_meta()
 	 * @covers EDD_Discount::add_meta()
 	 */
-	public function test_add_metadata() {
-		$this->assertFalse( $this->_discount->add_meta( '', '' ) );
-		$this->assertNotEmpty( $this->_discount->add_meta( 'test_key', '' ) );
-		$this->assertNotEmpty( $this->_discount->add_meta( 'test_key', '1' ) );
+	public function test_add_metadata_with_empty_key_value_should_return_false() {
+		$this->assertFalse( self::$discount->add_meta( '', '' ) );
+	}
+
+	/**
+	 * @covers EDD_DB_Discount_Meta::add_meta()
+	 * @covers EDD_Discount::add_meta()
+	 */
+	public function test_add_metadata_with_empty_value_should_be_empty() {
+		$this->assertNotEmpty( self::$discount->add_meta( 'test_key', '' ) );
+	}
+
+	/**
+	 * @covers EDD_DB_Discount_Meta::add_meta()
+	 * @covers EDD_Discount::add_meta()
+	 */
+	public function test_add_metadata_with_key_value_should_not_be_empty() {
+		$this->assertNotEmpty( self::$discount->add_meta( 'test_key', '1' ) );
 	}
 
 	/**
 	 * @covers EDD_DB_Discount_Meta::update_meta()
 	 * @covers EDD_Discount::update_meta()
 	 */
-	public function test_update_metadata() {
-		$this->assertEmpty( $this->_discount->update_meta( '', '' ) );
-		$this->assertNotEmpty( $this->_discount->update_meta( 'test_key_2', '' ) );
-		$this->assertNotEmpty( $this->_discount->update_meta( 'test_key_2', '1' ) );
+	public function test_update_metadata_with_empty_key_value_should_be_empty() {
+		$this->assertEmpty( self::$discount->update_meta( '', '' ) );
+	}
+
+	/**
+	 * @covers EDD_DB_Discount_Meta::update_meta()
+	 * @covers EDD_Discount::update_meta()
+	 */
+	public function test_update_metadata_with_empty_value_should_not_be_empty() {
+		$this->assertNotEmpty( self::$discount->update_meta( 'test_key_2', '' ) );
+	}
+
+	/**
+	 * @covers EDD_DB_Discount_Meta::update_meta()
+	 * @covers EDD_Discount::update_meta()
+	 */
+	public function test_update_metadata_with_key_value_should_not_be_empty() {
+		$this->assertNotEmpty( self::$discount->update_meta( 'test_key_2', '1' ) );
 	}
 
 	/**
 	 * @covers EDD_DB_Discount_Meta::get_meta()
 	 * @covers EDD_Discount::get_meta()
 	 */
-	public function test_get_metadata() {
-		$this->assertEmpty( $this->_discount->get_meta() );
-		$this->assertEmpty( $this->_discount->get_meta( 'key_that_does_not_exist', true ) );
-		$this->_discount->update_meta( 'test_key_2', '1' );
-		$this->assertEquals( '1', $this->_discount->get_meta( 'test_key_2', true ) );
-		$this->assertInternalType( 'array', $this->_discount->get_meta( 'test_key_2', false ) );
+	public function test_get_metadata_with_no_args_should_be_empty() {
+		$this->assertEmpty( self::$discount->get_meta() );
+	}
+
+	/**
+	 * @covers EDD_DB_Discount_Meta::get_meta()
+	 * @covers EDD_Discount::get_meta()
+	 */
+	public function test_get_metadata_with_invalid_key_should_be_empty() {
+		$this->assertEmpty( self::$discount->get_meta( 'key_that_does_not_exist', true ) );
+	}
+
+	/**
+	 * @covers EDD_DB_Discount_Meta::get_meta()
+	 * @covers EDD_Discount::get_meta()
+	 */
+	public function test_get_metadata_after_update_should_return_that_value() {
+		self::$discount->update_meta( 'test_key_2', '1' );
+		$this->assertEquals( '1', self::$discount->get_meta( 'test_key_2', true ) );
 	}
 
 	/**
 	 * @covers EDD_DB_Discount_Meta::delete_meta()
 	 * @covers EDD_Discount::delete_meta()
 	 */
-	public function test_delete_metadata() {
-		$this->_discount->update_meta( 'test_key', '1' );
-		$this->assertTrue( $this->_discount->delete_meta( 'test_key' ) );
-		$this->assertFalse( $this->_discount->delete_meta( 'key_that_does_not_exist' ) );
+	public function test_delete_metadata_with_valid_key_should_return_true() {
+		self::$discount->update_meta( 'test_key', '1' );
+		$this->assertTrue( self::$discount->delete_meta( 'test_key' ) );
+	}
+
+	/**
+	 * @covers EDD_DB_Discount_Meta::delete_meta()
+	 * @covers EDD_Discount::delete_meta()
+	 */
+	public function test_delete_metadata_with_invalid_key_should_return_false() {
+		$this->assertFalse( self::$discount->delete_meta( 'key_that_does_not_exist' ) );
 	}
 }
