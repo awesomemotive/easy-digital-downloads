a0e5123aa87cdcbb95ea29b259f02059d4fe2a34
DrewAPicture <info@drewapicture.com>
2018-01-24 16:57:36 -0800

    Flesh out Tile_Endpoint with an intentionally empty display() method and meta box registration.

--- includes/admin/reporting/data/class-tile-endpoint.php
+++ includes/admin/reporting/data/class-tile-endpoint.php
@@ -25,4 +25,68 @@ final class Tile_Endpoint extends Endpoint {
 	 */
 	protected $view = 'tile';
 
+	/**
+	 * Sets up the tile endpoint.
+	 *
+	 * @since 3.0
+	 *
+	 * @param array $args Tile endpoint attributes.
+	 */
+	public function __construct( array $args ) {
+		parent::__construct( $args );
+
+		$this->add_meta_box();
+	}
+
+	/**
+	 * Retrieves the meta box context for the current tile endpoint.
+	 *
+	 * @since 3.0
+	 *
+	 * @return string Meta box context for the tile.
+	 */
+	public function get_context() {
+		$display_args = $this->get_display_args();
+
+		if ( isset( $display_args['context'] ) ) {
+			$context = $display_args['context'];
+		} else {
+			$context = '';
+		}
+
+		return $context;
+	}
+
+	/**
+	 * Registers the tile as a meta box.
+	 *
+	 * @since 3.0
+	 */
+	public function add_meta_box() {
+		$id = $this->get_id();
+
+		add_meta_box(
+			"tile-{$id}",
+			$this->get_label(),
+			$this->get_display_callback(),
+			'download_page_edd-reports',
+			$this->get_context(),
+			'default',
+			array(
+				'endpoint'     => $this,
+				'data'         => $this->get_data(),
+				'display_args' => $this->get_display_args()
+			)
+		);
+	}
+
+	/**
+	 * Display logic for the current tile endpoint.
+	 *
+	 * Tiles are rendered via meta boxes, so this method is deliberately empty.
+	 *
+	 * @since 3.0
+	 */
+	public function display() {}
+
 }
