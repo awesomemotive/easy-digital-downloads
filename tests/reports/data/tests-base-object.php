6862c05c4f79a93ce7ee83193a1ba4eae7818079
DrewAPicture <info@drewapicture.com>
2018-01-29 16:15:26 -0700

    Rename Reports_Init to just Reports\Init for namespace consistency.

--- tests/reports/data/tests-base-object.php
+++ tests/reports/data/tests-base-object.php
@@ -1,8 +1,8 @@
 <?php
 namespace EDD\Reports\Data;
 
-if ( ! class_exists( '\EDD\Reports\Reports_Init' ) ) {
-	require_once( EDD_PLUGIN_DIR . 'includes/reports/class-reports-init.php' );
+if ( ! class_exists( '\EDD\Reports\Init' ) ) {
+	require_once( EDD_PLUGIN_DIR . 'includes/reports/class-init.php' );
 }
 
 /**
@@ -17,7 +17,7 @@ class Base_Object_Tests extends \EDD_UnitTestCase {
 	/**
 	 * Reports fixture.
 	 *
-	 * @var \EDD\Reports\Reports_init
+	 * @var \EDD\Reports\Init
 	 */
 	protected static $reports;
 
@@ -25,7 +25,7 @@ class Base_Object_Tests extends \EDD_UnitTestCase {
 	 * Set up fixtures once.
 	 */
 	public static function wpSetUpBeforeClass() {
-		self::$reports = new \EDD\Reports\Reports_init();
+		self::$reports = new \EDD\Reports\Init();
 	}
 
 	/**
