c4a01017299c6eaedfd4066b4bd2e5b527161c87
DrewAPicture <info@drewapicture.com>
2018-01-30 16:23:06 -0700

    Add a test class for Table_Endpoint.

---
+++ tests/reports/data/tests-table-endpoint.php
@@ -0,0 +1,43 @@
+<?php
+namespace EDD\Reports\Data;
+
+if ( ! class_exists( '\EDD\Reports\Init' ) ) {
+	require_once( EDD_PLUGIN_DIR . 'includes/reports/class-init.php' );
+}
+
+/**
+ * Tests for the Tile_Endpoint object.
+ *
+ * @group edd_reports
+ * @group edd_reports_endpoints
+ * @group edd_objects
+ *
+ * @coversDefaultClass \EDD\Reports\Data\Table_Endpoint
+ */
+class Table_Endpoint_Tests extends \EDD_UnitTestCase {
+
+	/**
+	 * Reports fixture.
+	 *
+	 * @var \EDD\Reports\Init
+	 * @static
+	 */
+	protected static $reports;
+
+	/**
+	 * Set up fixtures once.
+	 */
+	public static function wpSetUpBeforeClass() {
+		self::$reports = new \EDD\Reports\Init();
+	}
+
+	/**
+	 * @covers ::check_view()
+	 */
+	public function test_check_view_with_valid_view_should_set_that_view() {
+		$endpoint = new Table_Endpoint( array() );
+
+		$this->assertSame( 'table', $endpoint->get_view() );
+	}
+
+}
