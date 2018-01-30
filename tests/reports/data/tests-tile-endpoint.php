320a26b8b07d65fac78df79175d438a0963c1d29
DrewAPicture <info@drewapicture.com>
2018-01-30 16:22:49 -0700

    Define coversDefaultClass for Tile_Endpoint tests.

--- tests/reports/data/tests-tile-endpoint.php
+++ tests/reports/data/tests-tile-endpoint.php
@@ -11,6 +11,8 @@ if ( ! class_exists( '\EDD\Reports\Init' ) ) {
  * @group edd_reports
  * @group edd_reports_endpoints
  * @group edd_objects
+ *
+ * @coversDefaultClass \EDD\Reports\Data\Tile_Endpoint
  */
 class Tile_Endpoint_Tests extends \EDD_UnitTestCase {
 
@@ -30,9 +32,9 @@ class Tile_Endpoint_Tests extends \EDD_UnitTestCase {
 	}
 
 	/**
-	 * @covers \EDD\Reports\Data\Endpoint::set_view()
+	 * @covers ::check_view()
 	 */
-	public function test_set_view_with_valid_view_should_set_that_view() {
+	public function test_check_view_with_valid_view_should_set_that_view() {
 		$endpoint = new Tile_Endpoint( array() );
 
 		$this->assertSame( 'tile', $endpoint->get_view() );
