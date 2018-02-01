0494e57011c8e89a1366b5e4c51f72b8cc562f19
Sunny Ratilal <sunny@sunnyratilal.com>
2018-02-01 17:12:27 +0000

    Use static fixture and only set up once. Remove old post type/taxonomy unit tests. #3841

--- tests/tests-logging.php
+++ tests/tests-logging.php
@@ -1,85 +1,50 @@
 <?php
 
-
 /**
- * @group edd_logging
+ * EDD Logging Class Tests
+ *
+ * @group edd_logs
+ *
+ * @coversDefaultClass EDD_Logging
  */
 class Tests_Logging extends EDD_UnitTestCase {
-	protected $_object = null;
 
-	public function setUp() {
-		parent::setUp();
+	/**
+	 * EDD_Logging fixture.
+	 *
+	 * @var EDD_Logging
+	 * @static
+	 */
+	protected static $object;
 
-		$this->_object = new \EDD_Logging;
-		$this->_object->register_post_type();
-		$this->_object->register_taxonomy();
-	}
-
-	public function tearDown() {
-		parent::tearDown();
-	}
-
-	public function test_post_type() {
-		global $wp_post_types;
-		$this->assertArrayHasKey( 'edd_log', $wp_post_types );
-	}
-
-	public function test_post_type_labels() {
-		global $wp_post_types;
-		$this->assertEquals( 'Logs', $wp_post_types['edd_log']->labels->name );
-		$this->assertEquals( 'Logs', $wp_post_types['edd_log']->labels->singular_name );
-		$this->assertEquals( 'Add New', $wp_post_types['edd_log']->labels->add_new );
-		$this->assertEquals( 'Add New Post', $wp_post_types['edd_log']->labels->add_new_item );
-		$this->assertEquals( 'Edit Post', $wp_post_types['edd_log']->labels->edit_item );
-		$this->assertEquals( 'View Post', $wp_post_types['edd_log']->labels->view_item );
-		$this->assertEquals( 'Search Posts', $wp_post_types['edd_log']->labels->search_items );
-		$this->assertEquals( 'No posts found.', $wp_post_types['edd_log']->labels->not_found );
-		$this->assertEquals( 'No posts found in Trash.', $wp_post_types['edd_log']->labels->not_found_in_trash );
-		$this->assertEquals( 'Logs', $wp_post_types['edd_log']->labels->all_items );
-		$this->assertEquals( 'Logs', $wp_post_types['edd_log']->labels->menu_name );
-		$this->assertEquals( 'Logs', $wp_post_types['edd_log']->labels->name_admin_bar );
-		$this->assertEquals( '', $wp_post_types['edd_log']->publicly_queryable );
-		$this->assertEquals( 'post', $wp_post_types['edd_log']->capability_type );
-		$this->assertEquals( 1, $wp_post_types['edd_log']->map_meta_cap );
-		$this->assertEquals( '', $wp_post_types['edd_log']->rewrite );
-		$this->assertEquals( '', $wp_post_types['edd_log']->has_archive );
-		$this->assertEquals( 'Logs', $wp_post_types['edd_log']->label );
-	}
-
-	public function test_taxonomy_exist() {
-		global $wp_taxonomies;
-		$this->assertArrayHasKey( 'edd_log_type', $wp_taxonomies );
-	}
-
-	public function test_log_types() {
-		$types = $this->_object->log_types();
-		$this->assertEquals( 'sale', $types[0] );
-		$this->assertEquals( 'file_download', $types[1] );
-		$this->assertEquals( 'gateway_error', $types[2] );
-		$this->assertEquals( 'api_request', $types[3] );
+	/**
+	 * Set up fixtures once.
+	 */
+	public static function wpSetUpBeforeClass() {
+		self::$object = new EDD_Logging();
 	}
 
 	public function test_valid_log() {
-		$this->assertTrue( $this->_object->valid_type( 'file_download' ) );
+		$this->assertTrue( self::$object->valid_type( 'file_download' ) );
 	}
 
 	public function test_fake_log() {
-		$this->assertFalse( $this->_object->valid_type( 'foo' ) );
+		$this->assertFalse( self::$object->valid_type( 'foo' ) );
 	}
 
 	public function test_add() {
-		$this->assertNotNull( $this->_object->add() );
-		$this->assertInternalType( 'integer', $this->_object->add() );
+		$this->assertNotNull( self::$object->add() );
+		$this->assertInternalType( 'integer', self::$object->add() );
 	}
 
 	public function test_insert_log() {
-		$this->assertNotNull( $this->_object->insert_log( array( 'log_type' => 'sale' ) ) );
-		$this->assertInternalType( 'integer', $this->_object->insert_log( array( 'log_type' => 'sale' ) ) );
+		$this->assertNotNull( self::$object->insert_log( array( 'log_type' => 'sale' ) ) );
+		$this->assertInternalType( 'integer', self::$object->insert_log( array( 'log_type' => 'sale' ) ) );
 	}
 
 	public function test_get_logs() {
-		$log_id = $this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$out = $this->_object->get_logs( 1, 'sale' );
+		$log_id = self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		$out = self::$object->get_logs( 1, 'sale' );
 
 		$this->assertObjectHasAttribute( 'ID', $out[0] );
 		$this->assertObjectHasAttribute( 'post_author', $out[0] );
@@ -112,8 +77,8 @@ class Tests_Logging extends EDD_UnitTestCase {
 	}
 
 	public function test_get_connected_logs() {
-		$log_id = $this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$out = $this->_object->get_connected_logs( array( 'post_parent' => 1, 'log_type' => 'sale' ) );
+		$log_id = self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		$out = self::$object->get_connected_logs( array( 'post_parent' => 1, 'log_type' => 'sale' ) );
 
 		$this->assertObjectHasAttribute( 'ID', $out[0] );
 		$this->assertObjectHasAttribute( 'post_author', $out[0] );
@@ -146,23 +111,23 @@ class Tests_Logging extends EDD_UnitTestCase {
 	}
 
 	public function test_get_log_count() {
-		$this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
 
-		$this->assertInternalType( 'integer', $this->_object->get_log_count( 1, 'sale' ) );
-		$this->assertEquals( 5, $this->_object->get_log_count( 1, 'sale' ) );
+		$this->assertInternalType( 'integer', self::$object->get_log_count( 1, 'sale' ) );
+		$this->assertEquals( 5, self::$object->get_log_count( 1, 'sale' ) );
 	}
 
 	public function test_delete_logs() {
-		$this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$this->_object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
 
-		$this->assertNull( $this->_object->delete_logs( 1 ) );
+		$this->assertNull( self::$object->delete_logs( 1 ) );
 	}
 }
