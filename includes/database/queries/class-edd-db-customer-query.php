66bed926d18a99f06207ba03c413c4c0e07c16ac
John James Jacoby <johnjamesjacoby@me.com>
2018-02-01 11:26:03 -0600

    First pass (new) `EDD_Customer_Query` that extends `EDD_DB_Query`.
    This conflicts with the existing class, so it's not being included until it's value can be directly assessed.
    
    See #3841, #6331.

---
+++ includes/database/queries/class-edd-db-customer-query.php
@@ -0,0 +1,215 @@
+<?php
+
+/**
+ * Orders: EDD_Customer_Query class
+ *
+ * @package Plugins/EDD/Database/Queries/Customers
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class used for querying customers.
+ *
+ * @since 3.0.0
+ *
+ * @see EDD_Customer_Query::__construct() for accepted arguments.
+ */
+class EDD_Customer_Query extends EDD_DB_Query {
+
+	/** Table Properties ******************************************************/
+
+	/**
+	 * Name of the database table to query.
+	 *
+	 * @since 3.0.0
+	 * @access public
+	 * @var string
+	 */
+	public $table_name = 'edd_customers';
+
+	/**
+	 * String used to alias the database table in MySQL statement.
+	 *
+	 * @since 3.0.0
+	 * @access public
+	 * @var string
+	 */
+	public $table_alias = 'd';
+
+	/** Item ******************************************************************/
+
+	/**
+	 * Name for a single item
+	 *
+	 * @since 3.0.0
+	 * @access public
+	 * @var string
+	 */
+	public $item_name = 'customer';
+
+	/**
+	 * Plural version for a group of items.
+	 *
+	 * @since 3.0.0
+	 * @access public
+	 * @var string
+	 */
+	public $item_name_plural = 'customers';
+
+	/**
+	 * Callback function for turning IDs into objects
+	 *
+	 * @since 3.0.0
+	 * @access public
+	 * @var mixed
+	 */
+	public $single_item_callback = 'edd_get_customer';
+
+	/** Cache *****************************************************************/
+
+	/**
+	 * Group to cache queries and queried items in.
+	 *
+	 * @since 3.0.0
+	 * @access public
+	 * @var string
+	 */
+	public $cache_group = 'customers';
+
+	/** Columns ***************************************************************/
+
+	/**
+	 * Array of database column objects
+	 *
+	 * @since 3.0.0
+	 * @access public
+	 * @var array
+	 */
+	protected $columns = array(
+
+		// id
+		array(
+			'name'       => 'id',
+			'type'       => 'bigint',
+			'length'     => '20',
+			'unsigned'   => true,
+			'extra'      => 'auto_increment',
+			'primary'    => true,
+			'sortable'   => true
+		),
+
+		// user_id
+		array(
+			'name'       => 'user_id',
+			'type'       => 'bigint',
+			'length'     => '20',
+			'unsigned'   => true,
+			'default'    => '0'
+		),
+
+		// email
+		array(
+			'name'       => 'email',
+			'type'       => 'varchar',
+			'length'     => '100',
+			'searchable' => true,
+			'sortable'   => true
+		),
+
+		// name
+		array(
+			'name'       => 'name',
+			'type'       => 'mediumtext',
+			'searchable' => true,
+			'sortable'   => true
+		),
+
+		// purchase_value
+		array(
+			'name'       => 'purchase_value',
+			'type'       => 'decimal',
+			'length'     => '18,9',
+			'default'    => '0'
+		),
+
+		// purchase_count
+		array(
+			'name'       => 'purchase_count',
+			'type'       => 'bigint',
+			'length'     => '20',
+			'unsigned'   => true,
+			'default'    => '0'
+		),
+
+		// payment_ids
+		array(
+			'name'       => 'payment_ids',
+			'type'       => 'longtext',
+			'searchable' => false,
+			'sortable'   => false,
+			'in'         => false,
+			'not_in'     => false
+		),
+
+		// notes
+		array(
+			'name'       => 'notes',
+			'type'       => 'longtext',
+			'default'    => '',
+			'searchable' => false,
+			'sortable'   => false,
+			'in'         => false,
+			'not_in'     => false
+		),
+
+		// date_created
+		array(
+			'name'       => 'date_created',
+			'type'       => 'datetime',
+			'default'    => '0000-00-00 00:00:00',
+			'sortable'   => true
+		)
+	);
+
+	/** Methods ***************************************************************/
+
+	/**
+	 * Sets up the customer query, based on the query vars passed.
+	 *
+	 * @since 3.0.0
+	 * @access public
+	 *
+	 * @param string|array $query {
+	 *     Optional. Array or query string of customer query parameters. Default empty.
+	 *
+	 *     @type int          $id                   An customer ID to only return that customer. Default empty.
+	 *     @type array        $id__in               Array of customer IDs to include. Default empty.
+	 *     @type array        $id__not_in           Array of customer IDs to exclude. Default empty.
+	 *     @type array        $date_created_query   Date query clauses to limit customers by. See WP_Date_Query.
+	 *                                              Default null.
+	 *     @type array        $start_date_query     Date query clauses to limit customers by. See WP_Date_Query.
+	 *                                              Default null.
+	 *     @type array        $end_date_query       Date query clauses to limit customers by. See WP_Date_Query.
+	 *                                              Default null.
+	 *     @type bool         $count                Whether to return a customer count (true) or array of customer objects.
+	 *                                              Default false.
+	 *     @type string       $fields               Site fields to return. Accepts 'ids' (returns an array of customer IDs)
+	 *                                              or empty (returns an array of complete customer objects). Default empty.
+	 *     @type int          $limit                Limit number of customers to retrieve. Default null (no limit).
+	 *     @type int          $offset               Number of customers to offset the query. Used to build LIMIT clause.
+	 *                                              Default 0.
+	 *     @type bool         $no_found_rows        Whether to disable the `SQL_CALC_FOUND_ROWS` query. Default true.
+	 *     @type string|array $orderby              Accepts 'id', 'date_created', 'start_date', 'end_date'.
+	 *                                              Also accepts false, an empty array, or 'none' to disable `ORDER BY` clause.
+	 *                                              Default 'id'.
+	 *     @type string       $order                How to customer retrieved customers. Accepts 'ASC', 'DESC'. Default 'ASC'.
+	 *     @type string       $search               Search term(s) to retrieve matching customers for. Default empty.
+	 *     @type bool         $update_cache         Whether to prime the cache for found customers. Default false.
+	 * }
+	 */
+	public function __construct( $query = array() ) {
+		parent::__construct( $query );
+	}
+}
