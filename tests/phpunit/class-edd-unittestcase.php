9ba0d7b06ae724e743167705f74dc9484724986b
Sunny Ratilal <sunny@sunnyratilal.com>
2018-02-02 09:38:39 +0000

    Remove discounts data to ensure "vanilla" state. #5277

--- tests/phpunit/class-edd-unittestcase.php
+++ tests/phpunit/class-edd-unittestcase.php
@@ -33,9 +33,12 @@ class EDD_UnitTestCase extends WP_UnitTestCase {
 
 	protected static function _delete_all_data() {
 		global $wpdb;
+
 		foreach ( array(
 			EDD()->customers->table_name,
-			EDD()->customer_meta->table_name
+			EDD()->customer_meta->table_name,
+			EDD()->discounts->table_name,
+			EDD()->discount_meta->table_name
 		) as $table ) {
 			$wpdb->query( "DELETE FROM {$table}" );
 		}
