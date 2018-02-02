a7d3aff4e85f6d3f5952f5867e74f34c91af4946
Sunny Ratilal <sunny@sunnyratilal.com>
2018-02-02 17:19:38 +0000

    Remove edd_install() from running twice. Add discounts tables to _delete_all_data. #5277

--- tests/phpunit/class-edd-unittestcase.php
+++ tests/phpunit/class-edd-unittestcase.php
@@ -13,10 +13,6 @@ require_once dirname( __FILE__ ) . '/factory.php';
  */
 class EDD_UnitTestCase extends WP_UnitTestCase {
 
-	public static function wpSetUpBeforeClass() {
-		edd_install();
-	}
-
 	protected static function edd() {
 		static $factory = null;
 		if ( ! $factory ) {
@@ -24,7 +20,6 @@ class EDD_UnitTestCase extends WP_UnitTestCase {
 		}
 		return $factory;
 	}
-
 
 	public static function tearDownAfterClass() {
 		self::_delete_all_data();
@@ -38,6 +33,8 @@ class EDD_UnitTestCase extends WP_UnitTestCase {
 		foreach ( array(
 			EDD()->customers->table_name,
 			EDD()->customer_meta->table_name,
+			EDD()->discounts->table_name,
+			EDD()->discount_meta->table_name,
 		) as $table ) {
 			$wpdb->query( "DELETE FROM {$table}" );
 		}
