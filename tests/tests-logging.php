033a95a66233474c7ed2d4194d623585b1d6fc84
Sunny Ratilal <sunny@sunnyratilal.com>
2018-02-02 06:29:30 +0000

    Refactor EDD_Logging unit tests for performance improvements. #3841

--- tests/tests-logging.php
+++ tests/tests-logging.php
@@ -18,116 +18,82 @@ class Tests_Logging extends EDD_UnitTestCase {
 	protected static $object;
 
 	/**
+	 * Log test fixture.
+	 *
+	 * @var int
+	 * @static
+	 */
+	protected static $log_id;
+
+	/**
 	 * Set up fixtures once.
 	 */
 	public static function wpSetUpBeforeClass() {
 		self::$object = new EDD_Logging();
+
+		self::$log_id = self::$object->insert_log( array(
+			'log_type'     => 'gateway_error',
+			'post_parent'  => 1,
+			'post_title'   => 'Test Log',
+			'post_content' => 'This is a test log inserted from PHPUnit',
+		) );
 	}
 
+	/**
+	 * @covers ::valid_type()
+	 */
 	public function test_valid_log() {
 		$this->assertTrue( self::$object->valid_type( 'file_download' ) );
 	}
 
+	/**
+	 * @covers ::valid_type()
+	 */
 	public function test_fake_log() {
 		$this->assertFalse( self::$object->valid_type( 'foo' ) );
 	}
 
+	/**
+	 * @covers ::add()
+	 */
 	public function test_add() {
 		$this->assertNotNull( self::$object->add() );
-		$this->assertInternalType( 'integer', self::$object->add() );
 	}
 
-	public function test_insert_log() {
-		$this->assertNotNull( self::$object->insert_log( array( 'log_type' => 'sale' ) ) );
-		$this->assertInternalType( 'integer', self::$object->insert_log( array( 'log_type' => 'sale' ) ) );
-	}
-
+	/**
+	 * @covers ::get_logs()
+	 */
 	public function test_get_logs() {
-		$log_id = self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$out = self::$object->get_logs( 1, 'sale' );
+		$logs = (array) self::$object->get_logs( 1, 'sale' );
 
-		$this->assertObjectHasAttribute( 'ID', $out[0] );
-		$this->assertObjectHasAttribute( 'post_author', $out[0] );
-		$this->assertObjectHasAttribute( 'post_date', $out[0] );
-		$this->assertObjectHasAttribute( 'post_date_gmt', $out[0] );
-		$this->assertObjectHasAttribute( 'post_content', $out[0] );
-		$this->assertObjectHasAttribute( 'post_title', $out[0] );
-		$this->assertObjectHasAttribute( 'post_excerpt', $out[0] );
-		$this->assertObjectHasAttribute( 'post_status', $out[0] );
-		$this->assertObjectHasAttribute( 'comment_status', $out[0] );
-		$this->assertObjectHasAttribute( 'ping_status', $out[0] );
-		$this->assertObjectHasAttribute( 'post_password', $out[0] );
-		$this->assertObjectHasAttribute( 'post_name', $out[0] );
-		$this->assertObjectHasAttribute( 'to_ping', $out[0] );
-		$this->assertObjectHasAttribute( 'pinged', $out[0] );
-		$this->assertObjectHasAttribute( 'post_modified', $out[0] );
-		$this->assertObjectHasAttribute( 'post_modified_gmt', $out[0] );
-		$this->assertObjectHasAttribute( 'post_content_filtered', $out[0] );
-		$this->assertObjectHasAttribute( 'post_parent', $out[0] );
-		$this->assertObjectHasAttribute( 'guid', $out[0] );
-		$this->assertObjectHasAttribute( 'menu_order', $out[0] );
-		$this->assertObjectHasAttribute( 'post_type', $out[0] );
-		$this->assertObjectHasAttribute( 'post_mime_type', $out[0] );
-		$this->assertObjectHasAttribute( 'comment_count', $out[0] );
-		$this->assertObjectHasAttribute( 'filter', $out[0] );
-
-		$this->assertEquals( 'This is a test log inserted from PHPUnit', $out[0]->post_content );
-		$this->assertEquals( 'Test Log', $out[0]->post_title );
-		$this->assertEquals( 'edd_log', $out[0]->post_type );
+		$this->assertCount( 1, $logs );
 	}
 
+	/**
+	 * @covers ::get_connected_logs()
+	 */
 	public function test_get_connected_logs() {
-		$log_id = self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		$out = self::$object->get_connected_logs( array( 'post_parent' => 1, 'log_type' => 'sale' ) );
+		$logs = (array) self::$object->get_connected_logs( array(
+			'post_parent' => 1,
+			'log_type'    => 'sale'
+		) );
 
-		$this->assertObjectHasAttribute( 'ID', $out[0] );
-		$this->assertObjectHasAttribute( 'post_author', $out[0] );
-		$this->assertObjectHasAttribute( 'post_date', $out[0] );
-		$this->assertObjectHasAttribute( 'post_date_gmt', $out[0] );
-		$this->assertObjectHasAttribute( 'post_content', $out[0] );
-		$this->assertObjectHasAttribute( 'post_title', $out[0] );
-		$this->assertObjectHasAttribute( 'post_excerpt', $out[0] );
-		$this->assertObjectHasAttribute( 'post_status', $out[0] );
-		$this->assertObjectHasAttribute( 'comment_status', $out[0] );
-		$this->assertObjectHasAttribute( 'ping_status', $out[0] );
-		$this->assertObjectHasAttribute( 'post_password', $out[0] );
-		$this->assertObjectHasAttribute( 'post_name', $out[0] );
-		$this->assertObjectHasAttribute( 'to_ping', $out[0] );
-		$this->assertObjectHasAttribute( 'pinged', $out[0] );
-		$this->assertObjectHasAttribute( 'post_modified', $out[0] );
-		$this->assertObjectHasAttribute( 'post_modified_gmt', $out[0] );
-		$this->assertObjectHasAttribute( 'post_content_filtered', $out[0] );
-		$this->assertObjectHasAttribute( 'post_parent', $out[0] );
-		$this->assertObjectHasAttribute( 'guid', $out[0] );
-		$this->assertObjectHasAttribute( 'menu_order', $out[0] );
-		$this->assertObjectHasAttribute( 'post_type', $out[0] );
-		$this->assertObjectHasAttribute( 'post_mime_type', $out[0] );
-		$this->assertObjectHasAttribute( 'comment_count', $out[0] );
-		$this->assertObjectHasAttribute( 'filter', $out[0] );
-
-		$this->assertEquals( 'This is a test log inserted from PHPUnit', $out[0]->post_content );
-		$this->assertEquals( 'Test Log', $out[0]->post_title );
-		$this->assertEquals( 'edd_log', $out[0]->post_type );
+		$this->assertCount( 1, $logs );
 	}
 
+	/**
+	 * @covers ::get_log_count()
+	 */
 	public function test_get_log_count() {
-		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-
-		$this->assertInternalType( 'integer', self::$object->get_log_count( 1, 'sale' ) );
-		$this->assertEquals( 5, self::$object->get_log_count( 1, 'sale' ) );
+		$this->assertSame( 1, self::$object->get_log_count( 1, 'gateway_error' ) );
 	}
 
+	/**
+	 * @covers ::delete_logs()
+	 */
 	public function test_delete_logs() {
-		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
-		self::$object->insert_log( array( 'log_type' => 'sale', 'post_parent' => 1, 'post_title' => 'Test Log', 'post_content' => 'This is a test log inserted from PHPUnit' ) );
+		self::$object->delete_logs( self::$log_id );
 
-		$this->assertNull( self::$object->delete_logs( 1 ) );
+		$this->assertSame( 0, self::$object->get_log_count( 1, 'sale' ) );
 	}
 }
