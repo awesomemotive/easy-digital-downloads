db31c563abc17c1fc3e14e8132b8a06b02c45371
Drew Jaynes <drew@affiliatewp.com>
2018-02-04 23:08:12 -0700

    Set up magic properties for the WP offest, date, and time formats. Introduce the utils()->date and ->get_date_format() helpers to the top-level utils class.

--- includes/class-edd-utilities.php
+++ includes/class-edd-utilities.php
@@ -13,6 +13,10 @@
  * Class that bootstraps various utilities leveraged in EDD core.
  *
  * @since 3.0
+ *
+ * @property-read int    $wp_offset   The calculated WordPress gmt_offset in seconds.
+ * @property-read string $date_format The current WordPress date format.
+ * @property-read string $time_format The current WordPress time format.
  */
 class EDD_Utilities {
 
@@ -22,6 +26,10 @@ class EDD_Utilities {
 	 * @since 3.0
 	 */
 	public function __construct() {
+		$this->wp_offset   = get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS;
+		$this->date_format = get_option( 'date_format', 'M j, Y' );
+		$this->time_format = get_option( 'time_format', 'g:i a' );
+
 		$this->includes();
 	}
 
@@ -32,6 +40,10 @@ class EDD_Utilities {
 	 */
 	private function includes() {
 		$utils_dir = EDD_PLUGIN_DIR . 'includes/utilities/';
+
+		// Date management.
+		require_once EDD_PLUGIN_DIR . 'includes/libraries/Carbon.php';
+		require_once $utils_dir . 'class-date.php';
 
 		// Interfaces.
 		require_once $utils_dir . 'interface-edd-exception.php';
@@ -59,6 +71,7 @@ class EDD_Utilities {
 
 		switch( $name ) {
 			case 'reports':
+
 				if ( ! did_action( 'edd_reports_init' ) ) {
 
 					_doing_it_wrong( __FUNCTION__, 'The Reports registry cannot be retrieved prior to the edd_reports_init hook.', 'EDD 3.0' );
@@ -71,6 +84,7 @@ class EDD_Utilities {
 				break;
 
 			case 'reports:endpoints':
+
 				if ( ! did_action( 'edd_reports_init' ) ) {
 
 					_doing_it_wrong( __FUNCTION__, 'The Endpoints registry cannot be retrieved prior to the edd_reports_init hook.', 'EDD 3.0' );
@@ -88,6 +102,88 @@ class EDD_Utilities {
 		}
 
 		return $registry;
+	}
+
+	/**
+	 * Retrieves a date format string based on a given short-hand format.
+	 *
+	 * @since 3.0
+	 *
+	 * @param string $format Shorthand date format string. Accepts 'date', 'time', 'mysql', or
+	 *                       'datetime'. If none of the accepted values, the original value will
+	 *                       simply be returned. Default is the value of the `$date_format` property,
+	 *                       derived from the core 'date_format' option.
+	 * @return string date_format()-compatible date format string.
+	 */
+	public function get_date_format( $format ) {
+
+		if ( empty( $format ) ) {
+			$format = 'date';
+		}
+
+		if ( ! in_array( $format, array( 'date', 'time', 'datetime', 'mysql' ) ) ) {
+			return $format;
+		}
+
+		switch( $format ) {
+			case 'time':
+				$format = $this->time_format;
+				break;
+
+			case 'datetime':
+				$format = $this->date_format . ' ' . $this->time_format;
+				break;
+
+			case 'mysql':
+				$format = 'Y-m-d H:i:s';
+				break;
+
+			case 'date':
+			default:
+				$format = $this->date_format;
+				break;
+		}
+
+		return $format;
+	}
+
+	/**
+	 * Retrieves a date instance for the WP timezone (and offset) based on the given date string.
+	 *
+	 * @since 3.0
+	 *
+	 * @param string $date_string Optional. Date string. Default 'now'.
+	 * @param string $timezone    Optional. Timezone to generate the Carbon instance for.
+	 *                            Default is the timezone set in WordPress settings.
+	 * @return \EDD\Utils\Date Date instance.
+	 */
+	public function date( $date_string = 'now' ) {
+
+		$timezone = edd_get_timezone();
+
+		/*
+		 * Create the DateTime object with the "local" WordPress timezone.
+		 *
+		 * Note that supplying the timezone during DateTime instantiation doesn't actually
+		 * convert the UNIX timestamp, it just lays the groundwork for deriving the offset.
+		 */
+		$date = new EDD\Utils\Date( $date_string, new DateTimezone( $timezone ) );
+
+		return $date;
+	}
+
+	/**
+	 * Refreshes the wp_offset property for the benefit of PHPUnit.
+	 *
+	 * @since 3.0
+	 */
+	public function _refresh_wp_offset() {
+		if ( ! defined( 'WP_TESTS_DOMAIN' ) ) {
+			_doing_it_wrong( 'This method is only intended for use in phpunit tests', '3.0' );
+		} else {
+			$this->wp_offset = get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS;
+		}
+
 	}
 
 }
