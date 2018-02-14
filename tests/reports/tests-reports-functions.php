df9c10ca8b67b7df4a4b911e067e17fe4318124a
DrewAPicture <drew@affiliatewp.com>
2018-02-14 11:34:39 -0700

    Rename two test methods to better illustrate expected fallback behavior in get_dates_filter_range().

--- tests/reports/tests-reports-functions.php
+++ tests/reports/tests-reports-functions.php
@@ -748,7 +748,7 @@ class Reports_Functions_Tests extends \EDD_UnitTestCase {
 	 * @covers ::\EDD\Reports\get_dates_filter_range()
 	 * @group edd_dates
 	 */
-	public function test_get_dates_filter_range_should_default_to_last_30_days() {
+	public function test_get_dates_filter_range_with_no_report_id_should_fallback_to_last_30_days_if_range_var_not_set() {
 		$this->assertSame( 'last_30_days', get_dates_filter_range() );
 	}
 
@@ -766,7 +766,7 @@ class Reports_Functions_Tests extends \EDD_UnitTestCase {
 	 * @covers ::\EDD\Reports\get_dates_filter_range()
 	 * @group edd_dates
 	 */
-	public function test_get_dates_filter_range_with_range_var_set_should_return_the_sanitized_version_of_that_var() {
+	public function test_get_dates_filter_range_with_no_report_id_should_fallback_to_range_var_if_set() {
 		$_REQUEST['range'] = '1/2/3/4';
 
 		$this->assertSame( '1234', get_dates_filter_range() );
