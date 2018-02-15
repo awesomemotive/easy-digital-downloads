91334d6e436d121476e76f8858a8f2c3fbc0a5dd
Sunny Ratilal <sunny@sunnyratilal.com>
2018-02-15 21:58:55 +0000

    Remove calls to register_post_type() and register_taxonomy(). #3841

--- includes/class-edd-logging.php
+++ includes/class-edd-logging.php
@@ -49,12 +49,6 @@ class EDD_Logging {
 	 * @since 1.3.1
 	 */
 	public function __construct() {
-		// Create the log post type
-		add_action( 'init', array( $this, 'register_post_type' ), 1 );
-
-		// Create types taxonomy and default types
-		add_action( 'init', array( $this, 'register_taxonomy' ), 1 );
-
 		add_action( 'plugins_loaded', array( $this, 'setup_log_file' ), 0 );
 
 		// Backwards compatibility for API request logs
