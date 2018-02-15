bb7dfbcc03d45ea6d87cb418a4a43e447737a9fd
Sunny Ratilal <sunny@sunnyratilal.com>
2018-02-15 22:34:57 +0000

    Add tests for updating and deleting logs. #3841

--- tests/logs/tests-logs-db.php
+++ tests/logs/tests-logs-db.php
@@ -58,7 +58,31 @@ class Logs_DB_Tests extends \EDD_UnitTestCase {
 			'date_created' => date( 'Y-m-d H:i:s' ),
 		);
 
-		$this->assertSame( $expected, EDD()->logs->get_column_defaults() );
+		$this->assertEqualSets( $expected, EDD()->logs->get_column_defaults() );
+	}
+
+	/**
+	 * @covers ::update()
+	 */
+	public function test_update_should_return_true() {
+		$success = EDD()->logs->update( self::$logs[0], array(
+			'title' => 'Log title 45',
+		) );
+
+		$this->assertTrue( $success );
+	}
+
+	/**
+	 * @covers ::update()
+	 */
+	public function test_log_object_after_update_should_return_true() {
+		$success = EDD()->logs->update( self::$logs[0], array(
+			'title' => 'Log title 45',
+		) );
+
+		$log = new \EDD\Logs\Log( self::$logs[0] );
+
+		$this->assertEquals( 'Log title 45', $log->title );
 	}
 
 	/**
@@ -70,6 +94,40 @@ class Logs_DB_Tests extends \EDD_UnitTestCase {
 		) );
 
 		$this->assertFalse( $success );
+	}
+
+	/**
+	 * @covers ::delete()
+	 */
+	public function test_delete_should_return_true() {
+		$success = EDD()->logs->delete( self::$logs[0] );
+
+		$this->assertTrue( $success );
+	}
+
+	/**
+	 * @covers ::delete()
+	 */
+	public function test_delete_without_id_should_fail() {
+		$success = EDD()->logs->delete( '' );
+
+		$this->assertFalse( $success );
+	}
+
+	/**
+	 * @covers ::get_logs()
+	 */
+	public function test_get_logs() {
+		$logs = EDD()->logs->get_logs();
+
+		$this->assertCount( 5, $logs );
+	}
+
+	/**
+	 * @covers ::count()
+	 */
+	public function test_count() {
+		$this->assertEquals( 5, EDD()->logs->count() );
 	}
 
 }
