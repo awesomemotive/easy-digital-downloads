172769d6603cbd8103e42263445717fdab53e024
Sean Davis <sdavis2702@gmail.com>
2018-02-21 10:44:47 -0800

    check for output buffer before cleaning #6387

--- includes/process-download.php
+++ includes/process-download.php
@@ -743,8 +743,10 @@ function edd_get_file_ctype( $extension ) {
  */
 function edd_readfile_chunked( $file, $retbytes = true ) {
 
-	// Make sure output buffers are closed. See https://github.com/easydigitaldownloads/easy-digital-downloads/issues/6387
-	ob_clean();
+	// If output buffers exist, make sure they are closed. See https://github.com/easydigitaldownloads/easy-digital-downloads/issues/6387
+	if ( ob_get_length() ) {
+		ob_clean();
+	}
 
 	$chunksize = 1024 * 1024;
 	$buffer    = '';
