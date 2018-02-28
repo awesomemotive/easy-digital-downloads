9deb9db20bd5ab6263b610fbeff41c5259fced06
Sunny Ratilal <sunny@sunnyratilal.com>
2018-02-28 09:33:58 +0000

    Introduce unit test factory for notes. #6278

--- tests/phpunit/factory.php
+++ tests/phpunit/factory.php
@@ -1,4 +1,5 @@
 <?php
 
 require_once( dirname( __FILE__ ) . '/factories/class-edd-factory-for-discounts.php' );
+require_once( dirname( __FILE__ ) . '/factories/class-edd-factory-for-notes.php' );
 require_once( dirname( __FILE__ ) . '/factories/class-edd-factory.php' );
