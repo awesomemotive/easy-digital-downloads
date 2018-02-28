4234525e8de01ff4779e0131c4e5dc044c6ebd42
Sunny Ratilal <sunny@sunnyratilal.com>
2018-02-28 22:26:47 +0000

    Separate meta unit tests. #6278

--- tests/notes/tests-note-meta.php
+++ tests/notes/tests-note-meta.php
@@ -27,31 +27,58 @@ class Tests_Note_Meta extends \EDD_UnitTestCase {
 	}
 
 	/**
-	 * @covers EDD_DB_Discount_Meta::add_meta()
-	 * @covers EDD_Discount::add_meta()
+	 * @covers \EDD_DB_Note_Meta::add_meta()
+	 * @covers Note::add_meta()
 	 */
-	public function test_add_metadata() {
+	public function test_add_metadata_with_empty_key_value_should_return_false() {
 		$this->assertFalse( self::$note->add_meta( '', '' ) );
+	}
+
+	public function test_add_metadata_with_empty_value_should_not_be_empty() {
 		$this->assertNotEmpty( self::$note->add_meta( 'test_key', '' ) );
+	}
+
+	public function test_add_metadata_with_key_value_should_not_be_empty() {
 		$this->assertNotEmpty( self::$note->add_meta( 'test_key', '1' ) );
 	}
 
 	/**
-	 * @covers EDD_DB_Discount_Meta::update_meta()
-	 * @covers EDD_Discount::update_meta()
+	 * @covers \EDD_DB_Note_Meta::update_meta()
+	 * @covers Note::update_meta()
 	 */
-	public function test_update_metadata() {
+	public function test_update_metadata_with_empty_key_value_should_return_false() {
 		$this->assertEmpty( self::$note->update_meta( '', '' ) );
+	}
+
+	/**
+	 * @covers \EDD_DB_Note_Meta::update_meta()
+	 * @covers Note::update_meta()
+	 */
+	public function test_update_metadata_with_empty_value_should_not_be_empty() {
 		$this->assertNotEmpty( self::$note->update_meta( 'test_key_2', '' ) );
+	}
+
+	/**
+	 * @covers \EDD_DB_Note_Meta::update_meta()
+	 * @covers Note::update_meta()
+	 */
+	public function test_update_metadata_with_key_value_should_not_be_empty() {
 		$this->assertNotEmpty( self::$note->update_meta( 'test_key_2', '1' ) );
 	}
 
 	/**
-	 * @covers EDD_DB_Discount_Meta::get_meta()
-	 * @covers EDD_Discount::get_meta()
+	 * @covers \EDD_DB_Note_Meta::get_meta()
+	 * @covers Note::get_meta()
 	 */
-	public function test_get_metadata() {
+	public function test_get_metadata_with_no_key_should_be_empty() {
 		$this->assertEmpty( self::$note->get_meta() );
+	}
+
+	/**
+	 * @covers \EDD_DB_Note_Meta::get_meta()
+	 * @covers Note::get_meta()
+	 */
+	public function test_get_metadata_with_invalid_key_should_be_empty() {
 		$this->assertEmpty( self::$note->get_meta( 'key_that_does_not_exist', true ) );
 		self::$note->update_meta( 'test_key_2', '1' );
 		$this->assertEquals( '1', self::$note->get_meta( 'test_key_2', true ) );
@@ -59,11 +86,23 @@ class Tests_Note_Meta extends \EDD_UnitTestCase {
 	}
 
 	/**
-	 * @covers EDD_DB_Discount_Meta::delete_meta()
-	 * @covers EDD_Discount::delete_meta()
+	 * @covers \EDD_DB_Note_Meta::get_meta()
+	 * @covers Note::get_meta()
 	 */
-	public function test_delete_metadata() {
+	public function test_get_metadata_after_update_should_return_1_and_be_of_type_array() {
+		self::$note->update_meta( 'test_key_2', '1' );
+
+		$this->assertEquals( '1', self::$note->get_meta( 'test_key_2', true ) );
+		$this->assertInternalType( 'array', self::$note->get_meta( 'test_key_2', false ) );
+	}
+
+	/**
+	 * @covers \EDD_DB_Note_Meta::delete_meta()
+	 * @covers Note::delete_meta()
+	 */
+	public function test_delete_metadata_after_update() {
 		self::$note->update_meta( 'test_key', '1' );
+
 		$this->assertTrue( self::$note->delete_meta( 'test_key' ) );
 		$this->assertFalse( self::$note->delete_meta( 'key_that_does_not_exist' ) );
 	}
