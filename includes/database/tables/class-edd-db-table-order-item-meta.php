be0d36eacc865fbb00532b6fdfc17fe044ba703c
John James Jacoby <johnjamesjacoby@me.com>
2018-02-28 12:49:42 -0600

    Bump database versions to today.
    See #6331.

--- includes/database/tables/class-edd-db-table-order-item-meta.php
+++ includes/database/tables/class-edd-db-table-order-item-meta.php
@@ -33,7 +33,7 @@ final class EDD_DB_Table_Order_Item_Meta extends EDD_DB_Table {
 	 * @since 3.0.0
 	 * @var int
 	 */
-	protected $version = 201802120001;
+	protected $version = 201802280001;
 
 	/**
 	 * Setup the database schema
