42ae7e1225ba96e7b2ae26309e19ed393e425e2c
DrewAPicture <drew@affiliatewp.com>
2018-03-08 09:33:39 -0700

    Move the $options setter and getter up stream to the base Chart_Endpoint.

--- includes/reports/data/class-graph-endpoint.php
+++ includes/reports/data/class-graph-endpoint.php
@@ -46,53 +46,6 @@ final class Graph_Endpoint extends Chart_Endpoint {
 	 */
 	public function __construct( array $args ) {
 		parent::__construct( $args );
-
-		$options = empty( $args['options'] ) ? array() : $args['options'];
-
-		// TODO handle validation, or just use the defaults?
-		$this->set_options( $options );
-	}
-
-	/**
-	 * Retrieves the graphing library options set for the current endpoint.
-	 *
-	 * @since 3.0
-	 *
-	 * @return array Options set for the current graph endpoint.
-	 */
-	public function get_options() {
-		return $this->options;
-	}
-
-	/**
-	 * Sets options for displaying the graph.
-	 *
-	 * @since 3.0
-	 *
-	 * @param array $args Options for displaying the graph via the graphing library.
-	 */
-	private function set_options( $args ) {
-		$options = array(
-			'y_mode'             => null,
-			'x_mode'             => null,
-			'y_decimals'         => 0,
-			'x_decimals'         => 0,
-			'y_position'         => 'right',
-			'time_format'        => '%d/%b',
-			'ticksize_unit'      => 'day',
-			'ticksize_num'       => 1,
-			'multiple_y_axes'    => false,
-			'bgcolor'            => '#f9f9f9',
-			'bordercolor'        => '#ccc',
-			'color'              => '#bbb',
-			'borderwidth'        => 2,
-			'bars'               => false,
-			'lines'              => true,
-			'points'             => true,
-			'additional_options' => '',
-		);
-
-		$this->options = wp_parse_args( $args, $options );
 	}
 
 }
