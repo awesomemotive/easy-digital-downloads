612c691dcec1e8dde4133a96e450a467e62f2a2d
DrewAPicture <drew@affiliatewp.com>
2018-03-12 18:05:58 -0600

    Stub out a build_graph() method for Graph_Endpoint with the first run of arguments structured for integration with ChartJS.

--- includes/reports/data/class-graph-endpoint.php
+++ includes/reports/data/class-graph-endpoint.php
@@ -48,4 +48,101 @@ final class Graph_Endpoint extends Chart_Endpoint {
 		parent::__construct( $args );
 	}
 
+	/**
+	 * Builds and outputs the graph JS to the page.
+	 *
+	 * @since 3.0.0
+	 */
+	public function build_graph() {
+		$data = $this->get_data();
+
+		if ( empty( $data ) ) {
+			return;
+		}
+
+		$dataset_count = count( $data );
+		$current       = 0;
+		$canvas_id     = $this->get_display_arg( 'target', 'edd-reports-graph' );
+		?>
+		<canvas id="<?php echo esc_attr( $canvas_id ); ?>"></canvas>
+
+		<script type="application/javascript">
+
+			var date = moment( 'today', 'MMMM DD YYYY' );
+
+			var lineChartData = {
+				<?php foreach ( $data as $set => $atts ) : ?>
+					datasets: [{
+						label: <?php echo empty( $atts['label' ] ? '' : esc_js( $atts['label'] ) ); ?>,
+						borderColor: <?php echo empty( $atts['border_color'] ) ? 'rgb(237,194,64)' : esc_js( $atts['border_color'] ); ?>,
+						backgroundColor: <?php echo empty( $atts['bg_color'] ) ? 'rgb(237,194,64)' : esc_js( $atts['bg_color'] ); ?>,
+						fill: false,
+
+						data: [
+							<?php
+							if ( ! empty( $atts['data'] ) ) :
+								foreach ( $atts['data'] as $index => $axes ) :
+									$axes_count = count( $axes );
+									?>
+									{
+										<?php if ( 2 === $axes_count  ) : ?>
+											x: moment( <?php echo $axes[0] * 1000; ?> ),
+											y: <?php echo $axes[1]; ?>
+										<?php elseif ( 1 === $axes_count ) : ?>
+											x: moment( <?php echo $axes[0] * 1000; ?> ),
+										<?php endif; ?>
+									},
+								<?php
+								endforeach;
+							endif;
+							?>
+						],
+					<?php if ( ++$current !== $dataset_count ) : ?>
+						}, {
+					<?php endif; ?>
+				<?php endforeach; ?>
+					};
+			};
+
+			myLine = Chart.Line( $( '#edd-reports-graph' ), {
+				data: lineChartData,
+				options: {
+					responsive: true,
+					hoverMode: 'index',
+					stacked: false,
+					title:{
+						display: true,
+						text: <?php echo esc_js( $this->get_label() ); ?>
+					},
+					scales: {
+						yAxes: [{
+							type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+							display: true,
+							position: "left",
+							id: "y-axis-1",
+						} ],
+						xAxes: [{
+							type: 'time',
+							display: true,
+							id: 'x-axis-1',
+							ticks: {
+								source: 'auto',
+							},
+							time: {
+								min: moment().startOf( 'day' ),
+								max: moment().endOf( 'day' ),
+								unit: 'hour',
+								displayFormats: {
+									day: 'MMM D',
+								}
+							},
+						} ],
+					}
+				}
+			});
+
+		</script>
+		<?php
+	}
+
 }
