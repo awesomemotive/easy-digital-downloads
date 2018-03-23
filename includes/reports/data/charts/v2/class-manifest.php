98b3ae2d41c2c060e4fc782e9334b4bdb4d3a260
DrewAPicture <drew@affiliatewp.com>
2018-03-23 11:24:59 -0600

    Add private properties and setters to hold the $type and $options array for a manifest.

--- includes/reports/data/charts/v2/class-manifest.php
+++ includes/reports/data/charts/v2/class-manifest.php
@@ -11,6 +11,22 @@ namespace EDD\Reports\Data\Charts\v2;
 class Manifest {
 
 	/**
+	 * Represents the chart type to be manifested.
+	 *
+	 * @since 3.0
+	 * @var   string
+	 */
+	private $type;
+
+	/**
+	 * Represents the unfiltered chart options for the manifest.
+	 *
+	 * @since 3.0
+	 * @var   array
+	 */
+	private $options = array();
+
+	/**
 	 * Datasets associated with the current graph.
 	 *
 	 * @since 3.0.0
@@ -27,7 +43,30 @@ class Manifest {
 	 * @param array  $options Array of options to populate the manifest with.
 	 */
 	public function __construct( $type, $options ) {
+		$this->set_type( $type );
+		$this->set_options( $options );
+	}
 
+	/**
+	 * Sets the chart type for the manifest.
+	 *
+	 * @since 3.0
+	 *
+	 * @param string $type Chart type to be manifested.
+	 */
+	private function set_type( $type ) {
+		$this->type = sanitize_key( $type );
+	}
+
+	/**
+	 * Stores the unfiltered chart options for later access.
+	 *
+	 * @since 3.0
+	 *
+	 * @param array $options Array of chart options to be populated into datasets.
+	 */
+	private function set_options( $options ) {
+		$this->options = $options;
 	}
 
 }
