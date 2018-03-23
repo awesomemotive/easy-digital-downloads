e496c5ceecfd4bde2bae8b7204489fb8423cc2fc
DrewAPicture <drew@affiliatewp.com>
2018-03-23 17:00:40 -0600

    Stub out classes for building structured scale data for chart endpoint manifests.

---
+++ includes/reports/data/charts/v2/class-time-scale.php
@@ -0,0 +1,13 @@
+<?php
+namespace EDD\Reports\Data\Charts\v2;
+
+/**
+ * Validates time scaling properties for use in building a ChartJS manifest.
+ *
+ * @since 3.0
+ *
+ * @see Scale
+ */
+class Time_Scale extends Scale {
+
+}
