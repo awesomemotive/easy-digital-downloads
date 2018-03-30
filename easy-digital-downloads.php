c41316aa466a3f4534cae38d97680c0c9ea0e689
Sunny Ratilal <sunny@sunnyratilal.com>
2018-03-30 21:16:52 +0100

    Re-include EDD_DB.

--- easy-digital-downloads.php
+++ easy-digital-downloads.php
@@ -347,6 +347,7 @@ final class Easy_Digital_Downloads {
 			require_once EDD_PLUGIN_DIR . 'includes/class-edd-cli.php';
 		}
 		require_once EDD_PLUGIN_DIR . 'includes/class-edd-cron.php';
+		require_once EDD_PLUGIN_DIR . 'includes/class-edd-db.php';
 		require_once EDD_PLUGIN_DIR . 'includes/class-edd-fees.php';
 		require_once EDD_PLUGIN_DIR . 'includes/class-edd-html-elements.php';
 		require_once EDD_PLUGIN_DIR . 'includes/class-edd-license-handler.php';
