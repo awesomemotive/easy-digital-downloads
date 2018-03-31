94788d76dff4b6b7af0d0c41ee12ac9155ccba40
Sunny Ratilal <sunny@sunnyratilal.com>
2018-03-31 01:05:46 +0100

    Add helper methods for file download logs and update unit test factory. #3841

--- includes/logs/log-functions.php
+++ includes/logs/log-functions.php
@@ -59,3 +59,51 @@ function edd_update_log( $log_id = 0, $data = array() ) {
 
 	return $logs->update_item( $log_id, $data );
 }
+
+/**
+ * Add a file download log.
+ *
+ * @since 3.0.0
+ *
+ * @param array $data
+ * @return int|false ID of newly created log, false on error.
+ */
+function edd_add_file_download_log( $data = array() ) {
+	// A download ID and a payment ID must be supplied for every log that is inserted into the database.
+	if ( empty( $data['download_id'] ) || empty( $data['payment_id'] ) ) {
+		return false;
+	}
+
+	$logs = new EDD_Log_File_Download_Query();
+
+	return $logs->add_item( $data );
+}
+
+/**
+ * Delete a file download log.
+ *
+ * @since 3.0.0
+ *
+ * @param int $log_id Log ID.
+ * @return int
+ */
+function edd_delete_file_download_log( $log_id = 0 ) {
+	$logs = new EDD_Log_File_Download_Query();
+
+	return $logs->delete_item( $log_id );
+}
+
+/**
+ * Update a file download log.
+ *
+ * @since 3.0.0
+ *
+ * @param int   $log_id File download log ID.
+ * @param array $data   Updated file download log data.
+ * @return bool Whether or not the file download log was updated.
+ */
+function edd_update_file_download_log( $log_id = 0, $data = array() ) {
+	$logs = new EDD_Log_File_Download_Query();
+
+	return $logs->update_item( $log_id, $data );
+}
