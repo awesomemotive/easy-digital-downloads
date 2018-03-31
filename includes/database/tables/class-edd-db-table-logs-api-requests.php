df54a69a77145c80b8595daa21da32f904fb7ff9
Sunny Ratilal <sunny@sunnyratilal.com>
2018-03-31 01:18:24 +0100

    Add helpers for API request logs and fix up all associated unit tests. #3841

--- includes/database/tables/class-edd-db-table-logs-api-requests.php
+++ includes/database/tables/class-edd-db-table-logs-api-requests.php
@@ -15,7 +15,7 @@ if ( class_exists( 'EDD_DB_Table' ) ) :
 	 *
 	 * @since 3.0.0
 	 */
-	final class EDD_DB_Table_Log_API_Requests extends EDD_DB_Table {
+	final class EDD_DB_Table_Logs_Api_Requests extends EDD_DB_Table {
 
 		/**
 		 * Table name
