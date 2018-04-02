629a38cd99237bd4ceaffacf894cca5e5b05dad1
DrewAPicture <drew@affiliatewp.com>
2018-04-02 01:10:25 -0600

    Remove the registry and exception-related stuff from EDD_Utilities for the 3.0 branch.

--- includes/class-edd-utilities.php
+++ includes/class-edd-utilities.php
@@ -61,65 +61,8 @@ class EDD_Utilities {
 	private function includes() {
 		$utils_dir = EDD_PLUGIN_DIR . 'includes/utils/';
 
-		// Interfaces.
-		require_once $utils_dir . 'interface-static-registry.php';
-
-		// Exceptions.
-		require_once $utils_dir . 'class-edd-exception.php';
-		require_once $utils_dir . 'exceptions/class-attribute-not-found.php';
-		require_once $utils_dir . 'exceptions/class-invalid-argument.php';
-		require_once $utils_dir . 'exceptions/class-invalid-parameter.php';
-
 		// Date management.
 		require_once $utils_dir . 'class-date.php';
-
-		// Registry.
-		require_once $utils_dir . 'class-registry.php';
-	}
-
-	/**
-	 * Retrieves a given registry instance by name.
-	 *
-	 * @since 3.0
-	 *
-	 * @param string $name Registry name.
-	 * @return \EDD\Utils\Registry|\WP_Error The registry instance if it exists, otherwise a WP_Error..
-	 */
-	public function get_registry( $name ) {
-
-		switch( $name ) {
-			case 'reports':
-
-				if ( ! did_action( 'edd_reports_init' ) ) {
-
-					_doing_it_wrong( __FUNCTION__, 'The Reports registry cannot be retrieved prior to the edd_reports_init hook.', 'EDD 3.0' );
-
-				} elseif ( class_exists( '\EDD\Reports\Data\Reports_Registry' ) ) {
-
-					$registry = \EDD\Reports\Data\Reports_Registry::instance();
-
-				}
-				break;
-
-			case 'reports:endpoints':
-
-				if ( ! did_action( 'edd_reports_init' ) ) {
-
-					_doing_it_wrong( __FUNCTION__, 'The Endpoints registry cannot be retrieved prior to the edd_reports_init hook.', 'EDD 3.0' );
-
-				} elseif ( class_exists( '\EDD\Reports\Data\Endpoint_Registry' ) ) {
-
-					$registry = \EDD\Reports\Data\Endpoint_Registry::instance();
-
-				}
-				break;
-
-			default:
-				$registry = new \WP_Error( 'invalid_registry', "The '{$name}' registry does not exist." );
-				break;
-		}
-
-		return $registry;
 	}
 
 	/**
