0e3efceea9c223b0b2e2137105b600b5acdc98a0
DrewAPicture <drew@affiliatewp.com>
2018-04-02 18:58:46 -0600

    Move class-hydrator.php to the includes/utils directory, fix the file requires in the affected bootstraps.

--- includes/reports/class-init.php
+++ includes/reports/class-init.php
@@ -51,7 +51,6 @@ final class Init {
 		// Chart Dependencies.
 		require_once $reports_dir . '/data/charts/v2/class-manifest.php';
 		require_once $reports_dir . '/data/charts/v2/class-dataset.php';
-		require_once $reports_dir . '/data/charts/v2/class-hydrator.php';
 		require_once $reports_dir . '/data/charts/v2/class-bar-dataset.php';
 		require_once $reports_dir . '/data/charts/v2/class-line-dataset.php';
 	}
