1f620558be02293bd166be0e5adb972189205f83
DrewAPicture <drew@affiliatewp.com>
2018-04-05 00:57:17 -0600

    Ensure time values set in data pairs to the dataset are converted to milliseconds.

--- includes/reports/data/charts/v2/class-dataset.php
+++ includes/reports/data/charts/v2/class-dataset.php
@@ -214,7 +214,25 @@ abstract class Dataset implements Error_Logger {
 		$data = $this->get_data();
 
 		if ( ! empty( $data ) ) {
-			$options['data'] = $data;
+			$processed = array();
+
+			$first_key   = key( $data );
+			$value_count = count( $data[ $first_key ] );
+
+			foreach ( $data as $key => $value ) {
+				if ( isset( $value[1] ) ) {
+					$processed[ $key ] = array(
+						'x' => $value[0] * 1000,
+						'y' => $value[1],
+					);
+				} else {
+					$processed[ $key ] = array(
+						'x' => $value[0] * 1000
+					);
+				}
+			}
+
+			$options['data'] = $processed;
 
 			$this->options = $options;
 		} else {
