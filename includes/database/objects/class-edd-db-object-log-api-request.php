5fae86c4cfddbec9ec0607192e9fa16d2c50b338
Sunny Ratilal <sunny@sunnyratilal.com>
2018-04-06 20:19:26 +0100

    Introduce `EDD\Database\Object` namespace to intermediate DB object classes. #6331

--- includes/database/objects/class-edd-db-object-log-api-request.php
+++ includes/database/objects/class-edd-db-object-log-api-request.php
@@ -1,5 +1,23 @@
 <?php
+/**
+ * API Request Database Object Class.
+ *
+ * @package     EDD
+ * @subpackage  Database\Objects
+ * @copyright   Copyright (c) 2018, Easy Digital Downloads, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0.0
+ */
+namespace EDD\Database\Objects;
 
-class EDD_DB_Log_Api_Request extends EDD_DB_Object {
-	
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * API Request Log Class.
+ *
+ * @since 3.0.0
+ */
+class Log_Api_Request extends Base {
+
 }
--- includes/database/objects/class-edd-db-object-note.php
+++ includes/database/objects/class-edd-db-object-note.php
@@ -1,6 +1,23 @@
 <?php
+/**
+ * Note Database Object Class.
+ *
+ * @package     EDD
+ * @subpackage  Database\Objects
+ * @copyright   Copyright (c) 2018, Easy Digital Downloads, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0.0
+ */
+namespace EDD\Database\Objects;
 
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
 
-class EDD_DB_Note extends EDD_DB_Object {
-	
+/**
+ * Note Class.
+ *
+ * @since 3.0.0
+ */
+class Note extends Base {
+
 }
