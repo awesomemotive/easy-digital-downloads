2f57712fe18742deca3511bec8134bd88d111521
Sunny Ratilal <sunny@sunnyratilal.com>
2018-04-06 21:54:47 +0100

    Rename file to match naming convention. #6331

--- easy-digital-downloads.php
+++ easy-digital-downloads.php
@@ -381,7 +381,7 @@ final class Easy_Digital_Downloads {
 		require_once EDD_PLUGIN_DIR . 'includes/logs/class-edd-file-download-log.php';
 		require_once EDD_PLUGIN_DIR . 'includes/logs/class-edd-log.php';
 		require_once EDD_PLUGIN_DIR . 'includes/logs/functions.php';
-		require_once EDD_PLUGIN_DIR . 'includes/notes/class-edd-note.php';
+		require_once EDD_PLUGIN_DIR . 'includes/notes/class-note.php';
 		require_once EDD_PLUGIN_DIR . 'includes/notes/functions.php';
 		require_once EDD_PLUGIN_DIR . 'includes/discount-functions.php';
 		require_once EDD_PLUGIN_DIR . 'includes/payments/functions.php';
