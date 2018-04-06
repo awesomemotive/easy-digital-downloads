468d2d49b0e99451b7facd289a4063a9c6c5f7d4
Sunny Ratilal <sunny@sunnyratilal.com>
2018-04-06 23:53:07 +0100

    DocBlock. [skip ci] #6331

--- includes/class-base-object.php
+++ includes/class-base-object.php
@@ -4,7 +4,7 @@
  *
  * @package     EDD
  * @subpackage  Core
- * @copyright   Copyright (c) 2018, Pippin Williamson
+ * @copyright   Copyright (c) 2018, Easy Digital Downloads, LLC
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       3.0.0
  */
