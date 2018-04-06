f4a9cf244b57e1431a7e68ed7d1b31e7967cad6e
Sunny Ratilal <sunny@sunnyratilal.com>
2018-04-06 22:26:46 +0100

    Introduce new base object. #6331

---
+++ includes/class-base-object.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Base Core Object.
+ *
+ * @package     EDD
+ * @subpackage  Core
+ * @copyright   Copyright (c) 2018, Pippin Williamson
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0.0
+ */
+namespace EDD;
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Implements a base object to be extended by core objects.
+ *
+ * @since 3.0.0
+ * @abstract
+ */
+abstract class Base_Object {
+
+	/**
+	 * Object constructor.
+	 *
+	 * @access public
+	 * @since  1.9
+	 *
+	 * @param mixed $object Object to populate members for.
+	 */
+	public function __construct( $object = null ) {
+		if ( $object ) {
+			foreach ( get_object_vars( $object ) as $key => $value ) {
+				$this->{$key} = $value;
+			}
+		}
+	}
+
+	/**
+	 * Converts the given object to an array.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return array Array version of the given object.
+	 */
+	public function to_array() {
+		return get_object_vars( $this );
+	}
+}
