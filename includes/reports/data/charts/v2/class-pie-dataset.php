cc8d4392b43e692444a99449c633ae2338167b9b
DrewAPicture <drew@affiliatewp.com>
2018-04-19 18:04:58 -0600

    Add the Pie_Dataset with local fields.

---
+++ includes/reports/data/charts/v2/class-pie-dataset.php
@@ -0,0 +1,22 @@
+<?php
+namespace EDD\Reports\Data\Charts\v2;
+
+/**
+ * Represents a manifestation of a ChartJS v2 pie chart dataset in PHP form.
+ *
+ * @since 3.0.0
+ */
+class Pie_Dataset extends Dataset {
+
+	/**
+	 * Represents the list of fields for a given dataset.
+	 *
+	 * @since 3.0.0
+	 * @var   array
+	 */
+	protected $fields = array(
+		'hoverBackgroundColor', 'hoverBorderColor',
+		'hoverBorderWidth'
+	);
+
+}
