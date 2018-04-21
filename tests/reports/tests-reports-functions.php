114cd36167a968869cee1d3a2e07894bb6731627
DrewAPicture <drew@affiliatewp.com>
2018-04-20 20:18:20 -0600

    Fix another test failing due to a missing report group.

--- tests/reports/tests-reports-functions.php
+++ tests/reports/tests-reports-functions.php
@@ -59,7 +59,7 @@ class Reports_Functions_Tests extends \EDD_UnitTestCase {
 	public function test_get_active_tab_should_default_to_the_key_of_the_first_report() {
 		$registry = EDD()->utils->get_registry( 'reports' );
 
-		$reports = array_keys( $registry->get_items() );
+		$reports = array_keys( $registry->get_reports() );
 
 		if ( empty( $reports ) ) {
 			add_report( 'foo', array(
