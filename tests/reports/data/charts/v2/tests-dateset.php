ba275c687a888f14cf46175d76c43a611f81432e
DrewAPicture <drew@affiliatewp.com>
2018-04-24 11:47:55 -0600

    Add test coverage for Dataset::get_id(), ::get_endpoint(), ::get_fields(), ::get_global_fields(), and ::get_all_fields().

--- tests/reports/data/charts/v2/tests-dateset.php
+++ tests/reports/data/charts/v2/tests-dateset.php
@@ -20,6 +20,69 @@ new \EDD\Reports\Init();
 class Dataset_Tests extends \EDD_UnitTestCase {
 
 	/**
+	 * @var \EDD\Reports\Data\Charts\v2\Dataset
+	 */
+	protected $mock_Dataset;
+
+	/**
+	 * Set up before each test.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->mock_Dataset = $this->get_Dataset_mock( 'test' );
+	}
+
+	/**
+	 * @covers ::get_id()
+	 * @covers ::set_id()
+	 */
+	public function test_get_id_should_return_the_dataset_id_supplied_during_instantiation() {
+		$this->assertSame( 'test', $this->mock_Dataset->get_id() );
+	}
+
+	/**
+	 * @covers ::get_endpoint()
+	 *
+	 * @group edd_reports_endpoints
+	 */
+	public function test_get_endpoint_should_retrieve_an_Endpoint_object() {
+		$this->assertInstanceOf( 'EDD\\Reports\\Data\\Endpoint', $this->mock_Dataset->get_endpoint() );
+	}
+
+	/**
+	 * @covers ::get_fields()
+	 */
+	public function test_get_fields_should_return_the_dataset_fields() {
+		$this->assertEqualSets( array(), $this->mock_Dataset->get_fields() );
+	}
+
+	/**
+	 * @covers ::get_global_fields()
+	 */
+	public function test_get_global_fields_should_return_the_global_fields() {
+		$expected = array(
+			'label', 'xAxisID', 'yAxisID', 'data',
+			'backgroundColor', 'borderColor', 'borderWidth',
+		);
+
+		$this->assertEqualSets( $expected, $this->mock_Dataset->get_global_fields() );
+	}
+
+	/**
+	 * @covers ::get_all_fields()
+	 */
+	public function test_get_all_fields_should_return_fields_and_global_fields_combined() {
+		// Abstract fields are empty, so just expect the global fields.
+		$expected = array(
+			'label', 'xAxisID', 'yAxisID', 'data',
+			'backgroundColor', 'borderColor', 'borderWidth',
+		);
+
+		$this->assertEqualSets( $expected, $this->mock_Dataset->get_all_fields() );
+	}
+
+	/**
 	 * Mocks a Dataset fixture.
 	 *
 	 * @return \EDD\Reports\Data\Charts\v2\Dataset
