3a87fc388398f3a2203ac7b4f892e815931a0688
DrewAPicture <drew@affiliatewp.com>
2018-04-24 11:17:59 -0600

    Stub out the test class and mock for the Dataset abstract.

---
+++ tests/reports/data/charts/v2/tests-dateset.php
@@ -0,0 +1,62 @@
+<?php
+namespace EDD\Reports\Data\Charts\v2;
+
+use EDD\Reports\Data\Chart_Endpoint;
+
+if ( ! class_exists( 'EDD\\Reports\\Init' ) ) {
+	require_once( EDD_PLUGIN_DIR . 'includes/reports/class-init.php' );
+}
+
+new \EDD\Reports\Init();
+
+/**
+ * Tests for Dataset class.
+ *
+ * @group edd_reports
+ * @group edd_reports_data
+ *
+ * @coversDefaultClass \EDD\Reports\Data\Charts\v2\Dataset
+ */
+class Dataset_Tests extends \EDD_UnitTestCase {
+
+	/**
+	 * Mocks a Dataset fixture.
+	 */
+	protected function mock_Dataset( $args ) {
+		$defaults = array(
+			'label' => __( 'Foo Dataset', 'edd-example-report' ),
+			'views' => array(
+				'chart' => array(
+					'data_callback' => function() {
+						return array(
+							'sales' => array( 40, 20, 30, 10 ),
+						);
+					},
+					'type'    => 'pie',
+					'options' => array(
+						'cutoutPercentage' => 50,
+						'datasets'         => array(
+							'sales' => array(
+								'label'           => __( 'Sales' ),
+								'backgroundColor' => array(
+									'rgb(234,16,109)',
+									'rgb(98,133,193)',
+									'rgb(151,99,143)',
+									'rgb(244,10,43)',
+								),
+							),
+						),
+						'labels' => array( 'First', 'Second', 'Third', 'Fourth' ),
+					),
+				),
+			)
+		);
+
+		$args = array_merge( $defaults, $args );
+
+		return $this->getMockForAbstractClass(
+			'EDD\\Reports\\Data\\Charts\\v2\\Dataset',
+			array( 'foo', new Chart_Endpoint( $args ), $args['views']['chart']['options'] )
+		);
+	}
+}
