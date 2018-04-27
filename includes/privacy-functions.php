0424a834e40e0a9dcc125f5034369cf2b2e3856d
Chris Klosowski <cklosowski@gmail.com>
2018-04-27 15:17:49 -0700

    Adding in string, email, and domain masking functions #6502

--- includes/privacy-functions.php
+++ includes/privacy-functions.php
@@ -50,3 +50,64 @@ Handling this data also allows us to:
 	wp_add_privacy_policy_content( 'Easy Digital Downloads', wpautop( $content ) );
 }
 add_action( 'admin_init', 'edd_register_privacy_policy_template' );
+
+function edd_mask_string( $string = '' ) {
+
+	$first_char  = substr( $string, 0, 1 );
+	$last_char   = substr( $string, -1, 1 );
+
+	$masked_string = $string;
+
+	if ( strlen( $string ) > 2 ) {
+
+		$total_stars = strlen( $string ) - 2;
+		$masked_string = $first_char . str_repeat( '*', $total_stars ) . $last_char;
+
+	} elseif ( strlen( $string ) === 2 ) {
+
+		$masked_string = $first_char . '*';
+
+	}
+
+
+
+	return $masked_string;
+
+}
+
+function edd_mask_domain( $domain = '' ) {
+	$domain_parts = explode( '.', $domain );
+
+	if ( count( $domain_parts ) === 2 ) {
+
+		// We have a single entry tld like .org or .com
+		$domain_parts[0] = edd_mask_string( $domain_parts[0] );
+
+	} else {
+
+		$part_count     = count( $domain_parts );
+		$possible_cctld = strlen( $domain_parts[ $part_count - 2 ] ) <= 3 ? true : false;
+
+		$mask_parts = $possible_cctld ? array_slice( $domain_parts, 0, $part_count - 2 ) : array_slice( $domain_parts, 0, $part_count - 1 );
+
+		$i = 0;
+		while ( $i < count( $mask_parts ) ) {
+			$domain_parts[ $i ] = edd_mask_string( $domain_parts[ $i ]);
+			$i++;
+		}
+
+	}
+
+	return implode( '.', $domain_parts );
+}
+
+function edd_pseudo_mask_email( $email_address ) {
+	$email_parts = explode( '@', $email_address );
+	$name        = edd_mask_string( $email_parts[0] );
+	$domain      = edd_mask_domain( $email_parts[1] );
+
+	$email_address = $name . '@' . $domain;
+
+
+	return $email_address;
+}
