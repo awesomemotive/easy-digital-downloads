b7bd0e07bcbf8132bc2b6b6a997d8a479deee34d
Chris Klosowski <cklosowski@gmail.com>
2018-05-07 14:49:53 -0700

    Adds support for customer record and billing information from all payments for a customer record #6543

--- includes/privacy-functions.php
+++ includes/privacy-functions.php
@@ -116,3 +116,181 @@ function edd_pseudo_mask_email( $email_address ) {
 
 	return $email_address;
 }
+
+/**
+ * Register any of our Privacy Data Exporters
+ *
+ * @since 2.9.2
+ *
+ * @param $exporters
+ *
+ * @return array
+ */
+function edd_register_privacy_exporters( $exporters ) {
+
+	$exporters[] = array(
+		'exporter_friendly_name' => __( 'Customer Record', 'easy-digital-downloads' ),
+		'callback'               => 'edd_privacy_cutomer_record_exporter',
+	);
+
+	$exporters[] = array(
+		'exporter_friendly_name' => __( 'Billing Information', 'easy-digital-downloads' ),
+		'callback'               => 'edd_privacy_billing_information_exporter',
+	);
+
+	return $exporters;
+
+}
+add_filter( 'wp_privacy_personal_data_exporters', 'edd_register_privacy_exporters' );
+
+/**
+ * Retrieves the Customer record for the Privacy Data Exporter
+ *
+ * @since 2.9.2
+ * @param string $email_address
+ * @param int    $page
+ *
+ * @return array
+ */
+function edd_privacy_cutomer_record_exporter( $email_address = '', $page = 1 ) {
+
+	$customer    = new EDD_Customer( $email_address );
+	$export_data = array();
+
+	if ( ! empty( $customer->id ) ) {
+		$export_data = array(
+			'group_id'    => 'edd-customer-record',
+			'group_label' => __( 'Customer Record', 'easy-digital-downloads' ),
+			'item_id'     => "edd-customer-record-{$customer->id}",
+			'data'        => array(
+				array(
+					'name'  => __( 'Customer ID', 'easy-digital-downloads' ),
+					'value' => $customer->id
+				),
+				array(
+					'name'  => __( 'Primary Email', 'easy-digital-downloads' ),
+					'value' => $customer->email
+				),
+				array(
+					'name'  => __( 'Name', 'easy-digital-downloads' ),
+					'value' => $customer->name
+				),
+				array(
+					'name'  => __( 'Date Created', 'easy-digital-downloads' ),
+					'value' => $customer->date_created
+				),
+				array(
+					'name'  => __( 'All Email Addresses', 'easy-digital-downloads' ),
+					'value' => implode( ', ', $customer->emails )
+				),
+			)
+		);
+
+		if ( ! empty( $customer->get_meta( 'agree_to_terms_time' ) ) ) {
+			$export_data['data'][] = array(
+				'name' => __( 'Agreed to Terms' ),
+				'value' => date_i18n( get_option( 'date_format' ) . ' H:i:s', strtotime( $customer->get_meta( 'agree_to_terms_time' ) ) )
+			);
+		}
+
+		if ( ! empty( $customer->get_meta( 'agree_to_privacy_time' ) ) ) {
+			$export_data['data'][] = array(
+				'name' => __( 'Agreed to Privacy Policy' ),
+				'value' => date_i18n( get_option( 'date_format' ) . ' H:i:s', strtotime( $customer->get_meta( 'agree_to_privacy_time' ) ) )
+			);
+		}
+	}
+
+	return array( 'data' => array( $export_data ), 'done' => true );
+}
+
+/**
+ * Retrieves the billing information for the Privacy Exporter
+ *
+ * @since 2.9.2
+ * @param string $email_address
+ * @param int    $page
+ *
+ * @return array
+ */
+function edd_privacy_billing_information_exporter( $email_address = '', $page = 1 ) {
+
+	$customer = new EDD_Customer( $email_address );
+	$payments = edd_get_payments( array(
+		'customer' => $customer->id,
+		'output'   => 'payments',
+		'page'     => $page,
+	) );
+
+	// If we haven't found any payments for this page, just return that we're done.
+	if ( empty( $payments ) ) {
+		return array( 'data' => array(), 'done' => true );
+	}
+
+
+	$export_items = array();
+	foreach ( $payments as $payment ) {
+		$data_points = array(
+			array(
+				'name'  => __( 'Order ID', 'easy-digital-downloads' ),
+				'value' => $payment->ID,
+			),
+			array(
+				'name'  => __( 'First Name', 'easy-digital-downloads' ),
+				'value' => ! empty( $payment->user_info['first_name'] ) ? $payment->user_info['first_name'] : '',
+			),
+			array(
+				'name'  => __( 'Last Name', 'easy-digital-downloads' ),
+				'value' => ! empty( $payment->user_info['last_name'] ) ? $payment->user_info['last_name'] : '',
+			),
+			array(
+				'name'  => __( 'Email Address', 'easy-digital-downloads' ),
+				'value' => ! empty( $payment->email ) ? $payment->email : '',
+			),
+			array(
+				'name'  => __( 'Address', 'easy-digital-downloads' ),
+				'value' => ! empty( $payment->address['line1'] ) ? $payment->address['line1'] : '',
+			),
+			array(
+				'name'  => __( 'Address Line 2', 'easy-digital-downloads' ),
+				'value' => ! empty( $payment->address['line2'] ) ? $payment->address['line2'] : '',
+			),
+			array(
+				'name'  => __( 'City', 'easy-digital-downloads' ),
+				'value' => ! empty( $payment->address['city'] ) ? $payment->address['city'] : '',
+			),
+			array(
+				'name'  => __( 'State', 'easy-digital-downloads' ),
+				'value' => ! empty( $payment->address['state'] ) ? $payment->address['state'] : '',
+			),
+			array(
+				'name'  => __( 'Postal Code', 'easy-digital-downloads' ),
+				'value' => ! empty( $payment->address['zip'] ) ? $payment->address['zip'] : '',
+			),
+			array(
+				'name'  => __( 'Country', 'easy-digital-downloads' ),
+				'value' => ! empty( $payment->address['country'] ) ? $payment->address['country'] : '',
+			),
+			array(
+				'name'  => __( 'IP Address', 'easy-digital-downloads' ),
+				'value' => ! empty( $payment->ip ) ? $payment->ip : '',
+			),
+		);
+
+		$export_items[] = array(
+			'group_id'    => 'edd-billing-information',
+			'group_label' => __( 'Customer Billing information', 'easy-digital-downloads' ),
+			'item_id'     => "edd-billing-addresses-{$payment->ID}",
+			'data'        => $data_points,
+		);
+
+	}
+
+
+	// Add the data to the list, and tell the exporter to come back for the next page of payments.
+	return array(
+		'data' => $export_items,
+		'done' => false,
+	);
+
+}
