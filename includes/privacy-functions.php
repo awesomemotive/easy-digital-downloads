e43f430cc20f1d401705a88f166512560b40f624
Chris Klosowski <cklosowski@gmail.com>
2018-05-16 12:39:21 -0700

    Add edd_anonymize_email function #6498

--- includes/privacy-functions.php
+++ includes/privacy-functions.php
@@ -118,6 +118,38 @@ function edd_pseudo_mask_email( $email_address ) {
 }
 
 /**
+ * Return an anonymized email address.
+ *
+ * While WP Core supports anonymizing email addresses with the wp_privacy_anonymize_data function,
+ * it turns every email address into deleted@site.invalid, which does not work when some purchase/customer records
+ * are still needed for legal and regulatory reasons.
+ *
+ * This function will anonymize the email with an MD5 that is salted
+ * and given a randomized uniqid prefixed with the store URL in order to prevent connecting a single customer across
+ * multiple stores, as well as the timestamp at the time of anonymization (so it trying the same email again will not be
+ * repeatable and therefore connected), and return the email address as <hash>@site.invalid.
+ *
+ * @since 2.9.2
+ *
+ * @param string $email_address
+ *
+ * @return string
+ */
+function edd_anonymize_email( $email_address ) {
+
+	if ( empty( $email_address ) ) {
+		return $email_address;
+	}
+
+	$email_address    = strtolower( $email_address );
+	$email_parts      = explode( '@', $email_address );
+	$anonymized_email = wp_hash( uniqid( get_option( 'site_url' ), true ) . $email_parts[0] . current_time( 'timestamp' ), 'nonce' );
+
+
+	return $anonymized_email . '@site.invalid';
+}
+
+/**
  * Register any of our Privacy Data Exporters
  *
  * @since 2.9.2
