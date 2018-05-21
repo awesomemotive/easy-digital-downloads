899799598494b4020b35c00a008c1f705ebad244
John James Jacoby <johnjamesjacoby@me.com>
2018-05-21 10:43:00 -0500

    Update `edd_get_discount_note_html()` to use public methods.
    This stops discount notes from fatal erroring in `release/3.0`.

--- includes/admin/discounts/discount-functions.php
+++ includes/admin/discounts/discount-functions.php
@@ -25,7 +25,7 @@ function edd_get_discount_note_html( $note = 0, $discount_id = 0 ) {
 
 	// Get the note
 	if ( is_numeric( $note ) ) {
-		$note = new EDD\Notes\Note( $note );
+		$note = edd_get_note( $note );
 	}
 
 	// No note, so bail
@@ -34,23 +34,23 @@ function edd_get_discount_note_html( $note = 0, $discount_id = 0 ) {
 	}
 
 	// User
-	$user = ! empty( $note->user_id )
-		? get_userdata( $note->user_id )->display_name
+	$user = ! empty( $note->get_user_id() )
+		? get_userdata( $note->get_user_id() )->display_name
 		: __( 'EDD Bot', 'easy-digital-downloads' );
 
 	// URL to delete note
 	$delete_note_url = wp_nonce_url( add_query_arg( array(
 		'edd-action'  => 'delete_discount_note',
-		'note_id'     => $note->id,
+		'note_id'     => $note->get_id(),
 		'discount_id' => $discount_id,
-	), admin_url( 'edit.php?post_type=download&page=edd-discounts' ) ), 'edd_delete_discount_note_' . $note->id );
+	), admin_url( 'edit.php?post_type=download&page=edd-discounts' ) ), 'edd_delete_discount_note_' . $note->get_id() );
 
 	// Output
-	$note_html = '<div class="edd-discount-note" id="edd-discount-note-' . esc_attr( $note->id ) . '">';
+	$note_html = '<div class="edd-discount-note" id="edd-discount-note-' . esc_attr( $note->get_id() ) . '">';
 		$note_html .='<div><strong class="edd-discount-note-author">' . esc_html( $user ) . '</strong>';
-		$note_html .= '<time datetime="' . esc_attr( $note->date_created ) . '">' . edd_date_i18n( $note->date_created, 'datetime' ) . '</time>';
-		$note_html .= '<p>' . make_clickable( $note->content ) . '</p>';
-		$note_html .= '<a href="' . esc_url( $delete_note_url ) . '#edd-discount-notes" class="edd-delete-discount-note" data-note-id="' . esc_attr( $note->id ) . '" data-discount-id="' . esc_attr( $discount_id ) . '">' . __( 'Delete', 'easy-digital-downloads' ) . '</a>';
+		$note_html .= '<time datetime="' . esc_attr( $note->get_date_created() ) . '">' . edd_date_i18n( $note->get_date_created(), 'datetime' ) . '</time>';
+		$note_html .= '<p>' . make_clickable( $note->get_content() ) . '</p>';
+		$note_html .= '<a href="' . esc_url( $delete_note_url ) . '#edd-discount-notes" class="edd-delete-discount-note" data-note-id="' . esc_attr( $note->get_id() ) . '" data-discount-id="' . esc_attr( $discount_id ) . '">' . __( 'Delete', 'easy-digital-downloads' ) . '</a>';
 		$note_html .= '</div>';
 	$note_html .= '</div>';
 
