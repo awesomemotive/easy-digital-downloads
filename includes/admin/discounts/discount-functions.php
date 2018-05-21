f0560ced84ef899bea5185d77f6aec15b50bd268
John James Jacoby <johnjamesjacoby@me.com>
2018-05-21 10:56:55 -0500

    Move `$note->get_user_id()` call out of `empty()` to avoid fatals.

--- includes/admin/discounts/discount-functions.php
+++ includes/admin/discounts/discount-functions.php
@@ -34,8 +34,9 @@ function edd_get_discount_note_html( $note = 0, $discount_id = 0 ) {
 	}
 
 	// User
-	$user = ! empty( $note->get_user_id() )
-		? get_userdata( $note->get_user_id() )->display_name
+	$user_id = $note->get_user_id();
+	$user    = ! empty( $user_id )
+		? get_userdata( $user_id )->display_name
 		: __( 'EDD Bot', 'easy-digital-downloads' );
 
 	// URL to delete note
@@ -49,10 +50,11 @@ function edd_get_discount_note_html( $note = 0, $discount_id = 0 ) {
 	ob_start(); ?>
 
 	<div class="edd-discount-note" id="edd-discount-note-<?php echo esc_attr( $note->get_id() ); ?>">
-		<div><strong class="edd-discount-note-author"><?php echo esc_html( $user ); ?></strong>
-		<time datetime="<?php echo esc_attr( $note->get_date_created() ); ?>"><?php echo edd_date_i18n( $note->get_date_created(), 'datetime' ); ?></time>
-		<p><?php echo make_clickable( $note->get_content() ); ?></p>
-		<a href="<?php esc_url( $delete_note_url ); ?>#edd-discount-notes" class="edd-delete-discount-note" data-note-id="<?php echo esc_attr( $note->get_id() ); ?>" data-discount-id="<?php echo esc_attr( $discount_id ); ?>"><?php _e( 'Delete', 'easy-digital-downloads' ); ?></a>
+		<div>
+			<strong class="edd-discount-note-author"><?php echo esc_html( $user ); ?></strong>
+			<time datetime="<?php echo esc_attr( $note->get_date_created() ); ?>"><?php echo edd_date_i18n( $note->get_date_created(), 'datetime' ); ?></time>
+			<p><?php echo make_clickable( $note->get_content() ); ?></p>
+			<a href="<?php esc_url( $delete_note_url ); ?>#edd-discount-notes" class="edd-delete-discount-note" data-note-id="<?php echo esc_attr( $note->get_id() ); ?>" data-discount-id="<?php echo esc_attr( $discount_id ); ?>"><?php _e( 'Delete', 'easy-digital-downloads' ); ?></a>
 		</div>
 	</div>
 
