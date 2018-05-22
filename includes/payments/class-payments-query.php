0d3f95dc390f8a2512e20c7187c23a94e2263588
Jeroen Sormani <jeroen.sormani@gmail.com>
2018-05-22 20:50:24 +0200

    Fix search not fully working (#6472)

--- includes/payments/class-payments-query.php
+++ includes/payments/class-payments-query.php
@@ -466,7 +466,13 @@ class EDD_Payments_Query extends EDD_Stats {
 
 			$this->__unset( 's' );
 
-		} elseif ( edd_get_option( 'enable_sequential' ) ) {
+		} elseif (
+			edd_get_option( 'enable_sequential' ) &&
+			(
+				false !== strpos( $search, edd_get_option( 'sequential_prefix' ) ) ||
+				false !== strpos( $search, edd_get_option( 'sequential_postfix' ) )
+			)
+		) {
 
 			$search_meta = array(
 				'key'     => '_edd_payment_number',
@@ -487,6 +493,19 @@ class EDD_Payments_Query extends EDD_Stats {
 				$arr[] = $search;
 				$this->__set( 'post__in', $arr );
 				$this->__unset( 's' );
+			}
+
+			if ( edd_get_option( 'enable_sequential' ) ) {
+
+				$search_meta = array(
+					'key'     => '_edd_payment_number',
+					'value'   => $search,
+					'compare' => 'LIKE'
+				);
+
+				$this->__set( 'meta_query', $search_meta );
+				$this->__unset( 's' );
+
 			}
 
 		} elseif ( '#' == substr( $search, 0, 1 ) ) {
