2c70e15abea74b4e0b0c3035c7c7549ae5415bc6
Sunny Ratilal <sunny@sunnyratilal.com>
2018-05-26 11:51:10 +0100

    Load order files. #4576

--- includes/class-edd.php
+++ includes/class-edd.php
@@ -493,6 +493,12 @@ final class Easy_Digital_Downloads {
 		require_once EDD_PLUGIN_DIR . 'includes/notes/functions.php';
 		require_once EDD_PLUGIN_DIR . 'includes/notes/meta.php';
 
+		// Orders
+		require_once EDD_PLUGIN_DIR . 'includes/orders/class-order.php';
+		require_once EDD_PLUGIN_DIR . 'includes/orders/class-order-adjustment.php';
+		require_once EDD_PLUGIN_DIR . 'includes/orders/class-order-item.php';
+		require_once EDD_PLUGIN_DIR . 'includes/orders/functions.php';
+
 		// Payments
 		require_once EDD_PLUGIN_DIR . 'includes/payments/functions.php';
 		require_once EDD_PLUGIN_DIR . 'includes/payments/actions.php';
