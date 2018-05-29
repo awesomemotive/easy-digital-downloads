96e8e5fff99c983e696c19af127ee702c3b81ce2
Sunny Ratilal <sunny@sunnyratilal.com>
2018-05-29 16:22:09 +0100

    Remove `itemprop`. #5240

--- templates/shortcode-content-title.php
+++ templates/shortcode-content-title.php
@@ -1,3 +1,1 @@
-<h3 class="edd_download_title">
-	<a itemprop="url" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-</h3>
+<h3 class="edd_download_title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
