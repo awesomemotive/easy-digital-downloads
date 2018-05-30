c723d00bedbdd2249ef670c03b4dd43263dbf60e
Sunny Ratilal <sunny@sunnyratilal.com>
2018-05-30 23:59:44 +0100

    DB upgrade routine for customers. #6310

--- includes/database/tables/class-customers.php
+++ includes/database/tables/class-customers.php
@@ -37,7 +37,7 @@ final class Customers extends Base {
 	 * @since 3.0
 	 * @var int
 	 */
-	protected $version = 201805290002;
+	protected $version = 201805220001;
 
 	/**
 	 * Setup the database schema
@@ -64,6 +64,36 @@ final class Customers extends Base {
 	}
 
 	/**
+	 * Maybe upgrade the database table. Handles creation & schema changes.
+	 *
+	 * Hooked to the "admin_init" action.
+	 *
+	 * @since 3.0
+	 */
+	public function maybe_upgrade() {
+		global $wpdb;
+
+		$option = get_option( $wpdb->prefix . 'edd_customers_version', false );
+
+		if ( $option ) {
+
+			// In 3.0 we use new options to store the database version.
+			delete_option( $wpdb->prefix . 'edd_customers_version' );
+
+			$query = "
+			ALTER TABLE {$this->table_name} MODIFY `email` VARCHAR(100) NOT NULL default '';
+			ALTER TABLE {$this->table_name} MODIFY `user_id` bigint(20) unsigned NOT NULL default '0';
+			ALTER TABLE {$this->table_name} MODIFY `purchase_count` bigint(20) unsigned NOT NULL default '0';
+			ALTER TABLE {$this->table_name} ALTER COLUMN `date_created` SET DEFAULT '0000-00-00 00:00:00';
+			ALTER TABLE {$this->table_name} ADD COLUMN `date_modified` datetime DEFAULT '0000-00-00 00:00:00';
+		";
+			$this->get_db()->query( $query );
+		}
+
+		parent::maybe_upgrade();
+	}
+
+	/**
 	 * Handle schema changes
 	 *
 	 * @access protected
@@ -71,27 +101,6 @@ final class Customers extends Base {
 	 */
 	protected function upgrade() {
 
-		// 3.0 schema changes.
-		if ( ! version_compare( EDD_VERSION, 3.0, '>=' ) ) {
-			$this->v30_upgrade();
-		}
-	}
-
-	/**
-	 * EDD 3.0 schema changes.
-	 *
-	 * @access private
-	 * @since 3.0
-	 */
-	private function v30_upgrade() {
-		$query = "
-			ALTER TABLE {$this->table_name} MODIFY `email` VARCHAR(100) NOT NULL default '';
-			ALTER TABLE {$this->table_name} MODIFY `user_id` bigint(20) unsigned NOT NULL default '0';
-			ALTER TABLE {$this->table_name} MODIFY `purchase_count` bigint(20) unsigned NOT NULL default '0';
-			ALTER TABLE {$this->table_name} ALTER COLUMN `date_created` SET DEFAULT '0000-00-00 00:00:00';
-			ALTER TABLE {$this->table_name} ADD COLUMN `date_modified` datetime DEFAULT '0000-00-00 00:00:00';
-		";
-		$this->get_db()->query( $query );
 	}
 }
 endif;
