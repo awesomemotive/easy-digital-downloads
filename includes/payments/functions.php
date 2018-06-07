dcdefa119eafd72bd59f2eedb3e5e5ff813db475
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-07 08:36:41 +0100

    Second parameter does not exist for edd_delete_order().

--- includes/payments/functions.php
+++ includes/payments/functions.php
@@ -266,8 +266,8 @@ function edd_delete_purchase( $payment_id = 0, $update_customer = true, $delete_
 
 	}
 
-	// Remove the payment
-	edd_delete_order( $payment_id, true );
+	// Remove the order.
+	edd_delete_order( $payment_id );
 
 	// Remove related sale log entries
 	$edd_logs->delete_logs(
