9db8a979146f7eb06f290c7150b4161821dc6efb
John James Jacoby <johnjamesjacoby@me.com>
2018-06-08 01:31:15 -0500

    Make customer order totals sortable.

--- includes/database/schemas/class-customers.php
+++ includes/database/schemas/class-customers.php
@@ -94,7 +94,8 @@ class Customers extends Base {
 			'type'       => 'bigint',
 			'length'     => '20',
 			'unsigned'   => true,
-			'default'    => '0'
+			'default'    => '0',
+			'sortable'   => true
 		),
 
 		// date_created
