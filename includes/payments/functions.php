0059eb174bc5f7e48157ab3364451147905af6f5
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-09 13:23:13 +0100

    Final cleanup of payment functions. #4576

--- includes/payments/functions.php
+++ includes/payments/functions.php
@@ -1573,23 +1573,18 @@ function edd_get_purchase_id_by_key( $payment_key ) {
 }
 
 /**
- * Retrieve the purchase ID based on the transaction ID
+ * Retrieve the order ID based on the transaction ID.
  *
  * @since 2.4
- * @global object $wpdb Used to query the database using the WordPress
- *   Database API
- * @param string $key the transaction ID to search for
- * @return int $purchase Purchase ID
+ * @since 3.0 Dispatch to edd_get_order_id_from_transaction_id().
+ *
+ * @see edd_get_order_id_from_transaction_id()
+ *
+ * @param string $transaction_id Transaction ID to search for.
+ * @return int Order ID.
  */
-function edd_get_purchase_id_by_transaction_id( $key ) {
-	global $wpdb;
-
-	$purchase = $wpdb->get_var( $wpdb->prepare( "SELECT post_id FROM $wpdb->postmeta WHERE meta_key = '_edd_payment_transaction_id' AND meta_value = %s LIMIT 1", $key ) );
-
-	if ( $purchase != NULL )
-		return $purchase;
-
-	return 0;
+function edd_get_purchase_id_by_transaction_id( $transaction_id ) {
+	return edd_get_order_id_from_transaction_id( $transaction_id );
 }
 
 /**
@@ -1616,7 +1611,6 @@ function edd_get_payment_notes( $order_id = 0, $search = '' ) {
 
 	return $notes;
 }
-
 
 /**
  * Add a note to an order.
@@ -1712,21 +1706,24 @@ function edd_get_payment_note_html( $note, $payment_id = 0 ) {
 
 /**
  * Exclude notes (comments) on edd_payment post type from showing in Recent
- * Comments widgets
+ * Comments widgets.
  *
  * @since 1.4.1
- * @param obj $query WordPress Comment Query Object
- * @return void
+ *
+ * @param object $query WordPress Comment Query Object
  */
 function edd_hide_payment_notes( $query ) {
 	global $wp_version;
 
 	if ( version_compare( floatval( $wp_version ), '4.1', '>=' ) ) {
 		$types = isset( $query->query_vars['type__not_in'] ) ? $query->query_vars['type__not_in'] : array();
+
 		if ( ! is_array( $types ) ) {
 			$types = array( $types );
 		}
+
 		$types[] = 'edd_payment_note';
+
 		$query->query_vars['type__not_in'] = $types;
 	}
 }
@@ -1737,14 +1734,16 @@ add_action( 'pre_get_comments', 'edd_hide_payment_notes', 10 );
  * Comments widgets
  *
  * @since 2.2
- * @param array $clauses Comment clauses for comment query
- * @param obj $wp_comment_query WordPress Comment Query Object
- * @return array $clauses Updated comment clauses
+ *
+ * @param array $clauses           Comment clauses for comment query.
+ * @param object $wp_comment_query WordPress Comment Query Object.
+ *
+ * @return array $clauses Updated comment clauses.
  */
 function edd_hide_payment_notes_pre_41( $clauses, $wp_comment_query ) {
-	global $wpdb, $wp_version;
+	global $wp_version;
 
-	if( version_compare( floatval( $wp_version ), '4.1', '<' ) ) {
+	if ( version_compare( floatval( $wp_version ), '4.1', '<' ) ) {
 		$clauses['where'] .= ' AND comment_type != "edd_payment_note"';
 	}
 
@@ -1752,71 +1751,90 @@ function edd_hide_payment_notes_pre_41( $clauses, $wp_comment_query ) {
 }
 add_filter( 'comments_clauses', 'edd_hide_payment_notes_pre_41', 10, 2 );
 
-
 /**
- * Exclude notes (comments) on edd_payment post type from showing in comment feeds
+ * Exclude notes (comments) on edd_payment post type from showing in comment feeds.
  *
  * @since 1.5.1
- * @param array $where
- * @param obj $wp_comment_query WordPress Comment Query Object
- * @return array $where
+ *
+ * @param string $where
+ * @param object $wp_comment_query WordPress Comment Query Object
+ *
+ * @return string $where Updated WHERE clause.
  */
 function edd_hide_payment_notes_from_feeds( $where, $wp_comment_query ) {
-    global $wpdb;
+	global $wpdb;
 
-	$where .= $wpdb->prepare( " AND comment_type != %s", 'edd_payment_note' );
+	$where .= $wpdb->prepare( ' AND comment_type != %s', 'edd_payment_note' );
+
 	return $where;
 }
 add_filter( 'comment_feed_where', 'edd_hide_payment_notes_from_feeds', 10, 2 );
-
 
 /**
  * Remove EDD Comments from the wp_count_comments function
  *
  * @since 1.5.2
- * @param array $stats (empty from core filter)
- * @param int $post_id Post ID
- * @return array Array of comment counts
+ *
+ * @param array $stats   Empty from core filter.
+ * @param int   $post_id Post ID.
+ *
+ * @return object Comment counts.
 */
 function edd_remove_payment_notes_in_comment_counts( $stats, $post_id ) {
 	global $wpdb, $pagenow;
 
 	$array_excluded_pages = array( 'index.php', 'edit-comments.php' );
-	if( ! in_array( $pagenow, $array_excluded_pages )  ) {
+	if ( ! in_array( $pagenow, $array_excluded_pages, true ) ) {
 		return $stats;
 	}
 
 	$post_id = (int) $post_id;
 
-	if ( apply_filters( 'edd_count_payment_notes_in_comments', false ) )
+	if ( apply_filters( 'edd_count_payment_notes_in_comments', false ) ) {
 		return $stats;
+	}
 
 	$stats = wp_cache_get( "comments-{$post_id}", 'counts' );
 
-	if ( false !== $stats )
+	if ( false !== $stats ) {
 		return $stats;
+	}
 
 	$where = 'WHERE comment_type != "edd_payment_note"';
 
-	if ( $post_id > 0 )
+	if ( $post_id > 0 ) {
 		$where .= $wpdb->prepare( " AND comment_post_ID = %d", $post_id );
+	}
 
 	$count = $wpdb->get_results( "SELECT comment_approved, COUNT( * ) AS num_comments FROM {$wpdb->comments} {$where} GROUP BY comment_approved", ARRAY_A );
 
 	$total = 0;
-	$approved = array( '0' => 'moderated', '1' => 'approved', 'spam' => 'spam', 'trash' => 'trash', 'post-trashed' => 'post-trashed' );
+
+	$approved = array(
+		'0'            => 'moderated',
+		'1'            => 'approved',
+		'spam'         => 'spam',
+		'trash'        => 'trash',
+		'post-trashed' => 'post-trashed',
+	);
+
 	foreach ( (array) $count as $row ) {
-		// Don't count post-trashed toward totals
-		if ( 'post-trashed' != $row['comment_approved'] && 'trash' != $row['comment_approved'] )
+
+		// Don't count post-trashed toward totals.
+		if ( 'post-trashed' !== $row['comment_approved'] && 'trash' !== $row['comment_approved'] ) {
 			$total += $row['num_comments'];
-		if ( isset( $approved[$row['comment_approved']] ) )
-			$stats[$approved[$row['comment_approved']]] = $row['num_comments'];
+		}
+
+		if ( isset( $approved[ $row['comment_approved'] ] ) ) {
+			$stats[ $approved[ $row['comment_approved'] ] ] = $row['num_comments'];
+		}
 	}
 
 	$stats['total_comments'] = $total;
 	foreach ( $approved as $key ) {
-		if ( empty($stats[$key]) )
-			$stats[$key] = 0;
+		if ( empty( $stats[ $key ] ) ) {
+			$stats[ $key ] = 0;
+		}
 	}
 
 	$stats = (object) $stats;
@@ -1826,17 +1844,20 @@ function edd_remove_payment_notes_in_comment_counts( $stats, $post_id ) {
 }
 add_filter( 'wp_count_comments', 'edd_remove_payment_notes_in_comment_counts', 10, 2 );
 
-
 /**
- * Filter where older than one week
+ * Filter where older than one week.
  *
  * @since 1.6
- * @param string $where Where clause
- * @return string $where Modified where clause
+ *
+ * @param string $where Where clause.
+ * @return string $where Modified where clause.
 */
 function edd_filter_where_older_than_week( $where = '' ) {
-	// Payments older than one week
+
+	// Payments older than one week.
 	$start = date( 'Y-m-d', strtotime( '-7 days' ) );
+
 	$where .= " AND post_date <= '{$start}'";
+
 	return $where;
 }
