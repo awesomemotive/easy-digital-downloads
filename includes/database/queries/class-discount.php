7ce8d4fa06a79c386941c6ebb1358fcb7932204e
John James Jacoby <johnjamesjacoby@me.com>
2018-06-11 01:57:08 -0500

    Update PHPDoc regarding default `number` value for queries.
    See #6650.

--- includes/database/queries/class-discount.php
+++ includes/database/queries/class-discount.php
@@ -141,7 +141,7 @@ class Discount extends Base {
 	 *                                              Default false.
 	 *     @type string       $fields               Site fields to return. Accepts 'ids' (returns an array of discount IDs)
 	 *                                              or empty (returns an array of complete discount objects). Default empty.
-	 *     @type int          $number               Limit number of discounts to retrieve. Default null (no limit).
+	 *     @type int          $number               Limit number of discounts to retrieve. Default 100.
 	 *     @type int          $offset               Number of discounts to offset the query. Used to build LIMIT clause.
 	 *                                              Default 0.
 	 *     @type bool         $no_found_rows        Whether to disable the `SQL_CALC_FOUND_ROWS` query. Default true.
