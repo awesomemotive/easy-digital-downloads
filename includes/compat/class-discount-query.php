3a50ff82118dc50e40148cce087471d996672da0
John James Jacoby <johnjamesjacoby@me.com>
2018-06-12 01:44:58 -0500

    Enforce adjustment type in Discount query.
    This ensures Adjustment queries for discounts return only that type.
    
    See #6650.

--- includes/compat/class-discount-query.php
+++ includes/compat/class-discount-query.php
@@ -30,4 +30,28 @@ class Discount extends Adjustment {
 	 * @var mixed
 	 */
 	protected $item_shape = 'EDD_Discount';
+
+	/**
+	 * Queries the database and retrieves items or counts.
+	 *
+	 * This method exists for backwards compatibility with `discount->type`
+	 * properties, which is now `discount->amount_type`.
+	 *
+	 * @since 3.0
+	 *
+	 * @param string|array $query Array or URL query string of parameters.
+	 * @return array|int List of items, or number of items when 'count' is passed as a query var.
+	 */
+	public function query( $query = array() ) {
+
+		// Switch `type` to `amount_type`
+		if ( ! empty( $query['type'] ) ) {
+			$query['amount_type'] = $query['type'];
+		}
+
+		// Force `type` to `discount`
+		$query['type'] = 'discount';
+
+		return parent::query( $query );
+	}
 }
