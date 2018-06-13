9275fb03476a22882bf581a7c77e7fc1e8a5b067
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-13 22:17:51 +0100

    Expose order factor to `EDD\Tests\Factory`. #4576

--- tests/phpunit/factory.php
+++ tests/phpunit/factory.php
@@ -6,4 +6,5 @@ require_once( dirname( __FILE__ ) . '/factories/class-edd-factory-for-customers.
 require_once( dirname( __FILE__ ) . '/factories/class-edd-factory-for-file-download-logs.php' );
 require_once( dirname( __FILE__ ) . '/factories/class-edd-factory-for-logs.php' );
 require_once( dirname( __FILE__ ) . '/factories/class-edd-factory-for-notes.php' );
+require_once( dirname( __FILE__ ) . '/factories/class-edd-factory-for-orders.php' );
 require_once( dirname( __FILE__ ) . '/factories/class-edd-factory.php' );
