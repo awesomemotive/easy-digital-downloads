ce0bd615249298e58e0904e5220e24415cb017bb
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-14 22:31:39 +0100

    Add shell for Order_Item_Tests. #4576

--- tests/orders/tests-order-items.php
+++ tests/orders/tests-order-items.php
@@ -1,7 +1,30 @@
 <?php
+namespace EDD\Orders;
+
 /**
- * Created by PhpStorm.
- * User: sunnyratilal
- * Date: 14/06/2018
- * Time: 22:29
+ * Order Item Tests.
+ *
+ * @group edd_orders
+ * @group database
+ *
+ * @coversDefaultClass \EDD\Orders\Order_Item
  */
+class Order_Item_Tests extends \EDD_UnitTestCase {
+
+	/**
+	 * Order items fixture.
+	 *
+	 * @var array
+	 * @static
+	 */
+	protected static $order_items = array();
+
+	/**
+	 * Set up fixtures once.
+	 */
+	public static function wpSetUpBeforeClass() {
+		self::$order_items = parent::edd()->order_item->create_many( 5 );
+	}
+
+
+}
