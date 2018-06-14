72d00c0b5292c2551efaa61b243fbb98447d48f2
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-14 22:34:10 +0100

    Add unit tests for updating order items. #4576

--- tests/orders/tests-order-items.php
+++ tests/orders/tests-order-items.php
@@ -26,5 +26,27 @@ class Order_Item_Tests extends \EDD_UnitTestCase {
 		self::$order_items = parent::edd()->order_item->create_many( 5 );
 	}
 
+	/**
+	 * @covers ::edd_update_order_item
+	 */
+	public function test_update_should_return_true() {
+		$success = edd_update_order_item( self::$order_items[0], array(
+			'product_name' => 'Stripe',
+		) );
 
+		$this->assertSame( 1, $success );
+	}
+
+	/**
+	 * @covers ::edd_update_order_item
+	 */
+	public function test_order_object_after_update_should_return_true() {
+		edd_update_order_item( self::$order_items[0], array(
+			'product_name' => 'Stripe',
+		) );
+
+		$order_item = edd_get_order_item( self::$order_items[0] );
+
+		$this->assertSame( 'Stripe', $order_item->product_name );
+	}
 }
