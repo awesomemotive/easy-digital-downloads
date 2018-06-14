9baa83be8affc7f7398176bd52e1df157bcd222d
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-14 22:35:03 +0100

    Add unit tests for edd_delete_order_item(). #4576

--- tests/orders/tests-order-items.php
+++ tests/orders/tests-order-items.php
@@ -49,4 +49,33 @@ class Order_Item_Tests extends \EDD_UnitTestCase {
 
 		$this->assertSame( 'Stripe', $order_item->product_name );
 	}
+
+	/**
+	 * @covers ::edd_update_order_item
+	 */
+	public function test_update_without_id_should_fail() {
+		$success = edd_update_order_item( null, array(
+			'product_name' => 'Stripe',
+		) );
+
+		$this->assertFalse( $success );
+	}
+
+	/**
+	 * @covers ::edd_delete_order_item
+	 */
+	public function test_delete_should_return_true() {
+		$success = edd_delete_order_item( self::$order_items[0] );
+
+		$this->assertSame( 1, $success );
+	}
+
+	/**
+	 * @covers ::edd_delete_order_item
+	 */
+	public function test_delete_without_id_should_fail() {
+		$success = edd_delete_order_item( '' );
+
+		$this->assertFalse( $success );
+	}
 }
