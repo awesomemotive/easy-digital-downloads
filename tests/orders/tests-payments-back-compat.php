8cefc53223ed78636e3815202a352c65bccf10fa
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-15 21:16:27 +0100

    PHPUnit notation. [skip ci] #4576

--- tests/orders/tests-payments-back-compat.php
+++ tests/orders/tests-payments-back-compat.php
@@ -5,6 +5,8 @@ namespace EDD\Orders;
  * Payments backwards compatibility tests.
  *
  * @group edd_back_compat
+ *
+ * @coversDefaultClass \EDD\Compat\Payment
  */
 class Payment_Back_Compat_Tests extends \EDD_UnitTestCase {
 
@@ -23,10 +25,16 @@ class Payment_Back_Compat_Tests extends \EDD_UnitTestCase {
 		self::$orders = parent::edd()->order->create_many( 5 );
 	}
 
+	/**
+	 * @covers ::wp_count_posts
+	 */
 	public function test_wp_count_posts() {
 		$this->assertSame( 5, (int) wp_count_posts( 'edd_payment' )->publish );
 	}
 
+	/**
+	 * @covers ::pre_get_posts
+	 */
 	public function test_get_posts() {
 		$this->setExpectedIncorrectUsage( 'get_posts()/get_post()/WP_Query' );
 
