5d3c4310ea9c91ea56218be13b7933574fbdfae4
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-16 14:19:03 +0100

    Stub out calculation methods. #6665

--- includes/orders/class-stats.php
+++ includes/orders/class-stats.php
@@ -20,4 +20,110 @@ defined( 'ABSPATH' ) || exit;
  */
 class Stats {
 
+	/**
+	 * Constructor.
+	 *
+	 * @since 3.0
+	 */
+	public function __construct() {
+
+	}
+
+	/** Calculation Methods ***************************************************/
+
+	/** Orders ***************************************************************/
+
+	public function get_order_earnings() {
+		
+	}
+
+	public function get_order_count() {
+
+	}
+
+	public function get_order_refund_count() {
+
+	}
+
+	public function get_order_refund_amount() {
+
+	}
+
+	public function get_average_refund_time() {
+
+	}
+
+	public function get_refund_rate() {
+
+	}
+
+	/** Order Item ************************************************************/
+
+	public function get_order_item_earnings() {
+
+	}
+
+	public function get_order_item_count() {
+
+	}
+
+	public function get_most_valuable_order_item() {
+
+	}
+
+	/** Discounts ************************************************************/
+
+	public function get_discount_usage_count() {
+
+	}
+
+	public function get_customer_savings() {
+
+	}
+
+	public function get_average_discount_amount() {
+
+	}
+
+	/** Gateways *************************************************************/
+
+	public function get_gateway_sales() {
+
+	}
+
+	public function get_gateway_earnings() {
+
+	}
+
+	public function get_gateway_refund_amount() {
+
+	}
+
+	public function get_gateway_average_value() {
+
+	}
+
+	/** Tax ******************************************************************/
+
+	public function get_tax() {
+
+	}
+
+	/** Customers ************************************************************/
+
+	public function get_customer_lifetime_value() {
+
+	}
+
+	public function get_customer_orders() {
+
+	}
+
+	public function get_customer_age() {
+
+	}
+
+	public function get_most_valuable_customers() {
+
+	}
 }
