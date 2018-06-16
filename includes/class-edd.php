5576eea54c6402f10e610772b1a187fd847e3ee4
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-16 13:33:48 +0100

    Load EDD\Orders\Stats. #6665

--- includes/class-edd.php
+++ includes/class-edd.php
@@ -512,6 +512,7 @@ final class Easy_Digital_Downloads {
 		require_once EDD_PLUGIN_DIR . 'includes/orders/class-order.php';
 		require_once EDD_PLUGIN_DIR . 'includes/orders/class-order-adjustment.php';
 		require_once EDD_PLUGIN_DIR . 'includes/orders/class-order-item.php';
+		require_once EDD_PLUGIN_DIR . 'includes/orders/class-stats.php';
 		require_once EDD_PLUGIN_DIR . 'includes/orders/functions.php';
 		require_once EDD_PLUGIN_DIR . 'includes/orders/meta.php';
 
