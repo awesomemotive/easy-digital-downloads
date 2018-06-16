64987110a4661fed0be740c611668445083866a0
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-16 13:32:40 +0100

    Add shell for EDD\Orders\Stats . #6665

---
+++ includes/orders/class-stats.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Order Stats class.
+ *
+ * @package     EDD
+ * @subpackage  Orders
+ * @copyright   Copyright (c) 2018, Easy Digital Downloads, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0
+ */
+namespace EDD\Orders;
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Stats Class.
+ *
+ * @since 3.0
+ */
+class Stats {
+
+}
