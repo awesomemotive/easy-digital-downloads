d097299370e66dc4f70f8296d026494d1cf69892
John James Jacoby <johnjamesjacoby@me.com>
2018-06-20 14:25:51 -0500

    Bump the version number and compile the assets.
    This change makes sure testers will see updated designs without `SCRIPT_DEBUG`.

--- includes/class-edd.php
+++ includes/class-edd.php
@@ -259,7 +259,7 @@ final class Easy_Digital_Downloads {
 
 		// Plugin version.
 		if ( ! defined( 'EDD_VERSION' ) ) {
-			define( 'EDD_VERSION', '3.0.0-beta-1.0007' );
+			define( 'EDD_VERSION', '3.0.0-beta-1.0010' );
 		}
 
 		// Plugin Root File.
