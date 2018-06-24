d95586f8d758c08b56e4130312355042c01184c6
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-24 22:43:47 +0100

    Stub out Earnings_By_Taxonomy_List_Table. #6298 #6006

---
+++ includes/reports/data/downloads/class-earnings-by-taxonomy-list-table.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Earnings by Taxonomy list table.
+ *
+ * @package     EDD
+ * @subpackage  Reports/Data/File_Downloads
+ * @copyright   Copyright (c) 2018, Easy Digital Downloads, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0
+ */
+namespace EDD\Reports\Data\Downloads;
+
+use EDD\Reports as Reports;
+use EDD\Orders as Orders;
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Earnings_By_Taxonomy_List_Table class.
+ *
+ * @since 3.0
+ */
+class Earnings_By_Taxonomy_List_Table extends \WP_List_Table {
+
+}
