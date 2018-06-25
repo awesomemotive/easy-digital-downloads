920899ab0ec1d710abfd026f2459623fb5a52b90
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-25 22:20:16 +0100

    Remove checkboxes from list table. #6296

--- includes/reports/data/customers/class-most-valuable-customers-list-table.php
+++ includes/reports/data/customers/class-most-valuable-customers-list-table.php
@@ -74,6 +74,24 @@ class Most_Valuable_Customers_List_Table extends \EDD_Customer_Reports_Table {
 	}
 
 	/**
+	 * Retrieve the table columns.
+	 *
+	 * @since 3.0
+	 *
+	 * @return array $columns Array of all the list table columns.
+	 */
+	public function get_columns() {
+		$columns = parent::get_columns();
+
+		// Remove the checkbox if it exists.
+		if ( isset( $columns['cb'] ) ) {
+			unset( $columns['cb'] );
+		}
+
+		return $columns;
+	}
+
+	/**
 	 * Return empty array to disable sorting.
 	 *
 	 * @since 3.0
