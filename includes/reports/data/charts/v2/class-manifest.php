2d98b8b2adcf6d1561649376a20326687cae1ccb
Sunny Ratilal <sunny@sunnyratilal.com>
2018-06-28 20:35:21 +0100

    Code formatting. [skip ci] #6296

--- includes/reports/data/charts/v2/class-manifest.php
+++ includes/reports/data/charts/v2/class-manifest.php
@@ -387,57 +387,59 @@ class Manifest implements Error_Logger {
 					},
 
 					custom: function (tooltip) {
-						// Tooltip Element
-						var tooltipEl = document.getElementById('edd-chartjs-tooltip');
+						// Tooltip element.
+						var tooltipEl = document.getElementById( 'edd-chartjs-tooltip' );
 
-						if (!tooltipEl) {
-							tooltipEl = document.createElement('div');
+						if ( ! tooltipEl ) {
+							tooltipEl = document.createElement( 'div' );
 							tooltipEl.id = 'edd-chartjs-tooltip';
 							tooltipEl.innerHTML = '<table></table>';
-							this._chart.canvas.parentNode.appendChild(tooltipEl);
+							this._chart.canvas.parentNode.appendChild( tooltipEl );
 						}
 
-						// Hide if no tooltip
-						if (tooltip.opacity === 0) {
+						// Hide if no tooltip.
+						if ( tooltip.opacity === 0 ) {
 							tooltipEl.style.opacity = 0;
 							return;
 						}
 
-						// Set caret Position
-						tooltipEl.classList.remove('above', 'below', 'no-transform');
-						if (tooltip.yAlign) {
-							tooltipEl.classList.add(tooltip.yAlign);
+						// Set caret position.
+						tooltipEl.classList.remove( 'above', 'below', 'no-transform' );
+						if ( tooltip.yAlign ) {
+							tooltipEl.classList.add(tooltip.yAlign );
 						} else {
-							tooltipEl.classList.add('no-transform');
+							tooltipEl.classList.add( 'no-transform' );
 						}
 
-						function getBody(bodyItem) {
+						function getBody( bodyItem ) {
 							return bodyItem.lines;
 						}
 
 						// Set Text
-						if (tooltip.body) {
+						if ( tooltip.body ) {
 							var titleLines = tooltip.title || [];
-							var bodyLines = tooltip.body.map(getBody);
+							var bodyLines = tooltip.body.map( getBody );
 
 							var innerHtml = '<thead>';
 
-							titleLines.forEach(function (title) {
+							titleLines.forEach( function ( title ) {
 								innerHtml += '<tr><th>' + title + '</th></tr>';
 							});
+
 							innerHtml += '</thead><tbody>';
 
-							bodyLines.forEach(function (body, i) {
-								var colors = tooltip.labelColors[i];
+							bodyLines.forEach( function ( body, i ) {
+								var colors = tooltip.labelColors[ i ];
 								var style = 'background:' + colors.borderColor;
 								style += '; border-color:' + colors.borderColor;
 								style += '; border-width: 2px';
 								var span = '<span class="edd-chartjs-tooltip-key" style="' + style + '"></span>';
 								innerHtml += '<tr><td>' + span + body + '</td></tr>';
 							});
+
 							innerHtml += '</tbody>';
 
-							var tableRoot = tooltipEl.querySelector('table');
+							var tableRoot = tooltipEl.querySelector( 'table' );
 							tableRoot.innerHTML = innerHtml;
 						}
 
