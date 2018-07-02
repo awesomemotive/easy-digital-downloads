3045842d950dd1923e653bf1098c98d66650f29e
Sunny Ratilal <sunny@sunnyratilal.com>
2018-07-03 00:40:04 +0100

    Only loop if array exists. #6296

--- includes/reports/data/class-report-registry.php
+++ includes/reports/data/class-report-registry.php
@@ -69,12 +69,11 @@ class Report_Registry extends Reports\Registry implements Utils\Static_Registry 
 	 * @return mixed Results of the method call (if any).
 	 */
 	public function __call( $name, $arguments ) {
-
 		$report_id_or_sort = isset( $arguments[0] )
 			? $arguments[0]
 			: '';
 
-		switch( $name ) {
+		switch ( $name ) {
 			case 'get_report':
 				return parent::get_item( $report_id_or_sort );
 
@@ -118,30 +117,33 @@ class Report_Registry extends Reports\Registry implements Utils\Static_Registry 
 
 		try {
 			$this->validate_attributes( $attributes, $report_id );
-
-		} catch( \EDD_Exception $exception ) {
+		} catch ( \EDD_Exception $exception ) {
 			$error = true;
 
 			throw $exception;
 		}
 
-		foreach ( $attributes['endpoints'] as $view_group => $endpoints ) {
-			foreach ( $endpoints as $index => $endpoint ) {
-				if ( ! is_string( $endpoint ) && ! ( $endpoint instanceof \EDD\Reports\Data\Endpoint ) ) {					
-					unset( $attributes['endpoints'][ $view_group ][ $index ] );
+		if ( isset( $attributes['endpoints'] ) && is_array( $attributes['endpoints'] ) ) {
+			foreach ( $attributes['endpoints'] as $view_group => $endpoints ) {
+				foreach ( $endpoints as $index => $endpoint ) {
+					if ( ! is_string( $endpoint ) && ! ( $endpoint instanceof \EDD\Reports\Data\Endpoint ) ) {
+						unset( $attributes['endpoints'][ $view_group ][ $index ] );
 
-					throw new Utils\Exception( sprintf( 'The \'%1$s\' report contains one or more invalidly defined endpoints.', $report_id ) );
+						throw new Utils\Exception( sprintf( 'The \'%1$s\' report contains one or more invalidly defined endpoints.', $report_id ) );
+					}
 				}
 			}
 		}
 
-		foreach ( $attributes['filters'] as $index => $filter ) {
-			if ( ! Reports\validate_filter( $filter ) ) {
-				$message = sprintf( 'The \'%1$s\' report contains one or more invalid filters.', $report_id );
+		if ( isset( $attributes['filters'] ) && is_array( $attributes['filters'] ) ) {
+			foreach ( $attributes['filters'] as $index => $filter ) {
+				if ( ! Reports\validate_filter( $filter ) ) {
+					$message = sprintf( 'The \'%1$s\' report contains one or more invalid filters.', $report_id );
 
-				unset( $attributes['filters'][ $index ] );
+					unset( $attributes['filters'][ $index ] );
 
-				throw new Utils\Exception( $message );
+					throw new Utils\Exception( $message );
+				}
 			}
 		}
 
