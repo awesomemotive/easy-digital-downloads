f29267b741c826e12868c36039bc6bdc81db344d
Sunny Ratilal <sunny@sunnyratilal.com>
2018-07-04 09:09:29 +0100

    Set up reports filters for region and country. #6667

--- includes/admin/reporting/reports.php
+++ includes/admin/reporting/reports.php
@@ -1695,7 +1695,7 @@ function edd_register_taxes_report( $reports ) {
 					'total_tax_collected',
 				),
 			),
-			'filters'   => array( 'products' ),
+			'filters'   => array( 'products', 'regions', 'countries' ),
 		) );
 
 		$reports->register_endpoint( 'total_tax_collected', array(
