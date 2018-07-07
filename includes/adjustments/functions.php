6c7c8d8cc5327765a5c211d3821e5249ea386d00
Sunny Ratilal <sunny@sunnyratilal.com>
2018-07-07 20:48:59 +0200

    Stub out adjustmenta meta functions.

--- includes/adjustments/functions.php
+++ includes/adjustments/functions.php
@@ -126,3 +126,84 @@ function edd_count_adjustments( $args = array() ) {
 	// Return count(s)
 	return absint( $adjustments->found_items );
 }
+
+/** Meta **********************************************************************/
+
+/**
+ * Add meta data field to an adjustment.
+ *
+ * @since 3.0
+ *
+ * @param int     $adjustment_id Adjustment ID.
+ * @param string  $meta_key      Meta data name.
+ * @param mixed   $meta_value    Meta data value. Must be serializable if non-scalar.
+ * @param bool    $unique        Optional. Whether the same key should not be added.
+ *                               Default false.
+ *
+ * @return int|false Meta ID on success, false on failure.
+ */
+function edd_add_adjustment_meta( $adjustment_id, $meta_key, $meta_value, $unique = false ) {
+	return add_metadata( 'edd_adjustment', $adjustment_id, $meta_key, $meta_value, $unique );
+}
+
+/**
+ * Remove meta data matching criteria from an adjustment.
+ *
+ * You can match based on the key, or key and value. Removing based on key and
+ * value, will keep from removing duplicate meta data with the same key. It also
+ * allows removing all meta data matching key, if needed.
+ *
+ * @since 3.0
+ *
+ * @param int     $adjustment_id Adjustment ID.
+ * @param string  $meta_key      Meta data name.
+ * @param mixed   $meta_value    Optional. Meta data value. Must be serializable if
+ *                               non-scalar. Default empty.
+ *
+ * @return bool True on success, false on failure.
+ */
+function edd_delete_adjustment_meta( $adjustment_id, $meta_key, $meta_value = '' ) {
+	return delete_metadata( 'edd_adjustment', $adjustment_id, $meta_key, $meta_value );
+}
+
+/**
+ * Retrieve adjustment meta field for an adjustment.
+ *
+ * @since 3.0
+ *
+ * @param int     $adjustment_id Adjustment ID.
+ * @param string  $key           Optional. The meta key to retrieve. By default, returns
+ *                               data for all keys. Default empty.
+ * @param bool    $single        Optional, default is false.
+ *                               If true, return only the first value of the specified meta_key.
+ *                               This parameter has no effect if meta_key is not specified.
+ *
+ * @return mixed Will be an array if $single is false. Will be value of meta data
+ *               field if $single is true.
+ */
+function edd_get_adjustment_meta( $adjustment_id, $key = '', $single = false ) {
+	return get_metadata( 'edd_adjustment', $adjustment_id, $key, $single );
+}
+
+/**
+ * Update discount meta field based on discount ID.
+ *
+ * Use the $prev_value parameter to differentiate between meta fields with the
+ * same key and discount ID.
+ *
+ * If the meta field for the discount does not exist, it will be added.
+ *
+ * @since 3.0
+ *
+ * @param int     $adjustment_id  Adjustment ID.
+ * @param string  $meta_key     Meta data key.
+ * @param mixed   $meta_value   Meta data value. Must be serializable if non-scalar.
+ * @param mixed   $prev_value   Optional. Previous value to check before removing.
+ *                              Default empty.
+ *
+ * @return int|bool Meta ID if the key didn't exist, true on successful update,
+ *                  false on failure.
+ */
+function edd_update_adjustment_meta( $adjustment_id, $meta_key, $meta_value, $prev_value = '' ) {
+	return update_metadata( 'edd_adjustment', $adjustment_id, $meta_key, $meta_value, $prev_value );
+}
