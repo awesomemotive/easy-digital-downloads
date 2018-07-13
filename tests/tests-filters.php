934f562135aaa0c97635af06749c3f741c924f70
Sunny Ratilal <sunny@sunnyratilal.com>
2018-07-14 00:38:11 +0100

    Fix incomplete tests.

--- tests/tests-filters.php
+++ tests/tests-filters.php
@@ -259,12 +259,6 @@ class Tests_Filters extends EDD_UnitTestCase {
 		$this->assertArrayHasKey( 'edd_clear_user_history_cache', $wp_filter['edd_update_payment_status'][10] );
 	}
 
-	public function test_edd_delete_payment() {
-		global $wp_filter;
-		$this->markTestIncomplete( 'For some strange reason the edd_delete_payment key does not exist here.' );
-		//$this->assertArrayHasKey( 'edd_trigger_purchase_delete', $wp_filter['edd_delete_payment'][10] );
-	}
-
 	public function test_edd_upgrade_payments() {
 		global $wp_filter;
 		$this->assertArrayHasKey( 'edd_update_old_payments_with_totals', $wp_filter['edd_upgrade_payments'][10] );
