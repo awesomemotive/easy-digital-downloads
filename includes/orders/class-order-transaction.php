a063a8e2726b871d218343793240b9e220a29e6b
Sunny Ratilal <sunny@sunnyratilal.com>
2018-07-14 19:33:55 +0100

    Use mapping style of `object_id` and `object_type`. #6657

--- includes/orders/class-order-transaction.php
+++ includes/orders/class-order-transaction.php
@@ -21,16 +21,14 @@ defined( 'ABSPATH' ) || exit;
  * @since 3.0
  *
  * @property int $id
- * @property int $order_id
- * @property int $order_item_id
+ * @property int $object_id
+ * @property string $object_type
  * @property string $gateway
  * @property string $transaction_id
  * @property string $status
  * @property float $total
  * @property string $date_created
  * @property string $date_modified
- * @property \EDD\Orders\Order $order
- * @property \EDD\Orders\Order_Item $order_item
  */
 class Order_Transaction extends Objects\Order_Transaction {
 
@@ -43,20 +41,20 @@ class Order_Transaction extends Objects\Order_Transaction {
 	protected $id;
 
 	/**
-	 * Order ID.
+	 * Object ID.
 	 *
 	 * @since 3.0
 	 * @var   int
 	 */
-	protected $order_id;
+	protected $object_id;
 
 	/**
-	 * Order Item ID.
+	 * Object type
 	 *
 	 * @since 3.0
 	 * @var   int
 	 */
-	protected $order_item_id;
+	protected $object_type;
 
 	/**
 	 * Gateway.
@@ -105,40 +103,6 @@ class Order_Transaction extends Objects\Order_Transaction {
 	 * @var   string
 	 */
 	protected $date_modified;
-
-	/**
-	 * Order.
-	 *
-	 * @since 3.0
-	 * @var   \EDD\Orders\Order
-	 */
-	protected $order;
-
-	/**
-	 * Order Item.
-	 *
-	 * @since 3.0
-	 * @var   \EDD\Orders\Order_Item
-	 */
-	protected $order_item;
-
-	/**
-	 * Magic getter for immutability.
-	 *
-	 * @since 3.0
-	 *
-	 * @param string $key
-	 * @return mixed
-	 */
-	public function __get( $key = '' ) {
-		if ( 'order' === $key && null === $this->order ) {
-			$this->order = edd_get_order( $this->order_id );
-		} elseif ( 'order_item' === $key && null === $this->order_item ) {
-			$this->order_item = edd_get_order_item( $this->order_item_id );
-		}
-
-		return parent::__get( $key );
-	}
 
 	/**
 	 * Check if a transaction is complete.
