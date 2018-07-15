3a079392d9848e9dbd143e67dcb2ead2baabb6fc
Sunny Ratilal <sunny@sunnyratilal.com>
2018-07-15 21:25:26 +0100

    Base logic. #5295

--- includes/admin/payments/add-order.php
+++ includes/admin/payments/add-order.php
@@ -1,1 +1,18 @@
 <?php
+/**
+ * Add Order Page.
+ *
+ * @package     EDD
+ * @subpackage  Admin/Orders
+ * @copyright   Copyright (c) 2018, Easy Digital Downloads, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+// Bail if incorrect view.
+if ( ! isset( $_GET['view'] ) || 'add-order' !== $_GET['view'] ) {
+	wp_die( esc_html__( 'Something went wrong.', 'easy-digital-downloads' ), esc_html__( 'Error', 'easy-digital-downloads' ), array( 'response' => 400 ) );
+}
