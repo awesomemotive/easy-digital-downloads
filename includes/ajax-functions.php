a7d789f8bebdb89383de23096d7f666252b0ce9b
Sunny Ratilal <sunny@sunnyratilal.com>
2018-07-19 13:40:01 +0100

    Generate HTML for customer dropdown. #5295

--- includes/ajax-functions.php
+++ includes/ajax-functions.php
@@ -1085,16 +1085,24 @@ function edd_ajax_customer_addresses() {
 			if ( ! empty( $options ) ) {
 				$select = EDD()->html->select( array(
 					'name'             => '',
-					'id'               => 'edd-customer-addresses-select',
+					'id'               => 'add-order-customer-address-select',
+					'class'            => 'add-order-customer-address-select',
 					'options'          => $options,
 					'chosen'           => true,
 					'selected'         => false,
 					'placeholder'      => __( 'Select a previously used address', 'easy-digital-downloads' ),
 					'show_option_all'  => false,
-					'show_option_none' => false,
+					'show_option_none' => __( 'New address' ),
 				) );
+
+				$html  = '<select data-placeholder="Select a previously used address" class="edd-select-chosen add-order-customer-address-select">';
+				$html .= '<option></option>';
+				foreach ( $options as $key => $value ) {
+					$html .= '<option data-key="' . esc_attr( $key ) . '" value="' . esc_attr( $key ) . '">' . esc_attr( $value ). '</option>';
+				}
+				$html .= '</select>';
 				
-				$response['html'] = $select;
+				$response['html'] = $html;
 			}
 		}
 	}
