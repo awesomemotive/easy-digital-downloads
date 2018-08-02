87c4fe033ed210385e6da8564a658f1435ab4e04
Sunny Ratilal <sunny@sunnyratilal.com>
2018-08-03 00:50:24 +0100

    Introduce `edd_apply_order_credit()`. #2721

--- includes/orders/functions.php
+++ includes/orders/functions.php
@@ -2193,3 +2193,133 @@ function edd_refund_order_item( $order_item_id = 0, $data = array() ) {
 	// Trigger actions to run.
 	edd_update_order_status( $order->id, 'partial_refund' );
 }
+
+/**
+ * Apply credit to an order. This method should be used to apply a flat amount
+ * of credit to an order.
+ *
+ * @since 3.0
+ *
+ * @param int   $order_id Order ID.
+ * @param array $data     Credit data. For accepted parameters, see `edd_add_order_adjustment()`.
+ *
+ * @return int|false New order ID if successful, false otherwise.
+ */
+function edd_apply_order_credit( $order_id = 0, $data = array() ) {
+	global $wpdb;
+
+	// Bail if invalid data passed.
+	if ( empty( $order_id ) || empty( $data ) ) {
+		return false;
+	}
+
+	// Ensure the order ID is an integer.
+	$order_id = absint( $order_id );
+
+	// Fetch order.
+	$order = edd_get_order( $order_id );
+
+	// Bail if order was not found or it has been revoked.
+	if ( ! $order || 'revoked' === $order->status ) {
+		return false;
+	}
+
+	/**
+	 * Filter whether refunds should be allowed.
+	 *
+	 * @since 3.0
+	 *
+	 * @param int $order_id Order ID.
+	 */
+	$should_apply_credit = apply_filters( 'edd_should_apply_order_credit', '__return_true', $order_id );
+
+	// Bail if refund is blocked.
+	if ( ! $should_apply_credit ) {
+		return false;
+	}
+
+	/** Generate new order number ********************************************/
+
+	$last_order = $wpdb->get_row( $wpdb->prepare( "SELECT id, order_number
+		FROM {$wpdb->edd_orders}
+		WHERE parent = %d
+		ORDER BY id DESC
+		LIMIT 1", 0 ) );
+
+	/**
+	 * Filter the suffix applied to order numbers for refunds.
+	 *
+	 * @since 3.0
+	 *
+	 * @param string Suffix.
+	 */
+	$refund_suffix = apply_filters( 'edd_order_refund_suffix', '-R-' );
+
+	if ( $last_order ) {
+
+		// Check for order number first.
+		if ( $last_order->order_number && ! empty( $last_order->order_number ) ) {
+
+			// Order has been previously revised.
+			if ( false !== strpos( $last_order->order_number, $refund_suffix ) ) {
+				$number = $last_order->order_number;
+				++$number;
+
+				// First revision to order.
+			} else {
+				$number = $last_order->order_number . $refund_suffix . '1';
+			}
+
+			// Append to ID.
+		} else {
+			$number = $last_order->id . $refund_suffix . '1';
+		}
+	} else {
+		$number = $last_order->id . $refund_suffix . '1';
+	}
+
+	// Parse the adjustment data.
+	$data = wp_parse_args( $data, array(
+		'type_id'     => 0,
+		'description' => '',
+		'subtotal'    => 0.00,
+		'total'       => 0.00,
+	) );
+
+	// Bail if the adjustment is worth nothing.
+	if ( 0.00 === floatval( $data['total'] ) ) {
+		return false;
+	}
+
+	/** Insert order *********************************************************/
+
+	$order_data = array(
+		'parent'       => $order_id,
+		'order_number' => $number,
+		'status'       => 'partially_refunded',
+		'type'         => 'refund',
+		'user_id'      => $order->user_id,
+		'customer_id'  => $order->customer_id,
+		'email'        => $order->email,
+		'ip'           => $order->ip,
+		'gateway'      => $order->gateway,
+		'mode'         => $order->mode,
+		'currency'     => $order->currency,
+		'payment_key'  => strtolower( md5( uniqid() ) ),
+		'total'        => edd_negate_amount( $data['total'] ),
+	);
+
+	$new_order_id = edd_add_order( $order_data );
+
+	/** Insert adjustment *****************************************************/
+
+	edd_add_order_adjustment( array(
+		'object_type' => 'order',
+		'object_id'   => $new_order_id,
+		'type_id'     => absint( $data['type_id'] ),
+		'type'        => 'credit',
+		'description' => sanitize_text_field( $data['description'] ),
+		'subtotal'    => floatval( $data['subtotal'] ),
+		'total'       => floatval( $data['total'] ),
+	) );
+}
