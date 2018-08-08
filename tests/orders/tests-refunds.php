c12b7b2ab8fe45555dbe2a35d56d4e491b57602d
Sunny Ratilal <sunny@sunnyratilal.com>
2018-08-08 15:47:14 +0100

    First set of unit tests. #2721

--- tests/orders/tests-refunds.php
+++ tests/orders/tests-refunds.php
@@ -13,17 +13,60 @@ namespace EDD\Orders;
 class Refunds_Tests extends \EDD_UnitTestCase {
 
 	/**
-	 * Orders fixture.
+	 * Order fixture.
 	 *
-	 * @var array
+	 * @var int
 	 * @static
 	 */
-	protected static $orders = array();
+	protected static $order = 0;
+
+	/**
+	 * Order item fixture.
+	 *
+	 * @var int
+	 * @static
+	 */
+	protected static $order_item = 0;
 
 	/**
 	 * Set up fixtures once.
 	 */
 	public static function wpSetUpBeforeClass() {
-		self::$orders = parent::edd()->order->create_many( 5 );
+		self::$order = parent::edd()->order->create( array(
+			'status'          => 'publish',
+			'type'            => 'order',
+			'date_refundable' => EDD()->utils->date( 'now' )->addDays( 30 )->toDateTimeString(),
+			'email'           => 'guest@edd.local',
+			'ip'              => '10.0.0.1',
+			'gateway'         => 'stripe',
+			'mode'            => 'live',
+			'currency'        => 'USD',
+			'payment_key'     => md5( uniqid() ),
+			'subtotal'        => 100,
+			'discount'        => 5,
+			'tax'             => 20,
+			'total'           => 120,
+		) );
+
+		edd_add_order_adjustment( array(
+			'object_type' => 'order',
+			'object_id'   => self::$order,
+			'type'        => 'discount',
+			'description' => '5OFF',
+			'subtotal'    => 0,
+			'total'       => 5,
+		) );
+	}
+
+	public function test_is_order_refundable_should_return_true() {
+		$this->assertTrue( edd_is_order_refundable( self::$order ) );
+	}
+
+	public function test_get_order_total_should_be_120() {
+		$this->assertSame( 120.0, edd_get_order_total( self::$order ) );
+	}
+
+	public function test_get_order_item_total_should_be_100() {
+		$this->assertSame( 100.0, edd_get_order_item_total( self::$order_item ) );
 	}
 }
