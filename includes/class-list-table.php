6cdf982bbdf58743b6811700fa419e43d50a5a19
Sunny Ratilal <sunny@sunnyratilal.com>
2018-08-16 16:37:44 +0100

    Introduce `EDD\List_Table`.

---
+++ includes/class-list-table.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * List Table Base Class.
+ *
+ * @package     EDD
+ * @subpackage  Core
+ * @copyright   Copyright (c) 2018, Easy Digital Downloads, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0
+ */
+namespace EDD;
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Implements a base list table class to be extended by core.
+ *
+ * @since 3.0
+ * @abstract
+ */
+abstract class List_Table extends \WP_List_Table {
+
+}
