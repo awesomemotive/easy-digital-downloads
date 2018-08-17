1398557c37a062021a25bf7c92d752c22fc5fa88
John James Jacoby <johnjamesjacoby@me.com>
2018-08-17 08:46:53 -0500

    Orders: introduce `edd_get_order_types()` with a few simple labels.
    Will get used in more places where hard-coded strings are currently, as well as additional properties.

--- includes/orders/functions.php
+++ includes/orders/functions.php
@@ -1379,6 +1379,46 @@ function edd_clone_order( $order_id = 0, $clone_relationships = false, $args = a
 	return $new_order_id;
 }
 
+/**
+ * Retrieve the registered order types.
+ *
+ * @since 3.0
+ *
+ * @return array
+ */
+function edd_get_order_types() {
+	static $types = null;
+
+	// Avoid reprocessing the labels
+	if ( null === $types ) {
+
+		// Filter
+		$types = (array) apply_filters( 'edd_get_order_types', array(
+			'sale' => array(
+				'labels' => array(
+					'singular' => __( 'Sale',  'easy-digital-downloads' ),
+					'plural'   => __( 'Sales', 'easy-digital-downloads' )
+				)
+			),
+			'refund' => array(
+				'labels' => array(
+					'singular' => __( 'Refund',  'easy-digital-downloads' ),
+					'plural'   => __( 'Refunds', 'easy-digital-downloads' )
+				)
+			),
+			'invoice' => array(
+				'labels' => array(
+					'singular' => __( 'Invoice',  'easy-digital-downloads' ),
+					'plural'   => __( 'Invoices', 'easy-digital-downloads' )
+				)
+			)
+		) );
+	}
+
+	// Return
+	return (array) $types;
+}
+
 /** Order Items ***************************************************************/
 
 /**
