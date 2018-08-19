e578e2cba7db46bc57464eb6afe7db886a5d3721
Andrew Munro <andrew@amdrew.com>
2018-08-20 11:37:41 +1200

    Add ordering by sales and earnings with permission checks

--- includes/api/class-edd-api-v2.php
+++ includes/api/class-edd-api-v2.php
@@ -56,6 +56,20 @@ class EDD_API_V2 extends EDD_API_V1 {
 					$query_args['orderby']  = 'meta_value_num';
 					break;
 
+				case 'sales':
+					if ( user_can( $this->user_id, 'view_shop_sensitive_data' ) || current_user_can( 'view_shop_sensitive_data' ) || $this->override ) {
+						$query_args['meta_key'] = '_edd_download_sales';
+						$query_args['orderby']  = 'meta_value';
+					}
+					break;
+
+				case 'earnings':
+					if ( user_can( $this->user_id, 'view_shop_sensitive_data' ) || current_user_can( 'view_shop_sensitive_data' ) || $this->override ) {
+						$query_args['meta_key'] = '_edd_download_earnings';
+						$query_args['orderby']  = 'meta_value_num';
+					}
+					break;
+
 				default:
 					$query_args['orderby'] = 'date';
 					break;
