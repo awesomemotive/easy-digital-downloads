143378fd916b6c6c9e665f2fe5c6d3f2a13057ea
Andrew Munro <andrew@amdrew.com>
2018-08-22 17:09:57 +1200

    First pass at implementing SVGs to checkout

--- includes/checkout/template.php
+++ includes/checkout/template.php
@@ -675,38 +675,44 @@ function edd_show_payment_icons() {
 	foreach ( $payment_methods as $key => $card ) {
 
 		if ( edd_string_is_image_url( $key ) ) {
-
 			echo '<img class="payment-icon" src="' . esc_url( $key ) . '"/>';
-
 		} else {
+			$type = 'image';
 
 			$card_lowercase = strtolower( str_replace( ' ', '', $card ) );
 
 			if ( has_filter( 'edd_accepted_payment_' . $card_lowercase . '_image' ) ) {
-
 				$image = apply_filters( 'edd_accepted_payment_' . $card_lowercase . '_image', '' );
-
 			} else {
+				// Set the type to SVG.
+				$type = 'svg';
 
-				$image = edd_locate_template( 'images' . DIRECTORY_SEPARATOR . 'icons' . DIRECTORY_SEPARATOR . $card_lowercase . '.png', false );
+				// Get SVG dimensions.
+				$dimensions = edd_svg_dimensions( $key );
 
-				// Replaces backslashes with forward slashes for Windows systems
-				$plugin_dir  = wp_normalize_path( WP_PLUGIN_DIR );
-				$content_dir = wp_normalize_path( WP_CONTENT_DIR );
-				$image       = wp_normalize_path( $image );
-
-				$image = str_replace( $plugin_dir, WP_PLUGIN_URL, $image );
-				$image = str_replace( $content_dir, WP_CONTENT_URL, $image );
+				// Get SVG markup.
+				$image = edd_get_svg( 
+					array( 
+						'icon'    => $key,
+						'width'   => $dimensions['width'],
+						'height'  => $dimensions['height'],
+						'title'   => $card,
+						'classes' => array( 'payment-icon' )
+					)
+				);
 
 			}
 
 			if ( edd_is_ssl_enforced() || is_ssl() ) {
-
 				$image = edd_enforced_ssl_asset_filter( $image );
-
 			}
 
-			echo '<img class="payment-icon" src="' . esc_url( $image ) . '"/>';
+			if ( 'svg' === $type ) {
+				echo $image;
+			} else {
+				echo '<img class="payment-icon" src="' . esc_url( $image ) . '"/>';
+			}
+
 		}
 
 	}
