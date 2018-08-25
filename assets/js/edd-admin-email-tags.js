f90c79723120e077017cc17c6bfab57b0d2e8c3b
Spencer Finnell <spencerfinnell@gmail.com>
2018-08-25 16:42:26 -0400

    Whitespace fix.

--- assets/js/edd-admin-email-tags.js
+++ assets/js/edd-admin-email-tags.js
@@ -43,34 +43,34 @@ function normalizeTerm( term ) {
  * Make tags clickable and send them to the email content (wp_editor()).
  */
 function setupEmailTags() {
-  // Find all of the buttons.
-  var insertButtons = document.querySelectorAll( '.edd-email-tags-list__insert' );
+	// Find all of the buttons.
+	var insertButtons = document.querySelectorAll( '.edd-email-tags-list__insert' );
 
-  /**
-   * Listen for clicks on tag buttons.
-   *
-   * @param {object} node Button node.
-   */
-  insertButtons.forEach( function( node ) {
+	/**
+	 * Listen for clicks on tag buttons.
+	 *
+	 * @param {object} node Button node.
+	 */
+	insertButtons.forEach( function( node ) {
 
-    /**
-     * Listen for clicks on tag buttons.
-     */
-    node.addEventListener( 'click', function() {
-      // Close Thickbox.
-      tb_remove();
+		/**
+		 * Listen for clicks on tag buttons.
+		 */
+		node.addEventListener( 'click', function() {
+			// Close Thickbox.
+			tb_remove();
 
-      window.send_to_editor( node.dataset.to_insert );
-    } );
-  } );
+			window.send_to_editor( node.dataset.to_insert );
+		} );
+	} );
 }
 
 /**
  * Filter tags.
  */
 function filterEmailTags() {
-  var filterInput = document.querySelector( '.edd-email-tags-filter__input' );
-  var tagItems = document.querySelectorAll( '.edd-email-tags-list__tag' );
+	var filterInput = document.querySelector( '.edd-email-tags-filter__input' );
+	var tagItems = document.querySelectorAll( '.edd-email-tags-list__tag' );
 
 	filterInput.addEventListener( 'keyup', function( event ) {
 		var searchTerm = event.target.value;
