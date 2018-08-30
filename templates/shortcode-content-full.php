ce4941db48e8c68bea93477b872aab907779cd65
Spencer Finnell <spencerfinnell@gmail.com>
2018-08-30 11:43:55 -0400

    Allow [downloads] shortcode inside a single download's content. See #6072
    - Updates shortcode-content-excerpt.php template file.
    - Updates shortcode-content-full.php template file.
    - Does not pollute the global excerpt_length  filter.
    - Ensures no download buttons are appended to excerpts.
    - Ensures full content can still be output and non-[downloads] shortcodes rendered.

--- templates/shortcode-content-full.php
+++ templates/shortcode-content-full.php
@@ -1,4 +1,18 @@
-<?php $item_prop = edd_add_schema_microdata() ? ' itemprop="description"' : ''; ?>
+<?php
+/**
+ * Shortcode "Full Content" template.
+ *
+ * @since 1.2
+ *
+ * @package EDD
+ * @category Template
+ * @author Easy Digital Downloads
+ * @version 2.0.0
+ */
+
+$item_prop = edd_add_schema_microdata() ? ' itemprop="description"' : '';
+?>
+
 <div<?php echo $item_prop; ?> class="edd_download_full_content">
-	<?php echo apply_filters( 'edd_downloads_content', get_post_field( 'post_content', get_the_ID() ) ); ?>
+	<?php echo edd_download_shortcode_full_content(); ?>
 </div>
