34597ffb8b1cb40a82c7d1d5bca15d75a2fa8858
jjj <johnjamesjacoby@me.com>
2018-09-07 16:07:47 -0500

    Orders: improve type registration
    * Add `show_ui` flag, to allow some to be hidden
    * Continue to register the `invoice` type, but hide it, so it's reserved for later
    * Introduce helper function for getting and setting types

--- includes/orders/functions/types.php
+++ includes/orders/functions/types.php
@@ -1,14 +1,16 @@
 <?php
-
 /**
- * 
- * @since 3.0
+ * Order Type Functions
  *
- * @param array $args
+ * @package     EDD
+ * @subpackage  Orders
+ * @copyright   Copyright (c) 2018, Easy Digital Downloads, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0
  */
-function edd_register_order_type( $args = array() ) {
-	
-}
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Retrieve the registered order types.
@@ -18,34 +20,87 @@ function edd_register_order_type( $args = array() ) {
  * @return array
  */
 function edd_get_order_types() {
-	static $types = null;
 
-	// Avoid reprocessing the labels
-	if ( null === $types ) {
+	// Get the
+	$component = edd_get_component( 'order' );
 
-		// Filter
-		$types = (array) apply_filters( 'edd_get_order_types', array(
-			'sale' => array(
-				'labels' => array(
-					'singular' => __( 'Sale',  'easy-digital-downloads' ),
-					'plural'   => __( 'Sales', 'easy-digital-downloads' )
-				)
-			),
-			'refund' => array(
-				'labels' => array(
-					'singular' => __( 'Refund',  'easy-digital-downloads' ),
-					'plural'   => __( 'Refunds', 'easy-digital-downloads' )
-				)
-			),
-			'invoice' => array(
-				'labels' => array(
-					'singular' => __( 'Invoice',  'easy-digital-downloads' ),
-					'plural'   => __( 'Invoices', 'easy-digital-downloads' )
-				)
-			)
-		) );
+	// Setup an empty types array
+	if ( ! isset( $component->types ) ) {
+		$component->types = array();
 	}
 
-	// Return
-	return (array) $types;
+	// Return types
+	return (array) $component->types;
 }
+
+/**
+ * Register an Order Type.
+ *
+ * @since 3.0
+ *
+ * @param array $args
+ */
+function edd_register_order_type( $type = '', $args = array() ) {
+
+	// Sanitize the type
+	$type = sanitize_key( $type );
+
+	// Parse args
+	$r = wp_parse_args( $args, array(
+		'show_ui' => true,
+		'labels'  => array(
+			'singular' => '',
+			'plural'   => ''
+		)
+	) );
+
+	// Get the
+	$component = edd_get_component( 'order' );
+
+	// Setup an empty types array
+	if ( ! isset( $component->types ) ) {
+		$component->types = array();
+	}
+
+	// Add the order type to the `types` array
+	$component->types[ $type ] = $r;
+}
+
+/**
+ * Register the default Order Types.
+ *
+ * @since 3.0
+ */
+function edd_register_default_order_types( $name = '' ) {
+
+	// Bail if not the `order` name
+	if ( 'order' !== $name ) {
+		return;
+	}
+
+	// Sales
+	edd_register_order_type( 'sale', array(
+		'labels' => array(
+			'singular' => __( 'Sale',  'easy-digital-downloads' ),
+			'plural'   => __( 'Sales', 'easy-digital-downloads' )
+		)
+	) );
+
+	// Refunds
+	edd_register_order_type( 'refund', array(
+		'labels' => array(
+			'singular' => __( 'Refund',  'easy-digital-downloads' ),
+			'plural'   => __( 'Refunds', 'easy-digital-downloads' )
+		)
+	) );
+
+	// Invoices
+	edd_register_order_type( 'invoice', array(
+		'show_ui' => false,
+		'labels'  => array(
+			'singular' => __( 'Invoice',  'easy-digital-downloads' ),
+			'plural'   => __( 'Invoices', 'easy-digital-downloads' )
+		)
+	) );
+}
+add_action( 'edd_registered_component', 'edd_register_default_order_types' );
