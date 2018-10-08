d267caadd968faead5386fe02e786169d2b51158
Chris Klosowski <cklosowski@gmail.com>
2018-10-08 13:08:50 -0700

    Reset the attachment ID to 0 if the file URL is changed after blur #6713

--- assets/js/admin-scripts.js
+++ assets/js/admin-scripts.js
@@ -427,6 +427,20 @@ jQuery(document).ready(function ($) {
 				file_frame.open();
 			});
 
+			$('#edd_download_files')
+				.on('focus', '.edd_repeatable_upload_field', function() {
+					$(this).data('originalFile', $(this).val());
+				})
+				.on('blur', '.edd_repeatable_upload_field', function() {
+					var original_file = $(this).data('originalFile');
+
+					if(original_file !== $(this).val()) {
+						$(this).parent().parent().parent().prev('.edd-file-name').find('.edd_repeatable_attachment_id_field').each(function() {
+							$(this).val(0);
+						});
+					}
+				});
+
 			var file_frame;
 			window.formfield = '';
 		},
