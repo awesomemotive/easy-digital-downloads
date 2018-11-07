7818e2248c5d4bc63d37e8a82f4018f7aef34c01
Spencer Finnell <spencerfinnell@gmail.com>
2018-11-07 14:55:23 -0500

    Don't run when not on order creation page.

--- assets/js/admin/orders/override-amounts.js
+++ assets/js/admin/orders/override-amounts.js
@@ -3,28 +3,34 @@
  */
 import EDD_Add_Order from './index.js';
 
-const toggle = document.querySelector( '.edd-override' );
+( () => {
+	const toggle = document.querySelector( '.edd-override' );
 
-/**
- * A new download has been added.
- */
-$( document ).on( 'edd-admin-add-order-download', function() {
-	toggle.disabled = false;
+	if ( ! toggle ) {
+		return;
+	}
 
-	// Update on change.
-	document.querySelectorAll( '.overridable input' ).forEach( ( el ) => el.addEventListener( 'keyup', EDD_Add_Order.update_totals ) );
+	/**
+	 * A new download has been added.
+	 */
+	$( document ).on( 'edd-admin-add-order-download', function() {
+		toggle.disabled = false;
 
-	// Update on addition.
-	return EDD_Add_Order.update_totals();
-} );
+		// Update on change.
+		document.querySelectorAll( '.overridable input' ).forEach( ( el ) => el.addEventListener( 'keyup', EDD_Add_Order.update_totals ) );
 
-/**
- * Allow edits.
- */
-toggle.addEventListener( 'click', function() {
-	// Tell future download item additions to be editable.
-	document.querySelector( 'input[name="edd-order-download-is-overrideable"]' ).value = 1;
+		// Update on addition.
+		return EDD_Add_Order.update_totals();
+	} );
 
-	// Get a fresh set of inputs. Mark current inputs as editable.
-	document.querySelectorAll( '.overridable input' ).forEach( ( el ) => el.readOnly = false );
-} );
+	/**
+	 * Allow edits.
+	 */
+	toggle.addEventListener( 'click', function() {
+		// Tell future download item additions to be editable.
+		document.querySelector( 'input[name="edd-order-download-is-overrideable"]' ).value = 1;
+
+		// Get a fresh set of inputs. Mark current inputs as editable.
+		document.querySelectorAll( '.overridable input' ).forEach( ( el ) => el.readOnly = false );
+	} );
+} ) ();
