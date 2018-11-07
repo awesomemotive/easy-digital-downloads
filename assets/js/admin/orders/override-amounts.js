a7cc4554ae38ce85064614d7897a0b88ec13990b
Spencer Finnell <spencerfinnell@gmail.com>
2018-11-07 09:23:54 -0500

    Allow edits to be made to download item amounts. See #6789

---
+++ assets/js/admin/orders/override-amounts.js
@@ -0,0 +1,30 @@
+/**
+ * Internal dependencies.
+ */
+import EDD_Add_Order from './index.js';
+
+const toggle = document.querySelector( '.edd-override' );
+
+/**
+ * A new download has been added.
+ */
+$( document ).on( 'edd-admin-add-order-download', function() {
+	toggle.disabled = false;
+
+	// Update on change.
+	document.querySelectorAll( '.overridable input' ).forEach( ( el ) => el.addEventListener( 'keyup', EDD_Add_Order.update_totals ) );
+
+	// Update on addition.
+	return EDD_Add_Order.update_totals();
+} );
+
+/**
+ * Allow edits.
+ */
+toggle.addEventListener( 'click', function() {
+	// Tell future download item additions to be editable.
+	document.querySelector( 'input[name="edd-order-download-is-overrideable"]' ).value = 1;
+
+	// Get a fresh set of inputs. Mark current inputs as editable.
+	document.querySelectorAll( '.overridable input' ).forEach( ( el ) => el.disabled = false );
+} );
