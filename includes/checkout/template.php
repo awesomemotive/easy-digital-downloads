ff871b45875e37ddd10304a0bd00bcb277fb01f2
Andrew Munro <andrew@amdrew.com>
2018-11-08 13:35:50 +1300

    Remove space on each side of slash

--- includes/checkout/template.php
+++ includes/checkout/template.php
@@ -281,7 +281,7 @@ function edd_get_cc_form() {
 			<select id="card_exp_month" name="card_exp_month" class="card-expiry-month edd-select edd-select-small required">
 				<?php for( $i = 1; $i <= 12; $i++ ) { echo '<option value="' . $i . '">' . sprintf ('%02d', $i ) . '</option>'; } ?>
 			</select>
-			<span class="exp-divider"> / </span>
+			<span class="exp-divider">/</span>
 			<select id="card_exp_year" name="card_exp_year" class="card-expiry-year edd-select edd-select-small required">
 				<?php for( $i = date('Y'); $i <= date('Y') + 30; $i++ ) { echo '<option value="' . $i . '">' . substr( $i, 2 ) . '</option>'; } ?>
 			</select>
