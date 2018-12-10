f03f90760a966b2bfb5540e47011a544c71dda8c
Chris Klosowski <cklosowski@gmail.com>
2018-12-10 10:42:35 -0700

    Ensure chart data is available before trying to plot. (#7065)

--- assets/js/admin/reports/charts/index.js
+++ assets/js/admin/reports/charts/index.js
@@ -7,21 +7,24 @@ import { render as lineChartRender } from './line.js';
 import { render as pieChartRender } from './pie.js';
 import { isPieChart } from './utils.js';
 
-// Set ChartJS defaults.
-Chart.defaults.global.pointHitDetectionRadius = 5;
-
-// Get Bootstrapped chart data.
-const { charts } = eddAdminReportsCharts;
-
-/**
- * Render the registered charts.
- */
-_.each( charts, ( config ) => {
-	const isPie = isPieChart( config );
-	
-	if ( isPieChart( config ) ) {
-		pieChartRender( config );
-	} else {
-		lineChartRender( config );
+( () => {
+	if ( _.isUndefined( window.eddAdminReportsCharts ) ) {
+		return;
 	}
-} );
+
+	// Set ChartJS defaults.
+	Chart.defaults.global.pointHitDetectionRadius = 5;
+
+	/**
+	 * Render the registered charts.
+	 */
+	_.each( window.eddAdminReportsCharts.charts, ( config ) => {
+		const isPie = isPieChart( config );
+		
+		if ( isPieChart( config ) ) {
+			pieChartRender( config );
+		} else {
+			lineChartRender( config );
+		}
+	} );
+} )();
