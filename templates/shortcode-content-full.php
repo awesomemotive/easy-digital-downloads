86f5af593e0d77f72d21334e5b60eb5fc040c289
Spencer Finnell <spencerfinnell@gmail.com>
2019-01-10 12:56:37 -0500

    Remove deprecated microdata.

--- templates/shortcode-content-full.php
+++ templates/shortcode-content-full.php
@@ -9,10 +9,8 @@
  * @author Easy Digital Downloads
  * @version 2.0.0
  */
-
-$item_prop = edd_add_schema_microdata() ? ' itemprop="description"' : '';
 ?>
 
-<div<?php echo $item_prop; ?> class="edd_download_full_content">
+<div class="edd_download_full_content">
 	<?php echo edd_download_shortcode_full_content(); ?>
 </div>
