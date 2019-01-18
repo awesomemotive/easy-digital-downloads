8d1b266a339a647ef79193d5069da4e30303590d
Spencer Finnell <spencerfinnell@gmail.com>
2019-01-18 09:52:30 -0500

    Create a customer if one does not exist.

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -865,7 +865,17 @@ function edd_process_profile_editor_updates( $data ) {
 	// Update user.
 	$updated = wp_update_user( $userdata );
 
-	// Possibly update the customer.
+	// If the current user does not have an associated customer record, create one.
+	if ( ! $customer && $updated ) {
+		$customer_id = edd_add_customer( array(
+			'user_id' => $updated,
+			'email'   => $email,
+		) );
+
+		$customer = edd_get_customer_by( 'id', $customer_id );
+	}
+
+	// Try to update customer data.
 	if ( $customer ) {
 
 		// Update the primary address.
