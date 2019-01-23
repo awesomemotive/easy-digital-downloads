746335ef295b9cbc57f375d034daf3602bcf80d0
Spencer Finnell <spencerfinnell@gmail.com>
2019-01-23 10:30:18 -0500

    Add JS for chart generation back to render() method.

--- assets/js/admin/reports/charts/index.js
+++ assets/js/admin/reports/charts/index.js
@@ -7,24 +7,17 @@ import { render as lineChartRender } from './line.js';
 import { render as pieChartRender } from './pie.js';
 import { isPieChart } from './utils.js';
 
-( () => {
-	if ( _.isUndefined( window.eddAdminReportsCharts ) ) {
-		return;
+/**
+ * Render a chart based on config.
+ *
+ * @param {Object} config Chart config.
+ */
+window.eddRenderReportChart = ( config ) => {
+	const isPie = isPieChart( config );
+
+	if ( isPieChart( config ) ) {
+		pieChartRender( config );
+	} else {
+		lineChartRender( config );
 	}
-
-	// Set ChartJS defaults.
-	Chart.defaults.global.pointHitDetectionRadius = 5;
-
-	/**
-	 * Render the registered charts.
-	 */
-	_.each( window.eddAdminReportsCharts.charts, ( config ) => {
-		const isPie = isPieChart( config );
-
-		if ( isPieChart( config ) ) {
-			pieChartRender( config );
-		} else {
-			lineChartRender( config );
-		}
-	} );
-} )();
+};
