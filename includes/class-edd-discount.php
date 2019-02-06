b80db0222ed85545c328b9de740c32b9bee4345a
Phil Johnston <johnstonphilip@gmail.com>
2019-02-06 14:56:53 -0500

    Use UTC when checking start and expiration
    #7156

--- includes/class-edd-discount.php
+++ includes/class-edd-discount.php
@@ -1441,7 +1441,7 @@ class EDD_Discount {
 		if ( $this->start ) {
 			$start_date = strtotime( $this->start );
 
-			if ( $start_date < current_time( 'timestamp' ) ) {
+			if ( $start_date < time() ) {
 				// Discount has pased the start date
 				$return = true;
 			} elseif( $set_error ) {
@@ -1480,7 +1480,7 @@ class EDD_Discount {
 
 		$expiration = strtotime( $this->expiration );
 
-		if ( $expiration < current_time( 'timestamp' ) ) {
+		if ( $expiration < time() ) {
 			if ( $update ) {
 				$this->update_status( 'inactive' );
 				$this->update_meta( 'status', 'expired' );
