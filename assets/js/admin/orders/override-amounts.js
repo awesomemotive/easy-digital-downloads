7594f76d3b31370d266fec21e0b837cff81cab46
Spencer Finnell <spencerfinnell@gmail.com>
2019-04-17 12:12:30 -0400

    Better management of disabled state for override toggle.

--- assets/js/admin/orders/override-amounts.js
+++ assets/js/admin/orders/override-amounts.js
@@ -1,3 +1,5 @@
+/* global _ */
+
 /**
  * Internal dependencies.
  */
@@ -10,25 +12,31 @@ import EDD_Add_Order from './index.js';
 		return;
 	}
 
+	const isOverrideableEl = document.querySelector( 'input[name="edd-order-download-is-overrideable"]' );
+
 	/**
 	 * A new download has been added.
 	 */
-	$( document ).on( 'edd-admin-add-order-download', function() {
-		toggle.disabled = false;
-
+	$( document ).on( 'edd-admin-add-order-download', function( response ) {
 		// Update on change.
 		document.querySelectorAll( '.overridable input' ).forEach( ( el ) => el.addEventListener( 'keyup', EDD_Add_Order.update_totals ) );
 
 		// Update on addition.
-		return EDD_Add_Order.update_totals();
+		EDD_Add_Order.update_totals();
+
+		// Keep toggle disabled if necesseary.
+		toggle.disabled = 1 == isOverrideableEl.value;
 	} );
 
 	/**
 	 * Allow edits.
 	 */
 	toggle.addEventListener( 'click', function() {
+		// Disable the button.
+		this.disabled = true;
+
 		// Tell future download item additions to be editable.
-		document.querySelector( 'input[name="edd-order-download-is-overrideable"]' ).value = 1;
+		isOverrideableEl.value = 1;
 
 		// Get a fresh set of inputs. Mark current inputs as editable.
 		document.querySelectorAll( '.overridable input' ).forEach( ( el ) => el.readOnly = false );
