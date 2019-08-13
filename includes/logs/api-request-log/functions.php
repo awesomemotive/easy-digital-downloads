743d9f57c1ade9a6764556f220486d909f0f10d9
Phil Johnston <johnstonphilip@gmail.com>
2019-08-13 16:17:16 -0400

    Re-add original file download and api request functions
    #7285

--- includes/logs/api-request-log/functions.php
+++ includes/logs/api-request-log/functions.php
@@ -12,67 +12,58 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+
 /**
- * Add an api request log.
+ * Add an API request log.
  *
  * @since 3.0
  *
  * @param array $data {
- *     Array of log data. Default empty.
+ *     Array of API request log data. Default empty.
  *
  *     The `date_created` and `date_modified` parameters do not need to be passed.
  *     They will be automatically populated if empty.
  *
- *     @type int    $object_id     Object ID that the log refers to. This would
- *                                 be an ID that corresponds to the object type
- *                                 specified. E.g. an object ID of 25 with object
- *                                 type of `order` refers to order 25 in the
- *                                 `edd_orders` table. Default empty.
- *     @type string $object_type   Object type that the log refers to.
- *                                 E.g. `discount` or `order`. Default empty.
- *     @type int    $user_id       ID of the current WordPress user logged in.
- *                                 Default 0.
- *     @type string $type          Log type. Default empty.
- *     @type string $title         Log title. Default empty.
- *     @type string $content       Log content. Default empty.
+ *     @type int $user_id          WordPress user ID of the authenticated user
+ *                                 making the API request. Default 0.
+ *     @type string $api_key       User's API key. Default empty.
+ *     @type string $token         User's API token. Default empty.
+ *     @type string $version       API version used. Default empty.
+ *     @type string $request       API request query. Default empty.
+ *     @type string $error         Error(s), if any when making the API request.
+ *                                 Default empty.
+ *     @type string $ip            IP address of the client making the API
+ *                                 request. Default empty.
+ *     @type string $time          Time it took for API request to complete. Default empty.
  *     @type string $date_created  Optional. Automatically calculated on add/edit.
- *                                 The date & time the log was inserted.
+ *                                 The date & time the API request log was inserted.
  *                                 Format: YYYY-MM-DD HH:MM:SS. Default empty.
  *     @type string $date_modified Optional. Automatically calculated on add/edit.
- *                                 The date & time the log was last modified.
- *                                 Format: YYYY-MM-DD HH:MM:SS. Default empty.
+ *                                 The date & time the API request log was last
+ *                                 modified. Format: YYYY-MM-DD HH:MM:SS. Default empty.
  * }
- * @return int|false ID of newly created log, false on error.
+ * @return int|false ID of inserted API request log, false on error.
  */
 function edd_add_api_request_log( $data = array() ) {
 
-	// An object type must be supplied for every log that is
-	// inserted into the database.
-	if ( empty( $data['object_type'] ) ) {
-		// If the object_type is set to null, check the value of the object_id.
-		if ( is_null( $data['object_type'] ) ) {
-			// If the object_id is not empty but the object_type is, fail this log attempt.
-			if ( ! empty( $data['object_id'] ) ) {
-				return false;
-			}
-			// The object_type is not null, and it is empty. No log will take place.
-		} else {
-			return false;
-		}
+	// A request is required for every API request log that is inserted into
+	// the database.
+	if ( empty( $data['request'] ) ) {
+		return false;
 	}
 
-	// Instantiate a query object.
+	// Instantiate a query object
 	$api_request_logs = new EDD\Database\Queries\Log_Api_Request();
 
 	return $api_request_logs->add_item( $data );
 }
 
 /**
- * Delete an api request log.
+ * Delete an API request log.
  *
  * @since 3.0
  *
- * @param int $api_request_log_id Log ID.
+ * @param int $api_request_log_id API request log ID.
  * @return int|false `1` if the adjustment was deleted successfully, false on error.
  */
 function edd_delete_api_request_log( $api_request_log_id = 0 ) {
@@ -82,32 +73,31 @@ function edd_delete_api_request_log( $api_request_log_id = 0 ) {
 }
 
 /**
- * Update am api request log.
+ * Update an API request log.
  *
  * @since 3.0
  *
- * @param int   $api_request_log_id Log ID.
+ * @param int   $api_request_log_id API request log ID.
  * @param array $data {
- *     Array of log data. Default empty.
+ *     Array of API request log data. Default empty.
  *
- *     @type int    $object_id     Object ID that the log refers to. This would
- *                                 be an ID that corresponds to the object type
- *                                 specified. E.g. an object ID of 25 with object
- *                                 type of `order` refers to order 25 in the
- *                                 `edd_orders` table. Default empty.
- *     @type string $object_type   Object type that the log refers to.
- *                                 E.g. `discount` or `order`. Default empty.
- *     @type int    $user_id       ID of the current WordPress user logged in.
- *                                 Default 0.
- *     @type string $type          Log type. Default empty.
- *     @type string $title         Log title. Default empty.
- *     @type string $content       Log content. Default empty.
+ *     @type int $user_id          WordPress user ID of the authenticated user
+ *                                 making the API request. Default 0.
+ *     @type string $api_key       User's API key. Default empty.
+ *     @type string $token         User's API token. Default empty.
+ *     @type string $version       API version used. Default empty.
+ *     @type string $request       API request query. Default empty.
+ *     @type string $error         Error(s), if any when making the API request.
+ *                                 Default empty.
+ *     @type string $ip            IP address of the client making the API
+ *                                 request. Default empty.
+ *     @type string $time          Time it took for API request to complete. Default empty.
  *     @type string $date_created  Optional. Automatically calculated on add/edit.
- *                                 The date & time the log was inserted.
+ *                                 The date & time the API request log was inserted.
  *                                 Format: YYYY-MM-DD HH:MM:SS. Default empty.
  *     @type string $date_modified Optional. Automatically calculated on add/edit.
- *                                 The date & time the log was last modified.
- *                                 Format: YYYY-MM-DD HH:MM:SS. Default empty.
+ *                                 The date & time the API request log was last
+ *                                 modified. Format: YYYY-MM-DD HH:MM:SS. Default empty.
  * }
  *
  * @return int|false Number of rows updated if successful, false otherwise.
@@ -119,36 +109,38 @@ function edd_update_api_request_log( $api_request_log_id = 0, $data = array() ) 
 }
 
 /**
- * Get an api request log by ID.
+ * Get an API request log by ID.
  *
  * @since 3.0
  *
- * @param int $api_request_log_id Log ID.
- * @return EDD\Api_Request_Logs\Api_Request_Log|false Log object if successful, false otherwise.
+ * @param int $api_request_log_id API request log ID.
+ * @return \EDD\Logs\Api_Request_Log|false Api_Request_Log object if successful,
+ *                                         false otherwise.
  */
 function edd_get_api_request_log( $api_request_log_id = 0 ) {
 	return edd_get_api_request_log_by( 'id', $api_request_log_id );
 }
 
 /**
- * Get a api request log by a specific field value.
+ * Get an API request log by a specific field value.
  *
  * @since 3.0
  *
  * @param string $field Database table field.
  * @param string $value Value of the row.
  *
- * @return EDD\Api_Request_Logs\Api_Request_Log|false Log object if successful, false otherwise.
+ * @return \EDD\Logs\Api_Request_Log|false Api_Request_Log object if successful,
+ *                                         false otherwise.
  */
 function edd_get_api_request_log_by( $field = '', $value = '' ) {
 	$api_request_logs = new EDD\Database\Queries\Log_Api_Request();
 
-	// Return log.
+	// Return note
 	return $api_request_logs->get_item_by( $field, $value );
 }
 
 /**
- * Query for api request logs.
+ * Query for API request logs.
  *
  * @see \EDD\Database\Queries\Log_Api_Request::__construct()
  *
@@ -156,27 +148,24 @@ function edd_get_api_request_log_by( $field = '', $value = '' ) {
  *
  * @param array $args Arguments. See `EDD\Database\Queries\Log_Api_Request` for
  *                    accepted arguments.
- * @return \EDD\Api_Request_Logs\Api_Request_Log[] Array of `Log` objects.
+ * @return \EDD\Logs\Api_Request_Log[] Array of `Api_Request_Log` objects.
  */
 function edd_get_api_request_logs( $args = array() ) {
 
-	// Parse args.
-	$r = wp_parse_args(
-		$args,
-		array(
-			'number' => 30,
-		)
-	);
+	// Parse args
+	$r = wp_parse_args( $args, array(
+		'number' => 30
+	) );
 
-	// Instantiate a query object.
+	// Instantiate a query object
 	$api_request_logs = new EDD\Database\Queries\Log_Api_Request();
 
-	// Return api request logs.
+	// Return logs
 	return $api_request_logs->query( $r );
 }
 
 /**
- * Count api request logs.
+ * Count API request logs.
  *
  * @see \EDD\Database\Queries\Log_Api_Request::__construct()
  *
@@ -184,21 +173,18 @@ function edd_get_api_request_logs( $args = array() ) {
  *
  * @param array $args Arguments. See `EDD\Database\Queries\Log_Api_Request` for
  *                    accepted arguments.
- * @return int Number of logs returned based on query arguments passed.
+ * @return int Number of API request logs returned based on query arguments passed.
  */
 function edd_count_api_request_logs( $args = array() ) {
 
-	// Parse args.
-	$r = wp_parse_args(
-		$args,
-		array(
-			'count' => true,
-		)
-	);
+	// Parse args
+	$r = wp_parse_args( $args, array(
+		'count' => true
+	) );
 
-	// Query for count(s).
+	// Query for count(s)
 	$api_request_logs = new EDD\Database\Queries\Log_Api_Request( $r );
 
-	// Return the number of api request logs that were found.
+	// Return count(s)
 	return absint( $api_request_logs->found_items );
 }
--- includes/logs/file-download-log/functions.php
+++ includes/logs/file-download-log/functions.php
@@ -18,50 +18,41 @@ defined( 'ABSPATH' ) || exit;
  * @since 3.0
  *
  * @param array $data {
- *     Array of log data. Default empty.
+ *     Array of file download log data. Default empty.
  *
  *     The `date_created` and `date_modified` parameters do not need to be passed.
  *     They will be automatically populated if empty.
  *
- *     @type int    $object_id     Object ID that the log refers to. This would
- *                                 be an ID that corresponds to the object type
- *                                 specified. E.g. an object ID of 25 with object
- *                                 type of `order` refers to order 25 in the
- *                                 `edd_orders` table. Default empty.
- *     @type string $object_type   Object type that the log refers to.
- *                                 E.g. `discount` or `order`. Default empty.
- *     @type int    $user_id       ID of the current WordPress user logged in.
+ *     @type int $product_id
+ *     @type int $file_id          File ID corresponding to the URL used
+ *                                 to download the file. Default 0.
+ *     @type int $order_id         Order ID corresponding to the URL used
+ *                                 to download the file. Default 0.
+ *     @type int $price_id         Price ID of the download for which the file
+ *                                 is being downloaded. Default 0.
+ *     @type int $customer_id      ID of the customer downloading the file.
  *                                 Default 0.
- *     @type string $type          Log type. Default empty.
- *     @type string $title         Log title. Default empty.
- *     @type string $content       Log content. Default empty.
+ *     @type string $ip            IP address of the client downloading the file.
+ *                                 Default empty.
+ *     @type string $user_agent    User agent of the client downloading the file.
+ *                                 Default empty.
  *     @type string $date_created  Optional. Automatically calculated on add/edit.
- *                                 The date & time the log was inserted.
+ *                                 The date & time the file download log was inserted.
  *                                 Format: YYYY-MM-DD HH:MM:SS. Default empty.
  *     @type string $date_modified Optional. Automatically calculated on add/edit.
- *                                 The date & time the log was last modified.
+ *                                 The date & time the file download log was last modified.
  *                                 Format: YYYY-MM-DD HH:MM:SS. Default empty.
  * }
  * @return int|false ID of newly created log, false on error.
  */
 function edd_add_file_download_log( $data = array() ) {
 
-	// An object type must be supplied for every log that is
+	// A product ID and an order ID must be supplied for every log that is
 	// inserted into the database.
-	if ( empty( $data['object_type'] ) ) {
-		// If the object_type is set to null, check the value of the object_id.
-		if ( is_null( $data['object_type'] ) ) {
-			// If the object_id is not empty but the object_type is, fail this log attempt.
-			if ( ! empty( $data['object_id'] ) ) {
-				return false;
-			}
-			// The object_type is not null, and it is empty. No log will take place.
-		} else {
-			return false;
-		}
+	if ( empty( $data['product_id'] ) || empty( $data['order_id'] ) ) {
+		return false;
 	}
 
-	// Instantiate a query object.
 	$file_download_logs = new EDD\Database\Queries\Log_File_Download();
 
 	return $file_download_logs->add_item( $data );
@@ -72,7 +63,7 @@ function edd_add_file_download_log( $data = array() ) {
  *
  * @since 3.0
  *
- * @param int $file_download_log_id Log ID.
+ * @param int $file_download_log_id File download log ID.
  * @return int|false `1` if the adjustment was deleted successfully, false on error.
  */
 function edd_delete_file_download_log( $file_download_log_id = 0 ) {
@@ -86,27 +77,28 @@ function edd_delete_file_download_log( $file_download_log_id = 0 ) {
  *
  * @since 3.0
  *
- * @param int   $file_download_log_id Log ID.
+ * @param int   $file_download_log_id File download log ID.
  * @param array $data {
- *     Array of log data. Default empty.
+ *     Array of file download log data. Default empty.
  *
- *     @type int    $object_id     Object ID that the log refers to. This would
- *                                 be an ID that corresponds to the object type
- *                                 specified. E.g. an object ID of 25 with object
- *                                 type of `order` refers to order 25 in the
- *                                 `edd_orders` table. Default empty.
- *     @type string $object_type   Object type that the log refers to.
- *                                 E.g. `discount` or `order`. Default empty.
- *     @type int    $user_id       ID of the current WordPress user logged in.
+ *     @type int $product_id
+ *     @type int $file_id          File ID corresponding to the URL used
+ *                                 to download the file. Default 0.
+ *     @type int $order_id         Order ID corresponding to the URL used
+ *                                 to download the file. Default 0.
+ *     @type int $price_id         Price ID of the download for which the file
+ *                                 is being downloaded. Default 0.
+ *     @type int $customer_id      ID of the customer downloading the file.
  *                                 Default 0.
- *     @type string $type          Log type. Default empty.
- *     @type string $title         Log title. Default empty.
- *     @type string $content       Log content. Default empty.
+ *     @type string $ip            IP address of the client downloading the file.
+ *                                 Default empty.
+ *     @type string $user_agent    User agent of the client downloading the file.
+ *                                 Default empty.
  *     @type string $date_created  Optional. Automatically calculated on add/edit.
- *                                 The date & time the log was inserted.
+ *                                 The date & time the file download log was inserted.
  *                                 Format: YYYY-MM-DD HH:MM:SS. Default empty.
  *     @type string $date_modified Optional. Automatically calculated on add/edit.
- *                                 The date & time the log was last modified.
+ *                                 The date & time the file download log was last modified.
  *                                 Format: YYYY-MM-DD HH:MM:SS. Default empty.
  * }
  *
@@ -124,26 +116,26 @@ function edd_update_file_download_log( $file_download_log_id = 0, $data = array(
  * @since 3.0
  *
  * @param int $file_download_log_id Log ID.
- * @return EDD\File_Download_Logs\File_Download_Log|false Log object if successful, false otherwise.
+ * @return \EDD\Logs\Log|false Log object if successful, false otherwise.
  */
 function edd_get_file_download_log( $file_download_log_id = 0 ) {
 	return edd_get_file_download_log_by( 'id', $file_download_log_id );
 }
 
 /**
- * Get a file download log by a specific field value.
+ * Get a file download log by field and value.
  *
  * @since 3.0
  *
  * @param string $field Database table field.
  * @param string $value Value of the row.
  *
- * @return EDD\File_Download_Logs\File_Download_Log|false Log object if successful, false otherwise.
+ * @return \EDD\Logs\Log|false Log object if successful, false otherwise.
  */
 function edd_get_file_download_log_by( $field = '', $value = '' ) {
 	$file_download_logs = new EDD\Database\Queries\Log_File_Download();
 
-	// Return log.
+	// Return file download log
 	return $file_download_logs->get_item_by( $field, $value );
 }
 
@@ -154,51 +146,45 @@ function edd_get_file_download_log_by( $field = '', $value = '' ) {
  *
  * @since 3.0
  *
- * @param array $args Arguments. See `EDD\Database\Queries\Log_File_Download` for
- *                    accepted arguments.
- * @return \EDD\File_Download_Logs\File_Download_Log[] Array of `Log` objects.
+ * @param array $args Arguments. See `EDD\Database\Queries\Log_File_Download`
+ *                    for accepted arguments.
+ * @return \EDD\Logs\File_Download_Log[] Array of `File_Download_Log` objects.
  */
 function edd_get_file_download_logs( $args = array() ) {
 
-	// Parse args.
-	$r = wp_parse_args(
-		$args,
-		array(
-			'number' => 30,
-		)
-	);
+	// Parse args
+	$r = wp_parse_args( $args, array(
+		'number' => 30
+	) );
 
-	// Instantiate a query object.
+	// Instantiate a query object
 	$file_download_logs = new EDD\Database\Queries\Log_File_Download();
 
-	// Return file download logs.
+	// Return file download logs
 	return $file_download_logs->query( $r );
 }
 
 /**
- * Count logs.
+ * Count file download logs.
  *
  * @see \EDD\Database\Queries\Log_File_Download::__construct()
  *
  * @since 3.0
  *
- * @param array $args Arguments. See `EDD\Database\Queries\Log_File_Download` for
- *                    accepted arguments.
- * @return int Number of logs returned based on query arguments passed.
+ * @param array $args Arguments. See `EDD\Database\Queries\Log_File_Download`
+ *                    for accepted arguments.
+ * @return int Number of file download logs returned based on query arguments passed.
  */
 function edd_count_file_download_logs( $args = array() ) {
 
-	// Parse args.
-	$r = wp_parse_args(
-		$args,
-		array(
-			'count' => true,
-		)
-	);
+	// Parse args
+	$r = wp_parse_args( $args, array(
+		'count' => true
+	) );
 
-	// Query for count(s).
+	// Query for count(s)
 	$file_download_logs = new EDD\Database\Queries\Log_File_Download( $r );
 
-	// Return the number of file download logs that were found.
+	// Return count(s)
 	return absint( $file_download_logs->found_items );
 }
