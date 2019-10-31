d09957ee03273e4cc888dbd96ab550487a2c626b
Phil Johnston <johnstonphilip@gmail.com>
2019-10-31 12:24:49 -0400

    Add method prefixes
    #7397

--- includes/database/queries/class-order.php
+++ includes/database/queries/class-order.php
@@ -180,6 +180,71 @@ class Order extends Query {
 		parent::__construct( $query );
 	}
 
+	/**
+	 * Set up the filter callback to add the country and region from the order addresses table.
+	 *
+	 * @since 3.0
+	 * @access public
+	 *
+	 * @param string|array $query {
+	 *     Optional. Array or query string of order query parameters. Default empty.
+	 *
+	 *     @type int          $id                   An order ID to only return that order. Default empty.
+	 *     @type array        $id__in               Array of order IDs to include. Default empty.
+	 *     @type array        $id__not_in           Array of order IDs to exclude. Default empty.
+	 *     @type string       $order_number         An order number to only return that order. Default empty.
+	 *     @type array        $order_number__in     Array of order numbers to include. Default empty.
+	 *     @type array        $order_number__not_in Array of order numbers to exclude. Default empty.
+	 *     @type string       $status               An order status to only return that order. Default empty.
+	 *     @type array        $status__in           Array of order statuses to include. Default empty.
+	 *     @type array        $status__not_in       Array of order statuses to exclude. Default empty.
+	 *     @type string       $type                 An order type to only return that order. Default empty.
+	 *     @type array        $type__in             Array of order types to include. Default empty.
+	 *     @type array        $type__not_in         Array of order types to exclude. Default empty.
+	 *     @type int          $user_id              A user ID to only return that object. Default empty.
+	 *     @type array        $user_id__in          Array of user IDs to include. Default empty.
+	 *     @type array        $user_id__not_in      Array of user IDs to exclude. Default empty.
+	 *     @type int          $customer_id          A customer ID to only return that object. Default empty.
+	 *     @type array        $customer_id__in      Array of customer IDs to include. Default empty.
+	 *     @type array        $customer_id__not_in  Array of customer IDs to exclude. Default empty.
+	 *     @type string       $email                Limit results to those affiliated with a given email. Default empty.
+	 *     @type array        $email__in            Array of email to include affiliated orders for. Default empty.
+	 *     @type array        $email__not_in        Array of email to exclude affiliated orders for. Default empty.
+	 *     @type string       $gateway              Limit results to those affiliated with a given gateway. Default empty.
+	 *     @type array        $gateway__in          Array of gateways to include affiliated orders for. Default empty.
+	 *     @type array        $gateway__not_in      Array of gateways to exclude affiliated orders for. Default empty.
+	 *     @type string       $mode                 Limit results to those affiliated with a given mode. Default empty.
+	 *     @type array        $mode__in             Array of modes to include affiliated orders for. Default empty.
+	 *     @type array        $mode__not_in         Array of modes to exclude affiliated orders for. Default empty.
+	 *     @type string       $currency             Limit results to those affiliated with a given currency. Default empty.
+	 *     @type array        $currency__in         Array of currencies to include affiliated orders for. Default empty.
+	 *     @type array        $currency__not_in     Array of currencies to exclude affiliated orders for. Default empty.
+	 *     @type string       $payment_key          Limit results to those affiliated with a given payment key. Default empty.
+	 *     @type array        $payment_key__in      Array of payment keys to include affiliated orders for. Default empty.
+	 *     @type array        $payment_key__not_in  Array of payment keys to exclude affiliated orders for. Default empty.
+	 *     @type array        $date_query           Query all datetime columns together. See WP_Date_Query.
+	 *     @type array        $date_created_query   Date query clauses to limit orders by. See WP_Date_Query.
+	 *                                              Default null.
+	 *     @type array        $date_completed_query Date query clauses to limit orders by. See WP_Date_Query.
+	 *                                              Default null.
+	 *     @type bool         $count                Whether to return a order count (true) or array of order objects.
+	 *                                              Default false.
+	 *     @type string       $fields               Item fields to return. Accepts any column known names
+	 *                                              or empty (returns an array of complete order objects). Default empty.
+	 *     @type int          $number               Limit number of orders to retrieve. Default 100.
+	 *     @type int          $offset               Number of orders to offset the query. Used to build LIMIT clause.
+	 *                                              Default 0.
+	 *     @type bool         $no_found_rows        Whether to disable the `SQL_CALC_FOUND_ROWS` query. Default true.
+	 *     @type string|array $orderby              Accepts 'id', 'number', 'status', 'user_id', 'customer_id', 'email', 'gateway'
+	 *                                              'payment_key', 'date_created', 'date_completed', 'user_id__in', 'customer_id__in'.
+	 *                                              'email__in', 'gateway__in', 'payment_key__in'.
+	 *                                              Also accepts false, an empty array, or 'none' to disable `ORDER BY` clause.
+	 *                                              Default 'id'.
+	 *     @type string       $order                How to order retrieved orders. Accepts 'ASC', 'DESC'. Default 'DESC'.
+	 *     @type string       $search               Search term(s) to retrieve matching orders for. Default empty.
+	 *     @type bool         $update_cache         Whether to prime the cache for found orders. Default false.
+	 * }
+	 */
 	public function query( $query ) {
 		if ( ! empty( $query['country'] ) ) {
 			add_filter( 'edd_orders_query_clauses', array( $this, 'query_by_country' ) );
@@ -194,6 +259,14 @@ class Order extends Query {
 		return $result;
 	}
 
+	/**
+	 * Filter the query clasue to add the country and region from the order addresses table.
+	 *
+	 * @since 3.0
+	 * @access public
+	 *
+	 * @param string|array $clauses The clauses which will generate the final SQL query.
+	 */
 	public function query_by_country( $clauses ) {
 
 		$primary_alias  = $this->table_alias;
@@ -234,6 +307,12 @@ class Order extends Query {
 		return $clauses;
 	}
 
+	/**
+	 * Set the query var defaults for country and region.
+	 *
+	 * @since 3.0
+	 * @access public
+	 */
 	protected function set_query_var_defaults() {
 		parent::set_query_var_defaults();
 
