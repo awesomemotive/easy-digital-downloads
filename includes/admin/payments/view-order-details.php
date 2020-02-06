56bedcb2539a2613b77f289a2a808e0b90bbbe89
Spencer Finnell <spencerfinnell@gmail.com>
2020-02-06 13:56:56 -0500

    Remove "Viewing Refund" notice on Order Details

--- includes/admin/payments/view-order-details.php
+++ includes/admin/payments/view-order-details.php
@@ -39,20 +39,6 @@ if ( empty( $order ) ) {
 
 		<hr class="wp-header-end">
 
-		<?php if ( 'refund' === $order->type ) : ?>
-			<div class="notice notice-info">
-				<p>
-					<?php
-					printf(
-						__( 'You are viewing a refund order. To view the original order, %sclick here%s.', 'easy-digital-downloads' ),
-						'<a href="' . admin_url( 'edit.php?post_type=download&page=edd-payment-history&view=view-order-details&id=' . $order->parent ) . '">',
-						'</a>'
-					);
-					?>
-				</p>
-			</div>
-		<?php endif; ?>
-
 		<?php do_action( 'edd_view_order_details_before', $order->id ); ?>
 
 		<?php do_action( 'edd_view_order_details_form_top', $order->id ); ?>
