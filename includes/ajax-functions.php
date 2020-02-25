0586c52db03499489b98072be22addb9e0d85d3c
Spencer Finnell <spencerfinnell@gmail.com>
2020-02-25 10:34:40 -0500

    Strict tax rate check

--- includes/ajax-functions.php
+++ includes/ajax-functions.php
@@ -1417,7 +1417,7 @@ function edd_admin_order_get_item_amounts() {
 	$use_taxes = edd_use_taxes();
 	$tax_rate  = edd_get_tax_rate( $country, $region, $fallback = false );
 
-	if ( true === $use_taxes && 0 !== floatval( $tax_rate ) ) {
+	if ( true === $use_taxes && floatval( 0 ) !== floatval( $tax_rate ) ) {
 		$tax = edd_calculate_tax( floatval( $subtotal ), $country, $region );
 	} else { 
 		$tax = 0;
