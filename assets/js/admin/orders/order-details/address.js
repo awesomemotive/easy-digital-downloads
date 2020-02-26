ecc7b179587a30b67bbfee1e0040c89c7f10ade9
Spencer Finnell <spencerfinnell@gmail.com>
2020-02-26 13:28:08 -0500

    Refactor (quickly/hacky) Address changing to trigger events for Order Overview

--- assets/js/admin/orders/order-details/address.js
+++ assets/js/admin/orders/order-details/address.js
@@ -11,36 +11,6 @@ let CUSTOMER_SEARCH_RESULTS = {};
 
 jQueryReady( () => {
 
-	// Update base state field based on selected base country
-	$( 'select[name="edd-payment-address[0][country]"]' ).change( function() {
-		const select = $( this ),
-			data = {
-				action: 'edd_get_shop_states',
-				country: select.val(),
-				nonce: select.data( 'nonce' ),
-				field_name: 'edd-payment-address[0][region]',
-			};
-
-		$.post( ajaxurl, data, function( response ) {
-			const state_wrapper = $( '#edd-order-address-state-wrap select, #edd-order-address-state-wrap input' );
-
-			// Remove any chosen containers here too
-			$( '#edd-order-address-state-wrap .chosen-container' ).remove();
-
-			if ( 'nostates' === response ) {
-				state_wrapper.replaceWith( '<input type="text" name="edd-payment-address[0][region]" value="" class="edd-edit-toggles medium-text"/>' );
-			} else {
-				state_wrapper.replaceWith( response );
-				$( '#edd-order-address-state-wrap select' ).each( function() {
-					const el = $( this );
-					el.chosen( getChosenVars( el ) );
-				} );
-			}
-		} );
-
-		return false;
-	} );
-
 	$( '.edd-payment-change-customer-input' ).on( 'change', function() {
 		const $this = $( this ),
 			data = {
@@ -75,59 +45,100 @@ jQueryReady( () => {
 		return false;
 	} );
 
+	/**
+	 * Retrieves a list of states based on a Country HTML <select>.
+	 *
+	 * @since 3.0
+	 *
+	 * @param {HTMLElement} countryEl Element containing country information.
+	 * @param {string} fieldName the name of the field to use in response.
+	 * @return {$.promise} Region data response.
+	 */
+	function getStates( countryEl, fieldName ) {
+		const data = {
+			action: 'edd_get_shop_states',
+			country: countryEl.val(),
+			nonce: countryEl.data( 'nonce' ),
+			field_name: fieldName,
+		};
+
+		return $.post( ajaxurl, data );
+	}
+
+	/**
+	 * Replaces the Region area with the appropriate field type.
+	 *
+	 * @todo This is hacky and blindly picks elements from the DOM.
+	 *
+	 * @since 3.0
+	 *
+	 * @param {string} regions Regions response.
+	 */
+	function replaceRegionField( regions ) {
+		const state_wrapper = $( '#edd-order-address-state-wrap select, #edd-order-address-state-wrap input' );
+
+		// Remove any chosen containers here too
+		$( '#edd-order-address-state-wrap .chosen-container' ).remove();
+
+		if ( 'nostates' === regions ) {
+			state_wrapper
+				.replaceWith( '<input type="text" name="edd_order_address[region]" value="" class="wide-fat" style="max-width: none; width: 100%;" />' );
+		} else {
+			state_wrapper.replaceWith( regions );
+
+			$( '#edd-order-address-state-wrap select' ).each( function() {
+				const el = $( this );
+				el.chosen( getChosenVars( el ) );
+			} );
+		}
+	}
+
+	/**
+	 * Handles replacing a Region field when a Country field changes.
+	 *
+	 * @since 3.0
+	 */
+	function updateRegionFieldOnChange() {
+		getStates( $( this ), 'edd_order_address[region]' ).done( replaceRegionField );
+	}
+
 	$( document.body ).on( 'change', '.customer-address-select-wrap .add-order-customer-address-select', function() {
 		const $this = $( this ),
 			val = $this.val(),
-			select = $( '#edd-add-order-form select#edd_order_address_country' ),
 			address = CUSTOMER_SEARCH_RESULTS.addresses[ val ];
 
 		$( '#edd-add-order-form input[name="edd_order_address[address]"]' ).val( address.address );
 		$( '#edd-add-order-form input[name="edd_order_address[address2]"]' ).val( address.address2 );
 		$( '#edd-add-order-form input[name="edd_order_address[postal_code]"]' ).val( address.postal_code );
 		$( '#edd-add-order-form input[name="edd_order_address[city]"]' ).val( address.city );
-		select.val( address.country ).trigger( 'chosen:updated' );
 		$( '#edd-add-order-form input[name="edd_order_address[address_id]"]' ).val( val );
 
-		const data = {
-			action: 'edd_get_shop_states',
-			country: select.val(),
-			nonce: $( '.add-order-customer-address-select' ).data( 'nonce' ),
-			field_name: 'edd_order_address_region',
-		};
+		// Remove global `change` event handling to prevent loop.
+		$( '.edd-order-address-country' ).off( 'change', updateRegionFieldOnChange );
 
-		$.post( ajaxurl, data, function( response ) {
-			$( 'select#edd_order_address_region' ).find( 'option:gt(0)' ).remove();
+		// Set Country.
+		$( '#edd-add-order-form select#edd_order_address_country' )
+			.val( address.country )
+			.trigger( 'change' )
+			.trigger( 'chosen:updated' );
 
-			if ( 'nostates' !== response ) {
-				$( response ).find( 'option:gt(0)' ).appendTo( 'select#edd_order_address_region' );
-			}
+		// Set Region.
+		getStates( $( '#edd-add-order-form select#edd_order_address_country' ), 'edd_order_address[region]' )
+			.done( replaceRegionField )
+			.done( ( response ) => {
+				$( '[name="edd_order_address[region]"]' )
+					.val( address.region )
+					.trigger( 'change' )
+					.trigger( 'chosen:updated' );
+			} );
 
-			$( 'select#edd_order_address_region' ).trigger( 'chosen:updated' );
-			$( 'select#edd_order_address_region' ).val( address.region ).trigger( 'chosen:updated' );
-		} );
+		// Add back global `change` event handling.
+		$( '.edd-order-address-country' ).on( 'change', updateRegionFieldOnChange );
 
 		return false;
 	} );
 
 	// Country change.
-	$( '.edd-order-address-country' ).on( 'change', function() {
-		const select = $( this ),
-			data = {
-				action: 'edd_get_shop_states',
-				country: select.val(),
-				nonce: select.data( 'nonce' ),
-				field_name: 'edd-order-address-country',
-			};
-
-		$.post( ajaxurl, data, function( response ) {
-			$( 'select.edd-order-address-region' ).find( 'option:gt(0)' ).remove();
-
-			if ( 'nostates' !== response ) {
-				$( response ).find( 'option:gt(0)' ).appendTo( 'select.edd-order-address-region' );
-			}
-
-			$( 'select.edd-order-address-region' ).trigger( 'chosen:updated' );
-		} );
-	} );
+	$( '.edd-order-address-country' ).on( 'change', updateRegionFieldOnChange );
 
 } );
