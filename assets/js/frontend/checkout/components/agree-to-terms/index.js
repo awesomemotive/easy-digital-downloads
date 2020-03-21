cf1ffb57cd3076e855163606d48945095e9b2113
Spencer Finnell <spencerfinnell@gmail.com>
2020-03-20 20:32:26 -0400

    issue/7631 - Move "Agree to Terms" Toggle to External Script (#7655)
    * Move "Agree to terms" toggle to an external script
    
    * Rebuild assets
    
    * Adjust _doing_it_wrong notice
    
    * Move deprecated function

---
+++ assets/js/frontend/checkout/components/agree-to-terms/index.js
@@ -0,0 +1,29 @@
+/* global $ */
+
+/**
+ * Internal dependencies.
+ */
+import { jQueryReady } from 'utils/jquery.js';
+
+/**
+ * DOM ready.
+ *
+ * @since 3.0
+ */
+jQueryReady( () => {
+	/**
+	 * Toggles term content when clicked.
+	 *
+	 * @since unknown
+	 *
+	 * @param {Object} e Click event.
+	 */
+	$( document.body ).on( 'click', '.edd_terms_links', function( e ) {
+		e.preventDefault();
+
+		const terms = $( this ).parent();
+
+		terms.prev( '.edd-terms' ).slideToggle();
+		terms.find( '.edd_terms_links' ).toggle();
+	} );
+} );
