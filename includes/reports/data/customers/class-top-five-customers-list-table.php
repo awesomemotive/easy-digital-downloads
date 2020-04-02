bbe4d19c9f1ca7c973e96e93cc977e9c221147fd
Robin Cornett <robincornett@users.noreply.github.com>
2020-04-02 09:46:16 -0400

    Change type check to strict
    Co-Authored-By: Spencer Finnell <spencerfinnell@gmail.com>

--- includes/reports/data/customers/class-top-five-customers-list-table.php
+++ includes/reports/data/customers/class-top-five-customers-list-table.php
@@ -38,7 +38,7 @@ class Top_Five_Customers_List_Table extends \EDD_Customer_Reports_Table {
 		$data  = array();
 		$taxes = Reports\get_taxes_excluded_filter();
 
-		if ( ! $taxes ) {
+		if ( false === $taxes ) {
 			$args = array(
 				'number'  => 5,
 				'order'   => 'DESC',
