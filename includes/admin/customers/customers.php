37ca9acc59dfe32f357c10f241d1266a80a4ee10
Lisa Cee <LisaCee@users.noreply.github.com>
2020-04-13 13:19:08 -0700

    Update includes/admin/customers/customers.php
    Co-Authored-By: Spencer Finnell <spencerfinnell@gmail.com>

--- includes/admin/customers/customers.php
+++ includes/admin/customers/customers.php
@@ -800,7 +800,7 @@ function edd_customers_view( $customer = null ) {
 						<div class="add-customer-email-wrapper">
 							<input type="hidden" name="customer-id" value="<?php echo esc_attr( $customer->id ); ?>" />
 							<?php wp_nonce_field( 'edd-add-customer-email', 'add_email_nonce', false, true ); ?>
-							<label class="screen-reader-text" for="additional-email"><?php esc_html_e( 'Email Address', 'easy-ditigal-downloads' ); ?></label>
+							<label class="screen-reader-text" for="additional-email"><?php esc_html_e( 'Email Address', 'easy-digital-downloads' ); ?></label>
 							<input type="email" name="additional-email" id="additional-email" value="" placeholder="<?php esc_html_e( 'Email Address', 'easy-digital-downloads' ); ?>" />&nbsp;
 							<input type="checkbox" name="make-additional-primary" value="1" id="make-additional-primary" />&nbsp;<label for="make-additional-primary"><?php esc_html_e( 'Make Primary', 'easy-digital-downloads' ); ?></label>
 						</div>
