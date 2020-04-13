7e184ebace8fb860f59b958608b3cd279ca439be
Spencer Finnell <spencerfinnell@gmail.com>
2020-04-13 09:35:18 -0400

    Add failling test case

--- tests/reports/tests-reports-functions.php
+++ tests/reports/tests-reports-functions.php
@@ -426,6 +426,29 @@ class Reports_Functions_Tests extends \EDD_UnitTestCase {
 	 * @covers \EDD\Reports\parse_dates_for_range()
 	 * @group edd_dates
 	 */
+	public function test_parse_dates_for_range_with_overflow_last_month_range_should_return_those_dates() {
+		$overflow_day  = '2020-03-30';
+		$overflow_date = EDD()->utils->date( $overflow_day );
+
+		$expected = array(
+			'start' => $overflow_date->copy()->subMonthNoOverflow( 1 )->startOfMonth()->toDateTimeString(),
+			'end'   => $overflow_date->copy()->subMonthNoOverflow( 1 )->endOfMonth()->toDateTimeString(),
+			'range' => 'last_month',
+		);
+
+		$result = parse_dates_for_range( 'last_month', $overflow_day );
+
+		// Explicitly strip seconds in case the test is slow.
+		$expected = $this->strip_seconds( $expected );
+		$result   = $this->strip_seconds( $this->objects_to_date_strings( $result ) );
+
+		$this->assertEqualSetsWithIndex( $expected, $result );
+	}
+
+	/**
+	 * @covers \EDD\Reports\parse_dates_for_range()
+	 * @group edd_dates
+	 */
 	public function test_parse_dates_for_range_with_today_range_should_return_those_dates() {
 		$expected = array(
 			'start' => self::$date->copy()->setTimezone( edd_get_timezone_id() )->startOfDay()->setTimezone( 'UTC' ),
