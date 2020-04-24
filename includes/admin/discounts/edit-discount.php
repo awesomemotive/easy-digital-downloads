b5731a9d0b65d0b70ba7892ff9746a23580efe16
Robin Cornett <hello@robincornett.com>
2020-04-24 13:49:01 -0400

    Update product dropdown to show variations for discounts
    Fixes variations not showing for adding/editing discounts, per
    https://github.com/easydigitaldownloads/easy-digital-downloads/issues/7551#issuecomment-608110115
    
    Fixes #7551

--- includes/admin/discounts/edit-discount.php
+++ includes/admin/discounts/edit-discount.php
@@ -124,6 +124,7 @@ $minutes              = edd_get_minute_values();
 							'selected'    => $product_requirements,
 							'multiple'    => true,
 							'chosen'      => true,
+							'variations'  => true,
 							'placeholder' => sprintf( __( 'Select %s', 'easy-digital-downloads' ), edd_get_label_plural() )
 						) ); ?>
                         <div id="edd-discount-product-conditions"<?php echo $condition_display; ?>>
@@ -160,7 +161,8 @@ $minutes              = edd_get_minute_values();
                             'id'          => 'excluded_products',
                             'selected'    => $excluded_products,
                             'multiple'    => true,
-                            'chosen'      => true,
+							'chosen'      => true,
+							'variations'  => true,
                             'placeholder' => sprintf( __( 'Select %s', 'easy-digital-downloads' ), edd_get_label_plural() )
                         ) ); ?>
                         <p class="description"><?php printf( __( '%s this discount cannot be applied to. Leave blank for none.', 'easy-digital-downloads' ), edd_get_label_plural() ); ?></p>
