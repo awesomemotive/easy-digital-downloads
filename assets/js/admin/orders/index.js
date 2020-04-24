1ef5cdf0b95fa82416cf7e6fb6df290644b03dcb
Spencer Finnell <spencerfinnell@gmail.com>
2020-04-24 11:41:13 -0400

    Disable "Create Order" if there are no Order Items

--- assets/js/admin/orders/index.js
+++ assets/js/admin/orders/index.js
@@ -1,12 +1,34 @@
 /**
  * Internal dependencies
  */
-import './order-overview';
+import OrderOverview from './order-overview';
 import './order-details';
-
 import { jQueryReady } from 'utils/jquery.js';
 
 jQueryReady( () => {
+	// Order Overview.
+	if ( window.eddAdminOrderOverview ) {
+		OrderOverview.render();
+
+		/**
+		 * Enable "Create Order" button when an Order has items.
+		 *
+		 * @since 3.0
+		 */
+		( () => {
+			const createButton = document.getElementById( 'edd-order-submit' );
+
+			if ( ! createButton ) {
+				return;
+			}
+
+			const items = OrderOverview.options.state.get( 'items' );
+
+			items.on( 'add remove', function() {
+				createButton.disabled = items.length === 0;
+			} );
+		} )();
+	}
 
 	// Move `.update-nag` items below the top header.
 	// `#update-nag` is legacy styling, which core still supports.
@@ -16,30 +38,5 @@ jQueryReady( () => {
 	if ( 0 !== $( '.edit-post-editor-regions__header' ).length ) {
 		$( 'div.update-nag, div#update-nag' ).insertAfter( $( '.edit-post-editor-regions__header' ) );
 	}
-
-	// "Validate" order form before submitting.
-	// @todo move somewhere else?
-	$( '#edd-add-order-form' ).on( 'submit', function() {
-		$( '#publishing-action .spinner' ).css( 'visibility', 'visible' );
-		$( '#edd-order-submit' ).prop( 'disabled', true );
-
-		if ( $( '.orderitems tr.no-items' ).is( ':visible' ) ) {
-			$( '#edd-add-order-no-items-error' ).slideDown();
-		} else {
-			$( '#edd-add-order-no-items-error' ).slideUp();
-		}
-
-		if ( $( '.order-customer-info' ).is( ':visible' ) && '0' === $( '#customer_id' ).val() ) {
-			$( '#edd-add-order-customer-error' ).slideDown();
-		} else {
-			$( '#edd-add-order-customer-error' ).slideUp();
-		}
-
-		if ( $( '.notice:not(.updated)' ).is( ':visible' ) ) {
-			$( '#publishing-action .spinner' ).css( 'visibility', 'hidden' );
-			$( '#edd-order-submit' ).prop( 'disabled', false );
-			return false;
-		}
-	} );
 
 } );
