fd8387c94d7b01f1066f823db386787fec9694e5
LisaCee <lisacanini@sandhillsdev.com>
2020-05-15 15:32:16 -0700

    registered jquery-ui-css style

--- includes/scripts.php
+++ includes/scripts.php
@@ -82,6 +82,7 @@ function edd_register_styles() {
 		$url = trailingslashit( edd_get_templates_url() ) . $file;
 	}
 
+	wp_register_style( 'jquery-ui-css' );
 	wp_register_style( 'edd-styles', $url, array(), $version, 'all' );
 }
 add_action( 'init', 'edd_register_styles' );
