99896d61d096bf479c518961f16b94dcb402a8a7
Robin Cornett <hello@robincornett.com>
2020-05-18 11:21:43 -0400

    Fix graph/tick values for refunds
    #7907

--- includes/admin/reporting/reports-callbacks.php
+++ includes/admin/reporting/reports-callbacks.php
@@ -178,22 +178,22 @@ function edd_overview_refunds_chart() {
 			if ( $hour_by_hour ) {
 				// If the date of this db value matches the date on this line graph/chart, set the y axis value for the chart to the number in the DB result.
 				if ( $date_of_db_value->format( 'Y-m-d H' ) === $date_on_chart->format( 'Y-m-d H' ) ) {
-					$number[ $timestamp ][1] = $result->number;
-					$amount[ $timestamp ][1] = abs( $result->amount );
+					$number[ $timestamp ][1] += $result->number;
+					$amount[ $timestamp ][1] += abs( $result->amount );
 				}
 				// Add any refunds that happened during this day.
 			} elseif ( $day_by_day ) {
 				// If the date of this db value matches the date on this line graph/chart, set the y axis value for the chart to the number in the DB result.
 				if ( $date_of_db_value->format( 'Y-m-d' ) === $date_on_chart->format( 'Y-m-d' ) ) {
-					$number[ $timestamp ][1] = $result->number;
-					$amount[ $timestamp ][1] = abs( $result->amount );
+					$number[ $timestamp ][1] += $result->number;
+					$amount[ $timestamp ][1] += abs( $result->amount );
 				}
 				// Add any refunds that happened during this month.
 			} else {
 				// If the date of this db value matches the date on this line graph/chart, set the y axis value for the chart to the number in the DB result.
 				if ( $date_of_db_value->format( 'Y-m' ) === $date_on_chart->format( 'Y-m' ) ) {
-					$number[ $timestamp ][1] = $result->number;
-					$amount[ $timestamp ][1] = abs( $result->amount );
+					$number[ $timestamp ][1] += $result->number;
+					$amount[ $timestamp ][1] += abs( $result->amount );
 				}
 			}
 		}
