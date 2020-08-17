3effb216fbccb40d5ccb10c173e5f5d4e86b53ec
Chris Klosowski <chris@sandhillsdev.com>
2020-08-17 15:39:12 -0700

    Fix W3 validation issues caused by dupe `id` atts (#8086)
    * Fix W3 validation issues caused by dupe `id` atts
    
    Also, sanitize `$price_id` before printing.
    
    Use Price ID to differentiate the options. ![](https://i.gravityview.co/c9QjDO+)
    
    * Adding more esc_attr calls #7661
    
    Co-authored-by: Zack Katz <zack@katz.co>

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -213,7 +213,7 @@ function edd_get_purchase_link( $args = array() ) {
 
 		<input type="hidden" name="download_id" value="<?php echo esc_attr( $download->ID ); ?>">
 		<?php if ( $variable_pricing && isset( $price_id ) && isset( $prices[$price_id] ) ): ?>
-			<input type="hidden" name="edd_options[price_id][]" id="edd_price_option_<?php echo $download->ID; ?>_1" class="edd_price_option_<?php echo $download->ID; ?>" value="<?php echo $price_id; ?>">
+			<input type="hidden" name="edd_options[price_id][]" id="edd_price_option_<?php echo esc_attr( $download->ID ); ?>_<?php echo esc_attr( $price_id ); ?>" class="edd_price_option_<?php echo esc_attr( $download->ID ); ?>" value="<?php echo esc_attr( $price_id ); ?>">
 		<?php endif; ?>
 		<?php if( ! empty( $args['direct'] ) && ! $download->is_free( $args['price_id'] ) ) { ?>
 			<input type="hidden" name="edd_action" class="edd_action_input" value="straight_to_gateway">
