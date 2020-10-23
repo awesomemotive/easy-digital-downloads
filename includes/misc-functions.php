11302fc262ac4651adf32eb7411e36218748c844
LisaCee <lisacanini@sandhillsdev.com>
2020-10-23 09:59:58 -0700

    Add function to convert month number to full name #8112

--- includes/misc-functions.php
+++ includes/misc-functions.php
@@ -531,13 +531,29 @@ function edd_get_currency_name( $code = 'USD' ) {
  *
  * @since 1.0
  *
- * @param integer $n
+ * @param integer $n The number of the month.
  * @return string Short month name
  */
 function edd_month_num_to_name( $n ) {
 	$timestamp = mktime( 0, 0, 0, $n, 1, 2005 );
 
-	return date_i18n( "M", $timestamp );
+	return date_i18n( 'M', $timestamp );
+}
+
+/**
+ * Month Num To Long Name
+ *
+ * Takes a month number and returns the name full name of it.
+ *
+ * @since 3.0
+ *
+ * @param integer $n The number of the month.
+ * @return string Long month name
+ */
+function edd_month_num_to_long_name( $n ) {
+	$timestamp = mktime( 0, 0, 0, $n, 1, 2005 );
+
+	return date_i18n( 'F', $timestamp );
 }
 
 /**
