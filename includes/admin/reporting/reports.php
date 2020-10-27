2d7c14453d3857461ee776016354f3732e07bc39
LisaCee <lisacanini@sandhillsdev.com>
2020-10-27 14:44:19 -0700

    Unique ids for date dropdowns and (hopefully) update tests, fix label for taxonomy #8112

--- includes/admin/reporting/reports.php
+++ includes/admin/reporting/reports.php
@@ -2558,19 +2558,19 @@ function display_export_report() {
 						<p><?php esc_html_e( 'Download a CSV giving a detailed look into earnings over time.', 'easy-digital-downloads' ); ?></p>
 						<form id="edd-export-earnings" class="edd-export-form edd-import-export-form" method="post">
 							<div class="edd-to-and-from-container">
-							<label for="start_month" class="screen-reader-text"><?php esc_html_e( 'Start Month', 'easy-digital-downloads' ); ?></label>
-								<?php echo EDD()->html->month_dropdown( 'start_month' ); ?>
-							<label for="start_year" class="screen-reader-text"><?php esc_html_e( 'Start Year', 'easy-digital-downloads' ); ?></label>
-								<?php echo EDD()->html->year_dropdown( 'start_year' ); ?>
+							<label for="edd_export_earnings_start_month" class="screen-reader-text"><?php esc_html_e( 'Start Month', 'easy-digital-downloads' ); ?></label>
+								<?php echo EDD()->html->month_dropdown( 'edd_export_earnings', 'start_month' ); ?>
+							<label for="edd_export_earnings_start_year" class="screen-reader-text"><?php esc_html_e( 'Start Year', 'easy-digital-downloads' ); ?></label>
+								<?php echo EDD()->html->year_dropdown( 'edd_export_earnings', 'start_year' ); ?>
 							</div>
 
 							<span class="edd-to-and-from--separator"><?php echo _x( '&mdash; to &mdash;', 'Date one to date two', 'easy-digital-downloads' ); ?></span>
 
 							<div class="edd-to-and-from-container">
-							<label for="end_month" class="screen-reader-text"><?php esc_html_e( 'End Month', 'easy-digital-downloads' ); ?></label>
-								<?php echo EDD()->html->month_dropdown( 'end_month' ); ?>
-							<label for="end_year" class="screen-reader-text"><?php esc_html_e( 'End Year', 'easy-digital-downloads' ); ?></label>
-								<?php echo EDD()->html->year_dropdown( 'end_year' ); ?>
+							<label for="edd_export_earnings_end_month" class="screen-reader-text"><?php esc_html_e( 'End Month', 'easy-digital-downloads' ); ?></label>
+								<?php echo EDD()->html->month_dropdown( 'edd_export_earnings', 'end_month' ); ?>
+							<label for="edd_export_earnings_end_year" class="screen-reader-text"><?php esc_html_e( 'End Year', 'easy-digital-downloads' ); ?></label>
+								<?php echo EDD()->html->year_dropdown( 'edd_export_earnings', 'end_year' ); ?>
 							</div>
 							<?php wp_nonce_field( 'edd_ajax_export', 'edd_ajax_export' ); ?>
 							<input type="hidden" name="edd-export-class" value="EDD_Batch_Earnings_Report_Export"/>
@@ -2801,7 +2801,7 @@ function display_export_report() {
 								}
 							}
 							?>
-							<label for="taxonomy" class="screen-reader-text"><?php esc_html_e( 'Select Taxonomy', 'easy-digital-downloads' ); ?></label>
+							<label for="edd_export_taxonomy" class="screen-reader-text"><?php esc_html_e( 'Select Taxonomy', 'easy-digital-downloads' ); ?></label>
 								<?php
 								echo EDD()->html->select(
 									array(
