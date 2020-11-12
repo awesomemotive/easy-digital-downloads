b5a5e592fa4fafac171709c66c683245feb19b58
Ashley Gibson <ashley@nosegraze.com>
2020-11-12 11:06:22 +0000

    Finish up JavaScript & ajax logic #6880

--- assets/js/admin/upgrades/v3/index.js
+++ assets/js/admin/upgrades/v3/index.js
@@ -1,25 +1,61 @@
 const EDD_v3_Upgrades = {
+	inProgress: false,
+
 	init: function() {
 		// Listen for toggle on the checkbox.
-		$( 'edd-v3-migration-confirmation' ).on( 'change', ( e ) => {
-			if ( this.checked ) {
-				$( '#edd-v3-migration-button' ).removeClass( 'disabled' ).prop( 'disabled', false );
+		$( '.edd-v3-migration-confirmation' ).on( 'change', function( e ) {
+			const wrapperForm = $( this ).closest( '.edd-v3-migration' );
+			const formSubmit = wrapperForm.find( 'button' );
+
+			if ( e.target.checked ) {
+				formSubmit.removeClass( 'disabled' ).prop( 'disabled', false );
 			} else {
-				$( '#edd-v3-migration-button' ).addClass( 'disabled' ).prop( 'disabled', 'disabled' );
+				formSubmit.addClass( 'disabled' ).prop( 'disabled', true );
 			}
 		} );
 
-		$( '#edd-v3-migration' ).on( 'submit', ( e ) => {
+		$( '.edd-v3-migration' ).on( 'submit', function( e ) {
 			e.preventDefault();
 
-			EDD_v3_Upgrades.processStep( false, 1 );
+			if ( EDD_v3_Upgrades.inProgress ) {
+				return;
+			}
+
+			EDD_v3_Upgrades.inProgress = true;
+
+			const migrationForm = $( this );
+			const upgradeKeyField = migrationForm.find( 'input[name="upgrade_key"]' );
+			let upgradeKey = false;
+
+			if ( upgradeKeyField.length && upgradeKeyField.val() ) {
+				upgradeKey = upgradeKeyField.val();
+			}
+
+			// Disable submit button.
+			migrationForm.find( 'button' )
+				.removeClass( 'button-primary' )
+				.addClass( 'button-secondary disabled updating-message' )
+				.prop( 'disabled', true );
+
+			// Disable checkbox.
+			migrationForm.find( 'input' ).prop( 'disabled', true );
+
+			// If this is the main migration, mark the first non-complete item as in progress.
+			if ( 'edd-v3-migration' === migrationForm.attr( 'id' ) ) {
+				const firstNonCompleteUpgrade = $( '#edd-migration-progress li:not(.edd-upgrade-complete)' );
+				if ( firstNonCompleteUpgrade.length ) {
+					EDD_v3_Upgrades.markUpgradeInProgress( firstNonCompleteUpgrade.data( 'upgrade' ) );
+				}
+			}
+
+			EDD_v3_Upgrades.processStep( upgradeKey, 1, migrationForm.find( 'input[name="_wpnonce"]' ).val() );
 		} )
 	},
 
-	processStep: function( upgrade_key, step ) {
+	processStep: function( upgrade_key, step, nonce ) {
 		let data = {
 			action: 'edd_process_v3_upgrade',
-			_nonce: $( '#edd-v3-migration' ).find( '#_wpnonce' ).val(),
+			_ajax_nonce: nonce,
 			upgrade_key: upgrade_key,
 			step: step
 		}
@@ -30,17 +66,112 @@ const EDD_v3_Upgrades = {
 			url: ajaxurl,
 			success: function( response ) {
 				if ( response.data.upgrade_completed ) {
-					EDD_v3_Upgrades.markUpgradeComplete( upgrade_key );
+					EDD_v3_Upgrades.markUpgradeComplete( response.data.upgrade_processed );
+
+					// If we just completed legacy data removal then we're all done!
+					if ( 'v30_legacy_data_removed' === response.data.upgrade_processed ) {
+						EDD_v3_Upgrades.legacyDataRemovalComplete();
+
+						return;
+					}
+				} else if( response.data.percentage ) {
+					// Update percentage for the upgrade we just processed.
+					EDD_v3_Upgrades.updateUpgradePercentage( response.data.upgrade_processed, response.data.percentage );
 				}
 
-				EDD_v3_Upgrades.processStep( response.data.next_upgrade, response.data.next_step );
+				if ( response.data.next_upgrade && 'v30_legacy_data_removed' === response.data.next_upgrade ) {
+					EDD_v3_Upgrades.inProgress = false;
+
+					// Legacy data removal is next, which we do not start automatically.
+					EDD_v3_Upgrades.showLegacyDataRemoval();
+				} else if ( response.data.next_upgrade ) {
+					// If this is a different upgrade, mark the next one as in process.
+					if ( response.data.next_upgrade !== response.data.upgrade_processed ) {
+						EDD_v3_Upgrades.markUpgradeInProgress( response.data.next_upgrade );
+					}
+
+					// Start the next upgrade (or continuation of current) automatically.
+					EDD_v3_Upgrades.processStep( response.data.next_upgrade, response.data.next_step, response.data.nonce );
+				} else {
+					EDD_v3_Upgrades.inProgress = false;
+					EDD_v3_Upgrades.stopAllSpinners();
+				}
 			}
 		} ).fail( ( data ) => {
 			// @todo
 		} )
 	},
 
-	markUpgradeComplete: function( upgrade_key ) {
+	markUpgradeInProgress: function( upgradeKey ) {
+		const upgradeRow = $( '#edd-v3-migration-' + upgradeKey );
+		if ( ! upgradeRow.length ) {
+			return;
+		}
+
+		const statusIcon = upgradeRow.find( '.dashicons' );
+		if ( ! statusIcon.length ) {
+			return;
+		}
+
+		statusIcon.removeClass( 'dashicons-ellipsis' ).addClass( 'dashicons-update' );
+
+		upgradeRow.find( '.edd-migration-percentage' ).removeClass( 'edd-hidden' );
+	},
+
+	updateUpgradePercentage: function( upgradeKey, newPercentage ) {
+		const upgradeRow = $( '#edd-v3-migration-' + upgradeKey );
+		if ( ! upgradeRow.length ) {
+			return;
+		}
+
+		upgradeRow.find( '.edd-migration-percentage-value' ).text( newPercentage );
+	},
+
+	markUpgradeComplete: function( upgradeKey ) {
+		const upgradeRow = $( '#edd-v3-migration-' + upgradeKey );
+		if ( ! upgradeRow.length ) {
+			return;
+		}
+
+		upgradeRow.addClass( 'edd-upgrade-complete' );
+
+		const statusIcon = upgradeRow.find( '.dashicons' );
+		if ( ! statusIcon.length ) {
+			return;
+		}
+
+		statusIcon.removeClass( 'dashicons-ellipsis' ).addClass( 'dashicons-yes' );
+
+		// Update percentage to 100%;
+		upgradeRow.find( '.edd-migration-percentage-value' ).text( 100 );
+	},
+
+	showLegacyDataRemoval: function() {
+		// Un-spin the main submit button.
+		$( '#edd-v3-migration-button' ).removeClass( 'updating-message' );
+
+		// Show the "migration complete" message.
+		$( '#edd-v3-migration-complete' ).removeClass( 'edd-hidden' );
+
+		const dataRemovalWrapper = $( '#edd-v3-remove-legacy-data' );
+		if ( ! dataRemovalWrapper.length ) {
+			return;
+		}
+
+		dataRemovalWrapper.removeClass( 'edd-hidden' );
+	},
+
+	legacyDataRemovalComplete: function() {
+		const wrapper = $( '#edd-v3-remove-legacy-data' );
+		if ( ! wrapper.length ) {
+			return;
+		}
+
+		wrapper.find( 'form' ).addClass( 'edd-hidden' );
+		wrapper.find( '#edd-v3-legacy-data-removal-complete' ).removeClass( 'edd-hidden' );
+	},
+
+	stopAllSpinners: function() {
 
 	}
 }
--- includes/admin/upgrades/v3/upgrade-actions.php
+++ includes/admin/upgrades/v3/upgrade-actions.php
@@ -15,6 +15,8 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Handles the 3.0 upgrade process.
  *
+ * This loops through all upgrades that have not yet been completed, and steps through each process.
+ *
  * @since 3.0
  * @return void
  */
@@ -24,17 +26,35 @@ function edd_process_v3_upgrade() {
 	check_ajax_referer( 'edd_process_v3_upgrade' );
 
 	$all_upgrades = edd_get_v30_upgrades();
+	$upgrade_keys = array_keys( $all_upgrades );
 
 	$upgrade_key = ! empty( $_POST['upgrade_key'] ) && array_key_exists( $_POST['upgrade_key'], $all_upgrades )
 		? $_POST['upgrade_key']
-		: reset( $all_upgrades ); // First item in list.
+		: reset( $upgrade_keys ); // First item in list.
 
-	$step       = ! empty( $_POST['step'] ) ? absint( $_POST['step'] ) : 1;
+	if ( ! array_key_exists( $upgrade_key, $all_upgrades ) ) {
+		wp_send_json_error( __( 'This is not a valid 3.0 upgrade.', 'easy-digital-downloads' ) );
+	}
+
+	$step = ! empty( $_POST['step'] ) ? absint( $_POST['step'] ) : 1;
+
+	// If we have a step already saved, use that instead.
+	$saved_step = get_option( sprintf( 'edd_v3_migration_%s_step', sanitize_key( $upgrade_key ) ) );
+	if ( ! empty( $saved_step ) ) {
+		$step = absint( $saved_step );
+	}
+
 	$class_name = $all_upgrades[ $upgrade_key ]['class'];
 
 	// Load the required classes.
 	require_once EDD_PLUGIN_DIR . 'includes/admin/reporting/export/class-batch-export.php';
 	do_action( 'edd_batch_export_class_include', $class_name );
+
+	if ( ! class_exists( $class_name ) ) {
+		wp_send_json_error( __( 'Error loading migration class.', 'easy-digital-downloads' ) );
+	}
+
+	error_log( $class_name );                                                                                                                                                                                                                                                                                                                                                                                                                   // @todo remove                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     // @t remove
 
 	/** @var \EDD_Batch_Export $export */
 	$export = new $class_name( $step );
@@ -43,32 +63,38 @@ function edd_process_v3_upgrade() {
 		wp_die( -1, 403, array( 'response' => 403 ) );
 	}
 
-	$was_processed       = $export->process_step();
+	//$was_processed       = $export->process_step();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    // @todo remove
+	$was_processed       = false; // @todo remove                                                                                                                                                                                                                                                                                                                                                                                                       // @todo remove
 	$percentage_complete = $export->get_percentage_complete();
+	$percentage_complete = 100; // @todo remove
+
+	// Build some shared args.
+	$response_args = array(
+		'upgrade_processed' => $upgrade_key,
+		'nonce'             => wp_create_nonce( 'edd_process_v3_upgrade' )
+	);
 
 	if ( $was_processed ) {
-		wp_send_json_success( array(
+		// Data was processed, which means we'll want to repeat this upgrade again next time.
+		wp_send_json_success( wp_parse_args( array(
 			'upgrade_completed' => false,
 			'next_step'         => $step + 1,
 			'next_upgrade'      => $upgrade_key,
-			'percentage'        => $percentage_complete
-		) );
+			'percentage'        => $percentage_complete,
+		), $response_args ) );
 	} else {
-		// Figure out which upgrades are still left to do.
-		$upgrade_keys       = array_keys( $all_upgrades );
+		// No data was processed, which means it's time to move on to the next upgrade.
+
+		// Figure out which upgrade is next.
 		$remaining_upgrades = array_slice( $upgrade_keys, array_search( $upgrade_key, $upgrade_keys ) + 1 );
+		$next_upgrade       = ! empty( $remaining_upgrades ) ? reset( $remaining_upgrades ) : false;
 
-		// Remove `v30_legacy_data_removed` because this is handled separately.
-		unset( $remaining_upgrades['v30_legacy_data_removed'] );
-
-		$next_upgrade = reset( $remaining_upgrades );
-
-		wp_send_json_success( array(
+		wp_send_json_success( wp_parse_args( array(
 			'upgrade_completed' => true,
 			'next_step'         => 1,
 			'next_upgrade'      => $next_upgrade,
 			'percentage'        => 0
-		) );
+		), $response_args ) );
 	}
 
 }
