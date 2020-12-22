896772fd92308eb2795db0fe77755e499907c137
Ashley Gibson <ashley@nosegraze.com>
2020-12-22 13:50:24 +0000

    Remove unused credits functions #8075

--- includes/class-easy-digital-downloads.php
+++ includes/class-easy-digital-downloads.php
@@ -625,7 +625,6 @@ final class Easy_Digital_Downloads {
 		require_once EDD_PLUGIN_DIR . 'includes/orders/functions/actions.php';
 		require_once EDD_PLUGIN_DIR . 'includes/orders/functions/meta.php';
 		require_once EDD_PLUGIN_DIR . 'includes/orders/functions/items.php';
-		require_once EDD_PLUGIN_DIR . 'includes/orders/functions/credits.php';
 		require_once EDD_PLUGIN_DIR . 'includes/orders/functions/refunds.php';
 		require_once EDD_PLUGIN_DIR . 'includes/orders/functions/discounts.php';
 		require_once EDD_PLUGIN_DIR . 'includes/orders/functions/addresses.php';
