724ca2e18561a9b14380f1ffe4c66902d0fe71c6
Jakub Mikita <jakub@bracketspace.com>
2020-12-29 19:05:32 +0100

    Fix convert_object_to_array() method
    This prevents from "PHP Warning: Invalid argument supplied for foreach() in .../EDD_SL_Plugin_Updater.php on line 356"
    
    Probably will be a better idea to decode JSON directly to the array than converting it here

--- includes/EDD_SL_Plugin_Updater.php
+++ includes/EDD_SL_Plugin_Updater.php
@@ -353,7 +353,7 @@ class EDD_SL_Plugin_Updater {
 	 */
 	private function convert_object_to_array( $data ) {
 		$new_data = array();
-		foreach ( $data as $key => $value ) {
+		foreach ( (array) $data as $key => $value ) {
 			$new_data[ $key ] = is_object( $value ) ? $this->convert_object_to_array( $value ) : $value;
 		}
 
