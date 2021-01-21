e6396eaffa9baa2966f1527c75391e960b42722d
LisaCee <lisacanini@sandhillsdev.com>
2021-01-21 10:34:34 -0800

    #8301 Update php version requirements

--- easy-digital-downloads.php
+++ easy-digital-downloads.php
@@ -64,7 +64,7 @@ final class EDD_Requirements_Check {
 
 		// PHP
 		'php' => array(
-			'minimum' => '5.3.0',
+			'minimum' => '5.6',
 			'name'    => 'PHP',
 			'exists'  => true,
 			'current' => false,
