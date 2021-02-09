07c67200824fccd9ca21986b02cb3774952ffc76
Robin Cornett <hello@robincornett.com>
2021-02-09 14:02:36 -0500

    Fix object type not being correctly assigned
    #8345

--- includes/admin/views/tmpl-order-adjustment.php
+++ includes/admin/views/tmpl-order-adjustment.php
@@ -35,13 +35,15 @@ $view_url = edd_get_admin_url(
 				<?php esc_html_e( 'Order Fee', 'easy-digital-downloads' ); ?>
 			<# } #>
 
-			<# if ( '' !== data.description ) { #>
+			<# if ( '' !== data.description || false !== data.orderItem ) { #>
 				<br />
 				<small>
 					<# if ( false !== data.orderItem ) { #>
 						{{ data.orderItem.productName }}:&nbsp;
 					<# } #>
-					{{ data.description }}
+					<# if ( '' !== data.description ) { #>
+						{{ data.description }}
+					<# } #>
 				</small>
 			<# } #>
 		</div>
