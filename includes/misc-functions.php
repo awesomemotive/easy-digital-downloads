9de8e74e508959087579a438107e45e28a7fc87c
Robin Cornett <hello@robincornett.com>
2021-03-03 11:25:30 -0500

    Remove check for wp_doing_cron (4.8)
    #8443

--- includes/misc-functions.php
+++ includes/misc-functions.php
@@ -1243,12 +1243,8 @@ function edd_payment_get_ip_address_url( $order_id ) {
  */
 function edd_doing_cron() {
 
-	// Bail if not doing WordPress cron (>4.8.0)
-	if ( function_exists( 'wp_doing_cron' ) && wp_doing_cron() ) {
-		return true;
-
-	// Bail if not doing WordPress cron (<4.8.0)
-	} elseif ( defined( 'DOING_CRON' ) && ( true === DOING_CRON ) ) {
+	// Bail if doing WordPress cron.
+	if ( wp_doing_cron() ) {
 		return true;
 	}
 
