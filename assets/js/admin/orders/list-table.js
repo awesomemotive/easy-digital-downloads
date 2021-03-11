f504b17edf9d384eefcd094cdd00d52ca2f3f36c
LisaCee <lisacanini@sandhillsdev.com>
2021-03-11 13:18:12 -0800

    #8026 Update JS with reverted script key

--- assets/js/admin/orders/list-table.js
+++ assets/js/admin/orders/list-table.js
@@ -8,7 +8,7 @@ import { jQueryReady } from 'utils/jquery.js';
 jQueryReady( () => {
 
 	$( '.download_page_edd-payment-history .row-actions .delete a' ).on( 'click', function() {
-		if( confirm( edd_vars.delete_order ) ) {
+		if( confirm( edd_vars.delete_payment ) ) {
 			return true;
 		}
 		return false;
