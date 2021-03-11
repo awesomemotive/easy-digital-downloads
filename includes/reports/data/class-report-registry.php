a4093e9bb54cb86520e87d7da679adc461b7f2a9
Robin Cornett <hello@robincornett.com>
2021-03-11 10:39:12 -0500

    Allow reports to have no filters
    #8468

--- includes/reports/data/class-report-registry.php
+++ includes/reports/data/class-report-registry.php
@@ -119,7 +119,7 @@ class Report_Registry extends Reports\Registry implements Utils\Static_Registry 
 		$attributes = array_merge( $defaults, $attributes );
 
 		try {
-			$this->validate_attributes( $attributes, $report_id );
+			$this->validate_attributes( $attributes, $report_id, array( 'filters' ) );
 		} catch ( \EDD_Exception $exception ) {
 			$error = true;
 
