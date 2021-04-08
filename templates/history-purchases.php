9e24b4fa52c54c7a6013bc44a8e25518eac72fbd
Robin Cornett <hello@robincornett.com>
2021-04-08 13:35:31 -0400

    Update purchase_history template
    #8539

--- templates/history-purchases.php
+++ templates/history-purchases.php
@@ -8,64 +8,82 @@
  * @since 3.0 Allow details link to appear for `partially_refunded` orders.
  */
 
-if( ! empty( $_GET['edd-verify-success'] ) ) : ?>
-<p class="edd-account-verified edd_success">
-	<?php _e( 'Your account has been successfully verified!', 'easy-digital-downloads' ); ?>
-</p>
-<?php
+if ( ! empty( $_GET['edd-verify-success'] ) ) : ?>
+	<p class="edd-account-verified edd_success">
+		<?php esc_html_e( 'Your account has been successfully verified!', 'easy-digital-downloads' ); ?>
+	</p>
+	<?php
 endif;
 /**
  * This template is used to display the purchase history of the current user.
  */
-if ( is_user_logged_in() ):
+if ( ! is_user_logged_in() ) {
+	return;
+}
+$orders = edd_get_orders(
+	array(
+		'user_id' => get_current_user_id(),
+		'number'  => 20,
+		'type'    => 'sale',
+	)
+);
+if ( $orders ) :
 	$payments = edd_get_users_purchases( get_current_user_id(), 20, true, 'any' );
-	if ( $payments ) :
-		do_action( 'edd_before_purchase_history', $payments ); ?>
-		<table id="edd_user_history" class="edd-table">
-			<thead>
-				<tr class="edd_purchase_row">
-					<?php do_action('edd_purchase_history_header_before'); ?>
-					<th class="edd_purchase_id"><?php _e('ID','easy-digital-downloads' ); ?></th>
-					<th class="edd_purchase_date"><?php _e('Date','easy-digital-downloads' ); ?></th>
-					<th class="edd_purchase_amount"><?php _e('Amount','easy-digital-downloads' ); ?></th>
-					<th class="edd_purchase_details"><?php _e('Details','easy-digital-downloads' ); ?></th>
-					<?php do_action('edd_purchase_history_header_after'); ?>
-				</tr>
-			</thead>
-			<?php foreach ( $payments as $payment ) : ?>
-				<?php $payment = new EDD_Payment( $payment->ID ); ?>
-				<tr class="edd_purchase_row">
-					<?php do_action( 'edd_purchase_history_row_start', $payment->ID, $payment->payment_meta ); ?>
-					<td class="edd_purchase_id">#<?php echo $payment->number ?></td>
-					<td class="edd_purchase_date"><?php echo edd_date_i18n( $payment->date ); ?></td>
-					<td class="edd_purchase_amount">
-						<span class="edd_purchase_amount"><?php echo edd_currency_filter( edd_format_amount( $payment->total ) ); ?></span>
-					</td>
-					<td class="edd_purchase_details">
-						<?php if( ! in_array( $payment->status, array( 'complete', 'partially_refunded' ) ) ) : ?>
-							<span class="edd_purchase_status <?php echo $payment->status; ?>"><?php echo $payment->status_nicename; ?></span>
-							<?php if ( $payment->is_recoverable() ) : ?>
-								&mdash; <a href="<?php echo $payment->get_recovery_url(); ?>"><?php _e( 'Complete Purchase', 'easy-digital-downloads' ); ?></a>
-							<?php endif; ?>
-						<?php else: ?>
-							<a href="<?php echo esc_url( add_query_arg( 'payment_key', $payment->key, edd_get_success_page_uri() ) ); ?>"><?php _e( 'View Details and Downloads', 'easy-digital-downloads' ); ?></a>
-						<?php endif; ?>
-					</td>
-					<?php do_action( 'edd_purchase_history_row_end', $payment->ID, $payment->payment_meta ); ?>
-				</tr>
-			<?php endforeach; ?>
-		</table>
-		<?php
-			echo edd_pagination(
-				array(
-					'type'  => 'purchase_history',
-					'total' => ceil( edd_count_purchases_of_customer() / 20 ) // 20 items per page
-				)
-			);
-		?>
-		<?php do_action( 'edd_after_purchase_history', $payments ); ?>
-		<?php wp_reset_postdata(); ?>
-	<?php else : ?>
-		<p class="edd-no-purchases"><?php _e('You have not made any purchases','easy-digital-downloads' ); ?></p>
-	<?php endif;
+	do_action( 'edd_before_purchase_history', $payments );
+	?>
+	<table id="edd_user_history" class="edd-table">
+		<thead>
+			<tr class="edd_purchase_row">
+				<?php do_action( 'edd_purchase_history_header_before' ); ?>
+				<th class="edd_purchase_id"><?php esc_html_e( 'ID', 'easy-digital-downloads' ); ?></th>
+				<th class="edd_purchase_date"><?php esc_html_e( 'Date', 'easy-digital-downloads' ); ?></th>
+				<th class="edd_purchase_amount"><?php esc_html_e( 'Amount', 'easy-digital-downloads' ); ?></th>
+				<th class="edd_purchase_details"><?php esc_html_e( 'Details', 'easy-digital-downloads' ); ?></th>
+				<?php do_action( 'edd_purchase_history_header_after' ); ?>
+			</tr>
+		</thead>
+		<?php foreach ( $orders as $order ) : ?>
+			<?php $payment = new EDD_Payment( $order->id ); ?>
+			<tr class="edd_purchase_row">
+				<?php do_action( 'edd_purchase_history_row_start', $order->id, $payment->payment_meta ); ?>
+				<td class="edd_purchase_id">#<?php echo esc_html( $order->get_number() ); ?></td>
+				<td class="edd_purchase_date"><?php echo esc_html( edd_date_i18n( EDD()->utils->date( $order->date_created, null, true )->toDateTimeString() ) ); ?></td>
+				<td class="edd_purchase_amount">
+					<span class="edd_purchase_amount"><?php echo esc_html( edd_currency_filter( edd_format_amount( $order->total ) ) ); ?></span>
+				</td>
+				<td class="edd_purchase_details">
+					<?php
+					$link_text = ! in_array( $order->status, array( 'complete', 'partially_refunded' ), true ) ? __( 'View Details', 'easy-digital-downloads' ) : __( 'View Details and Downloads', 'easy-digital-downloads' );
+					?>
+					<a href="<?php echo esc_url( add_query_arg( 'payment_key', $order->payment_key, edd_get_success_page_uri() ) ); ?>"><?php echo esc_html( $link_text ); ?></a>
+					<?php if ( ! in_array( $order->status, array( 'complete' ), true ) ) : ?>
+						| <span class="edd_purchase_status <?php echo esc_attr( $order->status ); ?>"><?php echo esc_html( edd_get_status_label( $order->status ) ); ?></span>
+					<?php endif; ?>
+					<?php if ( $payment->is_recoverable() ) : ?>
+						&mdash; <a href="<?php echo esc_url( $payment->get_recovery_url() ); ?>"><?php esc_html_e( 'Complete Purchase', 'easy-digital-downloads' ); ?></a>
+					<?php endif; ?>
+				</td>
+				<?php do_action( 'edd_purchase_history_row_end', $order->id, $payment->payment_meta ); ?>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+	<?php
+	$count = edd_count_orders(
+		array(
+			'user_id' => get_current_user_id(),
+			'type'    => 'sale',
+		)
+	);
+	echo edd_pagination(
+		array(
+			'type'  => 'purchase_history',
+			'total' => ceil( $count / 20 ), // 20 items per page
+		)
+	);
+	?>
+	<?php do_action( 'edd_after_purchase_history', $payments ); ?>
+	<?php wp_reset_postdata(); ?>
+<?php else : ?>
+	<p class="edd-no-purchases"><?php esc_html_e( 'You have not made any purchases', 'easy-digital-downloads' ); ?></p>
+	<?php
 endif;
