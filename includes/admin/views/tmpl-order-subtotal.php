fcdfb2ac9b78885634d02d49d02a78e9eaf74947
Spencer Finnell <spencerfinnell@gmail.com>
2021-04-22 07:58:03 -0400

    Update includes/admin/views/tmpl-order-subtotal.php
    Co-authored-by: Ashley Gibson <ashley@nosegraze.com>

--- includes/admin/views/tmpl-order-subtotal.php
+++ includes/admin/views/tmpl-order-subtotal.php
@@ -4,7 +4,7 @@
  *
  * @package     EDD
  * @subpackage  Admin/Views
- * @copyright   Copyright (c) 2020, Sandhills Development, LLC
+ * @copyright   Copyright (c) 2021, Sandhills Development, LLC
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       3.0
  */
