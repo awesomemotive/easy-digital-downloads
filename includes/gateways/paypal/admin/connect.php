132f8a58806379e5640c06f82228a1725d923abd
Ashley Gibson <ashley@nosegraze.com>
2021-06-08 11:22:34 +0100

    Set return URL to PayPal settings page.

--- includes/gateways/paypal/admin/connect.php
+++ includes/gateways/paypal/admin/connect.php
@@ -89,7 +89,7 @@ function process_connect() {
 		),
 		'body'    => json_encode( array(
 			'mode'       => edd_is_test_mode() ? 'sandbox' : 'live',
-			'return_url' => admin_url()
+			'return_url' => admin_url( 'edit.php?post_type=download&page=edd-settings&tab=gateways&section=paypal_commerce' )
 		) )
 	) );
 
