27d242391023d99dffbaec6a94efcf4c482af935
Ashley Gibson <ashley@nosegraze.com>
2021-06-30 10:17:36 +0100

    Apply count_where() to ensure date range is respected #8727

--- includes/payments/class-payment-stats.php
+++ includes/payments/class-payment-stats.php
@@ -55,6 +55,9 @@ class EDD_Payment_Stats extends EDD_Stats {
 		}
 
 		if ( empty( $download_id ) ) {
+			// Global sale stats
+			add_filter( 'edd_count_payments_where', array( $this, 'count_where' ) );
+
 			if ( is_array( $status ) ) {
 				$count = 0;
 				foreach ( $status as $payment_status ) {
@@ -63,6 +66,8 @@ class EDD_Payment_Stats extends EDD_Stats {
 			} else {
 				$count = edd_count_payments()->$status;
 			}
+
+			remove_filter( 'edd_count_payments_where', array( $this, 'count_where' ) );
 		} else {
 			$this->timestamp = false;
 
@@ -146,8 +151,6 @@ class EDD_Payment_Stats extends EDD_Stats {
 		}
 
 		if ( empty( $download_id ) ) {
-			$statuses = array( 'complete', 'publish', 'revoked', 'refunded', 'partially_refunded' );
-
 			/**
 			 * Filters Order statuses that should be included when calculating stats.
 			 *
@@ -155,7 +158,7 @@ class EDD_Payment_Stats extends EDD_Stats {
 			 *
 			 * @param array $statuses Order statuses to include when generating stats.
 			 */
-			$statuses = apply_filters( 'edd_payment_stats_post_statuses', $statuses );
+			$statuses = apply_filters( 'edd_payment_stats_post_statuses', edd_get_gross_order_statuses() );
 
 			// Global earning stats
 			$args = array(
