1d60601260411a1a27cde8439967f1f3a26dbdee
Ashley Gibson <ashley@nosegraze.com>
2021-07-28 13:55:50 +0100

    Add hook after file log migration.

--- includes/admin/upgrades/v3/class-data-migrator.php
+++ includes/admin/upgrades/v3/class-data-migrator.php
@@ -263,6 +263,17 @@ class Data_Migrator {
 			$meta_to_migrate   = $post_meta;
 			$new_log_id        = edd_add_file_download_log( $log_data );
 			$add_meta_function = 'edd_add_file_download_log_meta';
+
+			/**
+			 * Triggers after a file download log has been migrated.
+			 *
+			 * @since 3.0
+			 *
+			 * @param int    $new_log_id ID of the newly created log.
+			 * @param object $data       Data from the posts table. (Essentially a `WP_Post`, without being that object.)
+			 * @param array  $post_meta  All meta associated with this log.
+			 */
+			do_action( 'edd_30_migrate_file_download_log', $new_log_id, $data, $post_meta );
 		} elseif ( 'api_request' === $data->slug ) {
 			$meta = $wpdb->get_results( $wpdb->prepare( "SELECT meta_key, meta_value FROM {$wpdb->postmeta} WHERE post_id = %d", absint( $data->ID ) ) );
 
