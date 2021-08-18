ac8b2b6e73090a75812826db05182512eca064f9
Ashley Gibson <ashley@nosegraze.com>
2021-08-18 12:48:44 +0100

    Make downgrade perform automatically #8805

--- includes/admin/upgrades/downgrades.php
+++ includes/admin/upgrades/downgrades.php
@@ -29,11 +29,9 @@ function edd_do_downgrade() {
 			( $downgraded_from && version_compare( $downgraded_from, '3.0-beta1', '>=' ) )
 		) {
 			/*
-			 * This site probably just downgraded from EDD 3.0. Let's store a flag so we can give them the option to properly roll back.
-			 * @see edd_show_downgrade_notices()
+			 * This site probably just downgraded from EDD 3.0. Let's perform a downgrade.
 			 */
-			update_option( 'edd_v3_downgrade', time() );
-			$did_downgrade = true;
+			$did_downgrade = edd_maybe_downgrade_from_v3();
 		}
 	}
 
@@ -47,105 +45,30 @@ function edd_do_downgrade() {
 add_action( 'admin_init', 'edd_do_downgrade' );
 
 /**
- * Display downgrade notices.
+ * Performs a database downgrade from EDD 3.0 to 2.11 if one is needed.
+ * The main operation here is changing the customer meta column from `edd_customer_id` (v3.0 version)
+ * back to `customer_id` for v2.x.
  *
  * @since 2.11
+ * @return bool Whether the downgrade was performed.
  */
-function edd_show_downgrade_notices() {
-	if ( ! current_user_can( 'manage_options' ) ) {
-		return;
-	}
-
-	/**
-	 * EDD 3.0 downgrade.
-	 */
-	if ( get_option( 'edd_v3_downgrade' ) ) {
-		$downgrade_url = wp_nonce_url( add_query_arg( 'edd_action', 'downgrade_v3' ), 'edd_downgrade_v3' );
-		$dismiss_url   = wp_nonce_url( add_query_arg( 'edd_action', 'dismiss_v3_downgrade' ), 'edd_dismiss_v3_downgrade' );
-		?>
-		<div class="notice notice-warning">
-			<h2><?php esc_html_e( 'Did you downgrade from Easy Digital Downloads 3.0?', 'easy-digital-downloads' ); ?></h2>
-			<p>
-				<?php _e( 'We\'ve detected that your site may have just downgraded from Easy Digital Downloads version 3.0. If that is correct, please click the "Complete Downgrade" button below to complete this process.', 'easy-digital-downloads' ); ?>
-			</p>
-			<p>
-				<?php esc_html_e( 'If you believe this message to be in error, please click "Dismiss Notice" instead, which will remove this notice with no further action.', 'easy-digital-downloads' ); ?>
-			</p>
-			<p>
-				<a href="<?php echo esc_url( $downgrade_url ); ?>" class="button button-primary"><?php esc_html_e( 'Complete Downgrade', 'easy-digital-downloads' ); ?></a>
-				<a href="<?php echo esc_url( $dismiss_url ); ?>" class="button"><?php esc_html_e( 'Dismiss Notice', 'easy-digital-downloads' ); ?></a>
-			</p>
-		</div>
-		<?php
-	}
-}
-
-add_action( 'admin_notices', 'edd_show_downgrade_notices' );
-
-/**
- * Handles the downgrade from EDD 3.0.
- *
- * @since 2.11
- */
-add_action( 'edd_downgrade_v3', function () {
-	if ( empty( $_GET['_wpnonce'] ) || ! wp_verify_nonce( $_GET['_wpnonce'], 'edd_downgrade_v3' ) ) {
-		wp_die(
-			__( 'You do not have permission to perform this action.', 'easy-digital-downloads' ),
-			__( 'Error', 'easy-digital-downloads' ), array( 'response' => 403 )
-		);
-	}
-
-	if ( ! current_user_can( 'manage_options' ) ) {
-		wp_die(
-			__( 'You do not have permission to perform this action.', 'easy-digital-downloads' ),
-			__( 'Error', 'easy-digital-downloads' ), array( 'response' => 403 )
-		);
-	}
-
-	if ( ! get_option( 'edd_v3_downgrade' ) ) {
-		wp_die(
-			__( 'Unexpected rollback operation.', 'easy-digital-downloads' ),
-			__( 'Error', 'easy-digital-downloads' ), array( 'response' => 400 )
-		);
-	}
-
+function edd_maybe_downgrade_from_v3() {
 	global $wpdb;
 	$customer_meta_table = EDD()->customer_meta->table_name;
+
+	// If there is no column called `edd_customer_id`, then we don't need to downgrade.
+	$columns = $wpdb->query( "SHOW COLUMNS FROM {$customer_meta_table} LIKE 'edd_customer_id'");
+	if ( empty( $columns ) ) {
+		return false;
+	}
+
 	$wpdb->query( "ALTER TABLE {$customer_meta_table} CHANGE `edd_customer_id` `customer_id` bigint(20) unsigned NOT NULL default '0'" );
 	$wpdb->query( "ALTER TABLE {$customer_meta_table} DROP INDEX edd_customer_id" );
 	$wpdb->query( "ALTER TABLE {$customer_meta_table} ADD INDEX customer_id (customer_id)" );
-	EDD()->customer_meta->create_table(); // This re-adds the version number for us.
 
+	// These two calls re-add the table version numbers for us.
+	EDD()->customer_meta->create_table();
 	EDD()->customers->create_table();
 
-	delete_option( 'edd_v3_downgrade' );
-
-	wp_safe_redirect( remove_query_arg( array( '_wpnonce', 'edd_action' ) ) );
-	exit;
-} );
-
-/**
- * Handles dismissing the downgrade notice.
- *
- * @since 2.11
- */
-add_action( 'edd_dismiss_v3_downgrade', function () {
-	if ( empty( $_GET['_wpnonce'] ) || ! wp_verify_nonce( $_GET['_wpnonce'], 'edd_dismiss_v3_downgrade' ) ) {
-		wp_die(
-			__( 'You do not have permission to perform this action.', 'easy-digital-downloads' ),
-			__( 'Error', 'easy-digital-downloads' ), array( 'response' => 403 )
-		);
-	}
-
-	if ( ! current_user_can( 'manage_options' ) ) {
-		wp_die(
-			__( 'You do not have permission to perform this action.', 'easy-digital-downloads' ),
-			__( 'Error', 'easy-digital-downloads' ), array( 'response' => 403 )
-		);
-	}
-
-	delete_option( 'edd_v3_downgrade' );
-
-	wp_safe_redirect( remove_query_arg( array( '_wpnonce', 'edd_action' ) ) );
-	exit;
-} );
+	return true;
+}
