d3945f7814db3396bbd1df0b8ad8ff29684eb963
Robin Cornett <hello@robincornett.com>
2021-10-20 08:18:24 -0400

    Add [failing] unit test
    #8917

--- tests/tests-process-dowload.php
+++ tests/tests-process-dowload.php
@@ -65,4 +65,27 @@ class Tests_Process_Download extends EDD_UnitTestCase {
 
 		$this->assertTrue( edd_validate_url_token( $url ) );
 	}
+
+	public function test_custom_parameters() {
+
+		$payment_id = EDD_Helper_Payment::create_simple_payment();
+		$payment    = edd_get_payment( $payment_id );
+		$download   = EDD_Helper_Download::create_simple_download();
+
+		add_filter( 'edd_get_download_file_url_args', function ( $args, $payment_id, $params ) {
+			$args['beta'] = 1;
+
+			return $args;
+		}, 10, 3 );
+
+		add_filter( 'edd_url_token_allowed_params', function ( $args ) {
+			$args[] = 'beta';
+
+			return $args;
+		} );
+
+		$url = edd_get_download_file_url( $payment->key, $payment->email, '', $download->ID );
+
+		$this->assertTrue( edd_validate_url_token( $url ) );
+	}
 }
