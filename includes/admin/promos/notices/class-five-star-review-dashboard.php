3b4a3a5b7cf378d93e647226b8ae06d7708ed99c
Robin Cornett <hello@robincornett.com>
2021-10-20 13:02:06 -0400

    Update review message

--- includes/admin/promos/notices/class-five-star-review-dashboard.php
+++ includes/admin/promos/notices/class-five-star-review-dashboard.php
@@ -44,6 +44,12 @@ class Five_Star_Review_Dashboard extends Notice {
 		return 'five-star-review';
 	}
 
+	/**
+	 * Display the notice.
+	 *
+	 * @since 2.11.x
+	 * @return void
+	 */
 	public function display() {
 		?>
 		<div
@@ -68,14 +74,45 @@ class Five_Star_Review_Dashboard extends Notice {
 	 */
 	public function _display() {
 		?>
-		<p><?php esc_html_e( 'Hey, I noticed you\'ve made quite a few sales with Easy Digital Downloads - that’s awesome! Could you please do me a BIG favor and give it a 5-star rating on WordPress to help us spread the word and boost our motivation?', 'easy-digital-downloads' ); ?></p>
-		<p><strong><?php echo wp_kses( __( '~ Chris Klosowski<br>President of Easy Digital Downloads', 'easy-digital-downloads' ), array( 'br' => array() ) ); ?></strong></p>
-		<p>
-			<a href="https://wordpress.org/support/plugin/easy-digital-downloads/reviews/?filter=5#new-post" class="edd-promo-notice-dismiss" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Ok, you deserve it', 'easy-digital-downloads' ); ?></a><br>
-
-			<a class="button-link edd-promo-notice-dismiss" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Nope, maybe later', 'easy-digital-downloads' ); ?></a><br>
-			<a class="button-link edd-promo-notice-dismiss" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'I already did', 'easy-digital-downloads' ); ?></a>
-		</p>
+		<div class="edd-review-step edd-review-step-1">
+			<p><?php esc_html_e( 'Hey, I noticed you\'ve made quite a few sales with Easy Digital Downloads! Are you enjoying Easy Digital Downloads?', 'easy-digital-downloads' ); ?></p>
+			<p>
+				<a class="edd-review-switch-step" data-step="3"><?php esc_html_e( 'Yes', 'easy-digital-downloads' ); ?></a><br />
+				<a class="edd-review-switch-step" data-step="2"><?php esc_html_e( 'Not Really', 'easy-digital-downloads' ); ?></a>
+			</p>
+		</div>
+		<div class="edd-review-step edd-review-step-2" style="display:none;">
+			<p><?php esc_html_e( 'We\'re sorry to hear you aren\'t enjoying Easy Digital Downloads. We would love a chance to improve. Could you take a minute and let us know what we can do better?', 'easy-digital-downloads' ); ?></p>
+			<p>
+				<a href="<?php echo esc_url( $this->url() ); ?>" class="edd-promo-notice-dismiss"><?php esc_html_e( 'Give Feedback', 'easy-digital-downloads' ); ?></a><br>
+				<a class="edd-promo-notice-dismiss"><?php esc_html_e( 'No thanks', 'easy-digital-downloads' ); ?></a>
+			</p>
+		</div>
+		<div class="edd-review-step edd-review-step-3" style="display:none;">
+			<p><?php esc_html_e( 'That\'s awesome! Could you please do me a BIG favor and give it a 5-star rating on WordPress to help us spread the word and boost our motivation?', 'easy-digital-downloads' ); ?></p>
+			<p><strong><?php echo wp_kses( __( '~ Chris Klosowski<br>President of Easy Digital Downloads', 'easy-digital-downloads' ), array( 'br' => array() ) ); ?></strong></p>
+			<p>
+				<a href="https://wordpress.org/support/plugin/easy-digital-downloads/reviews/?filter=5#new-post" class="edd-promo-notice-dismiss" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Ok, you deserve it', 'easy-digital-downloads' ); ?></a><br>
+				<a class="button-link edd-promo-notice-dismiss"><?php esc_html_e( 'Nope, maybe later', 'easy-digital-downloads' ); ?></a>
+			</p>
+		</div>
+		<script type="text/javascript">
+			jQuery( document ).ready( function ( $ ) {
+				$( document ).on( 'click', '.edd-review-switch-step', function ( e ) {
+					e.preventDefault();
+					var target = $( this ).attr( 'data-step' );
+					if ( target ) {
+						var notice = $( this ).closest( '.edd-promo-notice' );
+						var review_step = notice.find( '.edd-review-step-' + target );
+						if ( review_step.length > 0 ) {
+							notice.find( '.edd-review-step:visible' ).fadeOut( function () {
+								review_step.fadeIn();
+							} );
+						}
+					}
+				})
+			} );
+		</script>
 		<?php
 	}
 
@@ -104,5 +141,26 @@ class Five_Star_Review_Dashboard extends Notice {
 		}
 
 		return true;
+	}
+
+	/**
+	 * Builds the UTM parameters for the URLs.
+	 *
+	 * @since 2.11.x
+	 *
+	 * @return string[]
+	 */
+	private function url() {
+		$args = array(
+			'utm_source'   => urlencode( $this->screen ),
+			'utm_medium'   => 'review-request',
+			'utm_campaign' => 'admin',
+			'utm_content'  => 'top-promo',
+		);
+
+		return add_query_arg(
+			$args,
+			'https://easydigitaldownloads.com/plugin-feedback'
+		);
 	}
 }
