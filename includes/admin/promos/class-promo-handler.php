09c499551628ee247a93bcf7b3affcee92a1848b
Robin Cornett <hello@robincornett.com>
2021-10-20 11:14:59 -0400

    Add capability constant to abstract notice

--- includes/admin/promos/class-promo-handler.php
+++ includes/admin/promos/class-promo-handler.php
@@ -28,6 +28,13 @@ class PromoHandler {
 	);
 
 	/**
+	 * The capabilities required to view/dismiss notice.
+	 *
+	 * @var array
+	 */
+	private $capabilities = array();
+
+	/**
 	 * Notices constructor.
 	 */
 	public function __construct() {
@@ -58,13 +65,16 @@ class PromoHandler {
 				continue;
 			}
 
-			add_action( $notice_class_name::DISPLAY_HOOK, function () use ( $notice_class_name ) {
-				/** @var Notice $notice */
-				$notice = new $notice_class_name;
+			/** @var Notice $notice */
+			$notice = new $notice_class_name();
+
+			add_action( $notice_class_name::DISPLAY_HOOK, function () use ( $notice ) {
 				if ( $notice->should_display() ) {
 					$notice->display();
 				}
 			} );
+
+			$this->capabilities[ $notice->get_id() ] = $notice_class_name::CAPABILITY;
 		}
 	}
 
@@ -106,12 +116,13 @@ class PromoHandler {
 	 * @since 2.10.6
 	 */
 	public function dismiss_notice() {
-		if ( ! current_user_can( 'manage_options' ) ) {
-			wp_send_json_error( __( 'You do not have permission to perform this action.', 'easy-digital-downloads' ), 403 );
+		$notice_id = filter_input( INPUT_POST, 'notice_id', FILTER_SANITIZE_STRING );
+		if ( empty( $notice_id ) ) {
+			wp_send_json_error( __( 'Missing notice ID.', 'easy-digital-downloads' ), 400 );
 		}
 
-		if ( empty( $_POST['notice_id'] ) ) {
-			wp_send_json_error( __( 'Missing notice ID.', 'easy-digital-downloads' ), 400 );
+		if ( ! current_user_can( $this->capabilities[ $notice_id ] ) ) {
+			wp_send_json_error( __( 'You do not have permission to perform this action.', 'easy-digital-downloads' ), 403 );
 		}
 
 		if ( empty( $_POST['nonce'] ) || ! wp_verify_nonce( $_POST['nonce'], 'edd-dismiss-notice-' . sanitize_key( $_POST['notice_id'] ) ) ) {
