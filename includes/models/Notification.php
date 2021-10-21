5784fa563b95e23cd50c31adf7dbeaa4ec52b7be
Ashley Gibson <ashley@nosegraze.com>
2021-10-21 15:53:20 +0100

    Add attribute casting.

--- includes/models/Notification.php
+++ includes/models/Notification.php
@@ -31,8 +31,59 @@ class Notification {
 
 	public $date_updated;
 
-	public function __construct( $data = array() ) {
+	protected $casts = array(
+		'id'        => 'int',
+		'remote_id' => 'int',
+		'dismissed' => 'bool',
+	);
 
+	/**
+	 * Constructor
+	 *
+	 * @param array $data Row from the database.
+	 */
+	public function __construct( $data = array() ) {
+		foreach ( $data as $property => $value ) {
+			if ( property_exists( $this, $property ) ) {
+				$this->{$property} = $this->castAttribute( $property, $value );
+			}
+		}
+	}
+
+	/**
+	 * Casts a property to its designated type.
+	 *
+	 * @todo Move to trait or base class.
+	 *
+	 * @param string $propertyName
+	 * @param mixed  $value
+	 *
+	 * @return bool|float|int|mixed|string|null
+	 */
+	private function castAttribute( $propertyName, $value ) {
+		if ( ! array_key_exists( $propertyName, $this->casts ) ) {
+			return $value;
+		}
+
+		// Let null be null.
+		if ( is_null( $value ) ) {
+			return null;
+		}
+
+		switch ( $this->casts[ $propertyName ] ) {
+			case 'array' :
+				return json_decode( $value, true );
+			case 'bool' :
+				return (bool) $value;
+			case 'float' :
+				return (float) $value;
+			case 'int' :
+				return (int) $value;
+			case 'string' :
+				return (string) $value;
+			default :
+				return $value;
+		}
 	}
 
 }
