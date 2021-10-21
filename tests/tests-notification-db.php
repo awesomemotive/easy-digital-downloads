5543fe7033dabf09526092d7fc3a12faa2dab966
Ashley Gibson <ashley@nosegraze.com>
2021-10-21 15:53:10 +0100

    Add tests covering active query.

---
+++ tests/tests-notification-db.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Notification Database Tests
+ *
+ * @package   easy-digital-downloads
+ * @copyright Copyright (c) 2021, Easy Digital Downloads
+ * @license   GPL2+
+ */
+
+namespace EDD\Tests;
+
+use EDD\Models\Notification;
+
+/**
+ * @coversDefaultClass \EDD\Database\Notifications
+ */
+class NotificationDBTests extends \EDD_UnitTestCase {
+
+	/**
+	 * @covers \EDD\Database\Notifications::getActiveNotifications
+	 */
+	public function test_notification_included_in_active() {
+		EDD()->notifications->insert( array(
+			'title'     => 'Notification',
+			'content'   => 'Notification',
+			'type'      => 'success',
+			'dismissed' => 0,
+		), 'notification' );
+
+		$notifications = EDD()->notifications->getActiveNotifications();
+
+		$this->assertSame( 1, count( $notifications ) );
+		$this->assertTrue( $notifications[0] instanceof Notification );
+		$this->assertSame( 'Notification', $notifications[0]->title );
+	}
+
+	/**
+	 * @covers \EDD\Database\Notifications::getActiveNotifications
+	 */
+	public function test_dismissed_notification_not_included_in_active() {
+		EDD()->notifications->insert( array(
+			'title'     => 'Notification',
+			'content'   => 'Notification',
+			'type'      => 'success',
+			'dismissed' => 1,
+		), 'notification' );
+
+		$this->assertEmpty( EDD()->notifications->getActiveNotifications() );
+	}
+
+	/**
+	 * @covers \EDD\Database\Notifications::getActiveNotifications
+	 */
+	public function test_start_date_in_future_not_included_in_active() {
+		EDD()->notifications->insert( array(
+			'title'   => 'Notification',
+			'content' => 'Notification',
+			'type'    => 'success',
+			'start'   => date( 'Y-m-d H:i:s', strtotime( '+1 week' ) ),
+		), 'notification' );
+
+		$this->assertEmpty( EDD()->notifications->getActiveNotifications() );
+	}
+
+	/**
+	 * @covers \EDD\Database\Notifications::getActiveNotifications
+	 */
+	public function test_end_date_in_past_not_included_in_active() {
+		EDD()->notifications->insert( array(
+			'title'   => 'Notification',
+			'content' => 'Notification',
+			'type'    => 'success',
+			'end'     => date( 'Y-m-d H:i:s', strtotime( '-1 week' ) ),
+		), 'notification' );
+
+		$this->assertEmpty( EDD()->notifications->getActiveNotifications() );
+	}
+
+	/**
+	 * @covers \EDD\Database\Notifications::getActiveNotifications
+	 */
+	public function test_end_date_in_future_included_in_active() {
+		// Ends in 1 week - still valid.
+		EDD()->notifications->insert( array(
+			'title'   => 'Notification',
+			'content' => 'Notification',
+			'type'    => 'success',
+			'end'     => date( 'Y-m-d H:i:s', strtotime( '+1 week' ) ),
+		), 'notification' );
+
+		// Ended 1 week ago - should not be in results.
+		EDD()->notifications->insert( array(
+			'title'   => 'Notification',
+			'content' => 'Notification',
+			'type'    => 'success',
+			'end'     => date( 'Y-m-d H:i:s', strtotime( '-1 week' ) ),
+		), 'notification' );
+
+		$notifications = EDD()->notifications->getActiveNotifications();
+
+		$this->assertSame( 1, count( $notifications ) );
+	}
+
+}
