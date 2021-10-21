9d9a533f9fd3cfffb892be3a662fb27ab226d86e
Robin Cornett <hello@robincornett.com>
2021-10-21 08:48:09 -0400

    Add note to use edd_count_orders in 3.0

--- includes/admin/promos/notices/class-five-star-review-dashboard.php
+++ includes/admin/promos/notices/class-five-star-review-dashboard.php
@@ -135,6 +135,7 @@ class Five_Star_Review_Dashboard extends Notice {
 			update_option( 'edd_activation_date', time() );
 			return false;
 		}
+		// @todo Change this to edd_count_orders in 3.0
 		$payments = edd_count_payments();
 		if ( $payments && 50 > $payments->publish ) {
 			return false;
