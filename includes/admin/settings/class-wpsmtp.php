673f0256d99678cd09a728a7d409270a34861993
Robin Cornett <hello@robincornett.com>
2021-10-23 10:46:05 -0400

    Add activated/configured check methods

--- includes/admin/settings/class-wpsmtp.php
+++ includes/admin/settings/class-wpsmtp.php
@@ -20,9 +20,14 @@ class WP_SMTP {
 		add_action( 'edd_wpsmtp', array( $this, 'settings_field' ) );
 	}
 
+	/**
+	 * Register the setting to show the WP SMTP installer if it isn't active.
+	 *
+	 * @param array $settings
+	 * @return array
+	 */
 	public function register_setting( $settings ) {
-		$data = $this->get_data();
-		if ( ! empty( $data['plugin_activated'] ) ) {
+		if ( $this->is_smtp_activated() ) {
 			return $settings;
 		}
 		$settings['main']['wpsmtp'] = array(
@@ -35,18 +40,38 @@ class WP_SMTP {
 		return $settings;
 	}
 
+	/**
+	 * Output the settings field (installation helper).
+	 *
+	 * @param array $args
+	 * @return void
+	 */
 	public function settings_field( $args ) {
 		echo '<p>';
 		esc_html_e( 'WP Mail SMTP allows you to easily set up WordPress to use a trusted provider to reliably send emails, including sales notifications.', 'easy-digital-downloads' );
 		echo '</p>';
 
+		$data = $this->get_data();
 		if ( ! $this->data['plugin_installed'] && ! $this->data['pro_plugin_installed'] ) {
 			?>
-			<button class="button button-primary" data-plugin="https://downloads.wordpress.org/plugin/wp-mail-smtp.zip" data-action="install"><?php esc_html_e( 'Install WP Mail SMTP', 'easy-digital-downloads' ); ?></button>
+			<button
+				class="button button-primary"
+				data-plugin="<?php echo esc_url( $this->config['lite_download_url'] ); ?>"
+				data-action="install"
+			>
+				<?php esc_html_e( 'Install WP Mail SMTP', 'easy-digital-downloads' ); ?>
+			</button>
 			<?php
+		} elseif ( ! $this->is_smtp_activated() ) {
+			echo 'the plugin is not active';
 		}
 	}
 
+	/**
+	 * Get the data needed to manage plugin installation.
+	 *
+	 * @return array
+	 */
 	private function get_data() {
 		$this->data                         = array(
 			'all_plugins'      => \get_plugins(),
@@ -55,8 +80,101 @@ class WP_SMTP {
 		);
 		$this->data['plugin_installed']     = array_key_exists( $this->config['lite_plugin'], $this->data['all_plugins'] );
 		$this->data['pro_plugin_installed'] = array_key_exists( $this->config['pro_plugin'], $this->data['all_plugins'] );
+		$this->data['plugin_activated']     = $this->is_smtp_activated();
 
 		return $this->data;
+	}
+
+	/**
+	 * Whether WP Mail SMTP plugin configured or not.
+	 *
+	 * @since 2.11.x
+	 *
+	 * @return bool True if some mailer is selected and configured properly.
+	 */
+	protected function is_smtp_configured() {
+
+		if ( ! $this->is_smtp_activated() ) {
+			return false;
+		}
+
+		$phpmailer = $this->get_phpmailer();
+
+		$mailer             = \WPMailSMTP\Options::init()->get( 'mail', 'mailer' );
+		$is_mailer_complete = empty( $mailer ) ? false : wp_mail_smtp()->get_providers()->get_mailer( $mailer, $phpmailer )->is_mailer_complete();
+
+		return 'mail' !== $mailer && $is_mailer_complete;
+	}
+
+	/**
+	 * Whether WP Mail SMTP plugin active or not.
+	 *
+	 * @since 2.11.x
+	 *
+	 * @return bool True if SMTP plugin is active.
+	 */
+	protected function is_smtp_activated() {
+
+		return function_exists( 'wp_mail_smtp' ) && ( is_plugin_active( $this->config['lite_plugin'] ) || is_plugin_active( $this->config['pro_plugin'] ) );
+	}
+
+	/**
+	 * Get $phpmailer instance.
+	 *
+	 * @since 2.11.x
+	 *
+	 * @return \PHPMailer|\PHPMailer\PHPMailer\PHPMailer Instance of PHPMailer.
+	 */
+	protected function get_phpmailer() {
+
+		if ( version_compare( get_bloginfo( 'version' ), '5.5-alpha', '<' ) ) {
+			$phpmailer = $this->get_phpmailer_v5();
+		} else {
+			$phpmailer = $this->get_phpmailer_v6();
+		}
+
+		return $phpmailer;
+	}
+
+	/**
+	 * Get $phpmailer v5 instance.
+	 *
+	 * @since 2.11.x
+	 *
+	 * @return \PHPMailer Instance of PHPMailer.
+	 */
+	private function get_phpmailer_v5() {
+
+		global $phpmailer;
+
+		if ( ! ( $phpmailer instanceof \PHPMailer ) ) {
+			require_once ABSPATH . WPINC . '/class-phpmailer.php';
+			require_once ABSPATH . WPINC . '/class-smtp.php';
+			$phpmailer = new \PHPMailer( true ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		}
+
+		return $phpmailer;
+	}
+
+	/**
+	 * Get $phpmailer v6 instance.
+	 *
+	 * @since 2.11.x
+	 *
+	 * @return \PHPMailer\PHPMailer\PHPMailer Instance of PHPMailer.
+	 */
+	private function get_phpmailer_v6() {
+
+		global $phpmailer;
+
+		if ( ! ( $phpmailer instanceof \PHPMailer\PHPMailer\PHPMailer ) ) {
+			require_once ABSPATH . WPINC . '/PHPMailer/PHPMailer.php';
+			require_once ABSPATH . WPINC . '/PHPMailer/SMTP.php';
+			require_once ABSPATH . WPINC . '/PHPMailer/Exception.php';
+			$phpmailer = new \PHPMailer\PHPMailer\PHPMailer( true ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		}
+
+		return $phpmailer;
 	}
 }
 
