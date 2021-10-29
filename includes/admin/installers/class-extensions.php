735cdb78f9f14f57c82eecacafc084c9aaa736dd
Robin Cornett <hello@robincornett.com>
2021-10-29 17:58:20 -0400

    Update extension class to use edd get_version action

--- includes/admin/installers/class-extensions.php
+++ includes/admin/installers/class-extensions.php
@@ -6,8 +6,6 @@ class Extensions {
 
 	private $transient = 'edd_extensions_urls';
 
-	private $license;
-
 	/**
 	 * Returns the download URL for an extension.
 	 *
@@ -17,11 +15,8 @@ class Extensions {
 	 *
 	 * @return string
 	 */
-	public function get_url( $id ) {
-
-		$urls = $this->get_urls();
-
-		return empty( $urls[ $id ] ) ? '' : $urls[ $id ];
+	public function get_url( $id, $license_key ) {
+		return $this->get_url_from_database_or_remote( $id, $license_key );
 	}
 
 	/**
@@ -33,31 +28,19 @@ class Extensions {
 	 *
 	 * @return array|bool
 	 */
-	protected function get_urls( $force = false ) {
+	protected function get_url_from_database_or_remote( $item_id, $license_key ) {
 
-		if ( empty( $this->license ) ) {
-			// return false;
+		if ( empty( $item_id ) || empty( $license_key ) ) {
+			return false;
 		}
 
-		// Avoid multiple requests to the database.
-		static $urls = null;
+		$extension_data = get_option( "edd_extension_{$item_id}" );
 
-		if ( is_null( $urls ) ) {
-			$urls = get_transient( $this->transient );
+		if ( ! empty( $extension_data['url'] ) && ! empty( $extension_data['timeout'] ) && time() <= $extension_data['timeout'] ) {
+			return $extension_data['url'];
 		}
 
-		if ( ! $force && ! empty( $urls ) ) {
-			return $urls;
-		}
-
-		// Avoid multiple remote requests.
-		static $remote_urls = null;
-
-		if ( is_null( $remote_urls ) ) {
-			$remote_urls = $this->get_remote_urls();
-		}
-
-		return $remote_urls;
+		return $this->get_download_url( $item_id, $license_key );
 	}
 
 	/**
@@ -67,42 +50,53 @@ class Extensions {
 	 *
 	 * @return bool|array False if no key or failure, array of extension URLs data otherwise.
 	 */
-	protected function get_remote_urls() {
+	protected function get_download_url( $item_id = false, $license = false ) {
+		if ( ! $item_id || ! $license ) {
+			return false;
+		}
 
-		$extensions = wp_remote_post(
+		// $stored_urls = $this->get_stored_urls();
+		// if ( $stored_urls ) {
+		// 	return $stored_urls;
+		// }
+
+		$request = wp_remote_post(
 			$this->get_api_url(),
 			array(
 				'timeout'   => 15,
 				'sslverify' => true,
 				'body'      => array(
-					'edd_action'  => 'get_extension_urls',
-					'license'     => $this->license,
+					'edd_action'  => 'get_version',
+					'license'     => $license,
+					'item_id'     => $item_id,
 					'php_version' => phpversion(),
 					'wp_version'  => get_bloginfo( 'version' ),
 				),
 			)
 		);
-		rgc_error_log( $extensions );
 
 		// If there was an API error, set transient for only 10 minutes.
-		if ( ! $extensions || $extensions instanceof WP_Error ) {
-			set_transient( $this->transient, false, 10 * MINUTE_IN_SECONDS );
+		if ( ! $request || $request instanceof WP_Error ) {
+			// update_option( $this->transient, strtotime( '+10 minutes' ) );
 
 			return false;
 		}
 
-		$urls = array();
+		$response = json_decode( wp_remote_retrieve_body( $request ) );
 
-		foreach ( (array) $extensions as $extension ) {
-			if ( ! empty( $extension->slug ) ) {
-				$urls[ $extension->slug ] = ! empty( $extension->url ) ? $extension->url : '';
-			}
+		if ( ! empty( $response->download_link ) ) {
+			update_option(
+				"edd_extension_{$item_id}",
+				array(
+					'url'     => $response->download_link,
+					'timeout' => strtotime( '+1 day', time() ),
+				)
+			);
+
+			return $response->download_link;
 		}
 
-		// Otherwise, our request worked. Save the data and return it.
-		set_transient( $this->transient, $urls, DAY_IN_SECONDS );
-
-		return $urls;
+		return false;
 	}
 
 	/**
@@ -117,5 +111,33 @@ class Extensions {
 		}
 
 		return 'https://easydigitaldownloads.com/edd-sl-api';
+	}
+
+	private function get_stored_urls() {
+
+		$stored_urls = get_option( $this->transient );
+
+		// Request has never failed.
+		if ( empty( $stored_urls ) ) {
+			return false;
+		}
+
+		/*
+		 * Request previously failed, but the timeout has expired.
+		 * This means we're allowed to try again.
+		 */
+		if ( is_numeric( $stored_urls ) && time() > $stored_urls ) {
+			delete_option( $this->transient );
+
+			return false;
+		}
+
+		if ( empty( $stored_urls['timeout'] ) || time() > $stored_urls['timeout'] ) {
+			delete_option( $this->transient );
+
+			return false;
+		}
+
+		return $stored_urls;
 	}
 }
