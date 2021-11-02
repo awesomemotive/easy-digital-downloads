b6a7320d361b325b0017d00ed3743914b97d050d
Ashley Gibson <ashley@nosegraze.com>
2021-11-02 11:36:48 +0000

    Change option name to better match function name #8969

--- includes/class-edd-license-handler.php
+++ includes/class-edd-license-handler.php
@@ -364,7 +364,7 @@ class EDD_License {
 
 		// Clear the option for licensed extensions to force regeneration.
 		if ( ! empty( $api_data->license ) && 'valid' === $api_data->license ) {
-			delete_option( 'edd_licensed_products' );
+			delete_option( 'edd_licensed_extensions' );
 		}
 
 		update_option( $this->item_shortname . '_license_active', $license_data );
