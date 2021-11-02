8c04572239f0544b8ec0c05fb2dba4bc3f42825f
Robin Cornett <hello@robincornett.com>
2021-11-02 16:46:10 -0400

    Move email marketing list to new file

---
+++ includes/admin/settings/list-email-marketing.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+	648002 => array(
+		'item_id'    => 648002,
+		'name'       => __( 'ConvertKit', 'easy-digital-downloads' ),
+		'pro_plugin' => 'edd-convertkit/edd-convertkit.php',
+		'tab'        => 'marketing',
+		'section'    => 'convertkit',
+	),
+	22583  => array(
+		'item_id'    => 22583,
+		'name'       => __( 'ActiveCampaign', 'easy-digital-downloads' ),
+		'pro_plugin' => 'edd-activecampaign/edd-activecampaign.php',
+		'tab'        => 'marketing',
+		'section'    => 'activecampaign',
+	),
+	3318   => array(
+		'item_id'    => 3318,
+		'name'       => __( 'Mad Mimi', 'easy-digital-downloads' ),
+		'pro_plugin' => 'edd-madmimi/edd-madmimi.php',
+		'tab'        => 'marketing',
+		'section'    => 'mad-mimi',
+	),
+);
