4128a3d2df6776c8bddca09de8a1a062c4b2d819
Robin Cornett <hello@robincornett.com>
2021-11-02 16:53:32 -0400

    Add current email marketing extensions to list

--- includes/admin/settings/list-email-marketing.php
+++ includes/admin/settings/list-email-marketing.php
@@ -1,6 +1,20 @@
 <?php
 
 return array(
+	746    => array(
+		'item_id'    => 746,
+		'name'       => __( 'Mailchimp', 'easy-digital-downloads' ),
+		'pro_plugin' => 'edd-mail-chimp/edd-mail-chimp.php',
+		'tab'        => 'marketing',
+		'section'    => 'mailchimp',
+	),
+	2925   => array(
+		'item_id'    => 2925,
+		'name'       => __( 'AWeber', 'easy-digital-downloads' ),
+		'pro_plugin' => 'edd-aweber/edd-aweber',
+		'tab'        => 'marketing',
+		'section'    => 'aweber',
+	),
 	648002 => array(
 		'item_id'    => 648002,
 		'name'       => __( 'ConvertKit', 'easy-digital-downloads' ),
@@ -8,12 +22,33 @@ return array(
 		'tab'        => 'marketing',
 		'section'    => 'convertkit',
 	),
+	18200  => array(
+		'item_id'    => 18200,
+		'name'       => __( 'MailPoet', 'easy-digital-downloads' ),
+		'pro_plugin' => 'edd-mailpoet/edd-mailpoet.php',
+		'tab'        => 'marketing',
+		'section'    => 'mailpoet',
+	),
+	33826  => array(
+		'item_id'    => 33826,
+		'name'       => __( 'GetResponse', 'easy-digital-downloads' ),
+		'pro_plugin' => 'edd-getresponse/edd-getresponse.php',
+		'tab'        => 'marketing',
+		'section'    => 'getresponse',
+	),
 	22583  => array(
 		'item_id'    => 22583,
 		'name'       => __( 'ActiveCampaign', 'easy-digital-downloads' ),
 		'pro_plugin' => 'edd-activecampaign/edd-activecampaign.php',
 		'tab'        => 'marketing',
 		'section'    => 'activecampaign',
+	),
+	974    => array(
+		'item_id'    => 974,
+		'name'       => __( 'Campaign Monitor', 'easy-digital-downloads' ),
+		'pro_plugin' => 'edd-campaignmonitor/edd-campaignmonitor.php',
+		'tab'        => 'marketing',
+		'section'    => 'campaignmonitor',
 	),
 	3318   => array(
 		'item_id'    => 3318,
