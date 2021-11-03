a183ee472a8bf8e46e05fea2ffd6d90a96e75a18
Robin Cornett <hello@robincornett.com>
2021-11-03 15:04:18 -0400

    Update item ID behaviors

--- includes/admin/settings/list-email-marketing.php
+++ includes/admin/settings/list-email-marketing.php
@@ -2,57 +2,41 @@
 
 return array(
 	746    => array(
-		'item_id'    => 746,
-		'name'       => __( 'Mailchimp', 'easy-digital-downloads' ),
 		'pro_plugin' => 'edd-mail-chimp/edd-mail-chimp.php',
 		'tab'        => 'marketing',
 		'section'    => 'mailchimp',
 	),
 	2925   => array(
-		'item_id'    => 2925,
-		'name'       => __( 'AWeber', 'easy-digital-downloads' ),
 		'pro_plugin' => 'edd-aweber/edd-aweber',
 		'tab'        => 'marketing',
 		'section'    => 'aweber',
 	),
 	648002 => array(
-		'item_id'    => 648002,
-		'name'       => __( 'ConvertKit', 'easy-digital-downloads' ),
 		'pro_plugin' => 'edd-convertkit/edd-convertkit.php',
 		'tab'        => 'marketing',
 		'section'    => 'convertkit',
 	),
 	18200  => array(
-		'item_id'    => 18200,
-		'name'       => __( 'MailPoet', 'easy-digital-downloads' ),
 		'pro_plugin' => 'edd-mailpoet/edd-mailpoet.php',
 		'tab'        => 'marketing',
 		'section'    => 'mailpoet',
 	),
 	33826  => array(
-		'item_id'    => 33826,
-		'name'       => __( 'GetResponse', 'easy-digital-downloads' ),
 		'pro_plugin' => 'edd-getresponse/edd-getresponse.php',
 		'tab'        => 'marketing',
 		'section'    => 'getresponse',
 	),
 	22583  => array(
-		'item_id'    => 22583,
-		'name'       => __( 'ActiveCampaign', 'easy-digital-downloads' ),
 		'pro_plugin' => 'edd-activecampaign/edd-activecampaign.php',
 		'tab'        => 'marketing',
 		'section'    => 'activecampaign',
 	),
 	974    => array(
-		'item_id'    => 974,
-		'name'       => __( 'Campaign Monitor', 'easy-digital-downloads' ),
 		'pro_plugin' => 'edd-campaignmonitor/edd-campaignmonitor.php',
 		'tab'        => 'marketing',
 		'section'    => 'campaignmonitor',
 	),
 	3318   => array(
-		'item_id'    => 3318,
-		'name'       => __( 'Mad Mimi', 'easy-digital-downloads' ),
 		'pro_plugin' => 'edd-madmimi/edd-madmimi.php',
 		'tab'        => 'marketing',
 		'section'    => 'mad-mimi',
