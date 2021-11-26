2bde8f04ce5d3d82d6f1e599771c9f9b4b273b3d
Robin Cornett <hello@robincornett.com>
2021-11-26 11:38:57 -0500

    Update doc block
    #9003

--- includes/admin/admin-actions.php
+++ includes/admin/admin-actions.php
@@ -35,6 +35,7 @@ add_action( 'admin_init', 'edd_process_actions' );
  *
  * @since 2.8.9
  * @since 2.11.3 Unhooked this to revert to standard admin H1 tags.
+ * @since 3.0    Added back as download categories/tags have been removed from the admin menu.
  * @param $views
  *
  * @return mixed
