ca7d5fd1cdfb0abaf6c10aa468d486167f9e2d80
Robin Cornett <hello@robincornett.com>
2022-04-12 16:07:30 -0400

    Add edd_auto_register_login_user for PayPal Commerce
    #9133

---
+++ includes/gateways/paypal/integrations.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * PayPal Commerce Integrations
+ *
+ * @package    easy-digital-downloads
+ * @subpackage Gateways\PayPal
+ * @copyright  Copyright (c) 2022, Easy Digital Downloads
+ * @license    GPL2+
+ * @since      3.0
+ */
+
+namespace EDD\Gateways\PayPal;
+
+/**
+ * Tells Auto Register to log the user in when the PayPal Commerce action is detected.
+ * Added slightly early to not override anything more specific.
+ *
+ * @since 3.0
+ * @return bool
+ */
+function auto_register() {
+	return isset( $_POST['action'] ) && 'edd_capture_paypal_order' === $_POST['action'];
+}
+add_filter( 'edd_auto_register_login_user', __NAMESPACE__ . '\auto_register', 5 );
