96bb787806237a339ef6e1fd8c4eca330860b4b2
Robin Cornett <hello@robincornett.com>
2022-04-19 10:29:24 -0400

    Register edd_pre_order_history hook
    #9125

--- includes/payments/functions.php
+++ includes/payments/functions.php
@@ -1843,3 +1843,20 @@ function edd_filter_where_older_than_week( $where = '' ) {
 function edd_get_final_payment_id() {
 	return get_option( 'edd_v3_migration_pending', false );
 }
+
+/**
+ * Maybe adds a migration in progress notice to the order history.
+ *
+ * @since 3.0
+ * @return void
+ */
+add_action( 'edd_pre_order_history', function( $orders, $user_id ) {
+	if ( ! edd_get_final_payment_id() ) {
+		return;
+	}
+	?>
+	<p class="edd-notice">
+		<?php esc_html_e( 'A store migration is in progress. Past orders will not appear in your purchase history until they have been updated.', 'easy-digital-downloads' ); ?>
+	</p>
+	<?php
+}, 10, 2 );
