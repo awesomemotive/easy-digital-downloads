7ca0db9df951deb4cc3561a45722e595f6ea4f70
Chris Klosowski <cklosowski@awesomemotive.com>
2022-09-06 13:30:05 -0700

    Close the 'More' overlay when clicking outside of it #8478

--- assets/js/admin/components/advanced-filters/index.js
+++ assets/js/admin/components/advanced-filters/index.js
@@ -6,4 +6,10 @@ jQuery( document ).ready( function( $ ) {
 
 		$( this ).closest( '#edd-advanced-filters' ).toggleClass( 'open' );
 	} );
+
+	$( document ).on( 'click', function( e ) {
+		if ( $( '#edd-advanced-filters' ).hasClass('open') && $( e.target ).closest( '#edd-advanced-filters' ).length == 0 ) {
+			$( '#edd-advanced-filters' ).toggleClass( 'open' );
+		}
+	});
 } );
