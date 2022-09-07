c502f3e15231accd9474d0d9efce45ead9ce92d2
Chris Klosowski <cklosowski@awesomemotive.com>
2022-09-07 15:11:22 -0700

    Add support for ESC to close overlay, and make it more DRY #8478

--- assets/js/admin/components/advanced-filters/index.js
+++ assets/js/admin/components/advanced-filters/index.js
@@ -1,21 +1,48 @@
 /* global jQuery */
 
 jQuery( document ).ready( function( $ ) {
+
+	// when the 'More' button is clicked.
 	$( '.edd-advanced-filters-button' ).on( 'click', function( e ) {
 		e.preventDefault();
 
-		$( this ).closest( '#edd-advanced-filters' ).toggleClass( 'open' );
+		edd_maybe_toggle_advanced_order_filters();
 	} );
 
+	// If a click event is triggered.
 	$( document ).on( 'click', function( e ) {
-		var advancedFiltersWrapper = $( '#edd-advanced-filters' );
+		edd_maybe_toggle_advanced_order_filters( e.target );
+	});
 
-		if ( ! advancedFiltersWrapper.hasClass( 'open' ) ) {
-			return;
-		}
-
-		if ( ! advancedFiltersWrapper.is( e.target ) && ! advancedFiltersWrapper.has( e.target ).length ) {
-			$( '#edd-advanced-filters' ).toggleClass( 'open' );
+	// If the Escape key is pressed.
+	$( document ).on( 'keydown', function( event ) {
+		const key = event.key;
+		if ( key === "Escape" ) {
+			edd_maybe_toggle_advanced_order_filters();
 		}
 	});
 } );
+
+/**
+ * Given a target, determine if we should toggle the advanced orders filter overlay.
+ *
+ * Determines if the target is the advanced order filters wrappr or an element within it,
+ * or in the event of a keypress (target = false), toggles the 'open' class.
+ *
+ * @param {event|boolean} target The target requested the possible toggle.
+ * @returns void
+ */
+function edd_maybe_toggle_advanced_order_filters( target = false) {
+	var advancedFiltersWrapper = $( '#edd-advanced-filters' );
+
+	if ( false === target || ( ! advancedFiltersWrapper.is( target ) && ! advancedFiltersWrapper.has( target ).length ) ) {
+		edd_toggle_advanced_order_filters();
+	}
+}
+
+/**
+ * Toggles the 'open' class on the advanced order filters overlay.
+ */
+function edd_toggle_advanced_order_filters() {
+	$( '#edd-advanced-filters' ).toggleClass( 'open' );
+}
