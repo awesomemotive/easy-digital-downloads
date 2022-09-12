35c05570dddd98c3270db3b06b9dfc3d63572b61
zkawesome <zkeglic@awesomemotive.com>
2022-09-12 15:11:09 +0200

    Hide button to send test email if recipient changed

--- includes/admin/settings/register-settings.php
+++ includes/admin/settings/register-settings.php
@@ -870,7 +870,11 @@ function edd_get_registered_settings() {
 					'email_summary_buttons' => array(
 						'id'   => 'email_summary_buttons',
 						'name' => '',
-						'desc' => '<a href="' . esc_url( $email_summary_trigger_url ) . '" class="button" id="edd-send-test-summary">'. esc_html( __( 'Send Test Email', 'easy-digital-downloads' ) ) . '</a><div id="edd-send-test-summary-notice"></div>',
+						'desc' => '
+							<a href="' . esc_url( $email_summary_trigger_url ) . '" class="button" id="edd-send-test-summary">' . esc_html( __( 'Send Test Email', 'easy-digital-downloads' ) ) . '</a>
+							<div id="edd-send-test-summary-save-changes-notice"></div>
+							<div id="edd-send-test-summary-notice"></div>
+						',
 						'type' => 'descriptive_text',
 					),
 				),
