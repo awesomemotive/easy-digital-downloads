31a1a5eff633dd9f8d3d366bb1879a93142493a1
zkawesome <zkeglic@awesomemotive.com>
2022-09-15 14:20:55 +0200

    Change font family to system fonts

--- includes/emails/email-summary/edd-email-summary-template.php
+++ includes/emails/email-summary/edd-email-summary-template.php
@@ -9,7 +9,6 @@
 			<!--<![endif]-->
 
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
-			<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap" rel="stylesheet">
 		</head>
 		<style>
 			@media only screen and (max-width: 480px) {
@@ -169,7 +168,7 @@
 			<!-- HEADER HOLDER -->
 			<div class="email-header-holder" style="background: #343A40; max-height: 60px; height: 60px;">
 
-				<div class="email-container" style="max-width: 450px; margin: 0 auto; font-family: 'Source Sans Pro', sans-serif; color: #1F2937;">
+				<div class="email-container" style="max-width: 450px; margin: 0 auto; font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Cantarell, Ubuntu, roboto, noto, arial, sans-serif; color: #1F2937;">
 
 					<div class="logo-holder" style="padding: 12px 31px 7px 31px; display: inline-block;">
 						<img src="<?php echo esc_url( EDD_PLUGIN_URL . '/assets/images/edd-logo-white-2x.png' ); ?>" class="edd-logo" width="216" height="35" style="width: 100%; max-width: 100%; height: auto; max-width: 216px; max-height: 35px;">
@@ -178,7 +177,7 @@
 				</div>
 			</div>
 
-			<div class="email-container" style="max-width: 450px; margin: 0 auto; font-family: 'Source Sans Pro', sans-serif; color: #1F2937;">
+			<div class="email-container" style="max-width: 450px; margin: 0 auto; font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Cantarell, Ubuntu, roboto, noto, arial, sans-serif; color: #1F2937;">
 
 
 				<!-- MAIN EMAIL CONTENT HOLDER -->
@@ -375,7 +374,7 @@
 
 			<?php if ( ! empty( $blurb ) ) : ?>
 			<!-- PRO-TIP SECTION -->
-			<div class="email-container pro-tip-blurb" style="max-width: 450px; margin: 0 auto; font-family: 'Source Sans Pro', sans-serif; color: #1F2937;">
+			<div class="email-container pro-tip-blurb" style="max-width: 450px; margin: 0 auto; font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Cantarell, Ubuntu, roboto, noto, arial, sans-serif; color: #1F2937;">
 				<div class="content-box pro-tip-section" style="background: #FFF;">
 
 					<div class="content-holder pro-tip-section" style="padding: 0px 31px 27px 31px;">
