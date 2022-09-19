c467daf743d958f633cc840d50a09a99f1b5bd0a
Robin Cornett <hello@robincornett.com>
2022-09-19 08:57:25 -0400

    Update package.json, grunt to use WP translate, reorder work

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,80 +7,6 @@ module.exports = function( grunt ) {
 
 		pkg: grunt.file.readJSON( 'package.json' ),
 
-		checktextdomain: {
-			options: {
-				text_domain: 'easy-digital-downloads',
-				correct_domain: true,
-				keywords: [
-					'__:1,2d',
-					'_e:1,2d',
-					'_x:1,2c,3d',
-					'esc_html__:1,2d',
-					'esc_html_e:1,2d',
-					'esc_html_x:1,2c,3d',
-					'esc_attr__:1,2d',
-					'esc_attr_e:1,2d',
-					'esc_attr_x:1,2c,3d',
-					'_ex:1,2c,3d',
-					'_n:1,2,3,4d',
-					'_nx:1,2,4c,5d',
-					'_n_noop:1,2,3d',
-					'_nx_noop:1,2,3c,4d',
-					' __ngettext:1,2,3d',
-					'__ngettext_noop:1,2,3d',
-					'_c:1,2d',
-					'_nc:1,2,4c,5d',
-				],
-			},
-			files: {
-				src: [
-					'**/*.php', // Include all files
-					'!node_modules/**', // Exclude node_modules/
-					'!build/**', // Exclude build/
-				],
-				expand: true,
-			},
-		},
-
-		makepot: {
-			target: {
-				options: {
-					domainPath: '/languages/', // Where to save the POT file.
-					exclude: [ 'build/.*' ],
-					mainFile: 'easy-digital-downloads.php', // Main project file.
-					potFilename: 'easy-digital-downloads.pot', // Name of the POT file.
-					potHeaders: {
-						poedit: true, // Includes common Poedit headers.
-						'x-poedit-keywordslist': true, // Include a list of all possible gettext functions.
-					},
-					type: 'wp-plugin', // Type of project (wp-plugin or wp-theme).
-					updateTimestamp: true, // Whether the POT-Creation-Date should be updated without other changes.
-					processPot: function( pot, options ) {
-						pot.headers[ 'report-msgid-bugs-to' ] = 'https://easydigitaldownloads.com/';
-						pot.headers[ 'last-translator' ] = 'WP-Translations (http://wp-translations.org/)';
-						pot.headers[ 'language-team' ] = 'WP-Translations <wpt@wp-translations.org>';
-						pot.headers.language = 'en_US';
-						let translation, // Exclude meta data from pot.
-							excluded_meta = [
-								'Plugin Name of the plugin/theme',
-								'Plugin URI of the plugin/theme',
-								'Author of the plugin/theme',
-								'Author URI of the plugin/theme',
-							];
-						for ( translation in pot.translations[ '' ] ) {
-							if ( 'undefined' !== typeof pot.translations[ '' ][ translation ].comments.extracted ) {
-								if ( excluded_meta.indexOf( pot.translations[ '' ][ translation ].comments.extracted ) >= 0 ) {
-									console.log( 'Excluded meta: ' + pot.translations[ '' ][ translation ].comments.extracted );
-									delete pot.translations[ '' ][ translation ];
-								}
-							}
-						}
-						return pot;
-					},
-				},
-			},
-		},
-
 		// Clean up build directory
 		clean: {
 			main: [ 'build/<%= pkg.name %>' ],
@@ -174,5 +100,6 @@ module.exports = function( grunt ) {
 	} );
 
 	// Build task(s).
-	grunt.registerTask( 'build', [ 'replace', 'clean', 'copy', 'compress' ] );
+	grunt.registerTask( 'prep', [ 'clean', 'replace' ] );
+	grunt.registerTask( 'build', [ 'copy', 'compress' ] );
 };
