faad369a7f1186da58dce3e35ebf4a68c078616b
Robin Cornett <hello@robincornett.com>
2022-09-20 14:59:55 -0400

    Add Amazon icons array

--- includes/gateways/amazon-payments.php
+++ includes/gateways/amazon-payments.php
@@ -267,6 +267,7 @@ final class EDD_Amazon_Payments {
 				'admin_label'    => __( 'Amazon', 'easy-digital-downloads' ),
 				'checkout_label' => __( 'Amazon', 'easy-digital-downloads' ),
 				'supports'       => array(),
+				'icons'          => array( 'amazon' ),
 			),
 		);
 
