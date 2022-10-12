f9ffe5d2b1d54aeb57b175df328f4a89cdae8e24
Chris Klosowski <cklosowski@gmail.com>
2022-10-11 17:06:33 -0700

    Update version matrix

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -7,10 +7,10 @@ set -eu
 echo "steps:"
 
 phpVersions=('5.6' '7.0' '7.1' '7.2' '7.3' '7.4')
-wpVersions=('4.9.19' '5.0.15' '5.1.12' '5.2.14' '5.3.11' '5.4.9' '5.5.8' '5.6.7' '5.7.5' '5.8.3' 'latest')
+wpVersions=('4.9.21' '5.0.17' '5.1.14' '5.2.16' '5.3.13' '5.4.11' '5.5.10' '5.6.9' '5.7.7' '5.8.5' '5.9.4' 'latest')
 
 # Exclude combinations with <php version>-<wp version>
-exclusions=('7.3-4.9.19' '7.4-4.9.19' '7.4-5.0.15' '7.4-5.1.12' '7.4-5.2.14')
+exclusions=('7.3-4.9.21' '7.4-4.9.21' '7.4-5.0.17' '7.4-5.1.14' '7.4-5.2.16')
 
 # add a new command step to run the tests in each test directory
 for phpVersion in ${phpVersions[@]}; do
