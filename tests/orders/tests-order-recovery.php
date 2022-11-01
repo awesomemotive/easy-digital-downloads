fa5cf806be1b62db5a2b88311ce273643e94eb8f
Robin Cornett <hello@robincornett.com>
2022-11-01 10:53:12 -0400

    Separate order recovery tests
    #9541

---
+++ tests/orders/tests-order-recovery.php
@@ -0,0 +1,174 @@
+<?php
+
+/**
+ * Order Reovery Tests.
+ *
+ * @group edd_orders
+ *
+ */
+class Order_Recovery_Tests extends \EDD_UnitTestCase {
+
+	protected static $recoverable_payment_id;
+
+	protected static $recovered_payment_id;
+
+	public function setUp() {
+		/**
+		 * @internal This call is necessary as we need to flush the meta cache.
+		 */
+		wp_cache_flush();
+
+		self::$recoverable_payment_id = $this->generate_recoverable_order();
+		EDD()->session->set( 'edd_resume_payment', self::$recoverable_payment_id );
+		self::$recovered_payment_id = $this->generate_recovered_order();
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+
+		edd_destroy_order( self::$recoverable_payment_id );
+		edd_destroy_order( self::$recovered_payment_id );
+		wp_cache_flush();
+	}
+
+	public function test_recovering_payment_ids_match() {
+		$this->assertSame( self::$recoverable_payment_id, self::$recovered_payment_id );
+	}
+
+	public function test_recovering_payment_guest_to_guest() {
+
+		$payment           = edd_get_payment( self::$recovered_payment_id );
+		$payment_customer  = new \EDD_Customer( $payment->customer_id );
+		$recovery_customer = new \EDD_Customer( 'batman@thebatcave.co' );
+
+		$this->assertSame( $payment_customer->id, $recovery_customer->id );
+	}
+
+	public function test_recovering_payment_gateway_change() {
+		$order = edd_get_order( self::$recovered_payment_id );
+
+		$this->assertEquals( 'stripe', $order->gateway );
+	}
+
+	private function generate_recoverable_order() {
+		$initial_purchase_data = array(
+			'price'        => 299.0,
+			'date'         => date( 'Y-m-d H:i:s' ),
+			'user_email'   => 'bruce@waynefoundation.org',
+			'purchase_key' => '186c2fb5402d756487bd4b6192d59bc2',
+			'currency'     => 'USD',
+			'downloads'    =>
+				array(
+					0 =>
+						array(
+							'id'       => '1906',
+							'options'  =>
+								array(
+									'price_id' => '1',
+								),
+							'quantity' => 1,
+						),
+				),
+			'user_info'    =>
+				array(
+					'id'         => 0,
+					'email'      => 'bruce@waynefoundation.org',
+					'first_name' => 'Bruce',
+					'last_name'  => 'Wayne',
+					'discount'   => 'none',
+					'address'    =>
+						array(),
+				),
+			'cart_details' =>
+				array(
+					0 =>
+						array(
+							'name'        => 'Test Product 1',
+							'id'          => '1906',
+							'item_number' =>
+								array(
+									'id'       => '1906',
+									'options'  =>
+										array(
+											'price_id' => '1',
+										),
+									'quantity' => 1,
+								),
+							'item_price'  => 299.0,
+							'quantity'    => 1,
+							'discount'    => 0.0,
+							'subtotal'    => 299.0,
+							'tax'         => 0.0,
+							'fees'        =>
+								array(),
+							'price'       => 299.0,
+						),
+				),
+			'gateway'      => 'paypal',
+			'status'       => 'pending',
+		);
+
+		return edd_insert_payment( $initial_purchase_data );
+	}
+
+	private function generate_recovered_order() {
+		$recovery_purchase_data = array(
+			'price'        => 299.0,
+			'date'         => '2017-08-15 18:10:37',
+			'user_email'   => 'batman@thebatcave.co',
+			'purchase_key' => '4f2b5cda76c2a997996f4cf8b68255ed',
+			'currency'     => 'USD',
+			'downloads'    =>
+				array(
+					0 =>
+						array(
+							'id'       => '1906',
+							'options'  =>
+								array(
+									'price_id' => '1',
+								),
+							'quantity' => 1,
+						),
+				),
+			'user_info'    =>
+				array(
+					'id'         => 0,
+					'email'      => 'batman@thebatcave.co',
+					'first_name' => 'Batman',
+					'last_name'  => '',
+					'discount'   => 'none',
+					'address'    =>
+						array(),
+				),
+			'cart_details' =>
+				array(
+					0 =>
+						array(
+							'name'        => 'Test Product 1',
+							'id'          => '1906',
+							'item_number' =>
+								array(
+									'id'       => '1906',
+									'options'  =>
+										array(
+											'price_id' => '1',
+										),
+									'quantity' => 1,
+								),
+							'item_price'  => 299.0,
+							'quantity'    => 1,
+							'discount'    => 0.0,
+							'subtotal'    => 299.0,
+							'tax'         => 0.0,
+							'fees'        =>
+								array(),
+							'price'       => 299.0,
+						),
+				),
+			'gateway'      => 'stripe',
+			'status'       => 'pending',
+		);
+
+		return edd_insert_payment( $recovery_purchase_data );
+	}
+}
