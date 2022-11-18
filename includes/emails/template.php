e79647412defb259ddcceff0251c3c3e0deb5927
Robin Cornett <hello@robincornett.com>
2022-11-18 16:30:16 -0500

    Update receipt_link login rendering
    #9414

--- includes/emails/template.php
+++ includes/emails/template.php
@@ -246,13 +246,6 @@ function edd_render_receipt_in_browser( $data ) {
 	}
 
 	$key = urlencode( $data['payment_key'] );
-	$url = add_query_arg(
-		array(
-			'payment_key' => $key,
-			'edd_action'  => 'view_receipt',
-		),
-		home_url()
-	);
 
 	ob_start();
 
@@ -274,7 +267,7 @@ function edd_render_receipt_in_browser( $data ) {
 <body class="<?php echo apply_filters('edd_receipt_page_body_class', 'edd_receipt_page' ); ?>">
 	<div id="edd_receipt_wrapper">
 		<?php do_action( 'edd_render_receipt_in_browser_before' ); ?>
-		<?php echo do_shortcode( '[edd_receipt payment_key='. $key . ' redirect=' . esc_url_raw( $url ) . ']' ); ?>
+		<?php echo do_shortcode( '[edd_receipt payment_key=' . $key . ']' ); ?>
 		<?php do_action( 'edd_render_receipt_in_browser_after' ); ?>
 	</div>
 <?php wp_footer(); ?>
