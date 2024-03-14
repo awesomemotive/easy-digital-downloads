18d784e2d2c35d60115998b57599795705882597
j.ostrowka <ostrowkajakub@gmail.com>
2024-03-14 11:19:21 +0100

    PUB-626 Dostosować proces uzyskiwania dostępu do darmowych produktów

--- assets/js/edd-checkout-global.js
+++ assets/js/edd-checkout-global.js
@@ -218,6 +218,11 @@ window.EDD_Checkout = (function($) {
 			success: function (discount_response) {
 				if( discount_response ) {
 					if (discount_response.msg == 'valid') {
+						if (discount_response.previous_total === 0){
+							window.location.reload();
+							return;
+						}
+
 						$('.edd_cart_discount').html(discount_response.html);
 						$('.edd_cart_discount_row').show();
 						$('.edd_cart_amount').each(function() {
@@ -226,21 +231,6 @@ window.EDD_Checkout = (function($) {
 						$('#edd-discount', $checkout_form_wrap ).val('');
 
 						recalculate_taxes();
-
-						var inputs = $('#edd_cc_fields .edd-input, #edd_cc_fields .edd-select,#edd_cc_address .edd-input, #edd_cc_address .edd-select,#edd_payment_mode_select .edd-input, #edd_payment_mode_select .edd-select');
-
-						if( '0.00' == discount_response.total_plain ) {
-
-							$('#edd_cc_fields,#edd_cc_address,#edd_payment_mode_select').slideUp();
-							inputs.removeAttr('required');
-							$('input[name="edd-gateway"]').val( 'manual' );
-
-						} else {
-
-							inputs.attr('required','required');
-							$('#edd_cc_fields,#edd_cc_address').slideDown();
-
-						}
 
 						$body.trigger('edd_discount_applied', [ discount_response ]);
 
@@ -282,6 +272,10 @@ window.EDD_Checkout = (function($) {
 				withCredentials: true
 			},
 			success: function (discount_response) {
+				if (discount_response.previous_total === 0){
+					window.location.reload();
+					return;
+				}
 
 				var zero = '0' + edd_global_vars.decimal_separator + '00';
 
