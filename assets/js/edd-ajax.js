0580cb3630fdeef315522cf6c6063512de4e5036
j.ostrowka <ostrowkajakub@gmail.com>
2025-02-04 10:54:56 +0100

    pub-1071

--- assets/js/edd-ajax.js
+++ assets/js/edd-ajax.js
@@ -421,7 +421,7 @@ function edd_load_gateway( payment_mode ) {
 
 	// Show the ajax loader
 	jQuery('.edd-cart-ajax').show();
-	jQuery('#edd_purchase_form_wrap').html('<img src="' + edd_scripts.ajax_loader + '"/>');
+	jQuery('#edd_purchase_form_wrap').html('<img style="display:block; margin: 10px auto 0;"  src="' + edd_scripts.ajax_loader + '"/>');
 	var data = {
 		'edd_payment_mode': payment_mode,
 		'payment-mode': payment_mode,
